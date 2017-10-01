b2411ea97c2a26598431affc53cc94441d94eb88
rothe <leo@PC-leo>
2017-10-01 21:48:23 +0200

    removing undefined variable v in bubble_sort.cpp file

--- code/sorting/bubble_sort/bubble_sort.cpp
+++ code/sorting/bubble_sort/bubble_sort.cpp
@@ -14,7 +14,12 @@ void bubble_sort(std::vector<int> &v)
 int main()
 {
 	std::vector<int> input_array;
-	input_array.push_back(2);input_array.push_back(7);input_array.push_back(1);input_array.push_back(10);input_array.push_back(-3);input_array.push_back(8);v.push_back(5);v.push_back(7);
+	input_array.push_back(2);
+	input_array.push_back(7);
+	input_array.push_back(1);
+	input_array.push_back(10);
+	input_array.push_back(-3);
+	input_array.push_back(8);
 	bubble_sort(input_array);
 	for(auto x :input_array)
 		std::cout<<x<<" ";
