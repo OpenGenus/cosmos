cc6b4973941f08e30d391bf1c630cba7a3911df9
vstrimaitis <vstrimaitis@gmail.com>
2017-10-01 21:01:33 +0300

    Added comments for clarification.

--- code/sorting/counting_sort/CountingSort.cs
+++ code/sorting/counting_sort/CountingSort.cs
@@ -7,6 +7,8 @@ namespace CS
     {
         static int[] Sort(int[] arr)
         {
+            // Count the frequency of each number in the array
+            // The keys in this dictionary are the items themselves, the values are their frequencies
             SortedDictionary<int, int> frequency = new SortedDictionary<int, int>();
             foreach(var num in arr)
             {
@@ -16,20 +18,24 @@ namespace CS
                 }
                 frequency[num]++;
             }
+            // Create a new array for the output
             var sorted = new int[arr.Length];
             int index = 0;
             foreach(var entry in frequency)
             {
+                // Put each item (the key) from the dictionary into the new array as many times, as was the frequency (the value) of the item
                 for(int i = 0; i < entry.Value; i++)
                 {
                     sorted[index++] = entry.Key;
                 }
             }
+            // Return the constructed array
             return sorted;
         }
 
         static void Main(string[] args)
         {
+            // Create an array of 10 random numbers
             int[] numbers = new int[10];
             var rand = new Random();
             for(int i = 0; i < 10; i++)
@@ -42,6 +48,7 @@ namespace CS
                 Console.Write(num + " ");
             }
 
+            // Store the sorted result in a separate variable and print it out
             var sorted = Sort(numbers);
             Console.WriteLine("\nSorted:");
             foreach(var num in sorted)
