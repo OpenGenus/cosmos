6b6ae8fb3ac7464c6c29dd405b6ca9b14a7efe05
Jaye Culverhouse <jaye@valewoodtales.com>
2017-10-01 20:01:38 +0100

    Added Python Selection Sort

---
+++ code/sorting/selection_sort/SelectionSort.py
@@ -0,0 +1,13 @@
+def selectionSort(unsorted):
+	sorte = []
+	for i in range(len(unsorted)):
+		selected = unsorted[0]
+		index=0
+		for j in range(len(unsorted)):
+			if unsorted[j] < selected:
+				selected = unsorted[j]
+				print(selected)
+				index=j
+		sorte.append(selected)
+		unsorted.pop(index)
+	return sorte
