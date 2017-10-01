bad8118dd0c4785d1fa65eacc502439e000579ac
thanawat <tha_chookes@hotmail.com>
2017-10-01 19:38:23 +0700

    add bubble sort javascript

---
+++ code/sorting/bubble_sort/bubble_sort.js
@@ -0,0 +1,19 @@
+var a = [34, 203, 3, 746, 200, 984, 198, 764, 9];
+ 
+function bubbleSort (a) {
+    var swapped;
+    do {
+        swapped = false;
+        for (var i=0; i < a.length-1; i++) {
+            if (a[i] > a[i+1]) {
+                var temp = a[i];
+                a[i] = a[i+1];
+                a[i+1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+}
+ 
+bubbleSort(a);
+console.log(a);
