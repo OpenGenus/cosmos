d602743f05352e5b8f90718cb68e6ad149c9df01
Aditya Chatterjee <virtualadichat@gmail.com>
2017-10-02 19:45:33 +0530

    header comment added

--- code/graph-algorithms/bellman_ford_algorithm/bellman_ford.cpp
+++ code/graph-algorithms/bellman_ford_algorithm/bellman_ford.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int nodes,edges;
+// Path of Cosmos by OpenGenus Foundation
 void path_finding(int source,unordered_map<int,int> parent_map)
 {
 	string str;
