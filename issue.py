1e787312d7cfc0cfe9eb1f28d138976d69dfaf53
Anumeha Agrawal <anuzenith29@gmail.com>
2017-10-02 19:32:46 +0530

    Update issue.py

--- issue.py
+++ issue.py
@@ -1,4 +1,4 @@
-op=['+','-','*','/']
+operators=['+','-','*','/','**','%']
 a=input("Enter operand")
 b=float(input("Enter number1"))
 c=float(input("Enter number2"))
@@ -8,5 +8,19 @@ elif a=='-':
 	print(b-c)
 elif a=='*':
 	print(b*c)
+elif a=='/':
+	if c == 0:
+		print("Division by zero not allowed")
+	else:
+		print(b/c)
+
+elif a=='%':
+	if c == 0:
+		print("Modulo by zero not allowed")
+	else:
+		print(b%c)
+
+elif a=='**':
+	print(b**c)
 else:
-	print(b/c)
+	print("Invalid operation")
