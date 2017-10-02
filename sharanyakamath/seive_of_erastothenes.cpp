c035299ccea35402c43e5c81139f9ee12a89d135
Sharanya Kamath <sherrykamath@gmail.com>
2017-10-02 15:15:03 +0530

    Implement Sieve of Erastothenes

---
+++ sharanyakamath/seive_of_erastothenes.cpp
@@ -0,0 +1,33 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void Sieve(int n)
+{
+    bool prime[n+1];
+    memset(prime, true, sizeof(prime));
+ 
+    for (int p=2; p*p<=n; p++)
+    {
+        if (prime[p] == true)
+        {
+            for (int i=p*2; i<=n; i += p)
+                prime[i] = false;
+        }
+    }
+ 
+    for (int p=2; p<=n; p++)
+       if (prime[p])
+          cout << p << " ";
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    cout << "Following are the prime numbers smaller "
+         << " than or equal to " << n << endl;
+    Sieve(n);
+    return 0;
+}
