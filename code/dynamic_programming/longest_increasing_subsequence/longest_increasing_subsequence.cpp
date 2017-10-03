30043ac5da477c6c7b8ad22f919482897ae100d5
Vitorvgc <vitorvgc07@gmail.com>
2017-10-03 09:38:35 -0300

    Add more efficient solution to longest increasing subsequence problem in C++

--- code/dynamic_programming/longest_increasing_subsequence/longest_increasing_subsequence.cpp
+++ code/dynamic_programming/longest_increasing_subsequence/longest_increasing_subsequence.cpp
@@ -1,6 +1,8 @@
 /* Part of Cosmos by OpenGenus Foundation */
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -18,9 +20,22 @@ int lis(int v[], int n) {
     return ans;
 }
 
+// Bottom-up O(n*log(n)) approach
+int lis2(int v[], int n) {
+    // tail[i] strores the value of the lower possible value
+    // of the last element in a incresing sequence of size i
+    vector<int> tail;
+    for(int i = 0; i < n; ++i) {
+        vector<int>::iterator it = lower_bound(tail.begin(), tail.end(), v[i]);
+        if(it == tail.end()) tail.push_back(v[i]);
+        else *it = v[i];
+    } 
+    return tail.size();
+}
+
 int main() {
 
     int v[9] = {10, 22, 9, 33, 21, 50, 41, 60, 80};
-    cout << lis(v, 9) << '\n';
+    cout << lis(v, 9) << ", " << lis2(v, 9) << '\n';
     return 0;
 }
