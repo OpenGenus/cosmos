4c269c13c0f85acf09ea5829d0c1fa403385edbc
Aditya Chatterjee <virtualadichat@gmail.com>
2017-10-03 18:21:24 +0530

    header comment added

--- code/string-algorithms/z_algorithm/z_algorithm.cpp
+++ code/string-algorithms/z_algorithm/z_algorithm.cpp
@@ -3,6 +3,7 @@
   s = aaabaab => z[] = {-1,2,1,0,2,1,0}
   s = aaaaa => z[] = {-1,4,3,2,1}
   s = abacaba => z[] = {-1,0,1,0,3,0,1}
+  Part of Cosmos by OpenGenus Foundation
 */
 #include<bits/stdc++.h>
 using namespace std;
