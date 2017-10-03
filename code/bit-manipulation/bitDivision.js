31a95550443ee0ef3dcf9020b274e04837bb84c8
Esben Sparre Andreasen <esben@semmle.com>
2017-10-03 09:43:49 +0200

    add missing var decl

--- code/bit-manipulation/bitDivision.js
+++ code/bit-manipulation/bitDivision.js
@@ -10,7 +10,7 @@ function divide(dividend, divisor) {
     dividend = Math.abs(dividend);
     divisor = Math.abs(divisor);
 
-    answer = 0
+    var answer = 0
     while (dividend >= divisor) {
         let temp = divisor;
         let i = 1;
