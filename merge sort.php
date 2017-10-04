9fe0578a8f7940d979091362fa6b030d59c78ac9
Luis Bonilla <luis.bonillaii93@gmail.com>
2017-10-03 23:31:33 -0500

    Create merge sort.php

---
+++ merge sort.php
@@ -0,0 +1,49 @@
+unction mergeSort($array)
+{
+    if(count($array) == 1 )
+    {
+        return $array;
+    }
+
+    $mid = count($array) / 2;
+    $left = array_slice($array, 0, $mid);
+    $right = array_slice($array, $mid);
+    $left = mergeSort($left);
+    $right = mergeSort($right);
+
+    return merge($left, $right);
+}
+
+
+function merge($left, $right)
+{
+    $res = array();
+
+    while (count($left) > 0 && count($right) > 0)
+    {
+        if($left[0] > $right[0])
+        {
+            $res[] = $right[0];
+            $right = array_slice($right , 1);
+        }
+        else
+        {
+            $res[] = $left[0];
+            $left = array_slice($left, 1);
+        }
+    }
+
+    while (count($left) > 0)
+    {
+        $res[] = $left[0];
+        $left = array_slice($left, 1);
+    }
+
+    while (count($right) > 0)
+    {
+        $res[] = $right[0];
+        $right = array_slice($right, 1);
+    }
+
+    return $res;
+}
