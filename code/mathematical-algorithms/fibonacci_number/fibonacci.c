fa254a8ae99629629fbc110f86ce41f8f52397aa
Jaco Pretorius <jaco.pretorius@betterment.com>
2017-10-04 16:56:59 -0400

    Minor cleanups on the c solution for fibonacci

--- code/mathematical-algorithms/fibonacci_number/fibonacci.c
+++ code/mathematical-algorithms/fibonacci_number/fibonacci.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-int i, v, num1 = 0, num2 = 1, temp;
 
 int main() {
-  // Enter here number of times fib number is calculated;
-  scanf("%d", &v);
-  printf("Fibonacci numbers:");
-  
-  for (i; i <= v; i++) {
-    // This prints fibonacci number;
-    printf("%d, ", num1);
-    // This calculates fibonacci number;
+  int n, num1 = 0, num2 = 1, temp;
+
+  printf("How many Fibonacci numbers do you want?\n");
+  scanf("%d", &n);
+
+  for (int i = 0; i <= n; i++) {
+    printf("%d\n", num1);
+
     temp = num1 + num2;
     num1 = num2;
     num2 = temp;
