4106b6f5eb6556abf472b17eee4ef9fef1529e1f
Tanvi Kumar <tanvimatrix@gmail.com>
2017-10-05 23:04:55 +0530

    Create flippingbits.cpp
    Program to flip bits.

---
+++ code/bit-manipulation/flippingbits.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+    int T;
+    unsigned int n;
+    cin >> T;
+
+    while(T--){
+        cin >> n;
+        cout << ~n << endl;
+    }
+    return 0;
+}
