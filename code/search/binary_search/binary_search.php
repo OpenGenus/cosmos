0be6fe832a2ad2524595b668688aa2b7ade9e377
Burak Ozdemir <burakozdemir@protonmail.ch>
2017-10-05 12:59:54 +0300

    Added exponential search and jump search

--- code/search/binary_search/binary_search.php
+++ code/search/binary_search/binary_search.php
@@ -4,26 +4,28 @@
  * @param array $array
  * @param int   $low
  * @param int   $high
- * @param int   $target
+ * @param int   $search
  *
  * @return int
  */
-function binarySearch(array $array, $low, $high, $target)
+function binarySearch(array $array, $low, $high, $search)
 {
     if ($high >= $low) {
         $middle = (int) ($low + ($high - $low) / 2);
-        if ($array[$middle] === $target) {
+        if ($array[$middle] === $search) {
             return $middle;
-        } else if ($array[$middle] > $target) {
-            return binarySearch($array, $low, $middle - 1, $target);
+        } else if ($array[$middle] > $search) {
+            return binarySearch($array, $low, $middle - 1, $search);
         } else {
-            return binarySearch($array, $middle + 1, $high, $target);
+            return binarySearch($array, $middle + 1, $high, $search);
         }
     }
     return -1;
 }
 
-echo sprintf(
-    "Position of %d is %d\n",
-    $search = 33, binarySearch($array = [7, 11, 13, 33, 66], 0, count($array) - 1, $search)
-);
+if (!isset($dontPrintBinarySearchTest)) {
+    echo sprintf(
+        "Position of %d is %d\n",
+        $search = 33, binarySearch($array = [7, 11, 13, 33, 66], 0, count($array) - 1, $search)
+    );
+}
