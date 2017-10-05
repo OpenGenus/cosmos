4a14d9a62e5c05900a5777df3bc9100abde95982
Tushar Gautam <31215069+Tushark21@users.noreply.github.com>
2017-10-06 00:08:18 +0530

    Create Circular Linked List.cpp

---
+++ code/data_structures/Linked List/Circular Linked List.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+
+struct node{
+	int info;
+	node *next;
+}*ptr,*head,*start;
+
+int c=1,data;
+
+ptr=new node;
+ptr->next=NULL;
+
+start=head=ptr;
+
+while(c<3 && c>0){
+	cout<<"1.Insert\n2.Link List\n";
+	cin>>c;
+
+	switch(c){
+		case 1:
+			cout<<"Enter Data\n";
+			cin>>data;
+
+			ptr=new node;
+			ptr->next=start;
+			ptr->info=data;
+
+			head->next=ptr;
+			head=ptr;
+
+		break;
+
+		case 2:
+			ptr=start->next;
+
+			while(ptr!=start && ptr!=NULL){
+				cout<<ptr->info<<"->";
+				ptr=ptr->next;
+			}
+			cout<<"\n";
+		break;
+
+		default:
+			cout<<"Wrong Choice\nExiting...\n";
+	}
+
+}
+
+getch();
+return 0;
+}
