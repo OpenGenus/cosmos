b07956a944bad78a35c2f432262244c8fa1e5d79
Sourabh Garg <sourabh.8.june.1996@gmail.com>
2017-10-05 12:07:15 +0530

    Updated Miller_Rabin_Primality_Test.py

--- code/mathematical-algorithms/primality_tests/Miller_Rabin_Primality_Test.py
+++ code/mathematical-algorithms/primality_tests/Miller_Rabin_Primality_Test.py
@@ -4,20 +4,20 @@
 """
 import random
 
-#main method to check primality of a number
+#Main method(function) to check primality of a number
 def isprime(number):
-    iterations=4 #no. of iterations to run test
+    iterations = 4 #no. of iterations to run test
     
     #corner cases
-    if number<=1 or number==4:
+    if number <= 1 or number == 4:
         return False
-    elif number==2 or number==3 or number==5:
+    elif number == 2 or number == 3 or number == 5:
         return True
     else:
-        d=number-1
+        d = number - 1
         #Dividing 'd' till it become a odd number
-        while(d%2==0):
-            d/=2
+        while(d % 2==0):
+            d /= 2
         #checking primality using utility function "miller_rabin_primality" 
         #for no.of iteration times 
         for i in range(iterations):
@@ -28,18 +28,18 @@ def isprime(number):
         return True
         
 #Utility function to calculate (x^exp)%mod
-def power(x,exp,mod):
+def power(x, exp, mod):
     
-    result=1
-    x=x%mod
+    result = 1
+    x=x % mod
     
-    while exp>0:
+    while exp > 0:
         #if power is odd
-        if exp%2==1:
-            result=(result*x)%mod
+        if exp % 2 == 1:
+            result = (result*x) % mod
             
-        exp=exp//2
-        x=(x*x)%mod
+        exp = exp//2
+        x = (x*x) % mod
         
     return result
     
@@ -49,22 +49,22 @@ def power(x,exp,mod):
 def miller_rabin_primality(d,n):
     
     #choose a random number "a"
-    a=random.randint(2, n-2)%(n-4)
+    a=random.randint(2, n-2) % (n-4)
     #compute (a^d)%n
     x=power(a,d,n)
     
-    if x==1 or x==n-1:
+    if x == 1 or x == n-1:
         return True
         
-    while(d!=n-1):
+    while(d != n-1):
         #Square x till one of the folling condition satisfy:
             #1. (x^2)%n!=1, if (x^2)%n==1 , this implies 'n' fails the primality test
             #2. (x^2)%n!=(n-1)  if (x^2)%n==(n-1), this implies 'n' still may be a prime number
-        x=(x*x)%n
-        d*=2
-        if x==1:
+        x = (x*x) % n
+        d *= 2
+        if x == 1:
             return False
-        elif x==n-1:
+        elif x == n-1:
             return True
             
     return False
@@ -72,5 +72,5 @@ def miller_rabin_primality(d,n):
         
 if __name__ == '__main__':
     #illustration by provinding example for a prime number and a composite number
-    print(isprime(13))  #prime returns true
+    print(isprime(13))  #prime number returns true
     print(isprime(16))  # composite number returns False
