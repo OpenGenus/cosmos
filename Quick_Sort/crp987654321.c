b761cdd1236fa3b60195b78fd5dbea86f8c81b82
Chetan Prajapati <crprajapati111@gmail.com>
2017-10-06 07:56:22 +0530

    changed & solved Quick_Sort

--- Quick_Sort/crp987654321.c
+++ Quick_Sort/crp987654321.c
@@ -1,19 +1,12 @@
 /* C implementation QuickSort */
 #include<stdio.h>
- 
-// A utility function to swap two elements
+
 void swap(int* a, int* b)
 {
     int t = *a;
     *a = *b;
     *b = t;
 }
- 
-/* This function takes last element as pivot, places
-   the pivot element at its correct position in sorted
-    array, and places all smaller (smaller than pivot)
-   to left of pivot and all greater elements to right
-   of pivot */
 int partition (int arr[], int low, int high)
 {
     int pivot = arr[high];    // pivot
@@ -21,8 +14,7 @@ int partition (int arr[], int low, int high)
  
     for (int j = low; j <= high- 1; j++)
     {
-        // If current element is smaller than or
-        // equal to pivot
+
         if (arr[j] <= pivot)
         {
             i++;    // increment index of smaller element
@@ -33,26 +25,18 @@ int partition (int arr[], int low, int high)
     return (i + 1);
 }
  
-/* The main function that implements QuickSort
- arr[] --> Array to be sorted,
-  low  --> Starting index,
-  high  --> Ending index */
 void quickSort(int arr[], int low, int high)
 {
     if (low < high)
     {
-        /* pi is partitioning index, arr[p] is now
-           at right place */
+      
         int pi = partition(arr, low, high);
  
-        // Separately sort elements before
-        // partition and after partition
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
 }
  
-/* Function to print an array */
 void printArray(int arr[], int size)
 {
     int i;
@@ -61,16 +45,16 @@ void printArray(int arr[], int size)
     printf("\n");
 }
  
-// Driver program to test above functions
 int main()
 {
     int n;
     printf("Enter size of array\n");
     scanf("%d",&n);
     int arr[n],i;
-    printf("Enter elements of array\n");
+
     for(i=0;i<n;i++)
     {
+         printf("Enter element %d of array ",i+1);
          scanf("%d",&arr[i]);
     }
     quickSort(arr, 0,n-1);
