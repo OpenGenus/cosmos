3b452700af4706673bb9749ee6afd71057cf55aa
Cong <congusbongus@gmail.com>
2017-10-08 09:01:08 +1000

    Update Sleep_sort.cpp

--- code/sorting/Sleep_Sort/Sleep_sort.cpp
+++ code/sorting/Sleep_Sort/Sleep_sort.cpp
@@ -1,17 +1,21 @@
-#include <unistd.h>
 #include <iostream>
-#include <string>
+#include <thread>
+#include <vector>
+
+void routine(int value) {
+	std::chrono::milliseconds timespan(value);
+	std::this_thread::sleep_for(timespan);
+	std::cout << value << "\n";
+}
 
 int main(int argc, char *argv[]) {
-	int i;
-	for (i = 1; i < argc; i++) {
-		if (fork() != 0) {
-			break;
-		}
+	std::vector<int> arr = {12, 23, 42, 3};
+	std::vector<std::thread> threads;
+	for (int value : arr) {
+		threads.push_back(std::thread(routine, value));
 	}
-	int value = std::stoi(argv[i]);
-	sleep(value);
-	std::cout << value << "\n";
-	wait(0);
+	for (auto& thread : threads) {
+		thread.join();
+	}
 	return 0;
 }
