f9d4f6d72b6c468a24fc56fcdf31ea1b568d38c0
invincibel <31722976+invincibel@users.noreply.github.com>
2017-10-07 23:45:06 +0530

    add the sum of two numbers
    this is a program it takes two inputs from user then it prints their sum

---
+++ pr2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter first number: ");
+    scanf("%d",&a);
+    printf("\nenter the second number: ");
+    scanf("%d",&b);
+    printf("the sum of two numbers is %d",a+b);
+    return 0;
+}
