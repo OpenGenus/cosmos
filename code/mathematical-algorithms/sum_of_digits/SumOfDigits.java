6734f87c36840c8901698854fbb1ebce311b8cde
Bradley Donahue <donahue.bradley@gmail.com>
2017-10-07 00:41:11 -0400

    Java SumOfDigits method

---
+++ code/mathematical-algorithms/sum_of_digits/SumOfDigits.java
@@ -0,0 +1,10 @@
+public class SumOfDigits {
+    public static int sumDigits(long n) {
+        int sum = 0;
+        while (n >= 0) {
+            sum += n % 10;
+            n = n / 10;
+        }
+        return sum;
+    }
+}
