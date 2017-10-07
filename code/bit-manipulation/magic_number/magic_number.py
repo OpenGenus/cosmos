6fcd335d051f087bc6faa00f21ffbef637f400f6
阿迪亞 查特吉 <virtualadichat@gmail.com>
2017-10-07 16:21:46 +0530

    header comment added

--- code/bit-manipulation/magic_number/magic_number.py
+++ code/bit-manipulation/magic_number/magic_number.py
@@ -1,6 +1,6 @@
 # Python Program to Find nth Magic Number. 
 # A magic number is defined as a number which can be expressed as a power of 5 or sum of unique powers of 5
-
+# Part of Cosmos by OpenGenus Foundation
 # Function to find nth magic numebr
 def nthMagicNo(n):
     pow=1
