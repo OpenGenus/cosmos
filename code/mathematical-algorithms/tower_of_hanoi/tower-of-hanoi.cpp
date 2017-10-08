cbc8c49dff13b59661e1bfe768409649953e7c48
Pradyumn Sinha <pradyumn014@users.noreply.github.com>
2017-10-09 00:44:57 +0530

    Finished the Code
    Added necessary comments

--- code/mathematical-algorithms/tower_of_hanoi/tower-of-hanoi.cpp
+++ code/mathematical-algorithms/tower_of_hanoi/tower-of-hanoi.cpp
@@ -40,3 +40,30 @@ void Tower_of_Hanoi(int  A ,int B,int  C,int N)
 
 }
 
+int main()
+{
+	// N is the number of disks int source stack
+	int N;
+
+	// Taking the input for these parameters
+
+	cout<<"Enter the number of disks \n";
+	cin>>N;
+	cout<<"Enter the size of the disks \n"; 
+	cout<<"Note they must be in descending order \n \n"; 
+	for(int i=N;i>=1;i--)
+	{
+		int sz;
+		cin>>sz;
+		Stack[0].push(make_pair(sz,i));
+		cout<<"Disk : Size "<<sz<<" and Number "<<i<<" pushed into the Source Stack \n";
+	}
+
+	//Executing the Tower of Hanoi Function
+
+	cout<<" \n    Tower of Hanoi    \n";
+	cout<<" ----------------------------- \n \n";
+	Tower_of_Hanoi(0,1,2,N);
+
+	return 0;
+}
