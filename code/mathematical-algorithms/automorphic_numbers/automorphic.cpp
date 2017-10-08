34c9dfaccae8876e7485c097744d6bfcaf748805
arnavb11 <arnavborborah11@gmail.com>
2017-10-08 15:12:34 +0000

    Added explanation comment

--- code/mathematical-algorithms/automorphic_numbers/automorphic.cpp
+++ code/mathematical-algorithms/automorphic_numbers/automorphic.cpp
@@ -4,6 +4,13 @@
 
 using namespace std;
 
+/*
+* In mathematics an automorphic number (sometimes referred to as a circular number) is a number
+* whose square "ends" in the same digits as the number itself. 
+* For example, 5^2 = 25, 6^2 = 36, 76^2 = 5776, 376^2 = 141376,and 890625^2 = 793212890625, 
+* so 5, 6, 76 and 890625
+*/
+
 // Part of Cosmos by OpenGenus Foundation
 int main()
 {
