6196a5fd86b4a6e0cbfcd3e425ed4294406139ed
arnavb11 <arnavborborah11@gmail.com>
2017-10-08 15:09:55 +0000

    Added C++ automorphic numbers

---
+++ code/mathematical-algorithms/automorphic_numbers/automorphic.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <algorithm> // For std::equal
+
+using namespace std;
+
+int main()
+{
+    long long int number; // May have to deal with large numbers
+    cout << "Enter a number: ";
+    cin >> number;
+    
+    unsigned long long int numberSquared = number * number; // Squares can get really big
+    
+    string numberString = to_string(number);
+    string numberSquaredString = to_string(numberSquared);
+    
+    bool isAutomorphic = std::equal(numberString.rbegin(), numberString.rend(), numberSquaredString.rbegin()); // Checks if the number is at the end of the number squared
+    
+    cout << number << " is " << (isAutomorphic ? "" : "not ") << "an automorphic number." << endl;
+}
