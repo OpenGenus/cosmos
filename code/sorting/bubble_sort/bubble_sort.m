5dac487c3a8892a4c4708a889b8b82cc08fbeca8
DaiPei <daipei@hust.edu.cn>
2017-10-09 12:14:31 +0800

    add bubble sort code in Objective-C

---
+++ code/sorting/bubble_sort/bubble_sort.m
@@ -0,0 +1,55 @@
+//
+//  BubbleSort.m
+//  oc_for_cosmos
+//
+//  Created by DaiPei on 2017/10/9.
+//  Copyright © 2017年 DaiPei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BubbleSort : NSObject
+
+- (void) bubbleSort:(NSMutableArray<NSNumber *> *)array;
+
+@end
+
+@implementation BubbleSort
+
+- (void) bubbleSort:(NSMutableArray<NSNumber *> *)array {
+    for (int i = 0; i < array.count-1; i++) {
+        BOOL swapped = NO;
+        for (int j = 0; j < array.count-1-i; j++) {
+            if ([array[j] compare:array[j + 1]] == NSOrderedDescending) {
+                NSNumber *tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+                swapped = YES;
+            }
+        }
+        if (!swapped) {
+            break;
+        }
+    }
+}
+
+@end
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        int n = 0;
+        NSLog(@"What is the size of the array?");
+        scanf("%d", &n);
+        NSMutableArray *array = [NSMutableArray arrayWithCapacity:n];
+        NSLog(@"Enter elements of the array one by one:");
+        for (int i = 0; i < n; i++) {
+            int tmp;
+            scanf("%d", &tmp);
+            [array addObject:@(tmp)];
+        }
+        BubbleSort *bs = [[BubbleSort alloc] init];
+        [bs bubbleSort:array];
+        NSLog(@"%@", array);
+    }
+    return 0;
+}
