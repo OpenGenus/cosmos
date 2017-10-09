0cd0fc44a3dac129a3ecf21445d5f1d4bb725859
arnavb11 <arnavborborah11@gmail.com>
2017-10-09 13:49:16 +0000

    Added stream number average for C++

---
+++ code/mathematical-algorithms/average_stream_numbers/average_stream_numbers.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+double getAverage(double num)
+{
+    static double sum = 0, n = 0;
+ 
+    sum += num;
+    return sum/++n;
+}
+
+void streamAverage(vector<double> arr)
+{
+    double average = 0;
+    for(int i = 0; i < arr.size(); i++)
+    {
+        average  = getAverage(arr[i]);
+        cout << "Average of " << i+1  << " numbers is " << average << endl;
+    }
+}
+
+int main()
+{
+    vector<double> arr{10, 20, 30, 40, 50, 60};
+    streamAverage(arr);
+ 
+    return 0;
+}
