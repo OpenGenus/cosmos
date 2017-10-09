2c927c34a8e7f092a2508625d13163cf2692456e
LesnyRumcajs <LesnyRumcajs@users.noreply.github.com>
2017-10-09 17:36:17 +0200

    Median in Ruby

---
+++ code/dynamic_programming/Array_Median/median.rb
@@ -0,0 +1,8 @@
+def median(*nums)
+    nums.sort!
+    if nums.size.odd?
+        nums[(nums.size + 1) / 2 - 1]
+    else
+        (nums[nums.size / 2 - 1] + nums[nums.size / 2]).fdiv(2)
+    end
+end
