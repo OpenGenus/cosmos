21045db1b57e57baf8265be386feab8ff06a8c80
amitsin6h <amitsin6h@gmail.com>
2017-10-09 23:33:38 +0530

    plaindrome check using python, easy way

---
+++ code/string-algorithms/palindrome_checker/Plaindrome.py
@@ -0,0 +1,14 @@
+#Sleep Sort in PHP
+#Author: Amit Kr. Singh
+#Github: @amitsin6h
+#Social: @amitsin6h
+#OpenGenus Contributor
+
+def PlaindromeCheck(self):
+    plaindrome = self[::-1]
+    if (self == plaindrome):
+        print True
+    else:
+        print False
+
+PlaindromeCheck('madam')
