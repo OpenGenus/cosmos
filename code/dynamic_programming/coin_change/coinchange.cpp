8bcfd7a9974a91639d11318f58eb37ef767932e2
Amartya Chaudhuri (amarchau) <amarchau@cisco.com>
2017-10-11 00:23:18 +0530

    Addition of Python Implementation and CPP code fix.

--- code/dynamic_programming/coin_change/coinchange.cpp
+++ code/dynamic_programming/coin_change/coinchange.cpp
@@ -4,52 +4,33 @@ using namespace std;
 /*
  * Part of Cosmos by OpenGenus Foundation
 */
+#define MAX 100
 
-///Coin Change Problem 
-///Top Down DP
-int dp[100] = {0};
-int coinWaysTD(int amt,int coins[],int n){
-    if(amt==0){
-        dp[0] = 1;
-        return 1;
-    }
-    ///Recursive Case
-    int ans = 0;
+int coinWays(int amt,int coins[],int n){
+
+    // init the dp table
+    int dp[MAX] = {0};
+    dp[0] = 1; // base case
     
-    if(dp[amt]!=0){
-        return dp[amt];
-    }
-    for(int i=0;i<n;i++){
-        if(amt-coins[i]>=0){
-            ans += coinWaysTD(amt-coins[i],coins,n);
-        }
-    }
-    ///Store and return for the first time
-    dp[amt] = ans;
-    return ans;
-}
-
-///Bottom Up DP
-int coinWaysBU(int amt,int coins[],int n){
-    int dp[100] = {0};
-    dp[0] = 1;
-
-    for(int i=1;i<=amt;i++){
-        for(int j=0;j<n;j++){
-           if(i-coins[j]>=0){
-                dp[i] += dp[i-coins[j]];
+    for(int j=0;j<n;j++){
+        for(int i=1;i<=amt;i++){
+            if(i-coins[j]>=0){
+                // if coins[j] < i then add no. of ways - 
+                // - to form the amount by using coins[j]
+                dp[i] += dp[i-coins[j]]; 
            }
         }
     }
+
+    //final result at dp[amt]
     return dp[amt];
 }
 
 
 int main(){
 
-    int coins[] = {1,2,3,8};
-    int amount = 3;
-    cout<<coinWaysTD(amount,coins,4)<<endl;
-    cout<<coinWaysBU(amount,coins,4)<<endl;
-return 0;
+    int coins[] = {1,2,3,8}; // coin denominations 
+    int amount = 3;  // amount
+    cout<<coinWays(amount,coins,4)<<endl;
+    return 0;
 }
