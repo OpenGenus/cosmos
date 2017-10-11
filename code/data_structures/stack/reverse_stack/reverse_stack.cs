b99c2563c67c8f4ecd09ffbcc39add764d6b144c
阿迪亞 查特吉 <virtualadichat@gmail.com>
2017-10-11 08:49:16 +0530

    header comment added

--- code/data_structures/stack/reverse_stack/reverse_stack.cs
+++ code/data_structures/stack/reverse_stack/reverse_stack.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// Part of Cosmos by OpenGenus Foundation
 namespace reversed_stack
 {
     class Program
