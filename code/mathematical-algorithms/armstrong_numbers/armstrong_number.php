8fe09852f6fd4242508ed6b8428f303b4e1e85b6
Tera Byte aka Gurkirat Singh <tbhaxor@gmail.com>
2017-10-12 02:57:11 +0530

    armstrong number in php

---
+++ code/mathematical-algorithms/armstrong_numbers/armstrong_number.php
@@ -0,0 +1,21 @@
+<?php
+// declaring variable
+$number = 407;   // demo number
+$sum = 0;   // varibale to store the sum of number
+$dup = $number;   // duplicate number to traverse through digit
+
+// loop will work untill and unless all digits are being processed
+while($dup != 0)
+{
+  $sum = $sum + pow( $dup%10 ,3);  // power 3 of last digit
+  $dup = $dup / 10;
+}
+if($number == $sum)
+{
+echo "$number is an Armstrong number";
+}
+else
+{
+echo "$number is not an armstrong number";
+}
+?>
