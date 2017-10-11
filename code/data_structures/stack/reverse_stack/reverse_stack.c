2fce8124cb8fe89eb19060e977d664776747e17e
Tera Byte aka Gurkirat Singh <tbhaxor@gmail.com>
2017-10-11 22:10:06 +0530

    reverse stack in c

--- code/data_structures/stack/reverse_stack/reverse_stack.c
+++ code/data_structures/stack/reverse_stack/reverse_stack.c
@@ -1,1 +1,12 @@
+// heders file
 #include <stdio.h>
+// MAX SIZE
+#define MAX_SIZE 5
+
+int stack[MAX_SIZE];  // stack with size = MAX_SIZE
+int top = -1;  // 
+
+int main() {
+  printf("%d\n", &top);
+  return 0;
+}
