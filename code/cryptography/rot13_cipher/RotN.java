51a3f3c1c58125f665a5476f27ce53f63c242e4b
Lyxnx <a.julnes@gmail.com>
2017-10-11 18:44:47 +0100

    Add rot N cipher algorithm in Java

---
+++ code/cryptography/rot13_cipher/RotN.java
@@ -0,0 +1,27 @@
+public class RotN {
+
+    public static void main(String[] args) {
+        String test = "test";
+		
+        System.out.println(rot(test, 13)); // prints 'grfg'
+        System.out.println(rot(test, 26)); // prints 'test'
+    }
+    
+    public static String rot(String str, int n) {
+        n = Math.max(0, n);
+        
+        StringBuilder sb = new StringBuilder();
+    
+        for (char c : str.toCharArray()) {
+            if ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(c) < 0)
+                sb.append(c);
+            else {
+                int b = c - 'a';
+                int m = (b + n) % 26;
+                sb.append((char) (m + 'a'));
+            }
+        }
+        
+        return sb.toString();
+    }
+}
