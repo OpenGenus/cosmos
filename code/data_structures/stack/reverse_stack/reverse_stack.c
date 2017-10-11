8e06ddd0b9fd4e3b731469d10ddc654319936946
Tera Byte aka Gurkirat Singh <tbhaxor@gmail.com>
2017-10-11 22:22:50 +0530

    reverse stack in c

--- code/data_structures/stack/reverse_stack/reverse_stack.c
+++ code/data_structures/stack/reverse_stack/reverse_stack.c
@@ -4,9 +4,18 @@
 #define MAX_SIZE 5
 
 int stack[MAX_SIZE];  // stack with size = MAX_SIZE
-int top = -1;  // 
+int top = 0;  // top is initialised to 0 - first value of 
 
+void push(int val) {
+  if(top!=MAX_SIZE)
+    {
+      stack[top++] = val;
+      printf("%d pushed\n", val);
+    }
+}
+
+void print()
 int main() {
-  printf("%d\n", &top);
+
   return 0;
 }
