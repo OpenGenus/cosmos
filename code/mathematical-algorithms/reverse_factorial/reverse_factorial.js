7b63a478217a816298ea5d39826ba48c2c06b101
Abinodh Thomas <abinodh@steamclock.com>
2017-10-10 23:27:16 -0700

    Added Reverse Factorial

---
+++ code/mathematical-algorithms/reverse_factorial/reverse_factorial.js
@@ -0,0 +1,11 @@
+function unfactorial(num) {
+    var d = 1;
+    while (num > 1 && Math.round(num) === num) {
+        d += 1;
+        num /= d;
+    }
+    if (num === 1) 
+        return d+"!";
+    else 
+        return "NONE"
+}
