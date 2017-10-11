de32e6e8e2ba92e60cdfa9654b70d40dd542f319
David Medina <David@Helpdesks-MBP.home>
2017-10-10 22:53:52 -0400

    Added simple translation of smallest_digit_in_number to java.

---
+++ code/mathematical-algorithms/basic/Smallest_digit_in_number/smallest_digit_in_number.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class smallest_digit_in_number{
+    public static void main(String args[]) {
+        int num = 238493;
+        System.out.println(smallest_digit(num));
+    }
+
+    // returns the smallest integer of a given number.
+    public static int smallest_digit(int n)
+    {
+        int min = n%10;
+        n/=10;
+        while(n!=0)
+        {
+            if(min>n%10)
+                min=n%10;
+            n/=10;
+        }
+        return min;
+    }
+}
