f292301b2947dd8d98c1d5c82e4151d35d53d877
Ian Tay <yiyantay@gmail.com>
2017-10-11 17:45:52 -0400

    eggDropping memoized impl in Haskell

---
+++ code/dynamic_programming/egg_dropping_puzzle/eggDropping.hs
@@ -0,0 +1,16 @@
+--- Part of Cosmos by OpenGenus Foundation
+eggDropping :: Int -> Int -> Int
+eggDropping floors eggs = d floors eggs
+  where d floors' 1 = floors'
+        d 1 eggs'   = 1
+        d 0 _'   = 0
+        d _ 0 = error "no eggs left"
+        d floors' eggs' = minimum [1 + maximum [ table!(floors'-i, eggs')
+                                               , table!(i-1, eggs'-1)
+                                               ]
+                                  | i <- [1..floors']
+                                  ]
+
+        table = listArray bounds
+              [d i j | (i, j) <- range bounds] -- 2D array to store intermediate results
+        bounds = ((0, 0), (floors, eggs))
