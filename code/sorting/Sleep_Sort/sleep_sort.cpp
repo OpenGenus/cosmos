981996a97ca7cb140a80df15071b6b833b08f10b
arnavb11 <arnavborborah11@gmail.com>
2017-10-12 15:00:27 +0000

    using namespace std

--- code/sorting/Sleep_Sort/sleep_sort.cpp
+++ code/sorting/Sleep_Sort/sleep_sort.cpp
@@ -4,17 +4,19 @@
 #include <vector> 
 #include <thread>
 
+using namespace std;
+
 int main(int argc, char** argv)
 {
-    std::vector<std::thread> threads;
+    vector<thread> threads;
     
     for(auto i = 1; i < argc; ++i)
     {
         thread.emplace_back(
             [i, &argv]() {
-                int arg = std::stoi(argv[i]);
-                std::this_thread::sleep_for(std::chrono::seconds(arg));
-                std::cout << argv[i] << std::endl;
+                int arg = stoi(argv[i]);
+                this_thread::sleep_for(chrono::seconds(arg));
+                cout << argv[i] << endl;
             }
         );
     }
@@ -24,5 +26,5 @@ int main(int argc, char** argv)
         thread.join();
     }
     
-    return(0);
+    return 0;
 }
