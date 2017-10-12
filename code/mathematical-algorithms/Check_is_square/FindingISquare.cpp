55dc7e3b1c6b5864a18a30b1ec29f21787d7df80
rahul <rahul1809>
2017-10-12 16:35:59 +0530

    added to binary search to reduce computation from O(N) to O(logN)

--- code/mathematical-algorithms/Check_is_square/FindingISquare.cpp
+++ code/mathematical-algorithms/Check_is_square/FindingISquare.cpp
@@ -1,25 +1,52 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
+
+typedef long long int lld;
+bool IsSquare(lld number)
 {
-	int t;
-	cout<<"Enter no. of test cases"<<endl;
-	cin>>t;
-	while(t--)
-	{
-		int n;
-		cout<<"Enter the number"<<endl;
-		cin>>n;
-		int i;
-		for(i=1;i<=n;i++)
-		{
-			if(i*i==n)
-			{
-				cout<<"Yes";
-				return 0;
-			}
-		}
-	  	cout<<"No";
-		return 0;
-	}
+  lld min = 1;
+  lld max = number;
+  lld mid = min + (max - min)/2;
+  if (number == 0 || number == 1)
+  return true;
+  while (min<max) {
+
+    if (mid*mid > number)
+    {
+      max = mid -1;
+      mid = min + (max - min)/2;
+    }
+    else if (mid*mid < number)
+    {
+      min = mid + 1;
+      mid = min + (max - min)/2;
+
+    }
+    else
+        return true;
+
+  }
+  return false;
+}
+
+int main() {
+
+  int t;
+  cout << "Enter the Number of testcases:" << '\n';
+  cin >> t;
+  lld number;
+  while (t--)
+  {
+    cin >> number;
+    bool result = IsSquare(number);
+    if (result)
+    {
+      cout << "Square number" << '\n';
+    }
+    else
+    {
+      cout << "Not a Square number" << '\n';
+    }
+  }
+  return 0;
 }
