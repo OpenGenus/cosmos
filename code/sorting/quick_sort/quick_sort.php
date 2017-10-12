d42c079b3e617223dd1bb8dcca3c532f2529da1c
Julian Martin <julian.martin@russmedia.com>
2017-10-12 09:11:38 +0200

    Create quick_sort.php

---
+++ code/sorting/quick_sort/quick_sort.php
@@ -0,0 +1,18 @@
+function quick_sort( $array ) {
+
+	if ( count( $array ) <= 1 ) {
+		return $array;
+	} else {
+		$pivot = $array[0];
+		$left = $right = [];
+
+		for ( $i = 1; $i < count( $array ); $i ++ ) {
+			if ( $array[ $i ] < $pivot ) {
+				$left[] = $array[ $i ];
+			} else {
+				$right[] = $array[ $i ];
+			}
+		}
+		return array_merge( quick_sort( $left ), array( $pivot ), quick_sort( $right ) );
+	}
+}
