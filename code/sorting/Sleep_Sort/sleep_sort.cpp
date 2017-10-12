40332547170fa91c22d4da880d960313c649c55f
arnavb11 <arnavborborah11@gmail.com>
2017-10-12 14:59:32 +0000

    Fixed indentation

--- code/sorting/Sleep_Sort/sleep_sort.cpp
+++ code/sorting/Sleep_Sort/sleep_sort.cpp
@@ -10,12 +10,13 @@ int main(int argc, char** argv)
     
     for(auto i = 1; i < argc; ++i)
     {
-        thread.emplace_back([i, &argv]()
-        {
-        int arg = std::stoi(argv[i]);
-        std::this_thread::sleep_for(std::chrono::seconds(arg));
-        std::cout << argv[i] << std::endl;
-        });
+        thread.emplace_back(
+            [i, &argv]() {
+                int arg = std::stoi(argv[i]);
+                std::this_thread::sleep_for(std::chrono::seconds(arg));
+                std::cout << argv[i] << std::endl;
+            }
+        );
     }
     
     for(auto &thread: threads)
