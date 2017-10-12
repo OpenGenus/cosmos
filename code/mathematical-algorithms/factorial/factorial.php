6acf264764c3a465a8b5d0a1d1f1ae3c31ca8c03
阿迪亞 查特吉 <virtualadichat@gmail.com>
2017-10-13 01:04:15 +0530

    header comment added

--- code/mathematical-algorithms/factorial/factorial.php
+++ code/mathematical-algorithms/factorial/factorial.php
@@ -1,5 +1,6 @@
 <?php
 //recursive function to calculate factorial of a number
+// Part of Cosmos by OpenGenus Foundation
 function factorial($number) {          
         if ($number < 2) { 
             return 1; 
