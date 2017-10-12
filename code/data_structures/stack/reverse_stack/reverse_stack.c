1fd8d9000bf1397507f2a87b3128632f9246bd73
Tera Byte aka Gurkirat Singh <tbhaxor@gmail.com>
2017-10-13 02:27:32 +0530

    reverse stack changed

--- code/data_structures/stack/reverse_stack/reverse_stack.c
+++ code/data_structures/stack/reverse_stack/reverse_stack.c
@@ -1,45 +1,46 @@
 // heders file
 #include <stdio.h>
 // MAX SIZE
+
 #define MAX_SIZE 5
 
-int stack[MAX_SIZE];  // stack with size = MAX_SIZE
+int Stack[MAX_SIZE];  // Stack with size = MAX_SIZE
 int top = 0;  // top is initialised to 0 - first value of
 
-// pushing value in stack
+// pushing value in Stack
 void push(int val) {
   if(top!=MAX_SIZE)
     {
-      stack[top++] = val;
+      Stack[top++] = val;
       printf("%d pushed\n", val);
     }
 }
 
-// poping value from stack
+// poping value from Stack
 void pop()
 {
   top--;
 }
 
-// printing value of stack
+// printing value of Stack
 void print()
 {
   int i;
   for (i = top - 1; i >= 0; i--) {
-    printf("%d ", stack[i]);
+    printf("%d ", Stack[i]);
   }
   printf("\n");
 }
 
-// reversing stack
+// reversing Stack
 void reverse() {
   int i = top - 1;
   int j = 0;
   while(i > j)
   {
-    int temp = stack[i];
-    stack[i] = stack[j];
-    stack[j] = temp;
+    int temp = Stack[i];
+    Stack[i] = Stack[j];
+    Stack[j] = temp;
     i--;
     j++;
   }
@@ -49,29 +50,30 @@ void reverse() {
 int main() {
 
   /*
-      Adding value in stack
+      Adding value in Stack
   */
-  printf("\n\npushing values in the stack\n");
+  printf("\n\npushing values in the Stack\n");
   push(1);
   push(2);
   push(3);
+  push(4);
 
   /*
-      printing value in stack
+      printing value in Stack
   */
-  printf("\n\nprinting values in the stack\n");
+  printf("\n\nprinting values in the Stack\n");
   print();
 
   /*
-      reversing value in stack and storing in same stack variaus
+      reversing value in Stack and storing in same Stack variaus
   */
-  printf("\n\nreversing values in the stack\n");
+  printf("\n\nreversing values in the Stack\n");
   reverse();
 
   /*
-      printing reversed value in stack
+      printing reversed value in Stack
   */
-  printf("\n\nprinting reversed values in the stack\n");
+  printf("\n\nprinting reversed values in the Stack\n");
   print();
 
   return 0;
