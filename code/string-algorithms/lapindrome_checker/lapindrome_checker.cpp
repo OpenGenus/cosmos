c9073decf647e82c6d2fb591cc7883220013d398
Tera Byte aka Gurkirat Singh <tbhaxor@gmail.com>
2017-10-13 03:40:45 +0530

    lapindrome checker in cpp

--- code/string-algorithms/lapindrome_checker/lapindrome_checker.cpp
+++ code/string-algorithms/lapindrome_checker/lapindrome_checker.cpp
@@ -1,87 +1,43 @@
-// header files and standard libs
-#include <iostream>
-#include <string>
-#include <algorithm>
+// including headers and std liberaries
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<cstring>
 using namespace std;
 
+// main function
 
-// is_lapindrome function defined which will return true if string is lapindrome
-bool is_lapindrome(string val)
-{
-  // a flag strue
-  bool flag = true;
-	int len = val.length();
-
-  // checking if length is even or odd
-  if(len%2 == 0)
-  {
-    // this is for even
-  string first,second;  // declaring two string
-  first.assign(val.substr(0,len/2));  // slicing from 0 - 1/2 of the length
-  second.assign(val.substr(len/2,len));  // slicing from 1/2 of the length - actual length
-
-  // traversing from 0 - len/2 , the complete length for
-  for(int i=0;i<len/2;i++)
-  {
-    // main algo goes here
-    int a = count(first.begin(), first.end(), first[0]);   // counting occurances a letter from first slice
-    int b = count(second.begin(), second.end(), first[0]); // counting occurances of same letterfrom first slice
-    if(a!=b)
-    {
-      flag = false; // make flag =  false if occurances are not same and break
-      break;
-    }
-      // if any one occurances of ore is not equal then quit travering  -> mainly added to make algo faster
-  }
-
-  return flag;  // returning flag
-
-}
-else
-{
-  // for odd length
-  // actually every thing is same but diff is at slicing
-  // the difference is commented
-  string first,second;
-  first.assign(val.substr(0,len/2));
-  second.assign(val.substr(len/2 + 1,len)); // ignoring the middle element of string
-
-  for(int i=0;i<len/2;i++)
-  {
-    int a = count(first.begin(), first.end(), first[0]);
-    int b = count(second.begin(), second.end(), first[0]);
-    if(a!=b)
-    {
-      flag = false;
-      break;
-    }
-
-  }
-  return flag;
-}
-}
-
-/*
-
-    MAIN FUNCTION
-
-*/
 int main()
 {
-  // declaring a string variable
-	string str;
+  // the mid value for slicinng puspose
+  int mid;
+  string s,sl,sr;  // s - main string , sl -  left string slice and sr - right string slice
+  getline(cin,s);  // getting user input
 
-  // prompting for user input
-  cout<<"enter a value ";
+  int n = s.length(); // finding length of complete string
+  mid=n/2;     // divinding the string
 
-  // getting user input
-  getline(cin,str);
+  int iter=mid; //even
 
-  // checking from function
-  if(is_lapindrome(str))
-    cout<<"\""<<str<<"\" is a lapindrome"<<endl;
+  if(n%2) //odd
+  {
+    iter++;
+  }
+
+  // splicing string
+  sl=s.substr(0,mid);
+  sr=s.substr(iter,mid);
+
+  // sorting to make easy
+  sort(sl.begin(),sl.end());
+  sort(sr.begin(),sr.end());
+
+  // after sorting equating the strings
+  if(sl==sr)
+  cout<<"LAPINDROME STRING"<<endl;  // this says its lapindrome
   else
-    cout<<"\""<<str<<"\" is not a lapindrome"<<endl;
+  cout<<"NOT A LAPINDROME STRING"<<endl;  // this says not a lapindrome
 
-	return 0;
+  // return 0 for successful exit to OS
+  return 0;
 }
