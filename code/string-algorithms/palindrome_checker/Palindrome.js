da2fc42e3cbf1ac09875a56975f2caa104493382
Frank Kair <frankkair@gmail.com>
2017-10-11 21:43:31 -0300

    Fixes js palindrome formatting

--- code/string-algorithms/palindrome_checker/Palindrome.js
+++ code/string-algorithms/palindrome_checker/Palindrome.js
@@ -4,11 +4,11 @@
 //OpenGenus Contributor
 
 function checkPalindrome(str) {
-       	var palindrome_str = str.split('').reverse().join('');
-	if(palindrome_str == str){
-	    console.log('True');
-	}else{
-	    console.log('False');
+  var palindrome_str = str.split('').reverse().join('');
+	if (palindrome_str == str) {
+    console.log('True');
+	} else {
+    console.log('False');
 	}
 }
 checkPalindrome('malayalam')
