c92d3e404e4ea984d9d53ec901c63faea2d8b1a2
Kris <kris.weber.1994@gmail.com>
2017-10-13 12:18:06 -0700

    Add instructions

---
+++ code/string-algorithms/anagram_search/anagram_search.js
@@ -0,0 +1,8 @@
+// Part of Cosmos by OpenGenus Foundation
+
+// Checks if two strings are anagrams of each other, ignoring any whitespace.
+//
+// Create maps of the counts of every character in each string.
+// As well as keep a set of all characters used in both strings.
+//
+// Check to ensure every unique character is used in both strings the same number of times.
