eedca6bacf45b789a73f74daeb5352970384e5d5
hwchiu <hwchiu@linkernetworks.com>
2017-10-14 00:22:46 +0800

    fix compile fail

--- code/sorting/selection_sort/SelectionSort.go
+++ code/sorting/selection_sort/SelectionSort.go
@@ -1,12 +1,13 @@
 package main
+
 // Selection Sort in Golang
 // Part of Cosmos by OpenGenus Foundation
 import "fmt"
 
-func selectionSort(array [8]int) [8]int{
+func selectionSort(array [8]int) [8]int {
 	for i := 0; i < len(array); i++ {
 		min := i
-		for j := i+1; j < len(array); j++ {
+		for j := i + 1; j < len(array); j++ {
 			if array[j] < array[min] {
 				min = j
 			}
@@ -17,6 +18,7 @@ func selectionSort(array [8]int) [8]int{
 }
 
 func main() {
-	array := [8]int{5,6,1,2,7,9,8,4}
+	array := [8]int{5, 6, 1, 2, 7, 9, 8, 4}
 	sorted := selectionSort(array)
 	fmt.Printf("%v\n", sorted)
+}
