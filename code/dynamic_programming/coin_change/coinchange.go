d2585fe194e512857c23462c8c6eff3ed6831bae
hwchiu <hwchiu@linkernetworks.com>
2017-10-13 21:39:50 +0800

    solve coinchange with DP in golang

---
+++ code/dynamic_programming/coin_change/coinchange.go
@@ -0,0 +1,32 @@
+/*
+ * Part of Cosmos by OpenGenus Foundation
+ */
+package main
+
+import "fmt"
+
+//DP[i] += DP[i-coint[i]]
+//i = coinset,
+//j = for each money
+func solveCoinChange(coins []int, target int) int {
+
+	dp := make([]int, target+1)
+	dp[0] = 1 //basic
+
+	for _, v := range coins {
+		for i := 1; i <= target; i++ {
+			if i >= v {
+				dp[i] += dp[i-v]
+			}
+		}
+	}
+
+	return dp[target]
+}
+func main() {
+	coins := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	target := 8
+
+	way := solveCoinChange(coins, target)
+	fmt.Println(way)
+}
