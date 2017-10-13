46d46e6d0edffa2d14493a8bb77b644fa472c6d4
PShivani07 <31812153+PShivani07@users.noreply.github.com>
2017-10-13 17:46:16 +0530

    Update and rename code/Time conversion/24 Format to code/Time_conversion/24format.cpp

--- code/Time_conversion/24format.cpp
+++ code/Time_conversion/24format.cpp
@@ -6,7 +6,7 @@ int main()
 {
     string str[10];
     cout<<"\nenter time...example:'08:34:42PM'...";
-    for(int i=0;i<=9;i++)
+    for(int i=0;i<10;i++)
         cin>>str[i];
     if(str[8]=='A')
     {
@@ -14,20 +14,21 @@ int main()
             {
                 str[0]=0;
                 str[1]=0;
-                for(int i=0;i<=9;i++)
+                for(int i=0;i<10;i++)
                     cout<<str[i];
             }
     }
+    
     else
     {
         if((str[0]==1)&&(str[1]==12))
-        for(int i=0;i<=9;i++)
+        for(int i=0;i<10;i++)
                 cout<<str[i];
         else
         {
             str[0]+=1;
             str[1]+=2;
-             for(int i=0;i<=9;i++)
+            for(int i=0;i<10;i++)
                 cout<<str[i];
         }
     }
