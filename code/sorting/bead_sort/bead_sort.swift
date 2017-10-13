9f6367b1b3ef82a27581ec358cfb07e14852ddd9
DaiPei <daipei@hust.edu.cn>
2017-10-13 20:06:32 +0800

    make code in bead_sort.swift more readable

--- code/sorting/bead_sort/bead_sort.swift
+++ code/sorting/bead_sort/bead_sort.swift
@@ -9,38 +9,39 @@ import Foundation
 
 func beadSort(_ array: inout [Int]) {
     let n = array.count
-    var max = Int.min
-    for i in 0..<array.count {
-        if array[i] > max {
-            max = array[i]
+    var m = Int.min
+    // find the max value
+    for i in 0..<n {
+        if array[i] > m {
+            m = array[i]
         }
     }
-    var beads = [Int8](repeatElement(0, count: max * array.count))
+    var beads = [Int8](repeatElement(0, count: m * n))
     // put beads
-    for i in 0..<array.count {
-        for j in 0..<max {
+    for i in 0..<n {
+        for j in 0..<m {
             if j < array[i] {
-                beads[i * max + j] = 1
+                beads[i * m + j] = 1
             }
         }
     }
-    for j in 0..<max {
+    for j in 0..<m {
         // count beads
         var sum = 0
-        for i in 0..<array.count {
-            sum += Int(beads[i * max + j])
-            beads[i * max + j] = 0
+        for i in 0..<n {
+            sum += Int(beads[i * m + j])
+            beads[i * m + j] = 0
         }
         // move down beads
-        for i in n - sum..<array.count {
-            beads[i * max + j] = 1
+        for i in n - sum..<n {
+            beads[i * m + j] = 1
         }
     }
     // put sorted value in array using beads
-    for i in 0..<array.count {
+    for i in 0..<n {
         var j = 0
-        while j < max {
-            if beads[i * max + j] == 0 {
+        while j < m {
+            if beads[i * m + j] == 0 {
                 break
             }
             j += 1
