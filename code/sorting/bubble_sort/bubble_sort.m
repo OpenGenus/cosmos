29c1df741de9ea50204ddd1ddd008c64b6e865a8
DaiPei <daipei@hust.edu.cn>
2017-10-14 13:08:20 +0800

    fix unsigned int type overflow bug in bubble_sort.m

--- code/sorting/bubble_sort/bubble_sort.m
+++ code/sorting/bubble_sort/bubble_sort.m
@@ -16,7 +16,7 @@
 @implementation BubbleSort
 
 - (void)sort:(NSMutableArray<NSNumber *> *)array {
-    for (int i = 0; i < array.count-1; i++) {
+    for (int i = 0; i + 1 < array.count; i++) {
         BOOL swapped = NO;
         for (int j = 0; j < array.count-1-i; j++) {
             if ([array[j] compare:array[j + 1]] == NSOrderedDescending) {
@@ -38,22 +38,22 @@
 
 @end
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        int n = 0;
-        NSLog(@"What is the size of the array?");
-        scanf("%d", &n);
-        NSMutableArray *array = [NSMutableArray arrayWithCapacity:n];
-        NSLog(@"Enter elements of the array one by one:");
-        for (int i = 0; i < n; i++) {
-            int tmp;
-            scanf("%d", &tmp);
-            [array addObject:@(tmp)];
-        }
-        BubbleSort *bs = [[BubbleSort alloc] init];
-        [bs sort:array];
-        NSLog(@"%@", array);
-    }
-    return 0;
-}
+//int main(int argc, const char * argv[]) {
+//    @autoreleasepool {
+//        int n = 0;
+//        NSLog(@"What is the size of the array?");
+//        scanf("%d", &n);
+//        NSMutableArray *array = [NSMutableArray arrayWithCapacity:n];
+//        NSLog(@"Enter elements of the array one by one:");
+//        for (int i = 0; i < n; i++) {
+//            int tmp;
+//            scanf("%d", &tmp);
+//            [array addObject:@(tmp)];
+//        }
+//        BubbleSort *bs = [[BubbleSort alloc] init];
+//        [bs sort:array];
+//        NSLog(@"%@", array);
+//    }
+//    return 0;
+//}
 
