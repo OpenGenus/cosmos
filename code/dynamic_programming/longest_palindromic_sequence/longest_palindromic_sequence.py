1c5eb45e3c6a81192d8bc7b3a8b6e5ee3286450f
Sidharth Mishra <sidmishraw@gmail.com>
2017-10-13 19:26:22 -0700

    consumed AdiChat's merge, updating fork's longest_palindromic_sequence.py file accordingly.

--- code/dynamic_programming/longest_palindromic_sequence/longest_palindromic_sequence.py
+++ code/dynamic_programming/longest_palindromic_sequence/longest_palindromic_sequence.py
@@ -1,26 +1,49 @@
-def longest_palindromic_sequence(s):
-    l = len(s)
-    palindrome = [[ 0 if i != j else 1 
-                             for i in xrange(l)] 
-                                 for j in xrange(l)]
-    for j in xrange(1, l):
-        for i in xrange(l - 1):
-            # candidate index
-            y = i + j
-            # in case candidate index is after length of text break
-            if y >= l:
-                break
 
-            # in case equal characters, update length
-            if s[i] == s[y]:
-                if j == 1:
-                    palindrome[i][h] = 2
-                else:    
-                    palindrome[i][y] = palindrome[i + 1][y - 1] + 2 
+# Part of Cosmos by OpenGenus Foundation
+
+def longest_palindrome(text):
+    """ Find the maximum length of a palindrome subsequence
+
+    Dynamic Programming approach on solving the longest palindromic sequence.
+    Time complexity: O(n^2), n = length of text.
+
+    Args:
+        text: string which will be processed
+    Returns:
+        Integer of maximum palindrome subsequence length
+    """
+    length = len(text)
+    # create palindromes length list (space O(n))
+    palindromes_lengths = [1] * length
+    # iterate on each substring character
+    for fgap in xrange(1, length):
+        # get pre-calculated length
+        pre = palindromes_lengths[fgap]
+        # reversed iteration on each substring character
+        for rgap in reversed(xrange(0, fgap)):
+            tmp = palindromes_lengths[rgap]
+            if text[fgap] == text[rgap]:
+                # if equal characters, update palindromes_lengths
+                if rgap + 1 > fgap - 1:
+                    # if characters are neighbors, palindromes_lengths is 2
+                    palindromes_lengths[rgap] = 2
+                else:
+                    # else they're added to the pre-calculated length
+                    palindromes_lengths[rgap] = 2 + pre
             else:
-                # characters not equal take max
-                palindrome[i][y] = max(palindrome[i + 1][y], 
-                                              palindrome[i][y - 1])
+                # if first and last characters do not match, try the latter ones
+                palindromes_lengths[rgap] = max(palindromes_lengths[rgap + 1],
+                                                palindromes_lengths[rgap])
+            # update pre-calculated length
+            pre = tmp
+    # return the maximum palindrome length
+    return palindromes_lengths[0]
 
-    # return the maximum length
-    return palindrome[0][l - 1]   
+def main():
+    """ Main routine to test longest_palindrome funtion """
+    print longest_palindrome('bbabcbcab') # 7: babcbab | bacbcab
+    print longest_palindrome('abbaab')    # 4: abba | baab
+    print longest_palindrome('opengenus') # 3: ene | ege | ngn | nen
+
+if __name__ == '__main__':
+    main()
