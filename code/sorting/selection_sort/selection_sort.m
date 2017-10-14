87e8885aec0cb760350a49322c9f965a7455ab0f
DaiPei <daipei@hust.edu.cn>
2017-10-14 13:09:19 +0800

    fix unsigned int type overflow bug in selection_sort.m

--- code/sorting/selection_sort/selection_sort.m
+++ code/sorting/selection_sort/selection_sort.m
@@ -16,7 +16,7 @@
 @implementation SelectionSort
 
 - (void)sort:(NSMutableArray<NSNumber *> *)array {
-    for (int i = 0; i < array.count - 1; i++) {
+    for (int i = 0; i + 1 < array.count; i++) {
         int p = i;
         for (int j = i + 1; j < array.count; j++) {
             if ([array[j] compare:array[p]] == NSOrderedAscending) {
@@ -35,22 +35,22 @@
 
 @end
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        int n = 0;
-        NSLog(@"What is the size of the array?");
-        scanf("%d", &n);
-        NSMutableArray *array = [NSMutableArray arrayWithCapacity:n];
-        NSLog(@"Enter elements of the array one by one:");
-        for (int i = 0; i < n; i++) {
-            int tmp;
-            scanf("%d", &tmp);
-            [array addObject:@(tmp)];
-        }
-        SelectionSort *ss = [[SelectionSort alloc] init];
-        [ss sort:array];
-        NSLog(@"%@", array);
-    }
-    return 0;
-}
+//int main(int argc, const char * argv[]) {
+//    @autoreleasepool {
+//        int n = 0;
+//        NSLog(@"What is the size of the array?");
+//        scanf("%d", &n);
+//        NSMutableArray *array = [NSMutableArray arrayWithCapacity:n];
+//        NSLog(@"Enter elements of the array one by one:");
+//        for (int i = 0; i < n; i++) {
+//            int tmp;
+//            scanf("%d", &tmp);
+//            [array addObject:@(tmp)];
+//        }
+//        SelectionSort *ss = [[SelectionSort alloc] init];
+//        [ss sort:array];
+//        NSLog(@"%@", array);
+//    }
+//    return 0;
+//}
 
