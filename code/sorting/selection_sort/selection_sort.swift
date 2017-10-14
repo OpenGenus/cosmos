3d7ab02e9c1d281348849df1f7631de4f80185f7
DaiPei <daipei@hust.edu.cn>
2017-10-14 12:57:41 +0800

    make some function private in selection_sort.swift

--- code/sorting/selection_sort/selection_sort.swift
+++ code/sorting/selection_sort/selection_sort.swift
@@ -19,7 +19,7 @@ func selectionSort(_ array: inout [Int]) {
     }
 }
 
-func swap(_ array: inout [Int], at indexA: Int, and indexB: Int) {
+private func swap(_ array: inout [Int], at indexA: Int, and indexB: Int) {
     let tmp = array[indexA]
     array[indexA] = array[indexB]
     array[indexB] = tmp
