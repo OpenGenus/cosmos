0b81b6c105c0054389751f58bcda96b101b26504
Yatharth <yatharthshahjpr@gmail.com>
2017-10-15 10:24:51 +0530

    Update Reverse_queue.c
    The changes in the code are done by restructuring the code and by improving it's indentation for lucid understanding! :)

--- code/data_structures/queue/reverse_queue/Reverse_queue.c
+++ code/data_structures/queue/reverse_queue/Reverse_queue.c
@@ -1,176 +1,136 @@
-// cosmos
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 5
 
-#include<stdio.h>
-#include<stdlib.h>
-struct stackrecord
-  {
-  int *array;
-  int capacity;
-  int tos;
-  };
+struct tag
+{
+        int a[SIZE];
+        int top;
+};   //Structure definition for stack
+typedef struct tag stack;
 
-typedef struct stackrecord *stack;
-stack createstack(int max)
-  {
-  stack s;
-  s=malloc(sizeof(struct stackrecord));
-    if(s==NULL)
-      {
-        printf("out of space");
-      }
-  s->array=malloc((sizeof(int))*max);
-    if(s->array==NULL)
-      {
-        printf("out of space");
-      }
-  s->capacity=max-1;
-  s->tos=-1;
-  return(s);
-  }
-  int isemptys(stack s)
-  {
-    return s->tos==-1;
-  }
-  int isfulls(stack s)
-  {
-    return s->tos==s->capacity;
-  }
-void push(int x,stack s)
-  {
-    if(isfulls(s))
-      printf("Overflow");
-    else
-    {
-      printf("\n %d is pushed",x);
-      s->tos++;
-      s->array[s->tos]=x;
-    }
-    }
-  int topandpop(stack s)
-  {
-    if(isemptys(s))
-    {
-      printf("\n empty stack");
-      return;
-    }
-  else
-    {
-      printf("\n %d is popped",s->array[s->tos]);
-      return s->array[s->tos--];
-    }
-  }
-struct queuerecord
-  {
-    int *array;
-    int front;
-    int rear;
-    int capacity;
-  };
-typedef struct queuerecord *queue;
-queue createqueue(int max)
-  {
-  queue q;
-  q=malloc(sizeof(struct queuerecord));
-    if(q==NULL)
-      printf("Error");
-  q->array=malloc(sizeof(int)*max);
-    if(q->array==NULL)
-      printf("Error");
-  q->capacity=max-1;
-  q->front=-1;
-  q->rear=-1;
-  return q;
-  } 
-int isfullq(queue q)
-  {
-    return (q->rear==q->capacity);
-  }
-int isemptyq(queue q)
-  {
-    return (q->front==-1);
-  }
-void enqueue(queue q,int x)
-  {
-    if(isfullq(q))
-      printf("overflows");
-    else
-    {
-      printf("\n %d is enqueued",x);
-      q->rear++;
-      q->array[q->rear]=x;
-      if(q->front==-1)
-      q->front++;
-    }
-  }
-int frontanddelete(queue q)
-  {
-  int p;
-    if(isemptyq(q))
-    {
-      printf("underflow");
-      return;
-    }
-    else
-    {
-      p=q->array[q->front];
-      printf("\n %d is front and deleted",p);
-      q->front++;
-      return p;
-    }
-  }
-void display(queue q)
-  {
-  int i;
-    if(isemptyq(q))
-    {
-      printf("underflow");
-      return;
-    }
-    for(i=q->front;irear;i++)
-    printf("%d\t",q->array[i]);
-  }
+
+struct tagg
+{
+        int items[SIZE];
+        int rear;
+        int front;
+};  //Structure definition for queue
+typedef struct tagg QUEUE;
+
+
+void push();
+int pop();
+void display();
+void rev(); //Function declarations(parameters not required at declaration)
+
+
 int main()
-  {
-  int max,ele,i,choice,n=0,y,z;
-  queue q;
-  stack s;
-  printf("\n Enter the maximum elements:");
-  scanf("%d",&max);
-  q=createqueue(max);
-  s=createstack(max);
-    while(1)
-    {
-      printf("\n Menu:1.Insert 2.Display reversed order 3.exit");
-      printf("\n Enter the choice:");
-      scanf("%d",&choice);
-  switch(choice)
-  {
-    case 1:
-      printf("\n Enter the element:");
-      scanf("%d",&ele);
-      enqueue(q,ele);
-      n++;
-      break;
-    case 2:
-      printf("\n Contents of the queue:");
-      display(q);
-      for(i=0;i< capacity;i++)
-      {
-      z=frontanddelete(q),s;
-      push(z,s);
-      }
-      q->front=-1;
-      q->rear=-1;
-      for(i=0;i< capacity;i++)
-      {
-      y=topandpop(s);
-      enqueue(q,y);
-      }
-      printf("\n Reversed contents are:");
-      display(q);
-      break;
-    case 3:
-      exit(0);
-    }
-  }
+{
+        int ch, element, dnum;
+        stack st;
+        st.top= -1;
+ 
+        do
+        {       //Displaying menu for operations on stack
+                printf("\n1.) PUSH");
+                printf("\n2.) POP");
+                printf("\n3.) DISPLAY");
+                printf("\n4.) REVERSE");
+                printf("\n5.) EXIT");
+                printf("\nEnter your choice: ");
+                scanf("%d", &ch);
+  
+                switch(ch)
+                {
+                        case 1: printf("\nEnter element to push: ");
+                                    scanf("%d", &element);
+                                    push(&st, element);
+                                    break;
+     
+                        case 2: dnum = pop(&st);
+                                    if(dnum != -1)
+                                    printf("\nDeleted element: %d", dnum);
+                                    break;
+     
+                        case 3: display(st);
+                                    break;
+     
+                        case 4: rev(&st);
+                                    break;
+   
+                        case 5: break;
+   
+                        default: printf("\nWrong choice!! Enter choice again.");
+                  }
+         }while(ch!=5);
+ 
+}
+
+void push(stack *s, int ele)
+{
+        if(s->top == SIZE-1)
+        {
+                printf("\nStack is overflow");
+        }
+        else
+        {
+                (s->top)++;
+                s->a[s->top] = ele;;
+        }
+}
+
+int pop(stack *s)
+{
+        if(s->top == -1)
+        {
+                printf("\nStack is underflow");
+                return(-1);
+        }
+        else
+        {
+                return(s->a[(s->top)--]);
+        }
+}
+
+void display(stack s)
+{
+        int i;
+        if(s.top == -1)
+        {
+               printf("\nStack is empty");
+        }
+        else
+        {
+                for(i=s.top; i>=0; i--)
+                {
+                           printf("\n%d", s.a[i]);
+                }
+          printf("\n");
+         }
+}
+
+//Function to reverse a stack using queue
+void rev(stack *s)
+{
+        QUEUE q;
+        q.rear = -1;
+        q.front = 0;
+ 
+        if(s->top==-1)
+        {
+                printf("\nStack empty");
+                return;
+        }
+ 
+        while((s->top) > -1)
+        {
+                 q.items[++(q.rear)] = s->a[(s->top)--];
+        }
+ 
+        while(q.front <= q.rear)
+        {
+                  s->a[++(s->top)] = q.items[(q.front)++];
+        }
+ 
 }
