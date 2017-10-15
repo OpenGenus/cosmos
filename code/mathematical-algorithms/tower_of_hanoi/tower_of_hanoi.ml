d46d68beceaed2af8b877ac84fdb09743b239f86
Yueru Sun <yrsun@zenefits.com>
2017-10-14 23:44:28 -0700

    add heading

--- code/mathematical-algorithms/tower_of_hanoi/tower_of_hanoi.ml
+++ code/mathematical-algorithms/tower_of_hanoi/tower_of_hanoi.ml
@@ -1,6 +1,8 @@
+(* Part of Cosmos by OpenGenus Foundation *)
+
 let tower_of_hanoi n =
 	let rec move n from dest other =
-		if n = 0 then () else (
+		if n <= 0 then () else (
 		move (n-1) from other dest;
 		print_endline (from ^ " -> " ^ dest);
 		move (n-1) other dest from)
