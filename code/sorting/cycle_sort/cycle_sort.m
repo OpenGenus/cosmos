ee72a2848d0096176c5adf6d4ca2fde7925844b8
DaiPei <daipei@hust.edu.cn>
2017-10-15 23:15:55 +0800

    add header comment to cycle_sort.m

--- code/sorting/cycle_sort/cycle_sort.m
+++ code/sorting/cycle_sort/cycle_sort.m
@@ -1,9 +1,8 @@
+/* Part of Cosmos by OpenGenus Foundation */
+
 //
 //  cycle_sort.m
-//  oc_for_cosmos
-//
 //  Created by DaiPei on 2017/10/15.
-//  Copyright © 2017年 DaiPei. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
