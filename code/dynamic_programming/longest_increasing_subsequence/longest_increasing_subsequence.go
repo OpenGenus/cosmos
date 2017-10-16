9704409ce1b07e4e1ebae34fd1411df95925b2d1
hwchiu <hwchiu@linkernetworks.com>
2017-10-17 00:12:57 +0800

    add nlogn appraoch for LIS problem in golang

--- code/dynamic_programming/longest_increasing_subsequence/longest_increasing_subsequence.go
+++ code/dynamic_programming/longest_increasing_subsequence/longest_increasing_subsequence.go
@@ -36,8 +36,33 @@ func LIS(data []int) int {
 	return ans
 }
 
+//O(n*logn) approach
+func LISV2(data []int) int {
+	tail := make([]int, 0)
+
+	var find int
+	for i := range data {
+
+		find = -1
+		//lower bound, we search from back to head
+		for v := range tail {
+			if tail[len(tail)-v-1] > data[i] {
+				find = len(tail) - 1 - v
+			}
+		}
+		if find == -1 {
+			tail = append(tail, data[i])
+		} else {
+			tail[find] = data[i]
+		}
+	}
+	return len(tail)
+}
+
 func main() {
 	input := []int{10, 23, 5, 81, 36, 37, 12, 38, 51, 92}
 	ans := LIS(input)
+	fmt.Printf("The length of longest_increasing_subsequece in %v is %d\n", input, ans)
+	ans = LISV2(input)
 	fmt.Printf("The length of longest_increasing_subsequece in %v is %d\n", input, ans)
 }
