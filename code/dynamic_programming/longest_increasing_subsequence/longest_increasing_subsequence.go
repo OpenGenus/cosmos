7dee2db620ee96977284e8ccda69eb56b86dbf11
hwchiu <hwchiu@linkernetworks.com>
2017-10-16 23:56:53 +0800

    solve LIS with DP in golang

---
+++ code/dynamic_programming/longest_increasing_subsequence/longest_increasing_subsequence.go
@@ -0,0 +1,36 @@
+/* Part of Cosmos by OpenGenus Foundation */
+package main
+
+import "fmt"
+
+func max(n1, n2 int) int {
+	if n1 > n2 {
+		return n1
+	}
+	return n2
+}
+
+//O(n^2) approach
+func LIS(data []int) int {
+	dp := make([]int, len(data))
+
+	ans := 0
+	for i := range data {
+		dp[i] = 1
+		for j := 0; j < i; j++ {
+			if data[j] < data[i] {
+				dp[i] = max(dp[i], dp[j]+1)
+			}
+		}
+		ans = max(ans, dp[i])
+
+	}
+
+	return ans
+}
+
+func main() {
+	input := []int{10, 23, 5, 81, 36, 37, 12, 38, 51, 92}
+	ans := LIS(input)
+	fmt.Printf("The length of longest_increasing_subsequece is %d\n", ans)
+}
