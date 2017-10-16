a6db05a7275d1bc9493a76394e813ad1e6b87da8
Sarthak Gupta <sarthakgupta259@gmail.com>
2017-10-16 15:05:37 +0530

    Made mini batch implementation efficient.
    Suggested by @malikshubham827

--- code/artificial_intelligence/neural_network/neuralnetwork.py
+++ code/artificial_intelligence/neural_network/neuralnetwork.py
@@ -24,15 +24,15 @@ class neural_network:
             print("== EPOCH: ", j, " ==")
             while i+batch_size != len(inputs):
                 self.error = 0  
-                input_batch = []
-                label_batch = []
-                # print(i)
-                for i in range(i, i+batch_size):
-                    input_batch.append(inputs[i])
-                    label_batch.append(labels[i])   
-                self.forward_pass(input_batch)
-                self.calculate_error(label_batch)
-                self.back_pass(label_batch)
+#                 input_batch = []
+#                 label_batch = []
+#                 # print(i)
+#                 for i in range(i, i+batch_size):
+#                     input_batch.append(inputs[i])
+#                     label_batch.append(labels[i])   
+                self.forward_pass(inputs[i:i+batch_size])
+                self.calculate_error(labels[i:i+batch_size)
+                self.back_pass(labels[i:i+batch_size])
                 i += 1
             print("Error: ", self.error)
         dill.dump_session(filename)
