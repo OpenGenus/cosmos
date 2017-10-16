1b68b2b43b64987a2c420c29d1b3844188d38eaf
Cyandev <unixzii@163.com>
2017-10-16 03:30:51 -0500

    fix codestyle

--- code/bit-manipulation/count_set_bits/countSetBits.js
+++ code/bit-manipulation/count_set_bits/countSetBits.js
@@ -1,8 +1,8 @@
 export default function countSetBits(n) {
-	n = +n;
-  
+  n = +n;
+
   if (!(n >= 0 || n < 0)) {
-  	return 0;
+    return 0;
   } else if (n < 0) {
   	throw new Error('negtive numbers are not supported');
   }
