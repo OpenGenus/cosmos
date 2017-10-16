caa0b0c9f8c6989d3b54c9617c8fa96c5cd1a5a7
tanasegabriel <tanase.gabriel91@gmail.com>
2017-10-16 22:30:22 +0100

    Refactored logic

--- code/mathematical-algorithms/replace_0_with_5/change0to5.py
+++ code/mathematical-algorithms/replace_0_with_5/change0to5.py
@@ -1,6 +1,16 @@
 # Part of Cosmos by OpenGenus Foundation
-n = list(input("Enter the number: "))
-for i in range(len(n)):
-	if(n[i]=='0'):
-		n[i]='5'
-print("modified number: "+ ''.join(n))
+user_input = raw_input("Enter the number: ")
+try:
+  if isinstance(int(user_input), int):
+    int_to_str = str(user_input)
+    modified = ''
+    for i in range(len(int_to_str)):
+      if int_to_str[i] == '0':
+        modified += '5'
+      else:
+        modified += int_to_str[i]
+
+  print("Modified number: " + modified)
+
+except Exception:
+  print("You did not specify an integer")
