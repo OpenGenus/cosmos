a6d2cbec9b4bb2d1cbd43a95fbb9b9d0a7fe881a
Justin Dalrymple <justin.s.dalrymple@gmail.com>
2017-10-15 22:14:20 -0400

    Adding count digits function in go

---
+++ code/mathematical-algorithms/count_digits/count_digits.go
@@ -0,0 +1,18 @@
+func countDigits(n int) int {
+	count := 0
+
+	if n == 0
+		return 1
+
+    for(n != 0){
+        count++;
+        n /= 10;
+    }
+
+    return count;
+}
+
+func main() {
+  a := 154
+  fmt.Printf("How many digits are in %v ? %v\n", a, countDigits(a))
+}
