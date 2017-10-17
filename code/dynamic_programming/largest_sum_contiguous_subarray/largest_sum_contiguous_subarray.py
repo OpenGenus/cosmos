0dc928cdf75e17157ac31bcc0cfce95d5ed46bf5
Vitorvgc <vitorvgc07@gmail.com>
2017-10-17 00:52:29 -0200

    Remove unnecessary size parameter

--- code/dynamic_programming/largest_sum_contiguous_subarray/largest_sum_contiguous_subarray.py
+++ code/dynamic_programming/largest_sum_contiguous_subarray/largest_sum_contiguous_subarray.py
@@ -1,12 +1,12 @@
 # Part of Cosmos by OpenGenus Foundation
 
 # Function to find the maximum contiguous subarray using Kadane's algorithm
-def maxSubArraySum(a,size):
+def maxSubArraySum(a):
 
     max_so_far = a[0] 
     max_ending_here = max(a[0], 0)
 
-    for i in range(1, size):
+    for i in range(1, len(a)):
         max_ending_here += a[i]
         max_so_far = max(max_so_far, max_ending_here)
         max_ending_here = max(max_ending_here, 0);
@@ -16,4 +16,4 @@ def maxSubArraySum(a,size):
 # Enter the number seperated by spaces
 arr = [int(x) for x in input().split()]
 
-print("Maximum contiguous sum is", maxSubArraySum(arr,len(arr)))
+print("Maximum contiguous sum is", maxSubArraySum(arr))
