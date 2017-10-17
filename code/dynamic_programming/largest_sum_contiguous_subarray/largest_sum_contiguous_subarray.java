8161d674aaddb97dce82fd3bf8f01dda96a09db4
Vitorvgc <vitorvgc07@gmail.com>
2017-10-16 23:48:06 -0200

    Simplify code

--- code/dynamic_programming/largest_sum_contiguous_subarray/largest_sum_contiguous_subarray.java
+++ code/dynamic_programming/largest_sum_contiguous_subarray/largest_sum_contiguous_subarray.java
@@ -7,15 +7,13 @@ public class Max_subarray_problem {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int n;
         System.out.println("Enter the number of elements");
-        n = sc.nextInt();
+        int n = sc.nextInt();
         int[] a = new int[n];
-        System.out.println("Enter the value of Elements");
+        System.out.println("Enter the value of the elements");
         for(int i = 0; i < n; i++)
-        {
             a[i] = sc.nextInt();
-        }
+
         int m = findmaxsum(a, 0, a.length - 1);
 
         System.out.println(m);
@@ -24,50 +22,40 @@ public class Max_subarray_problem {
     private static int findmaxsum(int[] a, int l, int h) {
         int max;
         if(l==h)
-        {
             return a[l];
-        }
         else
         {
             int mid = (l + h) / 2;
-            int leftmaxsum;
 
-            leftmaxsum = findmaxsum(a, l, mid);
-
+            int leftmaxsum  = findmaxsum(a, l, mid);
             int rightmaxsum = findmaxsum(a , mid + 1, h);
             int crossmaxsum = findcrosssum(a, l, mid, h);
        
             max = Math.max(Math.max(leftmaxsum, rightmaxsum), crossmaxsum);
-       
         }
         return max;
       
     }
 
     private static int findcrosssum(int[] a, int l, int mid, int h) {
+
         int leftsum = Integer.MIN_VALUE;
         int lsum = 0;
-        int csum;
         for(int i = mid; i >= l; i--)
         {
-            lsum = lsum + a[i];
+            lsum += a[i];
             if(lsum > leftsum)
-            {
                 leftsum = lsum;
-            }
         }
         int rightsum = Integer.MIN_VALUE;
         int rsum = 0;
         for(int j = mid + 1; j <= h; j++)
         {
-            rsum = rsum + a[j];
+            rsum += a[j];
             if(rsum > rightsum)
-            {
                 rightsum = rsum;
-            }
         }
-        csum = rightsum + leftsum;
-        return csum;
+        return rightsum + leftsum;
     }
     
 }
