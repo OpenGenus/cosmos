bb41ed7f4b7fd2c8a0dc92ac4b71601d63840440
Vitorvgc <vitorvgc07@gmail.com>
2017-10-16 23:39:44 -0200

    Fix identation and improve readability

--- code/dynamic_programming/largest_sum_contiguous_subarray/largest_sum_contiguous_subarray.java
+++ code/dynamic_programming/largest_sum_contiguous_subarray/largest_sum_contiguous_subarray.java
@@ -3,70 +3,71 @@
 import java.util.Scanner;
 
 public class Max_subarray_problem {
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+
+        Scanner sc = new Scanner(System.in);
         int n;
         System.out.println("Enter the number of elements");
-        n=sc.nextInt();
-        int[] a=new int[n];
+        n = sc.nextInt();
+        int[] a = new int[n];
         System.out.println("Enter the value of Elements");
-        for(int i=0;i<n;i++)
+        for(int i = 0; i < n; i++)
         {
-            a[i]=sc.nextInt();
+            a[i] = sc.nextInt();
         }
-       int m= findmaxsum(a,0,a.length-1);
+        int m = findmaxsum(a, 0, a.length - 1);
 
-       System.out.println(m);
+        System.out.println(m);
     }
 
     private static int findmaxsum(int[] a, int l, int h) {
         int max;
-       if(l==h)
-       {
-           return a[l];
-       }
-       else
-       {
-           int mid=(l+h)/2;
-           int leftmaxsum;
+        if(l==h)
+        {
+            return a[l];
+        }
+        else
+        {
+            int mid = (l + h) / 2;
+            int leftmaxsum;
 
-           leftmaxsum=findmaxsum(a,l,mid);
+            leftmaxsum = findmaxsum(a, l, mid);
 
-           int rightmaxsum=findmaxsum(a,mid+1,h);
-           int crossmaxsum=findcrosssum(a,l,mid,h);
+            int rightmaxsum = findmaxsum(a , mid + 1, h);
+            int crossmaxsum = findcrosssum(a, l, mid, h);
        
-        max=Math.max(Math.max(leftmaxsum, rightmaxsum), crossmaxsum);
+            max = Math.max(Math.max(leftmaxsum, rightmaxsum), crossmaxsum);
        
-       }
-       return max;
+        }
+        return max;
       
     }
 
     private static int findcrosssum(int[] a, int l, int mid, int h) {
-        int leftsum=Integer.MIN_VALUE;
-        int lsum=0;
+        int leftsum = Integer.MIN_VALUE;
+        int lsum = 0;
         int csum;
-        for(int i=mid;i>=l;i--)
+        for(int i = mid; i >= l; i--)
         {
-            lsum=lsum+a[i];
-            if(lsum>leftsum)
+            lsum = lsum + a[i];
+            if(lsum > leftsum)
             {
-                leftsum=lsum;
+                leftsum = lsum;
             }
         }
-        int rightsum=Integer.MIN_VALUE;
-        int rsum=0;
-        for(int j=(mid+1);j<=h;j++)
+        int rightsum = Integer.MIN_VALUE;
+        int rsum = 0;
+        for(int j = mid + 1; j <= h; j++)
         {
-          rsum=rsum+a[j];
-          if(rsum>rightsum)
-          {
-              rightsum=rsum;
-          }
+            rsum = rsum + a[j];
+            if(rsum > rightsum)
+            {
+                rightsum = rsum;
+            }
         }
-        csum=rightsum+leftsum;
+        csum = rightsum + leftsum;
         return csum;
-        
-       }
+    }
     
 }
