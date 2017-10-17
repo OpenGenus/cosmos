4b2ce34ee59ab6f32b23958d5bd6ff1ac2639112
Ernest <iattempt.net@gmail.com>
2017-10-17 23:27:58 +0800

    change: replace multi-comment with one-line-comment

--- code/sorting/quick_sort/quick_sort.cpp
+++ code/sorting/quick_sort/quick_sort.cpp
@@ -2,7 +2,7 @@
     Part of Cosmos by OpenGenus Foundation
 
     quick sort synopsis
- 
+
 // implementation for 'standard random access container'
 template<typename _Random_Acccess_Iter, typename _Compare>
 void quicksort_impl(_Random_Acccess_Iter first, _Random_Acccess_Iter last, _Compare comp);
@@ -18,19 +18,10 @@ void quicksort(_Random_Acccess_Iter begin, _Random_Acccess_Iter end);
 template<typename _Compare>
 void quicksort(int *begin, int *end, _Compare comp);
 
-void quicksort(int *begin, int *end);
+ void quicksort(int *begin, int *end);
 */
 
 #include <functional>
-
-// for test
-// #include <iostream>
-// #include <algorithm>
-// #include <iomanip>
-// #include <vector>
-// #include <deque>
-// #include <array>
-// #include <string>
 
 // implementation for 'standard random access container'
 template<typename _Random_Acccess_Iter, typename _Compare>
@@ -82,53 +73,63 @@ void quicksort(int *begin, int *end) {
     return quicksort(begin, end, std::less<int>());
 }
 
-//// for test
-//void print(std::vector<int> &v) {
-//    for (size_t i = 0; i < v.size(); ++i)
-//        std::cout << std::setw(3) << v[i] << " ";
-//    std::cout << std::endl;
-//}
-//
-//void print(int v[], size_t len) {
-//    for (size_t i = 0; i < len; ++i)
-//        std::cout << std::setw(3) << v[i] << " ";
-//    std::cout << std::endl;
-//}
-//
-//int main()
-//{
-//    using namespace std;
-//
-//// STL Container (random access)
-//    vector<int> v, t;
-//    srand((unsigned)clock());
-//
-//    int sz = 20 + rand() % 2;
-//    for (int i = 0; i < sz; i++)
-//        v.push_back(rand() % 10);
-//    t = v;
-//
-//    quicksort(v.begin(), v.end());
-//    sort(t.begin(), t.end());
-//    for (int i = 0; i < sz; ++i)
-//        if (v.at(i) != t.at(i))
-//            cout << "error" << endl;
-//
-//    cout << endl;
-//
-//// POD, only implement int function template not allow partial specialization
-//    sz = 20 + rand() % 2;
-//    int a[sz], b[sz];
-//    for (int i = 0; i < sz; i++) {
-//        a[i] = rand() % 10;
-//        b[i] = a[i];
-//    }
-//
-//    quicksort(a, a + sz);
-//    sort(b, b + sz);
-//    for (int i = 0; i < sz; ++i)
-//        if (a[i] != b[i])
-//            cout << "error" << endl;
-//
-//    return 0;
-//}
+/*
+// for test
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <vector>
+#include <deque>
+#include <array>
+#include <string>
+
+void print(std::vector<int> &v) {
+    for (size_t i = 0; i < v.size(); ++i)
+        std::cout << std::setw(3) << v[i] << " ";
+    std::cout << std::endl;
+}
+
+void print(int v[], size_t len) {
+    for (size_t i = 0; i < len; ++i)
+        std::cout << std::setw(3) << v[i] << " ";
+    std::cout << std::endl;
+}
+
+int main()
+{
+    using namespace std;
+
+// STL Container (random access)
+    vector<int> v, t;
+    srand((unsigned)clock());
+
+    int sz = 20 + rand() % 2;
+    for (int i = 0; i < sz; i++)
+        v.push_back(rand() % 10);
+    t = v;
+
+    quicksort(v.begin(), v.end());
+    sort(t.begin(), t.end());
+    for (int i = 0; i < sz; ++i)
+        if (v.at(i) != t.at(i))
+            cout << "error" << endl;
+
+    cout << endl;
+
+// POD, only implement int function template not allow partial specialization
+    sz = 20 + rand() % 2;
+    int a[sz], b[sz];
+    for (int i = 0; i < sz; i++) {
+        a[i] = rand() % 10;
+        b[i] = a[i];
+    }
+
+    quicksort(a, a + sz);
+    sort(b, b + sz);
+    for (int i = 0; i < sz; ++i)
+        if (a[i] != b[i])
+            cout << "error" << endl;
+
+    return 0;
+}
+*/
