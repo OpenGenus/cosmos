58e4a37d39d14377bfa687ef2ea7559752686e82
hwchiu <hwchiu@linkernetworks.com>
2017-10-17 23:39:20 +0800

    solve poserset problem with backtracking in golang

---
+++ code/backtracking/powerset/powerset.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func powerSet(input, record []int, index int) {
+	if index == len(input) {
+		fmt.Printf("{")
+		for i, v := range record {
+			if v == 1 {
+				fmt.Printf("%v ", input[i])
+			}
+		}
+		fmt.Printf("}\n")
+		return
+	}
+
+	record[index] = 1
+	powerSet(input, record, index+1)
+	record[index] = 0
+	powerSet(input, record, index+1)
+
+}
+
+func main() {
+	input := []int{1, 2, 3}
+	record := make([]int, len(input))
+
+	powerSet(input, record, 0)
+}
