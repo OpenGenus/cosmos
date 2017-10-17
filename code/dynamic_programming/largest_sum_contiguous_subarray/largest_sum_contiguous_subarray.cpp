9a9b7e497f17635c3602f67ac1d12ca1a7c157e4
Vitorvgc <vitorvgc07@gmail.com>
2017-10-17 00:06:10 -0200

    Improve readability

--- code/dynamic_programming/largest_sum_contiguous_subarray/largest_sum_contiguous_subarray.cpp
+++ code/dynamic_programming/largest_sum_contiguous_subarray/largest_sum_contiguous_subarray.cpp
@@ -6,22 +6,25 @@ BY:- https://github.com/alphaWizard
 */
 
 #include <bits/stdc++.h>
+
 using namespace std;
+
 int maxsubarraysum(int ar[],int n)
 {
-    int msf=ar[0],mth=ar[0];
-    int p=0;
-    if(ar[0]<0) ++p;
-        int maxi=ar[0];
-    for(int i=1;i<n;i++)
+    int msf = ar[0], mth = ar[0];
+    int p = 0;
+    if(ar[0] < 0) ++p;
+    int maxi = ar[0];
+    for(int i=1; i<n; i++)
     {
-        maxi=max(maxi,ar[i]); if(ar[i]<0) ++p;// for handing case of all negative array elements
+        maxi = max(maxi, ar[i]);
+        if(ar[i] < 0) ++p; // for handing case of all negative array elements
        
-        mth+=ar[i];
-        if(mth>msf) msf=mth;
-        if(mth<0) mth=0;
+        mth += ar[i];
+        if(mth > msf) msf = mth;
+        if(mth < 0) mth = 0;
     }
-    return (p!=n)?msf:maxi;
+    return (p != n) ? msf : maxi;
 }
 
 int main() {
@@ -29,10 +32,12 @@ int main() {
     cin>>t;
     while(t--)
     {
-        int n;cin>>n;int ar[n]; //enter no, of terms
-        for(int i=0;i<n;++i)
-            cin>>ar[i];  //enter n elements of array
-        cout<<maxsubarraysum(ar,n)<<"\n";
+        int n;
+        cin>>n; //enter no. of terms
+        int ar[n];
+        for(int i = 0; i < n; ++i)
+            cin >> ar[i];  //enter n elements of array
+        cout << maxsubarraysum(ar, n) << "\n";
     }
     return 0;
 }
