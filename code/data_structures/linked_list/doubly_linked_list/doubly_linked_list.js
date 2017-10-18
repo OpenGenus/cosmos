fc907010c07495df6e1cfe9d840c8699d8d725b4
brunocalou <brunocaloualves@gmail.com>
2017-10-18 09:31:20 -0200

    Added Node function

--- code/data_structures/linked_list/doubly_linked_list/doubly_linked_list.js
+++ code/data_structures/linked_list/doubly_linked_list/doubly_linked_list.js
@@ -4,7 +4,17 @@
 
 "use strict";
 
-var Node = require('./node');
+/**
+ * Node class
+ * @constructor
+ * @public
+ * @param {any} value - The value to be stored
+ */
+function Node(value) {
+  this.value = value;
+  this.next = null;
+  this.prev = null;
+}
 
 /**
  * Linked list class
