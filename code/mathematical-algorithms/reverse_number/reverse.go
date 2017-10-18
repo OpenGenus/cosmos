c0aa68bab1442b59df32a87234105fd52d348b4b
Brett <blockloop@users.noreply.github.com>
2017-10-18 18:39:22 -0500

    Update reverse.go

--- code/mathematical-algorithms/reverse_number/reverse.go
+++ code/mathematical-algorithms/reverse_number/reverse.go
@@ -12,8 +12,11 @@ func main() {
 	fmt.Println("reverse of", num, "is", reverseNumber(num))
 }
 
-func reverseNumber(n int) string {
+func reverseNumber(n int) int {
 	splits := strings.Split(strconv.Itoa(n), "")
 	sort.Sort(sort.Reverse(sort.StringSlice(splits)))
-	return strings.Join(splits, "")
+	s := strings.Join(splits, "")
+	i, _ := strconv.Atoi(s)
+	return i
 }
+
