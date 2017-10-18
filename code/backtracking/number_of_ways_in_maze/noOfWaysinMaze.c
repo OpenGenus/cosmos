58d9aa8851d2b1c86fd84718660c2e5f7e49a33e
hwchiu <hwchiu@linkernetworks.com>
2017-10-18 23:18:29 +0800

    try all direction to solve the problems

--- code/backtracking/number_of_ways_in_maze/noOfWaysinMaze.c
+++ code/backtracking/number_of_ways_in_maze/noOfWaysinMaze.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
-
+#define MAX_SIZE 10
 /*
  * Part of Cosmos by OpenGenus Foundation
 */
 
 ///i,j = current cell, m,n = destination
 void solveMaze(char maze[][10],int sol[][10],int i,int j,int m,int n,int *ways){
-
     ///Base Case
     if(i==m && j==n){
         sol[m][n] = 1;
@@ -22,17 +21,30 @@ void solveMaze(char maze[][10],int sol[][10],int i,int j,int m,int n,int *ways){
         return ;
     }
 
+    if (sol[i][j]) {
+        return;
+    }
     ///Rec Case
     ///Assume there is a way from this cell
     sol[i][j] = 1;
 
+    /// Try going Right
     if(j+1<=n && maze[i][j+1]!='X'){
         solveMaze(maze,sol,i,j+1,m,n,ways);
+    }
+    /// Try going Left
+    if(j-1>=0 && maze[i][j-1]!='X'){
+        solveMaze(maze,sol,i,j-1,m,n,ways);
+    }
+    /// Try going Up
+    if(i-1>=0 && maze[i-1][j]!='X'){
+        solveMaze(maze,sol,i-1,j,m,n,ways);
     }
     /// Try going Down
     if(i+1<=m && maze[i+1][j]!='X'){
         solveMaze(maze,sol,i+1,j,m,n,ways);
     }
+
     ///Backtracking!
     sol[i][j] =0;
     return;
@@ -41,7 +53,7 @@ void solveMaze(char maze[][10],int sol[][10],int i,int j,int m,int n,int *ways){
 
 int main(){
 
-    char maze[10][10] = {
+    char maze[MAX_SIZE][MAX_SIZE] = {
         "00XXX",
         "00000",
         "0XX00",
@@ -51,7 +63,15 @@ int main(){
 
     int m = 4,n=4;
 
-    int sol[10][10] = {0};
+    int sol[MAX_SIZE][MAX_SIZE];
+
+    int i,j;
+    for (i = 0; i < MAX_SIZE; i++) {
+        for (j = 0; j <MAX_SIZE; j++) {
+            sol[i][j]=0;
+        }
+    }
+
     int ways=0;
     solveMaze(maze,sol,0,0,m,n,&ways);
     if(ways){
