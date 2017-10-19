edc12db62ed69079aa279c58412e2171c5756a21
Gurkirat Singh <tbhaxor@gmail.com>
2017-10-19 11:32:41 +0530

    Count digits in csharp (#1945)
    * updated
    
    * check_is_square in csharp
    
    * check_is_square in php
    
    * armstrong number in csharp
    
    * count digits in csharp

---
+++ code/mathematical-algorithms/count_digits/count_digits.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace count_digits
+{
+    class Program
+    {
+        static int digits(int number)
+        {
+            int count = 0;
+            while (number != 0)
+            {
+                count++;
+                number /= 10;
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            int number = 89922;
+            Console.WriteLine("number {0} has {1} digits", number, digits(number));
+            Console.ReadKey();
+        }
+    }
+}
