d704fbe7f00ba57f22bc4be3d47e84e5e9041bb2
Ahavaz <Ahavaz@users.noreply.github.com>
2017-10-19 10:17:48 -0200

    Added perfect square checker

--- code/string-algorithms/palindrome_checker/palindrome.cs
+++ code/string-algorithms/palindrome_checker/palindrome.cs
@@ -1,12 +1,9 @@
 using System;
 using System.Linq;
 
-namespace PalindromeChecker
-{
-	class Program
-	{
-		static void Main(string[] args)
-		{
+namespace PalindromeChecker {
+	class Program {
+		static void Main(string[] args) {
 			Console.WriteLine(IsPalindrome("Borrow or rob?")); // should output true
 			Console.WriteLine(IsPalindrome("To be, or not to be.")); // should output false
 			Console.WriteLine(IsPalindrome("A Toyota’s a Toyota.")); // should output true
@@ -16,8 +13,7 @@ namespace PalindromeChecker
 			Console.WriteLine(IsPalindrome("Are we not pure? “No, sir!” Panama’s moody Noriega brags. “It is garbage!” Irony dooms a man—a prisoner up to new era.")); // should output true
 		}
 
-		static bool IsPalindrome(string str)
-		{
+		static bool IsPalindrome(string str) {
 			string s = new string(str.ToLower().Where(c => Char.IsLetterOrDigit(c)).ToArray());
 			char[] arr = s.ToCharArray();
 			Array.Reverse(arr);
