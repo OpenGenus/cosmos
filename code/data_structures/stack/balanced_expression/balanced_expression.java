cf830c90916255f250e5cd72b8197d2354b554ba
Srebalaji Thirumalai <srebalaji@outlook.com>
2017-10-20 12:12:32 +0530

    Added few changes 1. Fixed indentation 2. Changed class name to java convention 3. Added appropriate breaks after if statements.

--- code/data_structures/stack/balanced_expression/balanced_expression.java
+++ code/data_structures/stack/balanced_expression/balanced_expression.java
@@ -1,33 +1,36 @@
 import java.util.*;
 
-class balanced_expression {
-  public static void main(String args[]) {
+class BalancedExpression {
+	public static void main(String args[]) {
 		System.out.println("Balanced Expression");
 		Scanner in = new Scanner(System.in);
 		String input = in.next();
-		boolean output = is_expression_balanced(input);
-		if (output == true) System.out.println("The expression is balanced");
-		else System.out.println("The expression is not balanced");
-  }
-
-  static boolean is_expression_balanced(String input) {
-  	Stack stack = new Stack();
-		for (int i=0; i<input.length(); i++) {
-			if (input.charAt(i) == '(' || input.charAt(i) == '{'|| input.charAt(i) == '['){
-				stack.push(input.charAt(i));
-			}
-			if (input.charAt(i) == ')' || input.charAt(i) == '}'|| input.charAt(i) == ']'){
-			
-				if (stack.empty()) return false;
-
-				char top_char = (char) stack.pop();
-
-				if ( (top_char == '(' && input.charAt(i) != ')') || (top_char == '{' && input.charAt(i) != '}') || (top_char == '[' && input.charAt(i) != ']') ){
-				return false;
-				}	  
-			}
+		if (is_expression_balanced(input)) {
+			System.out.println("The expression is balanced");
 		}
-		return stack.empty();
-  }
+		else {
+			System.out.println("The expression is not balanced");
+		}
+
+		static boolean is_expression_balanced(String input) {
+			Stack stack = new Stack();
+			for (int i=0; i<input.length(); i++) {
+				if (input.charAt(i) == '(' || input.charAt(i) == '{'|| input.charAt(i) == '[') {
+					stack.push(input.charAt(i));
+				}
+				if (input.charAt(i) == ')' || input.charAt(i) == '}'|| input.charAt(i) == ']') {
+					if (stack.empty()) {
+						return false;
+					}
+					char top_char = (char) stack.pop();
+					
+					if ( (top_char == '(' && input.charAt(i) != ')') || (top_char == '{' && input.charAt(i) != '}') || (top_char == '[' && input.charAt(i) != ']') ) {
+						return false;
+					}	  
+				}
+			}
+			return stack.empty();
+		}
+	}
 }
 
