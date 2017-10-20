7612c9ad2503a42965aa29ea0b53805c54683b9b
tbhaxor <tbhaxor@gmail.com>
2017-10-20 19:50:14 +0530

    russian peasant in csharp

---
+++ code/mathematical-algorithms/russian_peasant_multiplication/russian_peasant.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace russian_peasant
+{
+    class russ_peasant
+    {
+        int a, b;
+        public russ_peasant(int number1,int number2)
+        {
+            a = number1;
+            b = number2;
+        }
+        public int multiply()
+        {
+            int res = 0;
+            while (b > 0)
+            {
+                if (b % 2 == 1)
+                {
+                    res += a;
+                }
+                a <<= 2;
+                b >>= 2;
+            }
+            return res;
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            russ_peasant obj = new russ_peasant(15, 20);
+            Console.WriteLine(obj.multiply());
+
+            Console.ReadKey();
+        }
+    }
+}
