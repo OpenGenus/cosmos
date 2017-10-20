4228b665da8a584fadd2fb773571b2036b5b6f14
Ernest <iattempt.net@gmail.com>
2017-10-20 19:11:59 +0800

    change: encapsulate functions to class

--- code/data_structures/avl_tree/avl_tree.cpp
+++ code/data_structures/avl_tree/avl_tree.cpp
@@ -3,135 +3,167 @@
 // structure representing an AVL node
 #include <algorithm>
 #include <ostream>
-struct AVLNode {
-    int data;
-    AVLNode *left;
-    AVLNode *right;
-    int height;
-};
 
-// helper function to return height of a node
-int getHeight(AVLNode *node) {
-    if (node)
-        return node->height;
+class avl_tree {
+private:
+    struct AVLNode {
+        int data;
+        AVLNode *left;
+        AVLNode *right;
+        int height;
+    };
 
-    return -1;
-}
+    // LL rotation rooted at X
+    AVLNode *rotateLL(AVLNode * &X) {
+        AVLNode *W = X->left;
+        X->left = W->right;
+        W->right = X;
+        X->height = std::max(getHeight(X->left), getHeight(X->right)) + 1;
+        W->height = std::max(getHeight(W->left), getHeight(X)) + 1;
 
-// function to find the minimum element in the tree (lefftmost node)
-AVLNode *findMin(AVLNode *root) {
-    if (root != nullptr)
-        while (root->left != nullptr)
-            root = root->left;
+        return W;   // new root
+    }
 
-    return root;
-}
+    // RR rotation rooted at X
+    AVLNode *rotateRR(AVLNode * &X) {
+        AVLNode *W = X->right;
+        X->right = W->left;
+        W->left = X;
+        X->height = std::max(getHeight(X->left), getHeight(X->right)) + 1;
+        W->height = std::max(getHeight(X), getHeight(W->right));
 
-// function to find the maximum element in the tree (rightmost node)
-AVLNode *findMax(AVLNode *root) {
-    if (root != nullptr)
-        while (root->right != nullptr)
-            root = root->right;
+        return W;   // new root
+    }
 
-    return root;
-}
+    // LR rotation rooted at X
+    AVLNode *rotateLR(AVLNode * &X) {
+        X->left = rotateRR(X->left);
 
-// LL rotation rooted at X
-AVLNode *rotateLL(AVLNode * &X) {
-    AVLNode *W = X->left;
-    X->left = W->right;
-    W->right = X;
-    X->height = std::max(getHeight(X->left), getHeight(X->right)) + 1;
-    W->height = std::max(getHeight(W->left), getHeight(X)) + 1;
+        return rotateLL(X);
+    }
 
-    return W;   // new root
-}
+    // RL rotation rooted at X
+    AVLNode *rotateRL(AVLNode * &X) {
+        X->right = rotateLL(X->right);
 
-// RR rotation rooted at X
-AVLNode *rotateRR(AVLNode * &X) {
-    AVLNode *W = X->right;
-    X->right = W->left;
-    W->left = X;
-    X->height = std::max(getHeight(X->left), getHeight(X->right)) + 1;
-    W->height = std::max(getHeight(X), getHeight(W->right));
+        return rotateRR(X);
+    }
 
-    return W;   // new root
-}
+    // helper function to return height of a node
+    int getHeight(AVLNode *node) {
+        if (node)
+            return node->height;
 
-// LR rotation rooted at X
-AVLNode *rotateLR(AVLNode * &X) {
-    X->left = rotateRR(X->left);
+        return -1;
+    }
 
-    return rotateLL(X);
-}
+public:
+    avl_tree() :root(nullptr) {;}
 
-// RL rotation rooted at X
-AVLNode *rotateRL(AVLNode * &X) {
-    X->right = rotateLL(X->right);
+    ~avl_tree() {deleteTree(root);}
 
-    return rotateRR(X);
-}
+    // function to find the minimum element in the tree (lefftmost node)
+    AVLNode *findMin(AVLNode *root) {
+        if (root != nullptr)
+            while (root->left != nullptr)
+                root = root->left;
 
-// function to insert a node into the AVL tree
-AVLNode *insertIntoAVL(AVLNode * &root, int data) {
-    if (root == nullptr) {
-        AVLNode *newNode = new AVLNode;
-        newNode->data = data;
-        newNode->height = 0;
-        newNode->left = newNode->right = nullptr;
-        root = newNode;
-    } else if (data < root->data) {
-        root->left = insertIntoAVL(root->left, data);
-        if (getHeight(root->left) - getHeight(root->right) == 2) {
-            if (data < root->left->data) {
-                root = rotateLL(root);
-            } else {
-                root = rotateLR(root);
-            }
-        }
-    } else if (data > root->data) {
-        root->right = insertIntoAVL(root->right, data);
-        if (getHeight(root->right) - getHeight(root->left) == 2) {
-            if (data > root->right->data) {
-                root = rotateRR(root);
-            } else {
-                root = rotateRL(root);
-            }
+        return root;
+    }
+
+    // function to find the maximum element in the tree (rightmost node)
+    AVLNode *findMax(AVLNode *root) {
+        if (root != nullptr)
+            while (root->right != nullptr)
+                root = root->right;
+
+        return root;
+    }
+
+    // function to insert a node into the AVL tree
+    void insertIntoAVL(int data) {
+        root = insertIntoAVL(root, data);
+    }
+
+    // function to delete a node from the AVL tree
+    void deleteFromAVL(int data) {
+        root = deleteFromAVL(root, data);
+    }
+
+    // preorder traversal of the AVL tree
+    void preOrder(std::ostream &out) {
+        preOrder(root, out);
+    }
+
+    // inorder traversal of the AVL tree
+    void inOrder(std::ostream &out) {
+        inOrder(root, out);
+    }
+
+    // postorder traversal of the AVL tree
+    void postOrder(std::ostream &out) {
+        postOrder(root, out);
+    }
+
+    // function to free allocated memory
+    void deleteTree(AVLNode *root) {
+        if (root != nullptr) {
+            deleteTree((root)->left);
+            deleteTree((root)->right);
+            delete root;
         }
     }
-    root->height = std::max(getHeight(root->left), getHeight(root->right)) + 1;
 
-    return root;
-}
+private:
+    AVLNode *root;
 
-// function to delete a node from the AVL tree
-AVLNode *deleteFromAVL(AVLNode * &root, int data) {
-    if (root == nullptr) {
-        return nullptr;
-    } else if (data < root->data) {
-        root->left = deleteFromAVL(root->left, data);
-        if (getHeight(root->right) - getHeight(root->left) == 2) {
-            if (getHeight(root->right->right) > getHeight(root->right->left)) {
-                root = rotateRR(root);
-            } else {
-                root = rotateRL(root);
+    // function to insert a node into the AVL tree
+    AVLNode *insertIntoAVL(AVLNode *root, int data) {
+        if (root == nullptr) {
+            AVLNode *newNode = new AVLNode;
+            newNode->data = data;
+            newNode->height = 0;
+            newNode->left = newNode->right = nullptr;
+            root = newNode;
+        } else if (data < root->data) {
+            root->left = insertIntoAVL(root->left, data);
+            if (getHeight(root->left) - getHeight(root->right) == 2) {
+                if (data < root->left->data) {
+                    root = rotateLL(root);
+                } else {
+                    root = rotateLR(root);
+                }
+            }
+        } else if (data > root->data) {
+            root->right = insertIntoAVL(root->right, data);
+            if (getHeight(root->right) - getHeight(root->left) == 2) {
+                if (data > root->right->data) {
+                    root = rotateRR(root);
+                } else {
+                    root = rotateRL(root);
+                }
             }
         }
-    } else if (data > root->data) {
-        root->right = deleteFromAVL(root->right, data);
-        if (getHeight(root->left) - getHeight(root->right) == 2) {
-            if (getHeight(root->left->left) > getHeight(root->left->right)) {
-                root = rotateLL(root);
-            } else {
-                root = rotateLR(root);
+        root->height = std::max(getHeight(root->left), getHeight(root->right)) + 1;
+
+        return root;
+    }
+
+    // function to delete a node from the AVL tree
+    AVLNode *deleteFromAVL(AVLNode *root, int data) {
+        if (root == nullptr) {
+            return nullptr;
+        } else if (data < root->data) {
+            root->left = deleteFromAVL(root->left, data);
+            if (getHeight(root->right) - getHeight(root->left) == 2) {
+                if (getHeight(root->right->right) > getHeight(root->right->left)) {
+                    root = rotateRR(root);
+                } else {
+                    root = rotateRL(root);
+                }
             }
-        }
-    } else {
-        AVLNode *temp = nullptr;
-        if (root->left && root->right) {
-            temp = findMin(root->right);
-            root->data = temp->data;
-            root->right = deleteFromAVL(root->right, root->data);
+        } else if (data > root->data) {
+            root->right = deleteFromAVL(root->right, data);
             if (getHeight(root->left) - getHeight(root->right) == 2) {
                 if (getHeight(root->left->left) > getHeight(root->left->right)) {
                     root = rotateLL(root);
@@ -139,91 +171,98 @@ AVLNode *deleteFromAVL(AVLNode * &root, int data) {
                     root = rotateLR(root);
                 }
             }
-        } else if (root->left) {
-            temp = root;
-            root = root->left;
-            delete temp;
-        } else if (root->right) {
-            temp = root;
-            root = root->right;
-            delete temp;
         } else {
-            delete root;
+            AVLNode *temp = nullptr;
+            if (root->left && root->right) {
+                temp = findMin(root->right);
+                root->data = temp->data;
+                root->right = deleteFromAVL(root->right, root->data);
+                if (getHeight(root->left) - getHeight(root->right) == 2) {
+                    if (getHeight(root->left->left) > getHeight(root->left->right)) {
+                        root = rotateLL(root);
+                    } else {
+                        root = rotateLR(root);
+                    }
+                }
+            } else if (root->left) {
+                temp = root;
+                root = root->left;
+                delete temp;
+            } else if (root->right) {
+                temp = root;
+                root = root->right;
+                delete temp;
+            } else {
+                delete root;
 
-            return nullptr;
+                return nullptr;
+            }
+        }
+
+        return root;
+    }
+
+    // preorder traversal of the AVL tree
+    void preOrder(AVLNode *root, std::ostream &out) {
+        if (root != nullptr) {
+            out << (root)->data << " ";
+            preOrder((root)->left, out);
+            preOrder((root)->right, out);
         }
     }
 
-    return root;
-}
-
-// preorder traversal of the AVL tree
-void preOrder(AVLNode *root, std::ostream &out) {
-    if (root != nullptr) {
-        out << (root)->data << " ";
-        preOrder((root)->left, out);
-        preOrder((root)->right, out);
+    // inorder traversal of the AVL tree
+    void inOrder(AVLNode *root, std::ostream &out) {
+        if (root != nullptr) {
+            inOrder((root)->left, out);
+            out << (root)->data << " ";
+            inOrder((root)->right, out);
+        }
     }
-}
 
-// inorder traversal of the AVL tree
-void inOrder(AVLNode *root, std::ostream &out) {
-    if (root != nullptr) {
-        inOrder((root)->left, out);
-        out << (root)->data << " ";
-        inOrder((root)->right, out);
+    // postorder traversal of the AVL tree
+    void postOrder(AVLNode *root, std::ostream &out) {
+        if (root != nullptr) {
+            postOrder((root)->left, out);
+            postOrder((root)->right, out);
+            out << (root)->data << " ";
+        }
     }
-}
+};
 
-// postorder traversal of the AVL tree
-void postOrder(AVLNode *root, std::ostream &out) {
-    if (root != nullptr) {
-        postOrder((root)->left, out);
-        postOrder((root)->right, out);
-        out << (root)->data << " ";
-    }
-}
-
-// function to free allocated memory
-void deleteTree(AVLNode *root) {
-    if (root != nullptr) {
-        deleteTree((root)->left);
-        deleteTree((root)->right);
-        delete root;
-    }
-}
-
+// for test
 #include <iostream>
 using namespace std;
 int main() {
     int ch, data;
-    AVLNode *root = nullptr;
+    avl_tree avlt;
     while (1) {
         cout << "1. Insert 2. Delete 3. Preorder 4. Inorder 5. Postorder 6. Exit\n";
         cin >> ch;
         switch (ch) {
         case 1: cout << "Enter data\n";
             cin >> data;
-            root = insertIntoAVL(root, data);
+            avlt.insertIntoAVL(data);
             break;
         case 2: cout << "Enter data\n";
             cin >> data;
-            root = deleteFromAVL(root, data);
+            avlt.deleteFromAVL(data);
             break;
-        case 3: preOrder(root, cout);
+        case 3: avlt.preOrder(cout);
             cout << endl;
             break;
-        case 4: inOrder(root, cout);
+        case 4: avlt.inOrder(cout);
             cout << endl;
             break;
-        case 5: postOrder(root, cout);
+        case 5: avlt.postOrder(cout);
             cout << endl;
             break;
         }
         if (ch == 6)
             break;
     }
-    deleteTree(root);
 
     return 0;
 }
+
+// */
