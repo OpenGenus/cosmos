7aa476458ca10a3bedc7695f27f25608fe864e81
Leonardo Perez Pulido <leoperezpulido@gmail.com>
2017-10-21 16:30:57 -0400

    Java Coding Style Checked - Licence Header Eliminated

--- code/search/linear_search/LinearSearch.java
+++ code/search/linear_search/LinearSearch.java
@@ -1,22 +1,9 @@
 /*
  * Part of Cosmos by OpenGenus Foundation
  *
- * Copyrigh (C) 2017 Cosmos by OpenGenus Foundation
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
-
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
-
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+// A possible (imaginary) package.
 package org.opengenus.cosmos.search;
 
 import java.util.Scanner;
@@ -53,7 +40,6 @@ class LinearSearch {
      * @return  the index of the element, else -1.
      */
     static int linearSearch(int arr[], int key) {
-        
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == key)
                 return i;
@@ -90,8 +76,8 @@ class LinearSearch {
         return recursiveLinearSearch(arr, key, index + 1);
     }
     
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
+        
         // Object of scanner class to take input.
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array to be searched");
