66681d6d1b858a27f2b78686bcddca501a92d1c5
satishjasthi <satish27may@gmail.com>
2017-10-21 23:54:57 +0530

    Update insertion_sort.py as per python style.md Replace python2 code with python3 Change the format of code commenting as per style.md Change the format of function name as per style.md

--- code/sorting/insertion_sort/insertion_sort.py
+++ code/sorting/insertion_sort/insertion_sort.py
@@ -1,40 +1,21 @@
-#!/usr/bin/pyhton
+#Program for InsertionSort sorting
 
-"""
-Program for InsertionSort sorting
-"""
 import numpy as np
 import cProfile
 
-
-def InsertionSort(L):
-	"""
-	arguments:
-        ---------
-		L: list
-
-	return:
-	-------
-		L: list
-	
-	description:
-	------------
-		Function to return sorted list using Insertion sort.
-	"""
+#Function to return sorted list using Insertion sort.
+def insertion_sort(L):
 	length = len(L)
 	for i in range(length):
 		key = L[i]
 		j = i - 1
-
 		while j >= 0:
 			if key < L[j]:
 				j -= 1
 			else:
 				break
-
 		j += 1
 		L.insert(j,L.pop(i))
-
 	return L
 
 
@@ -42,8 +23,8 @@ if __name__ == "__main__":
 
 	#creating a random array of length 100
 	L = np.random.random_integers(100,size = (100,)).tolist()
-	print "Given array : \n",L
-	print "Sorted array : \n",InsertionSort(L)
+	print ("Given array : \n",L)
+	print ("Sorted array : \n",insertion_sort(L))
 	
 	
 
