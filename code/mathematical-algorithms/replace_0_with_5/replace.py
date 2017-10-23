9423fd21c626dd21224407d2c447b6f7baee93ab
FedericoPonzi <isaacisback92@gmail.com>
2017-10-23 11:48:20 +0200

    Symplifies previous solution

--- code/mathematical-algorithms/replace_0_with_5/replace.py
+++ code/mathematical-algorithms/replace_0_with_5/replace.py
@@ -1,8 +1,6 @@
 def convert0To5(num):
-	toRet = ""
-	for n in str(num):
-		toRet += "5" if n == "0" else n
-	return int(toRet)
+    toRet = str(num).replace("0", "5")
+    return int(toRet)
 
 def main():
 	num = 10120
