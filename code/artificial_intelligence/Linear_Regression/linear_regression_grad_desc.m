25f2b258622f43ae5e0970a03401037545a2fd72
Janak Raj Chadha <janakrajchadha@gmail.com>
2017-10-24 15:15:38 +0530

    Added linear regression solution using gradient descent in MATLAB

---
+++ code/artificial_intelligence/Linear_Regression/linear_regression_grad_desc.m
@@ -0,0 +1,14 @@
+function [theta] = gradient_descent(x, y, theta, alpha, num_iters)
+% Performs gradient descent to learn coefficient theta
+%   theta = gradient_descent(x, y, theta, alpha, num_iters) updates theta by
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+for iter = 1:num_iters
+
+theta = theta - alpha / m * ((x * theta - y)' * x)';
+
+end
+
+end
