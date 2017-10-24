6923a066199c9ac2c22e67926f5eac675f96997b
mudit8560 <mudit.1996garg@gmail.com>
2017-10-24 21:43:57 +0530

    no. of consecutive ones

--- code/dynamic_programming/count_consecutive_ones/no._of_consecutive_ones.cpp
+++ code/dynamic_programming/count_consecutive_ones/no._of_consecutive_ones.cpp
@@ -1,31 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
-void fast() {std::ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);}
-#define ll long long
-#define pb push_back
-#define mp1 make_pair
-#define ff first
-#define ss second
-#define vi vector<int>
-#define vii vector< pair<int,int> >
-#define map map<ll int,ll int>
-#define vll vector< pair<long long,long long> > 
-#define ve vector<ll int>
-#define clr(x) memset(x, 0, sizeof(x))
-#define loop(x,a,b) for(x=a;x<b;x++)
-#define rloop(x,a,b) for(x=a;x>=b;x--)
-#define itr for(std::map<ll int,ll int>::iterator it=mp.begin(); it!=mp.end(); ++it)
-const int mod=1e9+7;
-const int maxn=1e6+5;
 int main()
 {
-	ios_base::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
-	//freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
-	ll int a,b,c,d,i,j,sum=0,cnt=0;
+	long long int a,b,c,d,i,j,sum=0,cnt=0;
 	string s;
-	ve v;
+	vector<long long int>  v;
 	s="11000001111110101111010";
-	loop(i,0,s.size())
+	for(i=0;i<s.size();i++)
 	{
 		if(s[i]=='1')
 		{
@@ -33,7 +14,7 @@ int main()
 		}
 		else
 		{
-			v.pb(sum);
+			v.push_back(sum);
 			sum=0;
 		}
 	}
