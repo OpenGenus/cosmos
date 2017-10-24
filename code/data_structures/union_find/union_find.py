e4a0d806060eafa99bfa820f29a550ce8f726a4b
hir12111 <hir12111@gmail.com>
2017-10-24 22:05:15 +0100

    Allow counting elements in each group in union_find.py

--- code/data_structures/union_find/union_find.py
+++ code/data_structures/union_find/union_find.py
@@ -22,18 +22,21 @@ class UnionFind:
     def union(self, a, b):
         if a not in self.parent:
             self.parent[a] = a
-            self.rank[a] = 0
+            self.rank[a] = 1
         if b not in self.parent:
             self.parent[b] = b
-            self.rank[b] = 0
+            self.rank[b] = 1
         a_root = self.root(a)
         b_root = self.root(b)
         if self.rank[a_root] > self.rank[b_root]:
             self.parent[b_root] = a_root
-            self.rank[a_root] += 1
+            self.rank[a_root] += self.rank[b_root]
         else:
             self.parent[a_root] = b_root
-            self.rank[b_root] += 1
+            self.rank[b_root] += self.rank[a_root]
+
+    def count(self, a):
+        return self.rank[self.root(a)]
 
 
 def main():
@@ -45,11 +48,9 @@ def main():
     union_find.union(7, 8)
     union_find.union(7, 9)
     union_find.union(3, 9)
-    for i in range(1, 9):
-        for j in range(i + 1, 10):
-            state = "connected" if union_find.connected(i, j) else "disconnected"
-            print("{} and {} are {}".format(i, j, state))
-
+    for i in range(1, 10):
+        print("{} is in group {} with {} elements".format(
+            i, union_find.find(i), union_find.count(i)))
 
 if __name__ == "__main__":
     main()
