eccffba6f8b5782c9580e48f232260d9cb31ae91
Haritha Paul <harithapaul97@gmail.com>
2017-10-25 08:01:16 +0530

    An Iterative alternative for binary search
    Since iterative methods are more commonly used, seems useful.

--- code/search/binary_search/binary_search.c
+++ code/search/binary_search/binary_search.c
@@ -2,46 +2,40 @@
 /*
  * Part of Cosmos by OpenGenus Foundation
 */
-int recursiveBinarySearch(int arr[], int l, int r, int x)
-{
-   if (r >= l)
-   {
-        int mid = l + (r - l)/2;
- 
-        if (arr[mid] == x)  
-            return mid;
-         if (arr[mid] > x) 
-             return recursiveBinarySearch(arr, l, mid-1, x);
-       
-         return recursiveBinarySearch(arr, mid+1, r, x);
-   }
-   return -1;
-}
-
 int binarySearch(int arr[], int l, int r, int x)
 {
   while (l <= r)
   {
-    int mid = l + (r-l)/2;
-
-    if (arr[mid] == x) 
-        return mid;  
-
-    if (arr[mid] < x) 
-        l = mid + 1; 
-
+    int m = l + (r-l)/2;
+ 
+    // Check if x is present at mid
+    if (arr[m] == x) 
+        return m;  
+ 
+    // If x greater, ignore left half  
+    if (arr[m] < x) 
+        l = m + 1; 
+ 
+    // If x is smaller, ignore right half 
     else
-         r = mid - 1; 
+         r = m - 1; 
   }
+ 
+  // if we reach here, then element was not present
   return -1; 
 }
- 
-int main(void)
+
+void main()
 {
-   int arr[] = {1, 2, 3, 5};
-   int size = sizeof(arr)/ sizeof(arr[0]);
-   int find = 3;
-   printf("Position of %d is %d\n", find, recursiveBinarySearch(arr, 0, size-1, find));
-   printf("Position of %d is %d\n", find, binarySearch(arr, 0, size-1, find));
-   return 0;
+   int arr[] = {2, 3, 4, 10, 40};
+   int n = sizeof(arr)/ sizeof(arr[0]);
+   int x = 10;
+   int result = binarySearch(arr, 0, n-1, x);
+   if(result==-1)
+   
+      printf("Element not present in the array");
+   else
+      printf("Element is present at index %d", result);
+   
+   
 }
