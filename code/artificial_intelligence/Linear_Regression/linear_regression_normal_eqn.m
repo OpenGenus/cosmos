ddaac0a55254696eb3a34bbd15188831f1c200f7
Janak Raj Chadha <janakrajchadha@gmail.com>
2017-10-25 11:55:14 +0530

    Formula fix
    Corrected formula for normal equation

--- code/artificial_intelligence/Linear_Regression/linear_regression_normal_eqn.m
+++ code/artificial_intelligence/Linear_Regression/linear_regression_normal_eqn.m
@@ -2,6 +2,6 @@ function [theta] = normal_equation(x, y, theta)
 % Calculates linear regression coefficients using Normal Equation
 % An analytical solution to linear regression
 
-theta = inv(x' * x) * x' * y';
+theta = inv(x' * x) * x' * y;
 
 end
