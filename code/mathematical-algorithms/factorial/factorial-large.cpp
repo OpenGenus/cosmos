39a7173a180e612196862bafc8da8966a1c6eb88
Sudeep Biswas <sudeepb02@users.noreply.github.com>
2017-10-26 19:30:30 +0000

    FIx indentation

--- code/mathematical-algorithms/factorial/factorial-large.cpp
+++ code/mathematical-algorithms/factorial/factorial-large.cpp
@@ -4,32 +4,32 @@ using namespace std;
 
 int main()
 {
-	  vector<int> fact(1000);		//To store each digit of factorial
+	vector<int> fact(1000);		//To store each digit of factorial
   
-		int n;
-		cin>>n;
-    
-		int carry = 0;
-		int counter = 1;
+	int n;
+	cin>>n;
 
-		fact[0] = 1;
+	int carry = 0;
+	int counter = 1;
 
-		for(int i=1; i<=n; i++)
+	fact[0] = 1;
+
+	for(int i=1; i<=n; i++)
+	{
+		for(int j=0; j<counter; j++)
 		{
-			for(int j=0; j<counter; j++)
-			{
-				int prod = fact[j]*i + carry;
-				fact[j] = prod % 10;
-				carry = prod/10;
-			}
-
-			while(carry > 0)
-			{
-				fact[counter] = carry % 10;
-				carry /= 10;
-				counter++;
-			}
+			int prod = fact[j]*i + carry;
+			fact[j] = prod % 10;
+			carry = prod/10;
 		}
+
+		while(carry > 0)
+		{
+			fact[counter] = carry % 10;
+			carry /= 10;
+			counter++;
+		}
+	}
 
     for(int i=counter-1; i>=0; i--)
     {
