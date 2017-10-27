10ef25e3eb69cda79574c1c4c97529b1294e59c1
Shivansh Rai <shivansh@freebsd.org>
2017-10-27 23:24:02 +0530

    Fix memoization in 'fibonacci_memorized.swift'
    Fixes #2579

--- code/mathematical-algorithms/fibonacci_number/fibonacci_memorized.swift
+++ code/mathematical-algorithms/fibonacci_number/fibonacci_memorized.swift
@@ -1,11 +1,12 @@
+var cache = [0: 0, 1: 1]
+
 func fibonnaci(_ n: Int) -> Int {
     guard n > 1 else { return n }
     return fibonnaci(n-2) + fibonnaci(n-1)
 }
 
 func fibonnaciMemoized(_ n: Int) -> Int {
-    var cache = [0: 0, 1: 1]
-    if let number = cache[n] { 
+    if let number = cache[n] {
         return number
     }
     cache[n] = fibonnaciMemoized(n-2) + fibonnaciMemoized(n-1)
