ef26f63c9d66ca3e98c578317b9ba79b7bfb76b7
Shivansh Rai <shivansh@freebsd.org>
2017-10-27 23:12:38 +0530

    Fix memoization in 'fibonacci.py'
    Fixes #2578

--- code/mathematical-algorithms/fibonacci_number/fibonacci.py
+++ code/mathematical-algorithms/fibonacci_number/fibonacci.py
@@ -1,12 +1,13 @@
 import sys
 
+cache = {0: 0, 1: 1}
+
 def fibonacci(n):
   if n < 2:
     return n
   return fibonacci(n-2) + fibonacci(n-1)
 
 def fibonacci_memoized(n):
-  cache = {0: 0, 1: 1}
   if n in cache:
     return cache[n]
   cache[n] = fibonacci_memoized(n-2) + fibonacci_memoized(n-1)
