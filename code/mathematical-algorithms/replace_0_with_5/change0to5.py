335d39c7366e93b56ccfbaabd15e86240359f1b4
Arkady Telegin <akyshnik@protonmail.ch>
2017-10-27 10:09:46 +0400

    Add python3 compatibility, sys.version_info
    Add python3 compatibility and code execution based on sys.version_info output.

--- code/mathematical-algorithms/replace_0_with_5/change0to5.py
+++ code/mathematical-algorithms/replace_0_with_5/change0to5.py
@@ -1,26 +1,38 @@
 # Part of Cosmos by OpenGenus Foundation
+from sys import version_info
 
-# python 2 code:
-user_input = raw_input("Enter the number: ")
-try:
-  if isinstance(int(user_input), int):
-    int_to_str = str(user_input)
-    modified = ''
-    for i in range(len(int_to_str)):
-      if int_to_str[i] == '0':
-        modified += '5'
-      else:
-        modified += int_to_str[i]
+if version_info[0] < 3:   # python 2 code
+    def replace_0_5(user_input):
+        try:
+            if isinstance(int(user_input), int):
+                int_to_str = str(user_input)
+                modified = ''
+                for i in int_to_str:
+                    if i == '0':
+                        modified += '5'
+                    else:
+                        modified += i
+            return modified
+        except Exception:
+            print("You did not specify an integer")
 
-  print("Modified number: " + modified)
+    user_input = raw_input("Enter the number: ")
+    new_str = replace_0_5(user_input)
+    print("Modified number: " + new_str)
 
-except Exception:
-  print("You did not specify an integer")
-
-  
-# python 3 code:
-user_input = int(input('Enter the number: '))
-string_input = str(user_input)
-modified = string_input.replace('0', '5')
-print("Modified number: " + modified)
-  
+else:   # python 3 code
+    def replace_0_5_iterative(user_input):
+        modified = ''
+        for i in user_input:
+            if i == '0':
+                modified += '5'
+            else:
+                modified += i
+        return modified
+              
+    def replace_0_5_pythonic(user_input):
+        return user_input.replace('0', '5')
+ 
+    user_input = input('Enter the number: ')
+    new_str = replace_0_5_pythonic(user_input)
+    print('Modified number:', new_str)
