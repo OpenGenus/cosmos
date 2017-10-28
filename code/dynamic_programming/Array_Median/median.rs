8dc31a2ad7ab207a19fd4c61205f7bff77bb7e83
Elías Josué Puma Chávez <elpumitaelias@gmail.com>
2017-10-28 17:45:24 -0500

    Improved version of median in Rust (Not sorted input)

--- code/dynamic_programming/Array_Median/median.rs
+++ code/dynamic_programming/Array_Median/median.rs
@@ -10,7 +10,13 @@ fn median(arr: &[i32]) -> f64 {
 }
 
 fn main() {
-    let arr = vec![1, 3, 3, 4, 5, 6, 8, 9];
-    let median = median(&arr); //Assuming input is sorted array
-    println!("{}", median);
+    let arr1 = vec![1, 3, 3, 4, 5, 6, 8, 9];
+    let median1 = median(&arr1); //Assuming input is sorted array
+    println!("{}", median1);
+
+    let mut arr2 = vec![5, 6, 1, 3, 8, 9, 3, 4];
+    arr2.sort_by(|a, b| a.cmp(b));
+    let median2 = median(&arr2); //Assuming input is not sorted array
+
+    assert_eq!(median1, median2);
 }
