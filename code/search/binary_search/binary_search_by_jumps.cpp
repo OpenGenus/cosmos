167a3b9499f15679125611006e769a2a470cfc8f
rajat2797 <rajatsinghal1997@gmail.com>
2017-10-29 12:14:45 +0530

    added comments

--- code/search/binary_search/binary_search_by_jumps.cpp
+++ code/search/binary_search/binary_search_by_jumps.cpp
@@ -7,6 +7,11 @@ int main()
 	int k = 0, n, x;
 	n = sizeof(array)/sizeof(array[0]);
 	cin>>x;
+
+	/* The search goes through the array from left to right, and the initial jump
+	length is n /2. At each step, the jump length will be halved: first n /4, then n /8,
+	n /16, etc., until finally the length is 1. */
+
 	for (int b = n/2; b >= 1; b /= 2) {
 		while (k+b < n && array[k+b] <= x) k += b;
 	}
