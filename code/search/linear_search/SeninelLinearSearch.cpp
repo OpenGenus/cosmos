4dc0970d741b4e708e674108e885e0f27e0cb0a5
Visakh S <visakh1998@gmail.com>
2017-10-30 02:04:36 +0530

    Create SeninelLinearSearch.cpp

---
+++ code/search/linear_search/SeninelLinearSearch.cpp
@@ -0,0 +1,39 @@
+/*
+ * Part of Cosmos by OpenGenus Foundation
+*/
+
+/*
+ * Input: The number of elements in an array, The element to be searched, An integer array.
+ * Output: if found returns "found" else "not found", using the sentinel linear search algorithm.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+void sentinal_linear_search(std::vector<int> v,int n,int x)
+{
+	int last,i=0;
+	last=v[n-1];
+	v[n-1]=x;
+	while(v[i]!=x)
+	{
+		i++;
+	}
+	if(i<n-1||last==x)
+		cout<<"found";
+	else
+		cout<<"not found";
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);cin.tie(NULL);
+	int n,x,input;
+	std::vector<int> v;
+	cin>>n>>x;
+	for (int i = 0; i < n; ++i)
+	{
+		cin>>input;
+		v.push_back(input);
+	}
+	sentinal_linear_search(v,n,x);
+	return 0;
+}
