32cab4ecb71be77b257954cdf9384063d0369689
Frank Kair <frankkair@gmail.com>
2017-10-30 18:20:20 -0200

    Uses rubocop on cryptography/cellular_automaton/dynamic_programming/networking

--- code/cryptography/rot13_cipher/rot13.rb
+++ code/cryptography/rot13_cipher/rot13.rb
@@ -1,5 +1,5 @@
 # Part of Cosmos by OpenGenus Foundation
-puts "Enter text you want to encrypt: "
+puts 'Enter text you want to encrypt: '
 text = gets.chomp
 
 puts text.tr('A-Za-z', 'N-ZA-Mn-za-m')
