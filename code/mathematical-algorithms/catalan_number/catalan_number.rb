ae823e463d31c519a6cfeb9725c7e7d8fae73be8
Frank Kair <frankkair@gmail.com>
2017-10-30 18:11:03 -0200

    Uses rubocop on mathematical algorithms

--- code/mathematical-algorithms/catalan_number/catalan_number.rb
+++ code/mathematical-algorithms/catalan_number/catalan_number.rb
@@ -1,23 +1,20 @@
 def catalan(num)
-    if num<=1
-      return 1;
-    end
-    ans=0;
-    i=0;
-    
-    while i<num
-      first = catalan i;
-      second = catalan num-i-1;
-      ans = ans + (first*second);
-      i += 1;
-    end
-    return ans;
+  return 1 if num <= 1
+  ans = 0
+  i = 0
+
+  while i < num
+    first = catalan i
+    second = catalan num - i - 1
+    ans += (first * second)
+    i += 1
+  end
+  ans
 end
 
-
-Integer x = 1;
-while x<=10
-  res = catalan x;
-  puts res;
-  x = x + 1;
+Integer x = 1
+while x <= 10
+  res = catalan x
+  puts res
+  x += 1
 end
