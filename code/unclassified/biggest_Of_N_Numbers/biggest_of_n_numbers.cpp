07a31ea4216e60abfddaf37cb7a89afc5ae7247f
tbhaxor <tbhaxor@gmail.com>
2017-11-12 20:52:13 +0530

    biggest_of_n_numbers.cpp code optimised

--- code/unclassified/biggest_Of_N_Numbers/biggest_of_n_numbers.cpp
+++ code/unclassified/biggest_Of_N_Numbers/biggest_of_n_numbers.cpp
@@ -5,21 +5,15 @@
 #include <algorithm>
 using namespace std;
 
-int biggest_of_n(vector<int> v)
-{
-  // sorting vector
-  sort(v.begin(), v.end());
-
-  // returning last element of vector
-  return v[v.size() - 1];
-}
-
 int main()
 {
   // initialising amd constructing vector array
   vector<int> vec = {10,5,20,15,-1};
 
-  // ivoking biggest_of_n function
-  cout<<"biggest number is : "<<biggest_of_n(vec);
+  // sorting vector
+  sort(vec.begin(), vec.end());
+
+  // last element of vector
+  cout<<"biggest number is : "<<vec[vec.size() - 1];
   return 0;
 }
