6bd37001871afa970754eb9153177aba7222eca8
Ernest <iattempt.net@gmail.com>
2017-11-12 14:24:19 +0800

    encapsulate

--- code/data_structures/tree/tree_node/tree_node.cpp
+++ code/data_structures/tree/tree_node/tree_node.cpp
@@ -5,38 +5,97 @@
 
 // for normal binary tree
 template<typename _Type>
-struct TreeNode
+class TreeNode
 {
+protected:
     using SPNodeType = std::shared_ptr<TreeNode>;
     using ValueType = _Type;
 
+public:
+    TreeNode(ValueType v, SPNodeType l = nullptr, SPNodeType r = nullptr)
+        :value_(v), left_(l), right_(r) {}
+
+    ValueType value() {
+        return value_;
+    }
+
+    void value(ValueType v) {
+        value_ = v;
+    }
+
+    SPNodeType left() {
+        return left_;
+    }
+
+    void left(SPNodeType l) {
+        left_ = l;
+    }
+
+    SPNodeType right() {
+        return right_;
+    }
+
+    void right(SPNodeType r) {
+        right_ = r;
+    }
+
+private:
     ValueType value_;
     SPNodeType left_;
     SPNodeType right_;
-    TreeNode(ValueType v, SPNodeType l = nullptr, SPNodeType r = nullptr)
-        :value_(v), left_(l), right_(r) {}
 };
 
 // for derivative binary tree (e.g., avl tree, splay tree)
 template<typename _Type, class _Derivative>
-struct __BaseTreeNode
+class __BaseTreeNode
 {
+protected:
     using SPNodeType = std::shared_ptr<_Derivative>;
     using ValueType = _Type;
 
+public:
+    __BaseTreeNode(ValueType v, SPNodeType l = nullptr, SPNodeType r = nullptr)
+        :value_(v), left_(l), right_(r) {}
+
+    ValueType value() {
+        return value_;
+    }
+
+    void value(ValueType v) {
+        value_ = v;
+    }
+
+    SPNodeType left() {
+        return left_;
+    }
+
+    void left(SPNodeType l) {
+        left_ = l;
+    }
+
+    SPNodeType right() {
+        return right_;
+    }
+
+    void right(SPNodeType r) {
+        right_ = r;
+    }
+
+private:
     ValueType value_;
     SPNodeType left_;
     SPNodeType right_;
-    __BaseTreeNode(ValueType v, SPNodeType l = nullptr, SPNodeType r = nullptr)
-        :value_(v), left_(l), right_(r) {}
 };
 
 template<typename _Type>
-struct DerivativeTreeNode :__BaseTreeNode<_Type, DerivativeTreeNode<_Type>>
+class DerivativeTreeNode :public __BaseTreeNode<_Type, DerivativeTreeNode<_Type>>
 {
-    using SPNodeType = std::shared_ptr<DerivativeTreeNode>;
-    using ValueType = _Type;
+private:
+    using BaseNode = __BaseTreeNode<_Type, DerivativeTreeNode<_Type>>;
+    using SPNodeType = typename BaseNode::SPNodeType;
+    using ValueType = typename BaseNode::ValueType;
 
+public:
     DerivativeTreeNode(_Type v, SPNodeType l = nullptr, SPNodeType r = nullptr)
         :__BaseTreeNode<_Type, DerivativeTreeNode<_Type>>(v, l, r) {}
 };
@@ -48,37 +107,96 @@ struct DerivativeTreeNode :__BaseTreeNode<_Type, DerivativeTreeNode<_Type>>
 #define TREE_NODE_POLICY
 
 template<typename _Type>
-struct TreeNode
+class TreeNode
 {
+protected:
     using SPNodeType = std::shared_ptr<TreeNode>;
     using ValueType = _Type;
 
+public:
+    TreeNode(ValueType v, SPNodeType l = nullptr, SPNodeType r = nullptr)
+        :value_(v), left_(l), right_(r) {}
+
+    ValueType value() {
+        return value_;
+    }
+
+    void value(ValueType v) {
+        value_ = v;
+    }
+
+    SPNodeType left() {
+        return left_;
+    }
+
+    void left(SPNodeType l) {
+        left_ = l;
+    }
+
+    SPNodeType right() {
+        return right_;
+    }
+
+    void right(SPNodeType r) {
+        right_ = r;
+    }
+
+private:
     ValueType value_;
     SPNodeType left_;
     SPNodeType right_;
-    TreeNode(ValueType v, SPNodeType l = nullptr, SPNodeType r = nullptr)
-        :value_(v), left_(l), right_(r) {}
 };
 
 template<typename _Type, class _Derivative>
-struct __BaseTreeNode
+class __BaseTreeNode
 {
+protected:
     using SPNodeType = std::shared_ptr<_Derivative>;
     using ValueType = _Type;
 
+public:
+    __BaseTreeNode(ValueType v, SPNodeType l = nullptr, SPNodeType r = nullptr)
+        :value_(v), left_(l), right_(r) {}
+
+    ValueType value() {
+        return value_;
+    }
+
+    void value(ValueType v) {
+        value_ = v;
+    }
+
+    SPNodeType left() {
+        return left_;
+    }
+
+    void left(SPNodeType l) {
+        left_ = l;
+    }
+
+    SPNodeType right() {
+        return right_;
+    }
+
+    void right(SPNodeType r) {
+        right_ = r;
+    }
+
+private:
     ValueType value_;
     SPNodeType left_;
     SPNodeType right_;
-    __BaseTreeNode(ValueType v, SPNodeType l = nullptr, SPNodeType r = nullptr)
-        :value_(v), left_(l), right_(r) {}
 };
 
 template<typename _Type>
-struct DerivativeTreeNode :__BaseTreeNode<_Type, DerivativeTreeNode<_Type>>
+class DerivativeTreeNode :public __BaseTreeNode<_Type, DerivativeTreeNode<_Type>>
 {
-    using SPNodeType = std::shared_ptr<DerivativeTreeNode>;
-    using ValueType = _Type;
+private:
+    using BaseNode = __BaseTreeNode<_Type, DerivativeTreeNode<_Type>>;
+    using SPNodeType = typename BaseNode::SPNodeType;
+    using ValueType = typename BaseNode::ValueType;
 
+public:
     DerivativeTreeNode(_Type v, SPNodeType l = nullptr, SPNodeType r = nullptr)
         :__BaseTreeNode<_Type, DerivativeTreeNode<_Type>>(v, l, r) {}
 };
