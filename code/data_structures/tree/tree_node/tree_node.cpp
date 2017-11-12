4cdb0b919165df379f3cbb916c7e751399ca2900
Ernest <iattempt.net@gmail.com>
2017-11-12 13:54:59 +0800

    rename

--- code/data_structures/tree/tree_node/tree_node.cpp
+++ code/data_structures/tree/tree_node/tree_node.cpp
@@ -4,35 +4,41 @@
  tree node synopsis
 
 // for normal binary tree
-template<typename _Tp>
+template<typename _Type>
 struct TreeNode
 {
-    _Tp val;
-    std::shared_ptr<TreeNode> left;
-    std::shared_ptr<TreeNode> right;
-    TreeNode(_Tp v,
-             std::shared_ptr<TreeNode> l = nullptr,
-             std::shared_ptr<TreeNode> r = nullptr);
+    using SPNodeType = std::shared_ptr<TreeNode>;
+    using ValueType = _Type;
+
+    ValueType value_;
+    SPNodeType left_;
+    SPNodeType right_;
+    TreeNode(ValueType v, SPNodeType l = nullptr, SPNodeType r = nullptr)
+        :value_(v), left_(l), right_(r) {}
 };
 
 // for derivative binary tree (e.g., avl tree, splay tree)
-template<typename _Tp, class _Derivative>
+template<typename _Type, class _Derivative>
 struct __BaseTreeNode
 {
-    _Tp val;
-    std::shared_ptr<_Derivative> left;
-    std::shared_ptr<_Derivative> right;
-    __BaseTreeNode(_Tp v,
-                   std::shared_ptr<_Derivative> l = nullptr,
-                   std::shared_ptr<_Derivative> r = nullptr);
+    using SPNodeType = std::shared_ptr<_Derivative>;
+    using ValueType = _Type;
+
+    ValueType value_;
+    SPNodeType left_;
+    SPNodeType right_;
+    __BaseTreeNode(ValueType v, SPNodeType l = nullptr, SPNodeType r = nullptr)
+        :value_(v), left_(l), right_(r) {}
 };
 
-template<typename _Tp>
-struct DerivativeTreeNode :__BaseTreeNode<_Tp, DerivativeTreeNode<_Tp> >
+template<typename _Type>
+struct DerivativeTreeNode :__BaseTreeNode<_Type, DerivativeTreeNode<_Type>>
 {
-    DerivativeTreeNode(_Tp v,
-                       std::shared_ptr<DerivativeTreeNode> l = nullptr,
-                       std::shared_ptr<DerivativeTreeNode> r = nullptr);
+    using SPNodeType = std::shared_ptr<DerivativeTreeNode>;
+    using ValueType = _Type;
+
+    DerivativeTreeNode(_Type v, SPNodeType l = nullptr, SPNodeType r = nullptr)
+        :__BaseTreeNode<_Type, DerivativeTreeNode<_Type>>(v, l, r) {}
 };
  */
 
@@ -41,37 +47,40 @@ struct DerivativeTreeNode :__BaseTreeNode<_Tp, DerivativeTreeNode<_Tp> >
 #ifndef TREE_NODE_POLICY
 #define TREE_NODE_POLICY
 
-template<typename _Tp>
+template<typename _Type>
 struct TreeNode
 {
-    _Tp val;
-    std::shared_ptr<TreeNode> left;
-    std::shared_ptr<TreeNode> right;
-    TreeNode(_Tp v,
-             std::shared_ptr<TreeNode> l = nullptr,
-             std::shared_ptr<TreeNode> r = nullptr)
-        :val(v), left(l), right(r) {}
+    using SPNodeType = std::shared_ptr<TreeNode>;
+    using ValueType = _Type;
+
+    ValueType value_;
+    SPNodeType left_;
+    SPNodeType right_;
+    TreeNode(ValueType v, SPNodeType l = nullptr, SPNodeType r = nullptr)
+        :value_(v), left_(l), right_(r) {}
 };
 
-template<typename _Tp, class _Derivative>
+template<typename _Type, class _Derivative>
 struct __BaseTreeNode
 {
-    _Tp val;
-    std::shared_ptr<_Derivative> left;
-    std::shared_ptr<_Derivative> right;
-    __BaseTreeNode(_Tp v,
-                   std::shared_ptr<_Derivative> l = nullptr,
-                   std::shared_ptr<_Derivative> r = nullptr)
-        :val(v), left(l), right(r) {}
+    using SPNodeType = std::shared_ptr<_Derivative>;
+    using ValueType = _Type;
+
+    ValueType value_;
+    SPNodeType left_;
+    SPNodeType right_;
+    __BaseTreeNode(ValueType v, SPNodeType l = nullptr, SPNodeType r = nullptr)
+        :value_(v), left_(l), right_(r) {}
 };
 
-template<typename _Tp>
-struct DerivativeTreeNode :__BaseTreeNode<_Tp, DerivativeTreeNode<_Tp>>
+template<typename _Type>
+struct DerivativeTreeNode :__BaseTreeNode<_Type, DerivativeTreeNode<_Type>>
 {
-    DerivativeTreeNode(_Tp v,
-                       std::shared_ptr<DerivativeTreeNode> l = nullptr,
-                       std::shared_ptr<DerivativeTreeNode> r = nullptr)
-        :__BaseTreeNode<_Tp, DerivativeTreeNode<_Tp>>(v, l, r) {}
+    using SPNodeType = std::shared_ptr<DerivativeTreeNode>;
+    using ValueType = _Type;
+
+    DerivativeTreeNode(_Type v, SPNodeType l = nullptr, SPNodeType r = nullptr)
+        :__BaseTreeNode<_Type, DerivativeTreeNode<_Type>>(v, l, r) {}
 };
 
 #endif // TREE_NODE_POLICY
