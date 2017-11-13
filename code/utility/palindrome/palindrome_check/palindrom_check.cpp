d98850186764f520c86c46bba81201cb7a6081c3
Ernest <iattempt.net@gmail.com>
2017-11-13 13:33:16 +0800

    coding style

--- code/utility/palindrome/palindrome_check/palindrom_check.cpp
+++ code/utility/palindrome/palindrome_check/palindrom_check.cpp
@@ -2,70 +2,92 @@
 #include <type_traits>
 #include <functional>
 
-namespace palindrom_check {
-    template<typename _InputIter,
-             typename _ValueNotEqualTo = std::not_equal_to<typename _InputIter::value_type>,
-             typename _IterLess = std::less<_InputIter>>
-    bool isPalindromeRecursive(_InputIter begin, _InputIter end)
+namespace palindrom_check{
+template<typename _InputIter,
+         typename _ValueNotEqualTo = std::not_equal_to<typename _InputIter::value_type>,
+         typename _IterLess = std::less<_InputIter>>
+bool
+isPalindromeRecursive(_InputIter begin, _InputIter end)
+{
+    --end;
+    if (_IterLess()(begin, end))
     {
-        --end;
-        if (_IterLess()(begin, end)) {
-            if (_ValueNotEqualTo()(*begin, *end)) return false;
-            return isPalindromeRecursive<_InputIter,
-                                         _ValueNotEqualTo,
-                                         _IterLess>(++begin, end);
+        if (_ValueNotEqualTo()(*begin, *end))
+        {
+            return false;
         }
-        return true;
+
+        return isPalindromeRecursive<_InputIter, _ValueNotEqualTo, _IterLess>(++begin, end);
     }
-    
-    template<typename _POD>
-    bool isPalindromeRecursive(_POD* begin, _POD* end)
+
+    return true;
+}
+
+template<typename _POD>
+bool
+isPalindromeRecursive(_POD *begin, _POD *end)
+{
+    --end;
+    if (begin < end)
     {
-        --end;
-        if (begin < end) {
-            if (*begin != *end) return false;
-            return isPalindromeRecursive<_POD>(++begin, end);
+        if (*begin != *end)
+        {
+            return false;
         }
-        return true;
+
+        return isPalindromeRecursive<_POD>(++begin, end);
     }
-    
-    template<typename _InputIter,
-    typename _ValueNotEqualTo = std::not_equal_to<typename _InputIter::value_type>,
-    typename _IterLess = std::less<_InputIter>>
-    bool isPalindromeIterative(_InputIter begin, _InputIter end)
-    {
-        --end;
-        while (_IterLess()(begin, end)) {
-            if (_ValueNotEqualTo()(*begin++, *end--))
-                return false;
+
+    return true;
+}
+
+template<typename _InputIter,
+         typename _ValueNotEqualTo = std::not_equal_to<typename _InputIter::value_type>,
+         typename _IterLess = std::less<_InputIter>>
+bool
+isPalindromeIterative(_InputIter begin, _InputIter end)
+{
+    --end;
+    while (_IterLess()(begin, end))
+        if (_ValueNotEqualTo()(*begin++, *end--))
+        {
+            return false;
         }
-        return true;
-    }
-    
-    template<typename _POD>
-    bool isPalindromeIterative(_POD* begin, _POD* end)
-    {
-        --end;
-        while (begin < end) {
-            if (*begin++ != *end--)
-                return false;
+
+    return true;
+}
+
+template<typename _POD>
+bool
+isPalindromeIterative(_POD *begin, _POD *end)
+{
+    --end;
+    while (begin < end)
+        if (*begin++ != *end--)
+        {
+            return false;
         }
-        return true;
-    }
-    
-    template<typename _InputIter,
-    typename _ValueNotEqualTo = std::not_equal_to<typename _InputIter::value_type>,
-    typename _IterLess = std::less<_InputIter>>
-    inline bool isPalindrome(_InputIter begin, _InputIter end)
-    {
-        // default is iterative
-        return isPalindromeIterative<_InputIter, _ValueNotEqualTo, _IterLess>(begin, end);
-    }
-    
-    template<typename _POD>
-    inline bool isPalindrome(_POD* begin, _POD* end)
-    {
-        // default is iterative
-        return isPalindromeIterative<_POD>(begin, end);
-    }
+
+    return true;
+}
+
+template<typename _InputIter,
+         typename _ValueNotEqualTo = std::not_equal_to<typename _InputIter::value_type>,
+         typename _IterLess = std::less<_InputIter>>
+inline bool
+isPalindrome(_InputIter begin, _InputIter end)
+{
+
+    // default is iterative
+    return isPalindromeIterative<_InputIter, _ValueNotEqualTo, _IterLess>(begin, end);
+}
+
+template<typename _POD>
+inline bool
+isPalindrome(_POD *begin, _POD *end)
+{
+
+    // default is iterative
+    return isPalindromeIterative<_POD>(begin, end);
+}
 }
--- code/utility/palindrome/palindrome_check/palindrom_check_test.cpp
+++ code/utility/palindrome/palindrome_check/palindrom_check_test.cpp
@@ -8,7 +8,8 @@ using namespace std;
 using namespace palindrom_check;
 
 // for test
-struct MapValueEqual {
+struct MapValueEqual
+{
     bool operator()(std::pair<int, int> const &a, std::pair<int, int> const &b) const
     {
         return a.second != b.second;
@@ -16,167 +17,186 @@ struct MapValueEqual {
 };
 
 template<typename _Type>
-struct GreaterEqual {
-    bool operator()(typename _Type::iterator a,
-                    typename _Type::iterator b) const
+struct GreaterEqual
+{
+    bool operator()(typename _Type::iterator a, typename _Type::iterator b) const
     {
         return (*a).first < (*b).first;
     }
 };
 
-void testTrueRecursive() {
+void
+testTrueRecursive()
+{
     string s = "1221";
     assert(isPalindromeRecursive(s.begin(), s.end()));
     string s2 = "12321";
     assert(isPalindromeRecursive(s2.begin(), s2.end()));
-    
+
     vector<int> v = {1, 2, 2, 1};
     assert(isPalindromeRecursive(v.begin(), v.end()));
     vector<int> v2 = {1, 2, 3, 2, 1};
     assert(isPalindromeRecursive(v2.begin(), v2.end()));
-    
-    int* iv = new int(4);
+
+    int *iv = new int(4);
     *iv = 1;
-    *(iv+1) = 2;
-    *(iv+2) = 2;
-    *(iv+3) = 1;
-    assert(isPalindromeRecursive(iv, iv+4));
+    *(iv + 1) = 2;
+    *(iv + 2) = 2;
+    *(iv + 3) = 1;
+    assert(isPalindromeRecursive(iv, iv + 4));
     int *iv2 = new int(5);
     *iv2 = 1;
-    *(iv2+1) = 2;
-    *(iv2+2) = 3;
-    *(iv2+3) = 2;
-    *(iv2+4) = 1;
-    assert(isPalindromeRecursive(iv2, iv2+5));
-    
+    *(iv2 + 1) = 2;
+    *(iv2 + 2) = 3;
+    *(iv2 + 3) = 2;
+    *(iv2 + 4) = 1;
+    assert(isPalindromeRecursive(iv2, iv2 + 5));
+
     int iva[4] = {1, 2, 2, 1};
-    assert(isPalindromeRecursive(iva, iva+4));
+    assert(isPalindromeRecursive(iva, iva + 4));
     int iva2[5] = {1, 2, 3, 2, 1};
-    assert(isPalindromeRecursive(iva2, iva2+5));
-    
-    
+    assert(isPalindromeRecursive(iva2, iva2 + 5));
+
     map<int, int> se;
     se[1] = 1; se[2] = 2; se[4] = 2; se[5] = 1;
-    assert((isPalindromeRecursive<map<int, int>::iterator, MapValueEqual, GreaterEqual<map<int, int>>>(se.begin(), se.end())));
+    assert((isPalindromeRecursive<map<int, int>::iterator,
+                                  MapValueEqual,
+                                  GreaterEqual<map<int, int>>>(se.begin(), se.end())));
     se[3] = 3;
-    assert((isPalindromeRecursive<map<int, int>::iterator, MapValueEqual, GreaterEqual<map<int, int>>>(se.begin(), se.end())));
+    assert((isPalindromeRecursive<map<int, int>::iterator,
+                                  MapValueEqual,
+                                  GreaterEqual<map<int, int>>>(se.begin(),
+    se.end())));
 }
 
-void testFalseRecursive() {
+void
+testFalseRecursive()
+{
     string s = "1231";
     assert(!isPalindromeRecursive(s.begin(), s.end()));
     string s2 = "12331";
     assert(!isPalindromeRecursive(s2.begin(), s2.end()));
-    
+
     vector<int> v = {1, 2, 3, 1};
     assert(!isPalindromeRecursive(v.begin(), v.end()));
     vector<int> v2 = {1, 2, 3, 3, 1};
     assert(!isPalindromeRecursive(v2.begin(), v2.end()));
-    
-    int* iv = new int(4);
+
+    int *iv = new int(4);
     *iv = 1;
-    *(iv+1) = 2;
-    *(iv+2) = 3;
-    *(iv+3) = 1;
-    assert(!isPalindromeRecursive(iv, iv+4));
+    *(iv + 1) = 2;
+    *(iv + 2) = 3;
+    *(iv + 3) = 1;
+    assert(!isPalindromeRecursive(iv, iv + 4));
     int *iv2 = new int(5);
     *iv2 = 1;
-    *(iv2+1) = 2;
-    *(iv2+2) = 3;
-    *(iv2+3) = 3;
-    *(iv2+4) = 1;
-    assert(!isPalindromeRecursive(iv2, iv2+5));
-    
+    *(iv2 + 1) = 2;
+    *(iv2 + 2) = 3;
+    *(iv2 + 3) = 3;
+    *(iv2 + 4) = 1;
+    assert(!isPalindromeRecursive(iv2, iv2 + 5));
+
     int iva[4] = {1, 2, 3, 1};
-    assert(!isPalindromeRecursive(iva, iva+4));
+    assert(!isPalindromeRecursive(iva, iva + 4));
     int iva2[5] = {1, 2, 3, 3, 1};
-    assert(!isPalindromeRecursive(iva2, iva2+5));
-    
-    
+    assert(!isPalindromeRecursive(iva2, iva2 + 5));
+
     map<int, int> se;
     se[1] = 1; se[2] = 2; se[4] = 3; se[5] = 1;
-    assert(!(isPalindromeRecursive<map<int, int>::iterator, MapValueEqual, GreaterEqual<map<int, int>>>(se.begin(), se.end())));
+    assert(!(isPalindromeRecursive<map<int, int>::iterator,
+                                   MapValueEqual,
+                                   GreaterEqual<map<int, int>>>(se.begin(), se.end())));
     se[3] = 3;
-    assert(!(isPalindromeRecursive<map<int, int>::iterator, MapValueEqual, GreaterEqual<map<int, int>>>(se.begin(), se.end())));
+    assert(!(isPalindromeRecursive<map<int, int>::iterator,
+                                   MapValueEqual,
+                                   GreaterEqual<map<int, int>>>(se.begin(), se.end())));
 }
 
-void testTrueIterative() {
+void
+testTrueIterative()
+{
     string s = "1221";
     assert(isPalindrome(s.begin(), s.end()));
     string s2 = "12321";
     assert(isPalindrome(s2.begin(), s2.end()));
-    
+
     vector<int> v = {1, 2, 2, 1};
     assert(isPalindrome(v.begin(), v.end()));
     vector<int> v2 = {1, 2, 3, 2, 1};
     assert(isPalindrome(v2.begin(), v2.end()));
-    
-    int* iv = new int(4);
+
+    int *iv = new int(4);
     *iv = 1;
-    *(iv+1) = 2;
-    *(iv+2) = 2;
-    *(iv+3) = 1;
-    assert(isPalindrome(iv, iv+4));
+    *(iv + 1) = 2;
+    *(iv + 2) = 2;
+    *(iv + 3) = 1;
+    assert(isPalindrome(iv, iv + 4));
     int *iv2 = new int(5);
     *iv2 = 1;
-    *(iv2+1) = 2;
-    *(iv2+2) = 3;
-    *(iv2+3) = 2;
-    *(iv2+4) = 1;
-    assert(isPalindrome(iv2, iv2+5));
-    
+    *(iv2 + 1) = 2;
+    *(iv2 + 2) = 3;
+    *(iv2 + 3) = 2;
+    *(iv2 + 4) = 1;
+    assert(isPalindrome(iv2, iv2 + 5));
+
     int iva[4] = {1, 2, 2, 1};
-    assert(isPalindrome(iva, iva+4));
+    assert(isPalindrome(iva, iva + 4));
     int iva2[5] = {1, 2, 3, 2, 1};
-    assert(isPalindrome(iva2, iva2+5));
-    
-    
+    assert(isPalindrome(iva2, iva2 + 5));
+
     map<int, int> se;
     se[1] = 1; se[2] = 2; se[4] = 2; se[5] = 1;
-    assert((isPalindrome<map<int, int>::iterator, MapValueEqual, GreaterEqual<map<int, int>>>(se.begin(), se.end())));
+    assert((isPalindrome<map<int, int>::iterator, MapValueEqual,
+                         GreaterEqual<map<int, int>>>(se.begin(), se.end())));
     se[3] = 3;
-    assert((isPalindrome<map<int, int>::iterator, MapValueEqual, GreaterEqual<map<int, int>>>(se.begin(), se.end())));
+    assert((isPalindrome<map<int, int>::iterator, MapValueEqual,
+                         GreaterEqual<map<int, int>>>(se.begin(), se.end())));
 }
 
-void testFalseIterative() {
+void
+testFalseIterative()
+{
     string s = "1231";
     assert(!isPalindrome(s.begin(), s.end()));
     string s2 = "12331";
     assert(!isPalindrome(s2.begin(), s2.end()));
-    
+
     vector<int> v = {1, 2, 3, 1};
     assert(!isPalindrome(v.begin(), v.end()));
     vector<int> v2 = {1, 2, 3, 3, 1};
     assert(!isPalindrome(v2.begin(), v2.end()));
-    
-    int* iv = new int(4);
+
+    int *iv = new int(4);
     *iv = 1;
-    *(iv+1) = 2;
-    *(iv+2) = 3;
-    *(iv+3) = 1;
-    assert(!isPalindrome(iv, iv+4));
+    *(iv + 1) = 2;
+    *(iv + 2) = 3;
+    *(iv + 3) = 1;
+    assert(!isPalindrome(iv, iv + 4));
     int *iv2 = new int(5);
     *iv2 = 1;
-    *(iv2+1) = 2;
-    *(iv2+2) = 3;
-    *(iv2+3) = 3;
-    *(iv2+4) = 1;
-    assert(!isPalindrome(iv2, iv2+5));
-    
+    *(iv2 + 1) = 2;
+    *(iv2 + 2) = 3;
+    *(iv2 + 3) = 3;
+    *(iv2 + 4) = 1;
+    assert(!isPalindrome(iv2, iv2 + 5));
+
     int iva[4] = {1, 2, 3, 1};
-    assert(!isPalindrome(iva, iva+4));
+    assert(!isPalindrome(iva, iva + 4));
     int iva2[5] = {1, 2, 3, 3, 1};
-    assert(!isPalindrome(iva2, iva2+5));
-    
-    
+    assert(!isPalindrome(iva2, iva2 + 5));
+
     map<int, int> se;
     se[1] = 1; se[2] = 2; se[4] = 3; se[5] = 1;
-    assert(!(isPalindrome<map<int, int>::iterator, MapValueEqual, GreaterEqual<map<int, int>>>(se.begin(), se.end())));
+    assert(!(isPalindrome<map<int, int>::iterator, MapValueEqual,
+                          GreaterEqual<map<int, int>>>(se.begin(), se.end())));
     se[3] = 3;
-    assert(!(isPalindrome<map<int, int>::iterator, MapValueEqual, GreaterEqual<map<int, int>>>(se.begin(), se.end())));
+    assert(!(isPalindrome<map<int, int>::iterator, MapValueEqual,
+                          GreaterEqual<map<int, int>>>(se.begin(), se.end())));
 }
 
-int main () {
+int
+main()
+{
     testTrueRecursive();
     testFalseRecursive();
     testTrueIterative();
@@ -184,4 +204,5 @@ int main () {
 
     return 0;
 }
+
 // */
