96903a15da3d977ca2a769fc54b8b84b3cc263dc
Sudharsanaraj  R <jujusudhur@gmail.com>
2017-11-13 09:48:40 +0530

    Minor changes

--- code/string-algorithms/kasai_algorithm/kasai_algorithm.cpp
+++ code/string-algorithms/kasai_algorithm/kasai_algorithm.cpp
@@ -4,25 +4,25 @@
  * 
  */
 
-vector < int > kasaiAlgorithm(string s , vector < int > suffix_array)
+vector<int> kasaiAlgorithm(string s,vector<int> suffix_array)
 {
-    int n = s.size() , m = 0;
-    vector < int > LCP(n , 0);
-    vector < int > rank(n , 0);
+    int m=0;
+    vector<int> LCP(s.size(),0);
+    vector<int> rank(s.size(),0);
 
-    for (int i = 0 ; i < n ; i ++) rank[suffix_array[i]] = i;
+    for (int i=0 ; i<s.size() ; i++) rank[suffix_array[i]]=i;
 
-    for (int i = 0 ; i < n ; i ++ , m ? m -- : 0)
+    for (int i=0 ; i<s.size() ; i++ , m?m--:0)
     {
-        if (rank[i] == n - 1) 
+        if (rank[i]==s.size()-1) 
             { 
-                m = 0 ; 
+                m=0 ; 
                 continue ;
             }
-        int j = suffix_array[rank[i] + 1];
+        int j=suffix_array[rank[i]+1];
 
-        while (i + m < n && j + m < n && s[i + m] == s[j + m]) m ++;
-        LCP[rank[i]] = m;
+        while (i+m<s.size() && j+m<s.size() && s[i+m]==s[j+m])m++;
+        LCP[rank[i]]=m;
     }
     return LCP;
 }
