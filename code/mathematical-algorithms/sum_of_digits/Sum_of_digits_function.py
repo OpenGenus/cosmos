4dc81ba73d9f4a3e2ba7a803e5a77296dff8e2c6
Manas Gupta <manasg1998@gmail.com>
2017-11-14 09:24:53 +0530

    Update Sum_of_digits_function.py
    Removed python 2 implementation.

--- code/mathematical-algorithms/sum_of_digits/Sum_of_digits_function.py
+++ code/mathematical-algorithms/sum_of_digits/Sum_of_digits_function.py
@@ -3,11 +3,8 @@ def sum_of_digits(number):
     # converts the number into a string which is further converted into an array of numbers using map and 
     # sum function calculating the sum of numbers in the list
     return sum(map(int, str(number)))
-    
-try:
-    input_text = raw_input("Input your digits: ") # python2 - String input using raw_input
-except NameError:
-    input_text = input("Input your digits: ") # python3 - default input format is string
-               
+
+input_text = input("Input your digits: ") # python3 - default input format is string
+
 input_number = int(input_text.strip())
 print("Sum of your digits is: {0}".format(sum_of_digits(input_number)))
