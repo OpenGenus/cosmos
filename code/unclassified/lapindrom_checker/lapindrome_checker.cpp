5c7e7ae3a369597282434a1966511a305f17596e
tbhaxor <tbhaxor>
2017-11-19 01:45:01 +0530

    lapindrome checker in c++

---
+++ code/unclassified/lapindrom_checker/lapindrome_checker.cpp
@@ -0,0 +1,35 @@
+// part of cosmos by opengenus foundation
+
+#include <iostream>
+#include <algorithm>   // used for sort()
+using namespace std;
+
+bool is_lapindrome(string str)
+{
+  int mid = str.length() / 2;
+  int delim = (str.length() % 2) == 0 ? mid : mid + 1;
+
+  string first_half = str.substr(0, mid);
+  string second_half = str.substr(delim);
+
+  sort(first_half.begin(), first_half.end());
+  sort(second_half.begin(), second_half.end());
+
+  return first_half == second_half;
+}
+
+int main()
+{
+   string test_case = "addbbadd";
+
+   if (is_lapindrome(test_case))
+   {
+      cout<<"lapindrome\n";
+   }
+   else
+   {
+      cout<<"not a lapindrome\n";
+   }
+
+   return 0;
+}
