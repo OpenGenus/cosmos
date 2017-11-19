f7f0f8b9858760fdb553f5d770a784fcf0ceac4d
tbhaxor <tbhaxor@gmail.com>
2017-11-19 07:05:54 +0530

    spaces added

--- code/unclassified/leap_year/leap_year.cpp
+++ code/unclassified/leap_year/leap_year.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 bool isLeap(int year)
 {
-   return (year%4 == 0 && year%100 != 0) || year%400 == 0; 
+   return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0; 
 }
 
 int main()
