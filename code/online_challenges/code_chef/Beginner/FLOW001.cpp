e8631405ac7c57c42a78948013e0dbac36af23f9
tbhaxor <tbhaxor@gmail.com>
2017-11-21 04:02:19 +0530

    Add Two Numbers in cpp

---
+++ code/online_challenges/code_chef/Beginner/FLOW001.cpp
@@ -0,0 +1,24 @@
+// part of cosmos from opengenus foundation
+
+// problem code link https://www.codechef.com/problems/FLOW001
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    int arr[t], i=0;
+    while(t--)
+    {
+        int a,b;
+	cin>>a>>b;
+	arr[i++] = a + b;
+    }
+
+    for(i = 0; i < sizeof(arr)/sizeof(arr[0]); i++)
+        cout<<arr[i]<<endl;
+    return 0;
+}
