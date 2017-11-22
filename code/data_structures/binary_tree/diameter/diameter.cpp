3bb816b35f9a8c4dcb48e87621d1893dec7e7d1c
Ernest <iattempt.net@gmail.com>
2017-11-22 22:52:56 +0800

    fix coding style

--- code/data_structures/binary_tree/diameter/diameter.cpp
+++ code/data_structures/binary_tree/diameter/diameter.cpp
@@ -4,37 +4,47 @@
     diameter of tree synopsis
 
 template<typename _TreeNode>
-size_t diameter_impl(_TreeNode *node, size_t &max);
+size_t
+diameterRecursive(_TreeNode node, size_t &maximum);
 
 template<typename _TreeNode>
-size_t diameter(_TreeNode *node);
-*/
+size_t
+diameter(_TreeNode node);
+ */
 
-#include <functional>
+#include <algorithm>
+#include <memory>
+#include <stack>
+#include "../tree_node/tree_node.cpp"
 
 template<typename _TreeNode>
-size_t diameter_impl(_TreeNode *node, size_t &max) {
-    if (node != nullptr) {
-        size_t left_max{}, right_max{};
+size_t
+diameterRecursive(_TreeNode node, size_t &maximum)
+{
+    if (node != nullptr)
+    {
+        size_t leftMax{}, rightMax{};
 
         // DFS
-        size_t left_height = diameter_impl(node->left, left_max);
-        size_t right_height = diameter_impl(node->right, right_max);
+        size_t leftHeight = diameterRecursive(node->left(), leftMax);
+        size_t rightHeight = diameterRecursive(node->right(), rightMax);
 
-        max = left_height + right_height + 1;
-        max = std::max(max, left_max);
-        max = std::max(max, right_max);
+        maximum = leftHeight + rightHeight + 1;
+        maximum = std::max(maximum, leftMax);
+        maximum = std::max(maximum, rightMax);
 
-        return std::max(left_height, right_height) + 1;
+        return std::max(leftHeight, rightHeight) + 1;
     }
 
     return 0;
 }
 
 template<typename _TreeNode>
-size_t diameter(_TreeNode *node) {
+size_t
+diameter(_TreeNode node)
+{
     size_t res{};
-    diameter_impl(node, res);
+    diameterRecursive(node, res);
 
     return res;
 }
