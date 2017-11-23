996aaf04b188890728e5d40dcaefe9d8a1e7b80f
Vaibhav1671998 <vk530873@gmail.com>
2017-11-23 19:35:21 +0530

    Create reverse_number.c

---
+++ code/mathematical_algorithms/reverse_number/reverse_number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int reverse_number(int n){
+	int ans = 0;
+	while(n != 0){
+		ans *= 10;
+		ans += n % 10;
+		n /= 10;
+	}
+    return ans;
+}
+
+int main(){
+	int n;
+	printf("Enter the number : ");
+	scanf("%d",&n );
+	printf("Reverse of %d is %d",n,reverse_number(n));
+
+	return 0;
+}
