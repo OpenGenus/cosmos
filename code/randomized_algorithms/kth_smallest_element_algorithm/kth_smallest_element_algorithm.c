626e527d88dc2757df2fc481e9225206ef6d9209
Vaibhav1671998 <vk530873@gmail.com>
2017-11-25 09:05:05 +0530

    Update kth_smallest_element_algorithm.c

--- code/randomized_algorithms/kth_smallest_element_algorithm/kth_smallest_element_algorithm.c
+++ code/randomized_algorithms/kth_smallest_element_algorithm/kth_smallest_element_algorithm.c
@@ -5,15 +5,15 @@ void swap(int *a , int *b)
     *a = *b;
     *b = temp;
 }
-int find_element(int a[],int n,int k)
+int find_element(int arr[],int n,int k)
 {
     for(int i=0; i<n-1; i++)
     {
         for(int j=i; j<n-1; j++)
         {
-            if(a[j]>a[j+1])
+            if(arr[j]>arr[j+1])
             {
-                swap(&a[j],&a[j+1]);
+                swap(&arr[j],&arr[j+1]);
             }
         }
     }
@@ -24,16 +24,15 @@ int main()
     int n;
     printf("Enter number of elements : ");
     scanf("%d",&n);
-    int a[n];
+    int arr[n];
     printf("Enter Elements : \n");
     for (int i = 0; i < n; i++)
     {
-        scanf("%d",&a[i]);
+        scanf("%d",&arr[i]);
     }
     int k;
     printf("Enter k (nth smallest element to find) : ");
     scanf("%d",&k);
-    int result = find_element(a,n,k);
-    printf("kth smallest element is %d\n",result);
+    printf("kth smallest element is %d\n",find_element(arr,n,k));
     return 0;
 }
