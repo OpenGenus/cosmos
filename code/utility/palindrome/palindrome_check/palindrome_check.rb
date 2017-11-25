771bb070cc1dc3b826a04fd217111222e3140703
cpackingham <cpackingham@lwolf.com>
2017-11-25 17:24:05 -0600

    DEV: Formatted code properly.

--- code/utility/palindrome/palindrome_check/palindrome_check.rb
+++ code/utility/palindrome/palindrome_check/palindrome_check.rb
@@ -1,73 +1,76 @@
 #careful using any variables named word, scoping gets weird
 def  is_palindrome_recursive(word)
-    #set both indices for iterating through the word
-    word_start = 0;
-    word_end = word.length - 1;
+  #set both indices for iterating through the word
+  word_start = 0
+  word_end = word.length - 1
 
-    #check that the word is not one character or empty
-    if(word_end != word_start)
+  #check that the word is not one character or empty
+  if word_end != word_start 
 
-        #check that the word start index is less than the word end index
-        if(word_start < word_end)
+    #check that the word start index is less than the word end index
+    if word_start < word_end
 
-            #check if the characters at each index are the same 
-            if(word[word_start] != word[word_end])
+      #check if the characters at each index are the same 
+      if word[word_start] != word[word_end]
 
-                return false
+        return false
 
-            end
+      end
+      
+      #trim the word
+      word[word_start] = ''
+      word[word_end - 1] = ''
 
-            #trim the word
-            word[word_start] = ''
-            word[word_end - 1] = ''
-
-            #send the word back for another loop
-            return is_palindrome_recursive(word)
-
-        end
+      #send the word back for another loop
+      return is_palindrome_recursive(word)
 
     end
 
-    #if it gets to this statement, it's a palindrome
-    return true
-    
+  end
+
+  #if it gets to this statement, it's a palindrome
+  true
+  
 end
 
 def is_palindrome_iterative(word) 
-    #set both indices for iterating through the word
-    word_start = 0;
-    word_end = word.length - 1;
+  #set both indices for iterating through the word
+  word_start = 0
+  word_end = word.length - 1
 
-    #check that the word is not one character or empty
-    if (word_end != word_start)
+  #check that the word is not one character or empty
+  if word_end != word_start
 
-        #breaks when the indices reach one another
-        while(word_start < word_end)
+    #breaks when the indices reach one another
 
-            #check if the characters at each index are the same
-            if(word[word_start] != word[word_end])
+    while word_start < word_end
 
-                return false
-
-            end
-            
-            #iterate both indices, bringing them towards the middle of the word
-            word_start += 1
-            word_end -= 1
-
-        end
-
-        return true
+      #check if the characters at each index are the same
+      if word[word_start] != word[word_end]
         
+        return false
+
+      end
+
+      #iterate both indices, bringing them towards the middle of the word
+      word_start += 1
+      word_end -= 1
+
     end
 
-    return false
+    return true
+    
+  end
+
+  false
 
 end
 
 def is_palindrome(word)
-    
-    #use the iterative method as the default
-    return is_palindrome_iterative(word)
+  
+  #use the iterative method as the default
+  is_palindrome_iterative(word)
 
 end
+
+
