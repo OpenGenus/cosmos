f97c0c9a005adc485f68169fa935a1073a913dc3
Vaibhav1671998 <vk530873@gmail.com>
2017-11-25 09:10:08 +0530

    Update xor_swap.cpp

--- code/bit_manipulation/xor_swap/xor_swap.cpp
+++ code/bit_manipulation/xor_swap/xor_swap.cpp
@@ -1,20 +1,20 @@
 #include < iostream >
 using namespace std;
 
-void xor_swap(int * a, int * b) {
-    * a = * a ^ * b;
-    * b = * a ^ * b;
-    * a = * a ^ * b;
-
-  return;
+void xor_swap(int * a, int * b)
+{
+    *a = *a ^ *b;
+    *b = *a ^ *b;
+    *a = *a ^ *b;
 }
 
-int main() {
+int main()
+{
   int a = 10, b = 15;
 
   cout << "Before swapping: A = " << a << " and B = " << b << "\n";
 
-  xor_swap( & a, & b);
+  xor_swap(&a, &b);
 
   cout << "After swapping: A = " << a << " and B = " << b << "\n";
 
