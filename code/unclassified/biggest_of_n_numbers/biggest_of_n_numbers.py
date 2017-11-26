1a2e7e95d24bbb1809ec6d3171887514c66eb819
Vaibhav <vk530873@gmail.com>
2017-11-26 20:13:08 +0530

    Update biggest_of_n_numbers.py

--- code/unclassified/biggest_of_n_numbers/biggest_of_n_numbers.py
+++ code/unclassified/biggest_of_n_numbers/biggest_of_n_numbers.py
@@ -1,6 +1,6 @@
 elements = []
 n = int(input("Enter number of elements:"))
-for i in range(1, n + 1):
+for i in range(0, n):
   elements.append(int(input("Enter element:")))
 elements.sort()
 print("Largest element is : ", elements[n - 1])
