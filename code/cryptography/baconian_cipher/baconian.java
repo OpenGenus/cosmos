ee7feda05c54a5de89038c4ca21b587101153544
cpackingham <cpackingham1@gmail.com>
2017-11-27 20:43:38 -0600

    DEV: Pre-increment for slight optimization

--- code/cryptography/baconian_cipher/baconian.java
+++ code/cryptography/baconian_cipher/baconian.java
@@ -3,96 +3,96 @@ import java.util.Map;
 
 public class Baconian {
 
-  private Map<String ,String> cipherMap = new HashMap<>();
+    private Map<String ,String> cipherMap = new HashMap<>();
 
-  public Baconian() {
-    cipherMap.put("a", "aaaaa");
-    cipherMap.put("b", "aaaab");
-    cipherMap.put("c", "aaaba");
-    cipherMap.put("d", "aaabb");
-    cipherMap.put("e", "aabaa");
-    cipherMap.put("f", "aabab");
-    cipherMap.put("g", "aabba");
-    cipherMap.put("h", "aabbb");
-    cipherMap.put("i", "abaaa");
-    cipherMap.put("j", "abaab");
-    cipherMap.put("k", "ababa");
-    cipherMap.put("l", "ababb");
-    cipherMap.put("m", "abbaa");
-    cipherMap.put("n", "abbab");
-    cipherMap.put("o", "abbba");
-    cipherMap.put("p", "abbbb");
-    cipherMap.put("q", "baaaa");
-    cipherMap.put("r", "baaab");
-    cipherMap.put("s", "baaba");
-    cipherMap.put("t", "baabb");
-    cipherMap.put("u", "babaa");
-    cipherMap.put("v", "babab");
-    cipherMap.put("w", "babba");
-    cipherMap.put("x", "babbb");
-    cipherMap.put("y", "bbaaa");
-    cipherMap.put("z", "bbaab");
-
-  }
-
-  public String encode(String word) {
-
-    StringBuilder results = new StringBuilder();
-    
-    word = word.toLowerCase();
-
-    for(Character letter : word.toCharArray()) {
-
-      results.append(this.cipherMap.get(letter.toString()));
+    public Baconian() {
+        cipherMap.put("a", "aaaaa");
+        cipherMap.put("b", "aaaab");
+        cipherMap.put("c", "aaaba");
+        cipherMap.put("d", "aaabb");
+        cipherMap.put("e", "aabaa");
+        cipherMap.put("f", "aabab");
+        cipherMap.put("g", "aabba");
+        cipherMap.put("h", "aabbb");
+        cipherMap.put("i", "abaaa");
+        cipherMap.put("j", "abaab");
+        cipherMap.put("k", "ababa");
+        cipherMap.put("l", "ababb");
+        cipherMap.put("m", "abbaa");
+        cipherMap.put("n", "abbab");
+        cipherMap.put("o", "abbba");
+        cipherMap.put("p", "abbbb");
+        cipherMap.put("q", "baaaa");
+        cipherMap.put("r", "baaab");
+        cipherMap.put("s", "baaba");
+        cipherMap.put("t", "baabb");
+        cipherMap.put("u", "babaa");
+        cipherMap.put("v", "babab");
+        cipherMap.put("w", "babba");
+        cipherMap.put("x", "babbb");
+        cipherMap.put("y", "bbaaa");
+        cipherMap.put("z", "bbaab");
 
     }
 
-    return results.toString();
+    public String encode(String word) {
 
-  }
+        StringBuilder results = new StringBuilder();
 
-  public String decode(String word) {
+        word = word.toLowerCase();
 
-    StringBuilder results = new StringBuilder();
+        for(Character letter : word.toCharArray()) {
 
-    word = word.toLowerCase();
+            results.append(this.cipherMap.get(letter.toString()));
 
-    for(int start = 0; start < word.length() / 5; start++) {
+        }
 
-      int startIndex = start * 5;
-
-      StringBuilder code = new StringBuilder();
-
-      for(int index = startIndex; index < startIndex + 5; index++) {
-         
-        code.append(word.charAt(index));
-
-      }
-
-      String letter = getKeyFromValue(this.cipherMap, code.toString());
-
-      results.append(letter);
+        return results.toString();
 
     }
 
-    return results.toString();
+    public String decode(String word) {
 
-  }
+        StringBuilder results = new StringBuilder();
 
-  public static String getKeyFromValue(Map hashmap, Object value) {
+        word = word.toLowerCase();
 
-    for (Object key: hashmap.keySet()) {
+        for(int start = 0; start < word.length() / 5; ++start) {
 
-      if (hashmap.get(key).equals(value)) {
+            int startIndex = start * 5;
 
-        return key.toString();
+            StringBuilder code = new StringBuilder();
 
-      }
+            for(int index = startIndex; index < startIndex + 5; ++index) {
+
+                code.append(word.charAt(index));
+
+            }
+
+            String letter = getKeyFromValue(this.cipherMap, code.toString());
+
+            results.append(letter);
+
+        }
+
+        return results.toString();
 
     }
 
-    return null;
+    public static String getKeyFromValue(Map hashmap, Object value) {
 
-  }
+        for (Object key: hashmap.keySet()) {
+
+            if (hashmap.get(key).equals(value)) {
+
+                return key.toString();
+
+            }
+
+        }
+
+        return null;
+
+    }
 
 }
