001102fd96aa9de62a21910b32b02d5eddeed01e
arnavb11 <arnavborborah11@gmail.com>
2017-11-28 14:27:19 +0000

    Switched to bitset implementation

--- code/online_challenges/project_euler/problem_036/problem_036.cpp
+++ code/online_challenges/project_euler/problem_036/problem_036.cpp
@@ -1,3 +1,4 @@
+#include <bitset>
 #include <iostream>
 #include <string>
 
@@ -9,24 +10,13 @@ int main()
     int sum = 0;
     
     for (int i = 1; i < 1000000; ++i)
-        if (isPalindrome(std::to_string(i)) && isPalindrome(convertBase2(i)))
-            sum += i;
-    std::cout << sum << "\n";
-}
-
-std::string convertBase2(int number)
-{
-    std::string result;
-    
-    while (number > 0)
     {
-        int currentBit = number % 2;
-        int currentQuotient = number / 2;
-        result += std::to_string(currentBit);
-        number = currentQuotient;
+        std::string currentBinaryString = std::bitset<32>(i).to_string();
+        currentBinaryString.erase(0, currentBinaryString.find('1')); // Remove leading zeroes
+        if (isPalindrome(std::to_string(i)) && isPalindrome(currentBinaryString))
+            sum += i;
     }
-    
-    return std::string{ result.rbegin(), result.rend() };
+    std::cout << sum << "\n";
 }
 
 bool isPalindrome(const std::string& str)
