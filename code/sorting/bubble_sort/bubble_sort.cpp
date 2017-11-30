bce10920a63af28ff48bbbd53d8d5f590a1057f9
Ernest <iattempt.net@gmail.com>
2017-11-30 11:12:24 +0800

    generalize sorting algorithms

--- code/sorting/bubble_sort/bubble_sort.cpp
+++ code/sorting/bubble_sort/bubble_sort.cpp
@@ -1,82 +1,54 @@
-/* Part of Cosmos by OpenGenus Foundation */
-//Sorting array element (The bubble sort)
-
-#include<iostream>
-
-using namespace std;
-
-void bsort(int *,int);               //prototype
-void orderPointer(int *,int *);      //prototype
-
-//call by pointer
-void orderPointer(int * a,int * b)
-{
-    if(*a>*b)
-    {
-        int temp=*a;
-        *a=*b;
-        *b=temp;
-    }
-
-}
-
-void bsort(int* ptr,int s)
-{
-    for(int i=0; i<s-1 ;++i)
-    {
-        for(int j=i+1; j<s ;++j)
-            orderPointer(ptr+i,ptr+j);
-
-
-    }
-
-}
-
-int main()
-{
-    const int N=5;
-    int arr[N]={10,8,15,7,11};
-
-    bsort(arr,N);       //sort the array
-
-    for(int j=0; j<N ;++j)
-        cout<<arr[j]<<" ";
-    cout<<endl;
-
-    return 0;
-}
-
-
 /*
-Bubble sort algorithm (steps):
-algorithm compares the first two elements, and swaps the minimum number to be in beginning.
-    10 8 15 7 11
-    ---------------
-    first iteration
-    10 8 15 7 11
-    8 10 15 7 11
-    7 10 15 8 11
-    ---------------
-    second iteration
-    7 10 15 8 11
-    7 8  15 10 11
-    ---------------
-    third iteration
-    7 8  15 10 11
-    7 8  10 15 11
-    ---------------
-    fourth iteration
-    7 8  10 15 11
-    7 8  10 11 15
-    ---------------
+ Part of Cosmos by OpenGenus Foundation
 
-for(int i=0; i<N-1 ;++i)
+ bubble sort synopsis
+
+template<typename _Bidirectional_Iter,
+         typename _Tp = typename std::iterator_traits<_Bidirectional_Iter>::value_type,
+         typename _Compare = std::less<_Tp>>
+void
+bubbleSort(_Bidirectional_Iter begin, _Bidirectional_Iter end, _Compare compare);
+
+template<typename _Bidirectional_Iter,
+         typename _Tp = typename std::iterator_traits<_Bidirectional_Iter>::value_type>
+void
+bubbleSort(_Bidirectional_Iter begin, _Bidirectional_Iter end);
+ */
+
+#include <functional>
+
+template<typename _Bidirectional_Iter,
+         typename _Tp = typename std::iterator_traits<_Bidirectional_Iter>::value_type,
+         typename _Compare = std::less<_Tp>>
+void
+bubbleSort(_Bidirectional_Iter begin, _Bidirectional_Iter end, _Compare compare)
+{
+    if (begin != end)
     {
-        for(int j=i+1; j<N ;++j)
-            order(ptr+i,ptr+j);
+        auto frontOfSorted = end;
 
+        for (--frontOfSorted; frontOfSorted != begin; --frontOfSorted)
+        {
+            bool swapped{};
+            for (auto j = begin; j != frontOfSorted; ++j)
+            {
+                auto nextOfJ = j;
+                if (compare(*++nextOfJ, *j))
+                {
+                    std::iter_swap(nextOfJ, j);
+                    swapped = true;
+                }
+            }
+            if (swapped == false)
+                break;
+        }
     }
-*/
+}
 
-
-
+template<typename _Bidirectional_Iter,
+         typename _Tp = typename std::iterator_traits<_Bidirectional_Iter>::value_type>
+void
+bubbleSort(_Bidirectional_Iter begin, _Bidirectional_Iter end)
+{
+    bubbleSort(begin, end, std::less<_Tp>());
+}
--- code/sorting/selection_sort/selection_sort.cpp
+++ code/sorting/selection_sort/selection_sort.cpp
@@ -1,22 +1,45 @@
-#include <iostream>
-#include <vector>
+/*
+ Part of Cosmos by OpenGenus Foundation
 
-/* Part of Cosmos by OpenGenus Foundation */
-void selection_sort(std::vector<int> &v) {
-	for (int i = 0; i < v.size(); i++) {
-		int minIndex = i;
-		for (int j = i + 1; j < v.size(); j++) { // find ("select") the index of the element with the minimum value
-			if (v[j] < v[minIndex]) minIndex = j;
-		}
-		std::swap(v[i], v[minIndex]); // put the found element at position i
-	}
+ selection sort synopsis
+
+template<typename _Input_Iter,
+         typename _Tp = typename std::iterator_traits<_Input_Iter>::value_type,
+         typename _Compare = std::less<_Tp>>
+void
+selectionSort(_Input_Iter begin, _Input_Iter end, _Compare compare);
+
+template<typename _Input_Iter,
+         typename _Tp = typename std::iterator_traits<_Input_Iter>::value_type>
+void
+selectionSort(_Input_Iter begin, _Input_Iter end);
+ */
+
+#include <functional>
+
+template<typename _Input_Iter,
+         typename _Tp = typename std::iterator_traits<_Input_Iter>::value_type,
+         typename _Compare = std::less<_Tp>>
+void
+selectionSort(_Input_Iter begin, _Input_Iter end, _Compare compare)
+{
+    if (begin != end)
+        for (auto curr = begin; curr != end; ++curr)
+        {
+            auto minimum = curr;
+            auto forward = curr;
+            while (++forward != end)
+                if (compare(*forward, *minimum))
+                    minimum = forward;
+
+            std::iter_swap(minimum, curr);
+        }
 }
 
-int main() {
-	std::vector<int> v{1, 5, 2, 3, 4};
-	selection_sort(v);
-	for (auto num : v) {
-		std::cout << num << " ";
-	}
-	return 0;
+template<typename _Input_Iter,
+         typename _Tp = typename std::iterator_traits<_Input_Iter>::value_type>
+void
+selectionSort(_Input_Iter begin, _Input_Iter end)
+{
+    selectionSort(begin, end, std::less<_Tp>());
 }
