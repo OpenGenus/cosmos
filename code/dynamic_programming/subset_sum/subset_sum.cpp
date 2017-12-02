dd9599ad0fe2119691d9e6d29124e885d521e277
Ernest <iattempt.net@gmail.com>
2017-12-02 16:16:07 +0800

    substitute algorithm and separate container

--- code/dynamic_programming/subset_sum/subset_sum.cpp
+++ code/dynamic_programming/subset_sum/subset_sum.cpp
@@ -1,37 +1,38 @@
 /* Part of Cosmos by OpenGenus Foundation */
-
 #ifndef SUBSET_SUM
 #define SUBSET_SUM
 
-#include <cstring>
-
-#define MAX 110
-#define MAX_SUM 100100
-
-int reachable[MAX_SUM];
-bool dp[MAX_SUM];
+#include <iterator>
 
 /*
  *  Check whether is possible to
  *  get value sum from a subset
- *  of the array v
+ *  of the [begin:end)
  */
-bool canGetSum(int v[], int n, int sum) {
 
-    memset(dp, 0, sizeof dp);
-    reachable[0] = 0;
-    dp[0] = true;
-    for(int i = 0, q = 1; i < n && !dp[sum]; ++i) {
-        int aux = q;
-        for(int j = 0; j < aux; ++j) {
-            int x = reachable[j] + v[i];
-            if(x <= sum && !dp[x]) {
-                reachable[q++] = x;
-                dp[x] = true;
-            }
+template<typename _RandomAccessIter,
+         typename _ValueType = typename std::iterator_traits<_RandomAccessIter>::value_type>
+bool
+isSubsetSum(_RandomAccessIter begin, _RandomAccessIter end, _ValueType sum)
+{
+    auto sz = std::distance(begin, end);
+    bool subset[sum + 1][sz + 1];
+
+    for (int i = 0; i <= sz; ++i)
+        subset[0][i] = true;
+
+    for (int i = 1; i <= sum; ++i)
+        subset[i][0] = false;
+
+    for (int i = 1; i <= sum; ++i)
+        for (int j = 1; j <= sz; ++j)
+        {
+            subset[i][j] = subset[i][j - 1];
+            if (i >= *(begin + j - 1))
+                subset[i][j] = subset[i][j] || subset[i - *(begin + j - 1)][j - 1];
         }
-    }
-    return dp[sum];
+
+    return subset[sum][sz];
 }
 
 #endif // SUBSET_SUM
