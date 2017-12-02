67865517708389d4226478f98f16d8a1ca27a269
Ernest <iattempt.net@gmail.com>
2017-12-02 17:47:35 +0800

    add space(sum) version

--- code/dynamic_programming/subset_sum/subset_sum.cpp
+++ code/dynamic_programming/subset_sum/subset_sum.cpp
@@ -10,6 +10,41 @@
  *  of the [begin:end)
  */
 
+// complexity: time: O(sum * n), space: O(sum)
+template<typename _BidirectionalIter,
+         typename _ValueType = typename std::iterator_traits<_BidirectionalIter>::value_type>
+bool
+isSubsetSum(_BidirectionalIter begin, _BidirectionalIter end, _ValueType sum)
+{
+    auto sz = std::distance(begin, end);
+    bool subset[2][sum + 1];
+
+    for (int i = 0; i <= sz; i++)
+    {
+        auto x = begin;
+        std::advance(x, i - 1);
+
+        for (int j = 0; j <= sum; j++)
+        {
+            // A subset with sum 0 is always possible
+            if (j == 0)
+                subset[i % 2][j] = true;
+            // If there exists no element
+            // no sum is possible
+            else if (i == 0)
+                subset[i % 2][j] = false;
+            else if (*x <= j)
+                subset[i % 2][j] = subset[(i + 1) % 2][j - *x] || subset[(i + 1) % 2][j];
+            else
+                subset[i % 2][j] = subset[(i + 1) % 2][j];
+        }
+    }
+
+    return subset[sz % 2][sum];
+}
+
+/*
+// complexity: time: O(sum * n), space: O(sum * n)
 template<typename _BidirectionalIter,
          typename _ValueType = typename std::iterator_traits<_BidirectionalIter>::value_type>
 bool
@@ -37,5 +72,5 @@ isSubsetSum(_BidirectionalIter begin, _BidirectionalIter end, _ValueType sum)
 
     return subset[sum][sz];
 }
-
+*/
 #endif // SUBSET_SUM
