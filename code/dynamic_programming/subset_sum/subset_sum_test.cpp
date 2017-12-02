153afc847583850e6ccfd3ffbe1282ac3c972987
Ernest <iattempt.net@gmail.com>
2017-12-02 17:41:05 +0800

    replace output with assertion and add test for bidirectional container

--- code/dynamic_programming/subset_sum/subset_sum_test.cpp
+++ code/dynamic_programming/subset_sum/subset_sum_test.cpp
@@ -4,16 +4,24 @@
 #define SUBSET_SUM_TEST
 
 #include <iostream>
+#include <list>
+#include <cassert>
 #include "subset_sum.cpp"
 
 using namespace std;
 int
 main()
 {
+    int vz[0];
     int v[] = {1, 2, 15, 8, 5};
+    list<int> l{1, 2, 15, 8, 5};
 
-    cout << (isSubsetSum(v, v + sizeof(v) / sizeof(v[0]), 13) ? "possible" : "impossible") << "\n";
-    cout << (isSubsetSum(v, v + sizeof(v) / sizeof(v[0]), 4) ? "possible" : "impossible") << "\n";
+    assert(isSubsetSum(vz, vz, 0));
+    assert(isSubsetSum(v, v + sizeof(v) / sizeof(v[0]), 13));
+    assert(isSubsetSum(l.begin(), l.end(), 13));
+    assert(isSubsetSum(vz, vz, 13) == false);
+    assert(isSubsetSum(v, v + sizeof(v) / sizeof(v[0]), 4) == false);
+    assert(isSubsetSum(l.begin(), l.end(), 4) == false);
 
     return 0;
 }
