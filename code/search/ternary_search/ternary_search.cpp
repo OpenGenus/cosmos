345b1880635cb116e2995e6241a14d0e1dba2d62
Ernest <iattempt.net@gmail.com>
2017-12-03 00:45:42 +0800

    replace array with pointer for container

--- code/search/ternary_search/ternary_search.cpp
+++ code/search/ternary_search/ternary_search.cpp
@@ -3,23 +3,32 @@
  Ternary Search Uses Divide And Conquer Technique
  */
 
-int ternarySearch(int arr[],int l,int r, int x){
-    if(r>=l){
-        int mid1 = l + (r-l)/3;
-        int mid2 = r - (r-l)/3;
-        if(arr[mid1] == x)
+int* ternarySearch(int* first,int* last, int *const notFoundSentinel, const int &x){
+    if(last>=first){
+        auto mid1 = first + (last-first)/3;
+        auto mid2 = last - (last-first)/3;
+        if(*mid1 == x)
             return mid1;
-        /*
-            In this search, after each iteration it neglects (1/3)rd part of the array and repeats the same operations on the remaining 2/3rd 		    part of array 
-        */
-        if(arr[mid2] == x)
+        if(*mid2 == x)
             return mid2;
-        if(x<arr[mid1])
-            return ternarySearch(arr,l,mid1-1,x);
-        else if(x>arr[mid2])
-            return ternarySearch(arr,mid2+1,r,x);
+        if(x<*mid1)
+            return ternarySearch(first,mid1-1, notFoundSentinel,x);
+        else if(x>*mid2)
+            return ternarySearch(mid2+1,last, notFoundSentinel,x);
         else
-            return ternarySearch(arr,mid1+1,mid2-1,x);
+            return ternarySearch(mid1+1,mid2-1, notFoundSentinel,x);
     }
-    return -1; // if x is not found in array arr
+    return notFoundSentinel; // if x is not found in array arr
+}
+
+int* ternarySearch(int *begin, int *end, const int &x)
+{
+    if (begin < end)
+    {
+        auto res = ternarySearch(begin, end-1, end, x);
+
+        return res == end ? end : res;
+    }
+
+    return end;
 }
