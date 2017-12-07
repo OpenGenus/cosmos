eed43979f7193d81ee5dfb7c608979cf6ee54714
YashovardhanSiramdas <ced16i028@iiitdm.ac.in>
2017-12-07 18:06:42 +0530

    Added Pascal Triangle in C

---
+++ code/mathematical_algorithms/pascal_triangle/pascal_triangle.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+long long int a[101][101];
+
+void pascal_triangle()
+{
+	for(int i=0;i<101;i++){
+		for(int j=0;j<101;j++){
+			if(j>i){
+				break;
+			}
+			if(j==0 || j==i){
+				a[i][j]=1;
+			}
+			else{
+				a[i][j]=a[i-1][j-1]+a[i-1][j];
+			}
+
+		}
+	}
+}
+
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	pascal_triangle();
+	for(int i=0;i<=n;i++){
+		for(int j=0;j<=i;j++){
+			printf("%lli ",a[i][j]);
+		}
+		printf("\n");
+	}
+		
+	return (0);
+}
