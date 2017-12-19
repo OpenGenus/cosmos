b61e014e797c8efd363540155b56efd8322fe68e
dianagastrin <dianagastrin@hotmail.com>
2017-12-19 23:35:59 +0200

    Fix & Refactor

--- code/computational_geometry/jarvis_march/jarvis_march.py
+++ code/computational_geometry/jarvis_march/jarvis_march.py
@@ -8,76 +8,69 @@ class Point:
         self.y = y
 
     def equal(self, pt):
-        if self.x == pt.x and self.y == pt.y:
-            return True
-        return False
+        return self.x == pt.x and self.y == pt.y
 
     def __str__(self):
-        return "(" + str(self.x) + ","+str(self.y) + ")"
+        return "(" + str(self.x) + "," + str(self.y) + ")"
 
 
-def leftMostPoint(pts):
+def left_most_point(pts):
     if not isinstance(pts, list):
         return False
-    
     curr = pts[0]
     for pt in pts[1:]:
-        if pt.x == curr.x and pt.y < curr.y:
+        if pt.x < curr.x or (pt.x == curr.x and pt.y < curr.y):
             curr = pt
-        elif pt.x < curr.x and pt.x != curr.x:
-            curr = pt
-            
     return curr
 
-def cross_product(a, b, c):
-    x1 = a.x - b.x
-    x2 = a.x - c.x
-    y1 = a.y - b.y
-    y2 = a.y - c.y
-    return y2 * x1 - y1 * x2
+
+def cross_product(p, p1, p2):
+    return (p.y - p2.y) * (p.x - p1.x) - (p.y - p1.y) * (p.x - p2.x)
+
 
 def distance(p, p1, p2):
-    dist1 = math.hypot(p.x - p1.x, p.y - p1.y)
-    dist2 = math.hypot(p.x - p2.x, p.y - p2.y)
-    return dist1 - dist2
+    return math.hypot(p.x - p1.x, p.y - p1.y) - math.hypot(p.x - p2.x, p.y - p2.y)
 
 
 def iter(pts):
     colinear = list()
-    curr_pt = leftMostPoint(pts)
+    curr_pt = left_most_point(pts)
     convex_list = [curr_pt]
     
     for pt1 in pts:
-        if curr_pt == pt1:
+        if curr_pt.equal(pt1):
             continue
-
         target = pt1
         for pt2 in pts:
-            if pt2 == target or pt2 == curr_pt:
+            if pt2.equal(target) or pt2.equal(curr_pt):
                 continue
-            res = cross_product(curr_pt, target, pt2)  
+
+            res = cross_product(curr_pt, target, pt2)
             if res > 0:
                 colinear = list()
                 target = pt2
-            if res == 0:
+            elif res == 0:
                 if distance(curr_pt, target, pt2) > 0:
-                    colinear.append(pt2)
+                    if pt2 not in colinear:
+                        colinear.append(pt2)
                 else:
-                    colinear.append(target)
+                    if target not in colinear:
+                        colinear.append(target)
                     target = pt2
-
+        
         if target not in convex_list:
             convex_list.append(target)
 
-        if colinear not in convex_list:
-            convex_list += colinear
-
+        for colinear_pt in colinear:
+            if colinear_pt not in convex_list:
+                convex_list.append(colinear_pt)
+  
         curr_pt = target
-
     return convex_list
 
+    
 def main():
-    pts = [Point(x, y) for x, y in np.random.rand(1000,2)]
+    pts = [Point(x, y) for x, y in np.random.rand(100,2)]
     convex_lst = iter(pts)
     len_convex_pts = len(convex_lst)
 
