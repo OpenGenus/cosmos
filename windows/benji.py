d654fe1ca57ceb7109364f8d81470f21f180fc68
Abhimanyu Thakre <abhimanyuthakre@gmail.com>
2017-12-21 22:00:59 +0530

    Convert video file formats(video without audio can also be created)

--- windows/benji.py
+++ windows/benji.py
@@ -135,7 +135,36 @@ def events(frame,put):
             output = link[3] 
             subprocess.call(r'''ffmpeg -i {} -i {} -c:v copy -c:a copy {}'''.format(video,audio,output),shell=True) 
         except:
-            print("Unable to process requested service!")                
+            print("Unable to process requested service!")
+    #Convert video
+    elif link[0] == "convert":
+        try:
+            username = os.getlogin()
+            os.chdir(r'''C:\Users\{}\Desktop'''.format(username))
+            if link[1] == "na":
+                form_in = link[2]
+                video1 = link[3]
+                form_out = link[4]
+                video2 = link[5]
+                if form_in == "avi" and (form_out == "mp4" or form_out == "mkv"):
+                    subprocess.call(r'''ffmpeg -i {} -c:v libx264 -an {}'''.format(video1,video2), shell = True) 
+                elif form_in == "avi" and form_out == "webm":
+                    subprocess.call(r'''ffmpeg -i {} -c:v libvpx-vp9 -b:v 2M -an {}'''.format(video1,video2),shell=True)            
+            else:
+                form_in = link[1]
+                video1 = link[2]
+                form_out = link[3]
+                video2 = link[4]
+                if (form_in == "avi" or form_in == "webm") and (form_out == "mp4" or form_out == "mkv"):
+                    subprocess.call(r'''ffmpeg -i {} -c:v libx264 -acodec aac {}'''.format(video1,video2), shell = True) 
+                elif (form_in == "avi" or form_in == "mp4" or form_in == "mkv") and form_out == "webm":
+                    subprocess.call(r'''ffmpeg -i {} -c:v libvpx-vp9 -b:v 2M -cpu-used -5 -deadline realtime -c:a libvorbis {}'''.format(video1,video2), shell = True)
+                elif (form_in == "mp4" or form_in == "mkv" or form_in == "webm") and form_out == "avi":
+                    subprocess.call(r'''ffmpeg -i {} -c:v mpeg4 -vtag xvid -qscale:v 0 -acodec libmp3lame {}'''.format(video1,video2), shell = True)                
+                elif (form_in == "avi" or form_in == "webm" or form_in == "mp4" or form_in == "mkv" or form_in == "mp3" or form_in == "m4a") and (form_out == "m4a" or form_out == "mp3"):
+                    subprocess.call(r'''ffmpeg -i {} {}'''.format(video1,video2), shell = True)
+        except:
+            print("Unable to process requested service!")                        
     #Look for
     elif put.startswith('look for '):
         try:
