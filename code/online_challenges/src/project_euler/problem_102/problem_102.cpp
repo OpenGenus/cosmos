f870b710814c58586897eff326df48cba5cca447
arnavb11 <arnavborborah11@gmail.com>
2017-12-23 19:25:32 +0000

    Fixed indentation

--- code/online_challenges/src/project_euler/problem_102/problem_102.cpp
+++ code/online_challenges/src/project_euler/problem_102/problem_102.cpp
@@ -17,7 +17,7 @@ int doubleTriangleArea(Coord a, Coord b, Coord c) // Area doesn't actually need 
      * A = | (x1(y2 - y3) + x2(y3 - y1) + x3(y1 - y2))/2 |
      *
      */
-     return std::abs((a.x * (b.y - c.y)) + (b.x * (c.y - a.y)) + (c.x * (a.y - b.y)));
+    return std::abs((a.x * (b.y - c.y)) + (b.x * (c.y - a.y)) + (c.x * (a.y - b.y)));
 }
 
 bool containsOrigin(Coord a, Coord b, Coord c)
