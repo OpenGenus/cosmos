f97b36b5bc9c129322f67d6d638fa68ca4261137
Anukarsh Singh <anukarshsingh1@gmail.com>
2017-12-26 20:08:47 +0530

    Create minimax.py

---
+++ code/artificial_intelligence/minimax/minimax.py
@@ -0,0 +1,37 @@
+from collections import namedtuple
+import random
+
+#from utils import argmax
+
+infinity = float('inf')
+GameState = namedtuple('GameState', 'to_move, utility, board, moves')
+
+# ______________________________________________________________________________
+# Minimax Search
+
+
+def minimax_decision(state, game):
+    """Given a state in a game, calculate the best move by searching
+    forward all the way to the terminal states. [Figure 5.3]"""
+
+    player = game.to_move(state)
+
+    def max_value(state):
+        if game.terminal_test(state):
+            return game.utility(state, player)
+        v = -infinity
+        for a in game.actions(state):
+            v = max(v, min_value(game.result(state, a)))
+        return v
+
+    def min_value(state):
+        if game.terminal_test(state):
+            return game.utility(state, player)
+        v = infinity
+        for a in game.actions(state):
+            v = min(v, max_value(game.result(state, a)))
+        return v
+
+    # Body of minimax_decision:
+    return argmax(game.actions(state),
+                  key=lambda a: min_value(game.result(state, a)))
