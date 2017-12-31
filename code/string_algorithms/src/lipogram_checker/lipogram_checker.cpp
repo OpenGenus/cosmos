2fbe0181f76f4caee842a2fc18d18242cfd220c9
Aditya Patil <contactadityapatil@yahoo.in>
2017-12-31 17:07:06 +0530

    Fixed lipogram_checker.cpp

--- code/string_algorithms/src/lipogram_checker/lipogram_checker.cpp
+++ code/string_algorithms/src/lipogram_checker/lipogram_checker.cpp
@@ -1,42 +1,44 @@
+// Part of Cosmos by OpenGenus Foundation
 #include <iostream>
 using namespace std;
-// Part of Cosmos by OpenGenus Foundation
-string alphabets = "abcdefghijklmnopqrstuvwxyz";
- 
-void panLipogramChecker(string s)
-{   
-   
-    for(int i=0; i<s.length(); i++)
-    {
-        s[i] = tolower(s[i]);
-    }
-    int counter = 0 ;
-    for(int i=0 ; i<26 ; i++)
-    {   
-        int pos = s.find(alphabets[i]);
-   
-        if(pos<0 || pos>s.length())
-            counter += 1;
-    }
-   
-    if(counter == 0)
-        cout<<"Pangram"<<endl;
-    else if(counter >= 2)
-        cout<<"Not a pangram but might a lipogram"<<endl;
-    else
-        cout<<"Pangrammatic Lipogram"<<endl;
-}
- 
 
-int main()
+const string alphabets = "abcdefghijklmnopqrstuvwxyz";
+
+void 
+panLipogramChecker(string phrase)
 {
-    string str = "The quick brown fox jumped over the lazy dog";
-    panLipogramChecker(str);
-     
-    str = "The quick brown fox jumps over the lazy dog";
-    panLipogramChecker(str);
-     
-    str = "The quick brown fox jum over the lazy dog";
-    panLipogramChecker(str);
+
+    for(unsigned int i = 0; i < phrase.length(); i++)
+    {
+        phrase[i] = tolower(phrase[i]);
+    }
+    int counter = 0;
+    for(unsigned int i = 0; i < 26; i++)
+    { 
+        auto pos = phrase.find(alphabets[i]);
+    
+        if(pos > phrase.length())
+            counter += 1;
+    }
+    
+    if(counter == 0)
+        cout<<"Pangram"<<endl;
+    else if(counter >= 2)
+        cout<<"Not a pangram but might be a lipogram"<<endl;
+    else
+        cout<<"Pangrammatic Lipogram"<<endl;
 }
- 
+
+
+int 
+main()
+{
+    string str = "The quick brown fox jumped over the lazy dog";
+    panLipogramChecker(str);
+    
+    str = "The quick brown fox jumps over the lazy dog";
+    panLipogramChecker(str);
+        
+    str = "The quick brown fox jum over the lazy dog";
+    panLipogramChecker(str);
+}
