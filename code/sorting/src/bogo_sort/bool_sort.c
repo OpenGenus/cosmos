b5269d5965379a88cf666ae649e293402921e003
Riya <riyad17031998@gmail.com>
2018-01-02 23:57:08 +0530

    add bogo_sort.c
    bogo sort in C language

---
+++ code/sorting/src/bogo_sort/bool_sort.c
@@ -0,0 +1,56 @@
+    /* Part of cosmos by OpenGenus Foundation  */
+
+    //
+    // bogo_sort.c
+    // created by Riya
+    //
+
+    #include <stdio.h>
+    #include <stdlib.h>
+    
+    #define size 10
+
+    int main()
+    {
+        int numbers[size];
+        int i;    
+
+        printf("Enter the elements of array:");
+        for (i = 0; i < size;i++)
+        {
+            scanf("%d", &numbers[i]);
+        }
+        bogosort(numbers, size);
+        printf("The array after sorting is:");
+        for (i = 0;i < size;i++)
+        {
+            printf("%d\n", numbers[i]);
+        }
+        printf("\n");
+    }
+     
+    int is_sorted(int *a, int n)
+    {
+        while ( --n >= 1 ) {
+        if ( a[n] < a[n-1] ) return 0;
+    }
+    return 1;
+    }
+
+    void shuffle(int *a, int n)
+    {
+        int i, t, temp;
+        for (i = 0;i < n;i++)
+        {
+            t = a[i];
+            temp = rand() % n; 
+            a[i] = a[temp];
+            a[temp] = t;
+        }
+    }
+
+    void bogosort(int *a, int n)
+    {
+        while ( !is_sorted(a, n) ) 
+            shuffle(a, n);
+    }
