b3a5e3f5e8f450e785f5d354cc8e647a3483e50f
Riya <riyad17031998@gmail.com>
2018-01-03 14:00:10 +0530

    Update bogo_sort.c

--- code/sorting/src/bogo_sort/bogo_sort.c
+++ code/sorting/src/bogo_sort/bogo_sort.c
@@ -5,41 +5,39 @@
     * created by Riya
     */
 
-    #include <stdio.h>
-    #include <stdlib.h>
+    #include <stdio.h>    
+    #include <stdlib.h>    
 
-    int main()
-    {
-        int num[]= {3,6,1,8,2,9,5,4,0,7};
-        int i;    
-        
-        bogosort(num, 10);
-             printf("The array after sorting is:");
-             for (i = 0;i < 10;i++)  {
-                    printf("%d\n", num[i]);
-             }   printf("\n");
-    }
-     
-    int is_sorted(int *a, int n)
-    {
-        while ( --n >= 1 )  {
-            if ( a[n] < a[n-1] ) return 0;
-        }  return 1;
-    }
+    int main()    
+    {        
+        int num[10]={1,4,7,5,9,2,6,3,8,0};        
+        int i;            
+        bogosort(num, 10);        
+        printf("The array after sorting is:");        
+        for (i = 0;i < 10;i++)        {            
+            printf("%d\n", num[i]);        
+        }        printf("\n");    
+    }         
 
-    void shuffle(int *a, int n)
-    {
-        int i, t, temp;
-        for (i = 0;i < n;i++)   {
-            t = a[i];
-            temp = rand() % n; 
-            a[i] = a[temp];
-            a[temp] = t;
-        }
-    }
+    int is_sorted(int *a, int n)    
+    {        
+        while ( --n >= 1 ) {        
+            if ( a[n] < a[n-1] ) return 0;    
+        }    return 1;    
+    }    
 
-    void bogosort(int *a, int n)
-    {
-        while ( !is_sorted(a, n) ) 
-            shuffle(a, n);
+    void shuffle(int *a, int n)    
+    {        int i, t, temp;        
+            for (i = 0;i < n;i++)        {            
+                t = a[i];            
+                temp = rand() % n;             
+                a[i] = a[temp];            
+                a[temp] = t;        
+            }    
+    }    
+
+    void bogosort(int *a, int n)    
+    {        
+        while ( !is_sorted(a, n) )             
+        shuffle(a, n);    
     }
