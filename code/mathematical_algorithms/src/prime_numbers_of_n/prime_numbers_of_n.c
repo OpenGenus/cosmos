9ab5f5f9dc1c6440becc9994f5a993dc3cb395ac
YashovardhanSiramdas <ced16i028@iiitdm.ac.in>
2018-01-03 22:09:19 +0530

    Stored prime factors in array

--- code/mathematical_algorithms/src/prime_numbers_of_n/prime_numbers_of_n.c
+++ code/mathematical_algorithms/src/prime_numbers_of_n/prime_numbers_of_n.c
@@ -1,34 +1,43 @@
 # include <stdio.h>
 # include <math.h>
 
-void 
-primeFactors(int number)
+int 
+primeFactors(int number, int prime_factors[100])
 {
-    int i;
+    int i, j = -1;
     while (number % 2 == 0) {
-        printf("%d ", 2);
+    	++j;
+    	prime_factors[j] = 2;
         number = number / 2;
     }
 
     for (i = 3; i <= sqrt(number); i = i + 2) {
         while (number % i == 0) {
-            printf("%d ", i);
+            ++j;
+            prime_factors[j] = i;
             number = number / i;
         }
     }
 
-    if (number > 2)
-        printf ("%d ", number);
+    if (number > 2) {
+    	++j;
+    	prime_factors[j] = number;
+    }
+
+    return (j);
 }
 
 int 
 main()
 {
-    int number;
+    int number, prime_factors[100], i;
     printf("Enter a Natural Number \n");
     scanf("%d", &number);
+    int number_of_prime_factors=primeFactors(number, prime_factors);
+
     printf("Prime Factors of %d are:- \n", number);
-    primeFactors(number);
+    for(i=0; i <= number_of_prime_factors; i++)
+    	printf("%d ", prime_factors[i]);
     printf("\n");
 
     return (0);
