ee95b30d4fb6f77718413bf067eb8d96e5212f2c
Abdous Kamel <fk_abdous@esi.dz>
2018-01-05 17:15:24 +0100

    Update tsp.c
    Adding headers for functions

--- code/artificial_intelligence/src/tsp.c
+++ code/artificial_intelligence/src/tsp.c
@@ -10,6 +10,47 @@
 
 int a[10][10], visited[10], no, cost, sum, vs[10];
 
+void tsp(int city);
+int least(int c);
+void nearest_n(int city)
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	printf("Enter the No of Cities\n");
+	scanf("%d", &no);
+
+	printf("\nEnter the Adjacency Matrix\n");
+	for(i = 1; i <= no; i++)
+		for(j = 1; j <=no; j++)
+			scanf("%d", &a[i][j]);
+
+	printf("Using Dynamic Prog\n");
+	tsp(1);
+	printf("Cost is %d\n", cost);
+	printf("Using approx method\n");
+	nearest_n(1);
+	printf("Cost is %d\n", sum);
+	printf ("\nRatio is %f\n", (float)sum/cost);
+	return 0;
+}
+
+void tsp(int city)
+{
+	int ncity;
+	visited[city] = 1;
+	printf("%d ->",city);
+	ncity = least(city);
+	if(ncity == 999)
+	{
+		cost += a[city][1];
+		printf("1\n");
+		return;
+	}
+	tsp(ncity);
+}
+
 int least(int c)
 {
 	int i, nc = 999, min = 999, kmin;
@@ -28,22 +69,7 @@ int least(int c)
 	return nc;
 }
 
-void tsp(int city)
-{
-	int ncity;
-	visited[city] = 1;
-	printf("%d ->",city);
-	ncity = least(city);
-	if(ncity == 999)
-	{
-		cost += a[city][1];
-		printf("1\n");
-		return;
-	}
-	tsp(ncity);
-}
-
-void nearest_n(city)
+void nearest_n(int city)
 {
 	int min, j, i, u;
 	vs[city] = 1;
@@ -69,26 +95,4 @@ void nearest_n(city)
 	}
 	printf("1\n");
 	sum += a[u][1];
-}
-
-int main(int argc, char *argv[])
-{
-	int i, j;
-
-	printf("Enter the No of Cities\n");
-	scanf("%d", &no);
-
-	printf("\nEnter the Adjacency Matrix\n");
-	for(i = 1; i <= no; i++)
-		for(j = 1; j <=no; j++)
-			scanf("%d", &a[i][j]);
-
-	printf("Using Dynamic Prog\n");
-	tsp(1);
-	printf("Cost is %d\n", cost);
-	printf("Using approx method\n");
-	nearest_n(1);
-	printf("Cost is %d\n", sum);
-	printf ("\nRatio is %f\n", (float)sum/cost);
-	return 0;
 }
