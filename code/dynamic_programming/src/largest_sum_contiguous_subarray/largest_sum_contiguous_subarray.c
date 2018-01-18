be05295395567350e305ca3744a09f565fb776b0
YashovardhanSiramdas <ced16i028@iiitdm.ac.in>
2018-01-18 12:28:36 +0530

    Added Largest sum contiguous subarray.c

---
+++ code/dynamic_programming/src/largest_sum_contiguous_subarray/largest_sum_contiguous_subarray.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int kadanesAlgorithm(int a[], int n)
+{
+	int max_so_far = 0, max_ending_here = 0, i;
+	
+	for (i = 0; i < n; i++) {
+	    max_ending_here = max_ending_here + a[i];
+
+	    if (max_so_far < max_ending_here)
+	        max_so_far = max_ending_here;
+	
+	    if (max_ending_here < 0)
+	        max_ending_here = 0;
+	}
+	return (max_so_far);
+}
+
+int main()
+{
+	int n;
+	printf("Enter size of Array: ");
+	scanf("%d", &n);
+
+	int a[n], i;
+	printf("Enter %d Integers \n", n);
+	for (i = 0; i < n; i++)
+		scanf("%d", &a[i]);
+
+	printf("Largest Contiguous Subarray Sum: %d \n", kadanesAlgorithm(a, n));
+
+	return (0);
+}
