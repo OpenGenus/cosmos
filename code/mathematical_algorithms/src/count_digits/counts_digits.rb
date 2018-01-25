55a13472e510bdc3310be0cf153339806054317e
Frank Kair <frankkair@gmail.com>
2018-01-24 22:53:31 -0200

    Refactors counts digits using rubocop

--- code/mathematical_algorithms/src/count_digits/counts_digits.rb
+++ code/mathematical_algorithms/src/count_digits/counts_digits.rb
@@ -1,10 +1,10 @@
-def count_num(num)    
-    count = 0 
-    while num > 0
-      num = num/10        
-      count += 1
-    end 
-    count
+def count_num(num)
+  count = 0
+  while num > 0
+    num /= 10
+    count += 1
+  end
+  count
 end
 
-print "#{count_num(2765)}"
+print count_num(2765).to_s
