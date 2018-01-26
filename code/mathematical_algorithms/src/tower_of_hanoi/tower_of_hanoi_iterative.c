791fc3f1eb077cb17e301ae8ac3f4f81805655da
GuptaSamridhi <samridhigupta66@gmail.com>
2018-01-26 11:16:35 +0530

    Fix code according to style guide

--- code/mathematical_algorithms/src/tower_of_hanoi/tower_of_hanoi_iterative.c
+++ code/mathematical_algorithms/src/tower_of_hanoi/tower_of_hanoi_iterative.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <math.h>
 
-void toh (int n) {
-    int steps, start, end;
+void 
+toh (int n) {
+    int steps, start, end, i;
     steps = pow (2, n);    // Minimum number of moves required
-    for (int i = 1; i < steps; i ++) {
+    for (i = 1; i < steps; i ++) {
     start = (i & (i-1)) % 3;
     end = (1 + (i | (i-1))) % 3;
     printf ("Disc %d shifted from %d to %d\n", i, start + 1, end + 1);
     }
 }
 
-int main() {
+int
+main() {
     int n;
     printf ("Enter the number of discs : ");
     scanf ("%d", &n);
     toh(n);
-    return 0;
+    return (0);
 }
