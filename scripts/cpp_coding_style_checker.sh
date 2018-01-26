04b3997ab889d9492c3d020ead0b0302019fb94d
Ernest <iattempt.net@gmail.com>
2018-01-26 14:45:46 +0800

    add coding style check script for cpp

---
+++ scripts/cpp_coding_style_checker.sh
@@ -0,0 +1,61 @@
+cwd=$(pwd)
+
+error_times=0
+
+uncrustify_root_path='../third_party/uncrustify'
+uncrustify=$uncrustify_root_path/build/uncrustify
+uncrustify_config_path=$uncrustify_root_path/../uncrustify.cfg
+
+echo
+echo "###########################"
+echo "# Building uncrustify ... #"
+echo "###########################"
+rm -rf $uncrustify_root_path/build
+mkdir $uncrustify_root_path/build
+cd $uncrustify_root_path/build
+cmake ..
+cmake --build .
+
+cd $cwd/..
+
+echo
+echo "###############################"
+echo "# Creating files for diff ... #"
+echo "###############################"
+for cpp_file in `find -name '*.cpp'`
+do
+    # remove the output file if existed to prevent `uncrustify` is not override it
+    if [ -f "$cpp_file.uncrustify" ]; then
+        rm -f $cpp_file.uncrustify
+    fi;
+
+    $uncrustify -c $uncrustify_config_path $cpp_file
+done
+
+echo
+echo "##################"
+echo "# Diff files ... #"
+echo "##################"
+for cpp_file in `find -name '*.cpp'`
+do
+    d=$(diff $cpp_file $cpp_file.uncrustify)
+
+    if [ "$d" != "" ]; then
+        echo "# The \`$cpp_file\` is not passed"
+        echo
+        echo "diff $cpp_file $cpp_file.uncrustify"
+        echo
+        echo "$d"
+        echo
+        error_times=$(($error_times+1))
+    fi;
+done
+
+cd $cwd
+
+if [ $error_times != 0 ]; then
+    echo "$error_times error(s) generated."
+    exit 1
+else
+    echo "Done. \`checking coding style in c++ script\` exited with 0."
+fi;
