3b07270517534a76a83a0720409290123ca92ea8
Anishka0107 <rimjhim0107@gmail.com>
2018-01-27 22:26:12 +0530

    Follow style guide and naming conventions

--- code/data_structures/src/tree/binary_tree/binary_tree/traversal/zigzag/zigzag.cpp
+++ code/data_structures/src/tree/binary_tree/binary_tree/traversal/zigzag/zigzag.cpp
@@ -1,61 +1,70 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <stack>
 
-class Node {
+class Node
+{
     public:
         int info;
-        Node* lc;
-        Node* rc;
-        Node (int info) {
+        Node* left_child;
+        Node* right_child;
+        Node (int info)
+        {
             this->info = info;
-            lc = NULL;
-            rc = NULL;
+            left_child = nullptr;
+            right_child = nullptr;
         }
 };
 
-class BT {
+class BinaryTree
+{
     public:
         Node* root;
-        BT() {
-            root = NULL;
+        BinaryTree()
+        {
+            root = nullptr;
         }
         void zigzag_traversal();
 };
 
-void BT :: zigzag_traversal() {
-    stack<Node*> st1, st2;
+void BinaryTree :: zigzag_traversal()
+{
+    std::stack<Node*> st1, st2;
     st2.push (root);
-    while (!st1.empty() || !st2.empty()) {
-        while (!st1.empty()) {
+    while (!st1.empty() || !st2.empty())
+    {
+        while (!st1.empty())
+        {
             Node* curr = st1.top();
             st1.pop();
-            if (curr->rc)
-                st2.push (curr->rc);
-            if (curr->lc)
-                st2.push (curr->lc);
-            cout << curr->info << " ";
+            if (curr->right_child)
+                st2.push (curr->right_child);
+            if (curr->left_child)
+                st2.push (curr->left_child);
+            std::cout << curr->info << " ";
         }
-        while (!st2.empty()) {
+        while (!st2.empty())
+        {
             Node* curr = st2.top();
             st2.pop();
-            if (curr->lc)
-                st1.push (curr->lc);
-            if (curr->rc)
-                st1.push (curr->rc);
-            cout << curr->info << " ";
+            if (curr->left_child)
+                st1.push (curr->left_child);
+            if (curr->right_child)
+                st1.push (curr->right_child);
+            std::cout << curr->info << " ";
         }
     }
 }
 
-int main() {
-    BT bt;
-    bt.root = new Node (1);
-    bt.root->lc = new Node (2);
-    bt.root->rc = new Node (3);
-    bt.root->lc->lc = new Node (4);
-    bt.root->lc->rc = new Node (5);
-    bt.root->rc->lc = new Node (6);
-    bt.root->rc->rc = new Node (7);
-    bt.zigzag_traversal();
+int main()
+{
+    BinaryTree binary_tree;
+    binary_tree.root = new Node (1);
+    binary_tree.root->left_child = new Node (2);
+    binary_tree.root->right_child = new Node (3);
+    binary_tree.root->left_child->left_child = new Node (4);
+    binary_tree.root->left_child->right_child = new Node (5);
+    binary_tree.root->right_child->left_child = new Node (6);
+    binary_tree.root->right_child->right_child = new Node (7);
+    binary_tree.zigzag_traversal();
     return 0;
 }
