0356568d13a7ce94d6401b7e1bd2f9d6211e1a95
Mithilan Sivanesan <mithilan91@gmail.com>
2018-02-01 12:12:01 -0500

    resolve travici buildlog errors

--- code/cryptography/src/caesar_cipher/caesar_cipher.c
+++ code/cryptography/src/caesar_cipher/caesar_cipher.c
@@ -15,7 +15,7 @@ int main(int argc, char* argv[])
         
         printf("ciphertext: \n");
         
-        for(int i = 0; i < strlen(s); i++)
+        for(int i = 0; i < (int)strlen(s); i++)
         {
             if(isalpha(s[i]))
             {
