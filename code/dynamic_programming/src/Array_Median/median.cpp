a579c93ce173e36affb7b131a73a855c8bf1d718
Puranjay25 <puranjay.makhija@gmail.com>
2018-02-09 15:42:39 +0530

    Update median.cpp
    Made changes in indentation.

--- code/dynamic_programming/src/Array_Median/median.cpp
+++ code/dynamic_programming/src/Array_Median/median.cpp
@@ -3,24 +3,24 @@
 #include<vector>
 using namespace std;
 
-float median(int n, vector <int>arr)
+float median(int n, vector<int> arr)
 {
     if (n % 2 != 0)
         return (float)arr[n / 2];
 
-	return ((float)(arr[(n - 1) / 2] + arr[n / 2]) / 2);
+    return ((float)(arr[(n - 1) / 2] + arr[n / 2]) / 2);
 }
 
 int main()
 {
     int n;
-	cout<<"Enter size of the Array";
-	cin>>n;
-	vector <int>arr(n);
-	cout<<"Enter "<<n<<" integers";
-	for (int i = 0 ; i < n ; i++)
+    cout<<"Enter size of the Array";
+    cin>>n;
+    vector<int> arr(n);
+    cout<<"Enter "<<n<<" integers";
+    for (int i = 0 ; i < n ; i++)
         cin>>arr[i];
-	sort(arr.begin(), arr.end());
-	cout<<"Median= "<<median(n, arr);
-	return 0;
+    sort(arr.begin(), arr.end());
+    cout<<"Median= "<<median(n, arr);
+    return 0;
 }
