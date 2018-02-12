f05cb34b766c33f84cb115361fe426b9e6c861bc
earora97 <earora97@gmail.com>
2018-02-13 01:55:44 +0530

    Comments added

--- code/sorting/src/insertion_sort/insertion_sort.c
+++ code/sorting/src/insertion_sort/insertion_sort.c
@@ -5,26 +5,27 @@ void insertion(int*, int);
 
 int main()
 {
-	int n;
+	int n; // Number of elements
+	printf("Enter number of elements to be sorted\n");
 	scanf("%d", &n);
-	int arr[n];
+	int arr[n]; // Input array of size n
+	printf("Enter the elements:\n");
 	for(int i=0;i<n;i++)
 	{
-		scanf("%d", &arr[i]);
+		scanf("%d", &arr[i]); // Scanning each element of the array
 	}
-	insertion(arr, n);
+	insertion(arr, n); // calling insertion sort. The array is sorted in-place
 	return 0;
 }
 
 void insertion(int arr[], int n)
 {
-	int key;
-	int j;
-	for(int i=1;i<n;i++)
+	int key,j,i;
+	for(i=1;i<n;i++) // We start from first element. Because one element is always sorted. 
 	{
 		key = arr[i];
 		j = i-1;
-		while(j>=0 && arr[j]>key)
+		while(j>=0 && arr[j]>key) //Until we find the position of key
 		{
 			arr[j+1] = arr[j];
 			j--;
@@ -32,9 +33,9 @@ void insertion(int arr[], int n)
 		arr[j+1] = key;
 	}
 	
-	for(int i=0;i<n;i++)
+	for(i=0;i<n;i++) //printing the array
 	{
-		printf("%d\t", arr[i]);
+		printf("%d ", arr[i]);
 	}
 	printf("\n");
 
