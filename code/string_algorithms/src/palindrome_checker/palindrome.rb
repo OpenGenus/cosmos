58aaca4f0e2154fa328908fb4d47210420a410e3
Frank Kair <frankkair@gmail.com>
2018-02-12 19:26:20 -0200

    Corrects ruby palindrome (using open class)

--- code/string_algorithms/src/palindrome_checker/palindrome.rb
+++ code/string_algorithms/src/palindrome_checker/palindrome.rb
@@ -1,7 +1,9 @@
 # Part of Cosmos by OpenGenus Foundation
-def palindrome?
-  self == to_s.reverse
+class String
+  def palindrome?
+    self == to_s.reverse
+  end
 end
 
 puts 'test'.palindrome?
-puts 'haha'.palindrome?
+puts 'hahah'.palindrome?
