edfda09f1fbca05e109e7b8c5d969f9131552972
Frank Kair <frankkair@gmail.com>
2018-02-12 19:46:17 -0200

    Makes factorial an Int extension and added tests

--- code/mathematical_algorithms/src/factorial/factorial.swift
+++ code/mathematical_algorithms/src/factorial/factorial.swift
@@ -1,6 +1,11 @@
-func factorial(n: Int) -> Int {
-    if n <= 1 {
-        return 1
+extension Int {
+    func factorial() -> Int {
+        guard self > 0 else { return 1 }
+        return self * (self - 1).factorial()
     }
-    return n * factorial(n: n - 1)
 }
+
+let x = -5
+print(x.factorial())
+print(0.factorial())
+print(5.factorial())
