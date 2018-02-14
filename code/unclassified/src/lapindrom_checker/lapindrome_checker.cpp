080c4584181d2d3653708d5f2eb0ed28adafc5ea
earora97 <earora97@gmail.com>
2018-02-14 18:30:12 +0530

    cout statement modified

--- code/unclassified/src/lapindrom_checker/lapindrome_checker.cpp
+++ code/unclassified/src/lapindrom_checker/lapindrome_checker.cpp
@@ -25,9 +25,9 @@ int main()
     cout << "Input the string to be checked" << endl;
     cin >> test_case;
     if (isLapindrome(test_case))
-       cout<<"lapindrome\n";
+       cout << test_case << "is a lapindrome.\n";
     else
-       cout<<"not a lapindrome\n";
+       cout << test_case << "is not a lapindrome.\n";
 
     return 0;
 }
