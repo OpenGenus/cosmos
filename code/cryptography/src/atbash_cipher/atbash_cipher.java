820f545abdc691b0cef95c1c155e72ed9bc5449c
Sneha Raina <32961290+SnehaR26@users.noreply.github.com>
2018-02-23 22:27:49 +0530

    Updated atbash_cipher.java file
    Indented the comments in the same line as code. Removed blank lines from conditions.

--- code/cryptography/src/atbash_cipher/atbash_cipher.java
+++ code/cryptography/src/atbash_cipher/atbash_cipher.java
@@ -13,32 +13,25 @@
 		
 			newStr = s;
 		
-		// Converting string to character array
+		        // Converting string to character array
 			char[] n = newStr.toCharArray();
 		
 			for(int i = 0; i < s.length(); i++) {
-			
 				alpha = s.charAt(i);
 			
 				if(Character.isUpperCase(alpha) == true ) { 
-				
 					n[i] = (char) ('Z' - ( alpha - 'A' ));
 			
 				} else if(Character.isLowerCase(alpha) == true ) {
-				
 					n[i] = (char) ( 'z' -( alpha - 'a' ));
 			
 				} else { 
-				
 					n[i] = alpha;
 			      	}
-		
-			}
-		// Converting character array back to string
+		 	}
+			// Converting character array back to string
 			String cipher = new String(n);
 		
 			System.out.println(cipher);
-		
 		}
-	
 	}
