cc031c9d2f26d04ba9344dac78ab7d13b397644e
Mansi Agrawal <mansi.29ag@gmail.com>
2018-02-23 18:49:51 +0530

    Update problem_026.cpp

--- code/online_challenges/src/project_euler/problem_026/problem_026.cpp
+++ code/online_challenges/src/project_euler/problem_026/problem_026.cpp
@@ -1,41 +1,42 @@
 #include <iostream>
 #include <vector>
+
 int main()
 {
 
-   int remainder, value, position, sequenceLength = 0;
+    int remainder, value, position, sequenceLength = 0;
 
-   for (int i = 1000; i > 0; --i)
-   {
+    for (int i = 1000; i > 0; --i)
+    {
 
-       std::vector <int> cycleCheckArray;
+        std::vector<int> cycleCheckArray;
 
-       for (int j = 0; j < i; ++j)
-	       cycleCheckArray.push_back(0);
+        for (int j = 0; j < i; ++j)
+	    cycleCheckArray.push_back(0);
 
-       remainder = 1, value = 1, position = 0;
+        remainder = 1, value = 1, position = 0;
+        while (true)
+	{
 
-       while (true)
-	   {
+	    value = remainder * 10;
+	    remainder = value % i;
 
-	       value = remainder * 10;
-	       remainder = value % i;
+	    if (cycleCheckArray[remainder])
+	    {
+	        sequenceLength = position - cycleCheckArray[remainder];
+	        break;
+	    }
 
-	       if (cycleCheckArray[remainder])
-	       {
-	           sequenceLength = position - cycleCheckArray[remainder];
-	           break;
-	       }
+	    cycleCheckArray[remainder] = position;
+	    ++position;
 
-	       cycleCheckArray[remainder] = position;
-	       position++;
+        }
 
-       }
-
-       if (sequenceLength == i - 1)
-       {
-	       std::cout << i <<"\n";
-	       return 0;
-       }
-   }
+        if (sequenceLength == i - 1)
+        {
+	        std::cout << i <<"\n";
+	        return 0;
+        }
+	    
+    }
 }
