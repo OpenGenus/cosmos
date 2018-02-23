c87051116d3451679b647d7a75283904bf5ee027
mansiag <mansi.29ag@gmail.com>
2018-02-24 00:26:19 +0530

    indentation

--- code/online_challenges/src/project_euler/problem_026/problem_026.cpp
+++ code/online_challenges/src/project_euler/problem_026/problem_026.cpp
@@ -12,31 +12,31 @@ int main()
         std::vector<int> cycleCheckArray;
 
         for (int j = 0; j < i; ++j)
-	    cycleCheckArray.push_back(0);
+        cycleCheckArray.push_back(0);
 
         remainder = 1, value = 1, position = 0;
         while (true)
-	{
+        {
 
-	    value = remainder * 10;
-	    remainder = value % i;
+            value = remainder * 10;
+            remainder = value % i;
 
-	    if (cycleCheckArray[remainder])
-	    {
-	        sequenceLength = position - cycleCheckArray[remainder];
-	        break;
-	    }
+            if (cycleCheckArray[remainder])
+            {
+                sequenceLength = position - cycleCheckArray[remainder];
+                break;
+            }
 
-	    cycleCheckArray[remainder] = position;
-	    ++position;
+            cycleCheckArray[remainder] = position;
+            ++position;
 
         }
 
         if (sequenceLength == i - 1)
         {
-	        std::cout << i <<"\n";
-	        return 0;
+            std::cout << i <<"\n";
+            return 0;
         }
-	    
+        
     }
 }
