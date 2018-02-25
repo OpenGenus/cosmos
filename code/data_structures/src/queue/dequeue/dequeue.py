6e0aaf72b50838c4a81a6ab93244c98b376b2260
varshika03 <varshika03@users.noreply.github.com>
2018-02-25 22:15:10 +0530

    Update dequeue.py

--- code/data_structures/src/queue/dequeue/dequeue.py
+++ code/data_structures/src/queue/dequeue/dequeue.py
@@ -1,9 +1,8 @@
-#  data_structures|queue|dequeue|Python
+# data_structures|queue|dequeue|python
 # Part of Cosmos by OpenGenus Foundation
 
 # Implementation using lists
 class Dequeue:
-
 	def __init__(self, container=None):
 		if container is None:
 			container = []
@@ -22,12 +21,12 @@ class Dequeue:
 		return self.__container.pop()
 
 if __name__ == '__main__':
-	dq = Dequeue();
-	for i in range(1,11):
-		dq.insert_at_front(i);
-		dq.insert_at_rear(-1*i);
+	dq = Dequeue()
+	for i in range(1, 11):
+		dq.insert_at_front(i)
+		dq.insert_at_rear(-1 * i)
 	
-	for i in range(1,11):
+	for i in range(1, 11):
 		print(dq.remove_front())   
 		print(dq.remove_rear())  
 		print('\n')
