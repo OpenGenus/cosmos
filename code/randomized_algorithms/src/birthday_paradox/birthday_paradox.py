ede96beeab2899e2b13f129964858ebf1a0c71a5
Vandana <32227090+vandana1212@users.noreply.github.com>
2018-02-25 12:00:22 +0530

    Updated code
    Fixed spacing

--- code/randomized_algorithms/src/birthday_paradox/birthday_paradox.py
+++ code/randomized_algorithms/src/birthday_paradox/birthday_paradox.py
@@ -5,7 +5,7 @@ import math
 # Function returns number of people that should be present for a given probability, p
 def compute_people(p):
     
-    return math.ceil(math.sqrt(2 * 365 * math.log(1 / (1 - p))));
+    return math.ceil(math.sqrt(2 * 365 * math.log(1 / (1-p))));
     
 # Driver code 
 print (compute_people(0.5))
