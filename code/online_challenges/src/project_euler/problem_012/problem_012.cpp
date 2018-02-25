c0b44694794ebe4ba7929089b0c353250afd44e1
mansiag <mansi.29ag@gmail.com>
2018-02-25 11:31:27 +0530

    update

--- code/online_challenges/src/project_euler/problem_012/problem_012.cpp
+++ code/online_challenges/src/project_euler/problem_012/problem_012.cpp
@@ -1,29 +1,31 @@
 #include<iostream>
-#include<math.h>
+#include<cmath>
 
 int main()
 {
-    int divisorCount=0;
+    int divisorCount = 0;
     int triangleNumberIndex = 0, triangleNumber = 0;
     
     while (divisorCount < 500)
     { 
         divisorCount = 0;
         ++triangleNumberIndex;
-        triangleNumber += j;
-        for(int i = 1; i <= pow(triangleNumber,0.5); ++i)
+        triangleNumber += triangleNumberIndex ;
+
+        for (int i = 1; i <= pow(triangleNumber, 0.5); ++i)
         {
             if (triangleNumber % i ==  0)
-                {
-                    if (i*i == triangleNumber)
-                        divisorCount += 1;
-                    else
-                        divisorCount += 2;
-                }
+            {
+                if (i*i == triangleNumber)
+                    divisorCount += 1;
+
+                else
+                    divisorCount += 2;
+            }
         }
         
     }
 
-    std::cout<< n <<"\n";
+    std:: cout << triangleNumber <<"\n";
     return 0;
 }
