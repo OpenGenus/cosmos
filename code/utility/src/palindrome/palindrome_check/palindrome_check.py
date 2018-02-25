279301366a8b1fa2882d7c23a927e2d22e734291
earora97 <earora97@gmail.com>
2018-02-25 13:41:44 +0530

    syntax fixed

--- code/utility/src/palindrome/palindrome_check/palindrome_check.py
+++ code/utility/src/palindrome/palindrome_check/palindrome_check.py
@@ -1,8 +1,8 @@
 def isPalindrome(str):
  
     # Run loop from 0 to len/2 
-    for i in range(0, len(str)//2) : 
-        if str[i] != str[len(str)-i-1] :
+    for i in range(0, len(str) // 2): 
+        if str[i] != str[len(str) - i - 1]:
             return False
     return True
 
