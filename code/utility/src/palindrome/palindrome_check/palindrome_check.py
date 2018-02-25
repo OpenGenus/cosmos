6846f0d5fe8871eec35cee5d01846d09fcf75acb
earora97 <earora97@gmail.com>
2018-02-25 12:55:35 +0530

    Changes made to syntax

--- code/utility/src/palindrome/palindrome_check/palindrome_check.py
+++ code/utility/src/palindrome/palindrome_check/palindrome_check.py
@@ -1,16 +1,18 @@
 def isPalindrome(str):
  
     # Run loop from 0 to len/2 
-    for i in xrange(0, len(str)/2): 
-        if str[i] != str[len(str)-i-1]:
+    for i in range(0, len(str)//2) : 
+        if str[i] != str[len(str)-i-1] :
             return False
     return True
+
+
 if __name__ == '__main__':
 	print("Give input string:")
-	s = raw_input()
+	s = input()
 	ans = isPalindrome(s)
 	 
-	if (ans):
+	if ans:
 	    print("Yes " + s + " is a palindrome")
 	else:
 	    print("No " + s + " is not a palindrome")
