b05b2ce41b0328bf1c6ff5694b6e0a4754e2c1a5
Vandana <32227090+vandana1212@users.noreply.github.com>
2018-02-25 11:56:53 +0530

    Update longest_repeating_subsequence.cpp

--- code/dynamic_programming/src/longest_repeating_subsequence/longest_repeating_subsequence.cpp
+++ code/dynamic_programming/src/longest_repeating_subsequence/longest_repeating_subsequence.cpp
@@ -9,7 +9,7 @@
 int longestRepeatingSubsequence(std::string s)
 {
     int n = s.size();  // Obtaining the length of the string
-    std::vector<std::vector<int> >dp( n+1, std::vector<int>(n+1, 0) );
+    std::vector<std::vector<int>>dp(n + 1, std::vector<int>(n + 1, 0));
 
     // Implementation is very similar to Longest Common Subsequence problem
     for (int x = 1; x <= n; ++x)
