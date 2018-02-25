f3ab2a17f3b634edccfb240dbac90d74972925fe
Vandana <32227090+vandana1212@users.noreply.github.com>
2018-02-25 22:31:55 +0530

    Removed new line
    Removed new line

--- code/bit_manipulation/src/addition_using_bits/addition_using_bits.py
+++ code/bit_manipulation/src/addition_using_bits/addition_using_bits.py
@@ -2,7 +2,6 @@
 
 # Function adds 2 numbers a and b without using + operator
 def addition(a, b):
-
     while (b != 0):
         c = a & b   # carry contains set bits of both a and b
         a = a ^ b   # a ^ b is equal to 1 if, the bits of a and b are different
