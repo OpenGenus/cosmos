4d87874a26fd9b5f4a3f3f98238c956c55b13b9e
mansiag <mansi.29ag@gmail.com>
2018-02-26 20:13:28 +0530

    removed tab spaces

--- code/mathematical_algorithms/src/newman_conway/Newman_Conway_Sequence.cpp
+++ code/mathematical_algorithms/src/newman_conway/Newman_Conway_Sequence.cpp
@@ -3,19 +3,19 @@
 
 int nthNewmanConwayNumber(int number)
 {
-	std::vector<int> arr;
-	arr.push_back(0);
-	arr.push_back(1);
-	arr.push_back(1);
+    std::vector<int> arr;
+    arr.push_back(0);
+    arr.push_back(1);
+    arr.push_back(1);
     for(size_t i = 3; i <= number; ++i)
-		arr.push_back(arr[arr[i-1]] + arr[i - arr[i-1]]);
+        arr.push_back(arr[arr[i-1]] + arr[i - arr[i-1]]);
     return arr[number];
 }
 
 int main()
 {
-	int number;
+    int number;
     std::cout << "Enter number:\n";
-	std::cin >> number;
+    std::cin >> number;
     std::cout << nthNewmanConwayNumber(number) << "\n"; 
 }
