d11053a890b7d3e19fec0ab611d865685b982ec5
parulagg27 <parulagg27@gmail.com>
2018-02-28 04:22:22 +0530

    Changed file location

--- code/sorting/src/comb_sort/comb_sort.py
+++ code/sorting/src/comb_sort/comb_sort.py
@@ -2,17 +2,17 @@ def combsort(array):
   gap = len(array)
   swap = True
   while gap > 1 or swap:
-    gap = max(1, int(gap / 1.25))  
+    gap = max(1, int(gap / 1.25))
     swap = False
     for i in range(len(array) - gap):
-      j = i+gap
+      j = i + gap
       if array[i] > array[j]:
         array[i], array[j] = array[j], array[i]
         swap = True
- 
-numbers = [3,4,7,2,4,1,6,8,9,10]
-print(numbers)
- 
-combsort(numbers)
+
+
+numbers = [3, 4, 7, 2, 4, 1, 6, 8, 9, 10]
 print(numbers)
 
+combsort(numbers)
+print(numbers)
