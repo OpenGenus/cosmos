48d6d769058b776574d0ec88f9b831336cf8e314
sggoyal1994 <32759884+sggoyal1994@users.noreply.github.com>
2018-02-28 17:22:51 +0530

    Update Palindrome.py

--- code/utility/src/palindrome/palindrome_check/Palindrome.py
+++ code/utility/src/palindrome/palindrome_check/Palindrome.py
@@ -2,7 +2,7 @@
 def main():
     """
     Determine whether or not a string is a palindrome.
-    :return: 'That's a palindrome.' if the string is a palindrome,
+    :print: 'That's a palindrome.' if the string is a palindrome,
              'That isn't a palindrome' otherwise.
     """
     input_str = input("Enter a string: ")
@@ -26,7 +26,7 @@ def is_palindrome(string):
         return True
 
     # check if the characters at each index are the same
-    if string[0] == string[len(string) - 1]:
+    if string[0] == string[-1]:
 
         # if it gets to this statement, it's a palindrome
         return is_palindrome(string[1:len(string) - 1])
