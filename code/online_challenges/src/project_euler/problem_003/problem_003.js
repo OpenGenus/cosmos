e80ef973fcd154770c3c76ac6caf6e27da77269a
Arnav Borborah <arnavborborah11@gmail.com>
2018-03-01 07:42:10 -0500

    Added Problem #3 in JS

---
+++ code/online_challenges/src/project_euler/problem_003/problem_003.js
@@ -0,0 +1,13 @@
+let num = 600851475143;
+let highestPrimeFactor = 0;
+let currentFactor = 2;
+
+while (num !== 1) {
+  if (num % currentFactor === 0 && currentFactor > highestPrimeFactor) {
+    highestPrimeFactor = currentFactor;
+    num /= currentFactor;
+  }
+  ++currentFactor;
+}
+
+console.log(highestPrimeFactor);
