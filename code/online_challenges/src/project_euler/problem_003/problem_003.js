c1bdc2f6885a67675ed5211ba165d3d655aa88f4
arnavb11 <arnavborborah11@gmail.com>
2018-03-02 21:00:31 +0000

    Fixed algorithm

--- code/online_challenges/src/project_euler/problem_003/problem_003.js
+++ code/online_challenges/src/project_euler/problem_003/problem_003.js
@@ -2,15 +2,15 @@ let num = 600851475143;
 let highestPrimeFactor = 0;
 let currentFactor = 2;
 
-while (num !== 1) {
-  if (num % currentFactor === 0 && currentFactor > highestPrimeFactor) {
+while (num > highestPrimeFactor) {
+  if (num % currentFactor === 0) {
     highestPrimeFactor = currentFactor;
     num /= currentFactor;
-    currentFactor = 2;
   }
   else {
-    ++currentFactor; 
+    ++currentFactor;
   }
+  
 }
 
 console.log(highestPrimeFactor);
