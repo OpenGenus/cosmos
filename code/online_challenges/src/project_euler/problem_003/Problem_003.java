98e84fdaed0ee201534187db20b8e365fb57943e
Garvit Kothari <31393258+Garvit-k@users.noreply.github.com>
2018-03-04 01:45:58 +0530

    Java implementation of problem 003

---
+++ code/online_challenges/src/project_euler/problem_003/Problem_003.java
@@ -0,0 +1,25 @@
+package Problem_003;
+
+public class Problem_003 {
+
+	public static long smallestFactor(long n) {
+		if (n <= 1)
+			throw new IllegalArgumentException();
+		for (long i = 2 ;i <= (long)Math.sqrt(n); i++) {
+			if (n % i == 0)
+				return i;
+			}
+		return n;
+		}
+	public static void main(String []args) {
+		long n = 600851475143L;
+		long big = 1;
+		while(big != n) {
+			big = smallestFactor(n);
+			if (big < n)
+				n /= big;
+			else
+				System.out.println(big); //6857
+		}
+	}
+}
