6b1dfcfd746d98d7f6a414fb36bff951c4694781
mansiag <mansi.29ag@gmail.com>
2018-03-03 21:32:49 +0530

    Added projectEuler 67 in python

---
+++ code/online_challenges/src/project_euler/problem_067/problem_067.py
@@ -0,0 +1,11 @@
+def main():
+	prob = []
+	for i in range(100):
+		prob.append([int(x) for x in input().strip().split()])
+	for i in range(98,-1,-1):
+		for j in range(len(prob[i])):
+			prob[i][j] += max(prob[i + 1][j],prob[i + 1][j + 1])
+	print (prob[0][0])
+
+if __name__ == '__main__':
+	main()
