0f1b180cf72261c8c323866df067f11809d5675c
Yashovardhan Siramdas <ced16i028@iiitdm.ac.in>
2018-03-04 17:47:55 +0530

    Fixed some codes according to Style Guide

--- code/divide_conquer/src/quick_sort/quick_sort.c
+++ code/divide_conquer/src/quick_sort/quick_sort.c
@@ -4,7 +4,8 @@
 #include<stdio.h>
 
 // A utility function to swap two elements
-void swap(int* a, int* b)
+void 
+swap(int* a, int* b)
 {
     int t = *a;
     *a = *b;
@@ -16,7 +17,8 @@ void swap(int* a, int* b)
     array, and places all smaller (smaller than pivot)
    to left of pivot and all greater elements to right
    of pivot */
-int partition (int arr[], int low, int high)
+int 
+partition (int arr[], int low, int high)
 {
     int pivot = arr[high];    // pivot
     int i = (low - 1);  // Index of smaller element
@@ -39,7 +41,8 @@ int partition (int arr[], int low, int high)
  arr[] --> Array to be sorted,
   low  --> Starting index,
   high  --> Ending index */
-void quickSort(int arr[], int low, int high)
+void 
+quickSort(int arr[], int low, int high)
 {
     if (low < high)
     {
@@ -55,21 +58,23 @@ void quickSort(int arr[], int low, int high)
 }
 
 /* Function to print an array */
-void printArray(int arr[], int size)
+void 
+printArray(int arr[], int size)
 {
     int i;
-    for (i=0; i < size; i++)
+    for (i = 0; i < size; i++)
         printf("%d ", arr[i]);
     printf("n");
 }
 
 // Driver program to test above functions
-int main()
+int 
+main()
 {
     int arr[] = {10, 7, 8, 9, 1, 5};
     int n = sizeof(arr)/sizeof(arr[0]);
-    quickSort(arr, 0, n-1);
+    quickSort(arr, 0, n - 1);
     printf("Sorted array: n");
     printArray(arr, n);
-    return 0;
+    return (0);
 }
--- code/dynamic_programming/src/longest_increasing_subsequence/longest_increasing_subsequence.c
+++ code/dynamic_programming/src/longest_increasing_subsequence/longest_increasing_subsequence.c
@@ -3,31 +3,33 @@
 #include<stdio.h>
 
 // Max function 
-int max(int a,int b)
+int 
+max(int a, int b)
 {
-    if( a > b)
-        return a;
-    return b;
+    return (a > b ? a : b);
 }
 
 // O(n^2) approach
-int lis(int arr[], int n)
+int 
+lis(int arr[], int n)
 {
     int dp[n], ans = 0 , i = 0, j = 0;
-    for(i = 0; i < n; ++i)
-    {
+    for (i = 0; i < n; ++i) {
         dp[i] = 1;
-        for(j = 0; j < i; j++)
+        for (j = 0; j < i; j++)
             if(arr[j] < arr[i])
                 dp[i] = max(dp[i], 1 + dp[j]);
         ans = max(ans, dp[i]);
     }
-    return ans;
+    return (ans);
 }
 
-int main() {
+int 
+main() 
+{
+    int arr[] = {10, 22, 9, 33, 21, 50, 41, 60};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("LIS is : %d\n",lis(arr, n));
 
-    int arr[8] = {10, 22, 9, 33, 21, 50, 41, 60};
-    printf("LIS is : %d\n",lis(arr, 8));
-    return 0;
+    return (0);
 }
