d584b46fe0bff8d974c73ba954f56e9676222ab5
KtlTheBest <dbatyr09@gmail.com>
2018-03-04 16:11:13 +0600

    Update centroid_decompostition.cpp
    Hello, @arnavb. I tried to do as you said and correct the mistakes you pointed out, but I couldn't resolve the pointers thing. I am new to OO programming and Github as whole, so I hope you could help me to learn and code better. Thanks

--- centroid_decompostition.cpp
+++ centroid_decompostition.cpp
@@ -1,70 +1,92 @@
 #include <iostream>
-#include <list>
+#include <vector>
 
 using namespace std;
 
-struct Graph 
+class Graph 
 {
-	int V;
-	list<int> *adj;
-	int *size;
-	bool *deleted;
-
-	Graph (int num) : V(num) {
-		adj = new list<int>[V];
-		size = new int[V];
-		deleted = new bool[V];	
+private:
+	int ver;
+	vector<int>* adj;
+	vector<int>* centroidTree;
+	int* sizes;
+	bool* deleted;
+public:
+	Graph(int num) : ver(num) {
+		adj = new vector<int>(ver + 1);
+		centroidTree = new vector<int>(ver + 1);
+		sizes = new int[ver + 1];
+		deleted = new bool[ver + 1];	
 	}
+	
+	~Graph();
+	
+	void addEdge(int x, int y, vector<int> arr[]);
 
-	void addEdge (int x, int y);
+	void calcSizes(int cur, int par);
 
-	void calcSizes (int cur, int par);
+	int findCentroid(int cur, int par, int vertices);
 
-	int findCentroid (int cur, int par, int vertices);
-
-	void decomposeTree (int cur, int total);
+	void decomposeTree(int cur, int total);
 };
 
-void Graph::addEdge (int x, int y)
+Graph::~Graph()
 {
-	adj[x].push_back(y);
+	delete []adj;
+	delete []sizes;
+	delete []deleted;
 }
 
-void Graph::calcSizes (int cur, int par)
+void Graph::addEdge(int x, int y, vector<int> arr[])
 {
-	size[cur] = 1;
-	list<int>::iterator i;
-	for (i = adj[cur].begin(); i != adj[cur].end(); ++ i)
-	{
-		if (*i == par || deleted[*i] == true) continue;
-		calcSizes (*i, cur);
-		size[cur] += size[*i];
-	}
+	arr[x].push_back(y);
 }
 
-int Graph::findCentroid (int cur, int par, int vertices){
-	list<int>::iterator i;
-	for (i = adj[cur].begin(); i != adj[cur].end(); ++ i)
+void Graph::calcSizes(int cur, int par)
+{
+	sizes[cur] = 1;
+	for (int i = 0; i < adj[cur].size(); ++ i)
 	{
-		if (*i == par || deleted[*i] == true) continue;
-		if (size[*i] > vertices / 2)
+		int to = adj[cur][i];
+		if (!(to == par || deleted[to] == true))
 		{
-			return findCentroid (*i, cur, vertices);
+			calcSizes (to, cur);
+			sizes[cur] += sizes[to];
 		}
 	}
-	return cur;
 }
 
-void Graph::decomposeTree (int cur, int total)
+int Graph::findCentroid(int cur, int par, int vertices){
+	for (int i = 0; i < adj[cur].size(); ++ i)
+	{
+		int to = adj[cur].size();
+		if (!(to == par || deleted[to] == true))
+		{
+			if (sizes[to] > vertices / 2)
+			{
+				return findCentroid(to, cur, vertices);
+			}
+		}
+		return cur;
+	}
+}
+
+void Graph::decomposeTree(int cur, int total)
 {
 	calcSizes (cur, -1);
-	int centroid = findCentroid (cur, -1, size[cur]);
+	int centroid = findCentroid (cur, -1, sizes[cur]);
 	calcSizes (centroid, -1);
 	deleted[centroid] = true;
-	list<int>::iterator i;
-	for(i = adj[centroid].begin(); i != adj[centroid].end(); ++ i)
+	for (int i = 0; i < adj[centroid].size(); ++ i)
 	{
-		if (deleted[*i] == true) continue;
-		decomposeTree(*i, size[*i]);
+		int to = adj[centroid][i];
+		if (deleted[to] == true) continue;
+		decomposeTree(to, sizes[to]);
+		addEdge(cur, to, centroidTree);
 	}
+}
+
+int main()
+{
+	return 0;	
 }
