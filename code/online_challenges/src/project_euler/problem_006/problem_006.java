6a40762966a9d0094586229f3f0d7806966b9a7f
Awani-Shukla <31660543+Awani-Shukla@users.noreply.github.com>
2018-03-05 14:39:09 +0530

    Update problem_006.java

--- code/online_challenges/src/project_euler/problem_006/problem_006.java
+++ code/online_challenges/src/project_euler/problem_006/problem_006.java
@@ -6,7 +6,7 @@ class Problem006{
 	public static void main(String[] args) {
         int sum = 0;
         int sqsum = 0;
-        for (int i= 1; i <= 100; i ++) {
+        for (int i = 1; i <= 100; i ++) {
             sqsum += i * i;
             sum += i;
         }
