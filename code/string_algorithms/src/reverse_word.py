7c54eefe5146c6731f70caba2bb86f99c8100280
Awani-Shukla <31660543+Awani-Shukla@users.noreply.github.com>
2018-03-06 01:59:19 +0530

    Update reverse_word.py

--- code/string_algorithms/src/reverse_word.py
+++ code/string_algorithms/src/reverse_word.py
@@ -7,14 +7,14 @@ def reverse(array, i, j):
 def reverse_words(string):
     arr = list(string)
     n = len(arr)
-    reverse(arr, 0, n-1)
+    reverse(arr, 0, n - 1)
     start = None
     for i in range(n):
         if arr[i] == " ":
             if start is not None:
-                reverse(arr, start, i-1)
+                reverse(arr, start, i - 1)
                 start = None
-        elif i == n-1:
+        elif i == n - 1:
             if start is not None:
                 reverse(arr, start, i)
         else:
