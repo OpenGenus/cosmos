335fb85f2b518401a18c866e4824e71d5fa42306
Awani-Shukla <31660543+Awani-Shukla@users.noreply.github.com>
2018-03-06 01:53:53 +0530

    Update make_sentence.py

--- code/string_algorithms/src/make_sentence.py
+++ code/string_algorithms/src/make_sentence.py
@@ -1,20 +1,18 @@
 count = 0
 
-def make_sentence(str_piece, dictionarys):
+def make_sentence(str_piece, dictionaries):
     global count
     if len(str_piece) == 0:
         return True
     for i in range(0, len(str_piece)):
         prefix, suffix = str_piece[0:i], str_piece[i:]
-        if ((prefix in dictionarys and suffix in dictionarys)
-                or (prefix in dictionarys
-                    and make_sentence(suffix, dictionarys))):
+        if ((prefix in dictionaries and suffix in dictionaries)
+                or (prefix in dictionaries
+                    and make_sentence(suffix, dictionaries))):
             count += 1
     return True
-
-
 if __name__ == "__main__":
-    dictionarys = ["", "app", "let", "t", "apple", "applet"]
+    dictionaries = ["", "app", "let", "t", "apple", "applet"]
     word = "applet"
-    make_sentence(word, dictionarys)
+    make_sentence(word, dictionaries)
 print count
