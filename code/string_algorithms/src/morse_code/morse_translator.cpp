e6051ebe8e4f1849f7e12f881690dd0a3bf37886
shobhit <jshobhit141@gmail.com>
2018-03-05 16:28:47 +0530

    modified indentation

--- code/string_algorithms/src/morse_code/morse_translator.cpp
+++ code/string_algorithms/src/morse_code/morse_translator.cpp
@@ -3,49 +3,50 @@
 
 using namespace std;
 
-string wordToMorse(string input, std::map<string,string> mp, map<string,string>::iterator it);
-string morseToWord(string input, std::map<string,string> mp, map<string,string>::iterator it);
+string wordToMorse(string input, map<string,string> mp, map<string,string>::iterator it);
+string morseToWord(string input, map<string,string> mp, map<string,string>::iterator it);
 
-int main() {
-	map<string,string>key;
-	map<string,string> :: iterator it;
+int main() 
+{
+    map <string,string> key;
+    map <string,string> :: iterator it;
 
-	key["A"] = ".-";
-	key["B"] = "-...";
-	key["C"] = "-.-.";
-	key["D"] = "-..";
-	key["E"] = ".";
-	key["F"] = "..-.";
-	key["G"] = "--.";
-	key["H"] = "....";
-	key["I"] = "..";
-	key["J"] = ".---";
-	key["K"] = "-.-";
-	key["L"] = ".-..";
-	key["M"] = "--";
-	key["N"] = "-.";
-	key["O"] = "---";
-	key["P"] = ".--.";
-	key["Q"] = "--.-";
-	key["R"] = ".-.";
-	key["S"] = "...";
-	key["T"] = "-";
-	key["U"] = "..-";
-	key["V"] = "...-";
-	key["W"] = ".--";
-	key["X"] = "-..-";
-	key["Y"] = "-.--";
-	key["Z"] = "--..";
-	key["1"] = ".----";
-	key["2"] = "..---";
-	key["3"] = "...--";
-	key["4"] = "....-";
-	key["5"] = ".....";
-	key["6"] = "-....";
-	key["7"] = "--...";
-	key["8"] = "---..";
-	key["9"] = "----.";
-	key["0"] = "-----";
+    key["A"] = ".-";
+    key["B"] = "-...";
+    key["C"] = "-.-.";
+    key["D"] = "-..";
+    key["E"] = ".";
+    key["F"] = "..-.";
+    key["G"] = "--.";
+    key["H"] = "....";
+    key["I"] = "..";
+    key["J"] = ".---";
+    key["K"] = "-.-";
+    key["L"] = ".-..";
+    key["M"] = "--";
+    key["N"] = "-.";
+    key["O"] = "---";
+    key["P"] = ".--.";
+    key["Q"] = "--.-";
+    key["R"] = ".-.";
+    key["S"] = "...";
+    key["T"] = "-";
+    key["U"] = "..-";
+    key["V"] = "...-";
+    key["W"] = ".--";
+    key["X"] = "-..-";
+    key["Y"] = "-.--";
+    key["Z"] = "--..";
+    key["1"] = ".----";
+    key["2"] = "..---";
+    key["3"] = "...--";
+    key["4"] = "....-";
+    key["5"] = ".....";
+    key["6"] = "-....";
+    key["7"] = "--...";
+    key["8"] = "---..";
+    key["9"] = "----.";
+    key["0"] = "-----";
     key[".-"] = "A";
     key["-..."] = "B";
     key["-.-."] = "C";
@@ -83,37 +84,46 @@ int main() {
     key["----."] = "9";
     key["-----"] = "0";
 
-    cout << morseToWord(".-- .... -.-- / .. / .- -- / .... . .-. .",key,it) << endl;
-    cout << wordToMorse("opengeNuS",key,it) << endl;
+    cout << morseToWord(".-- .... -.-- / .. / .- -- / .... . .-. .", key, it) << endl;
+    cout << wordToMorse("opengeNuS", key, it) << endl;
 
 	return 0;
 }
-string wordToMorse(string input, std::map<string,string> mp, map<string,string>::iterator it) {
-	string encoded = "", a = "";
-	for(int i = 0; i < input.length(); i++) {
+
+string wordToMorse(string input, map<string,string> mp, map<string,string>::iterator it) 
+{
+	string encoded = "", temp = "";
+	for(int i = 0; i < input.length(); i++) 
+	{
 		if(input[i] == ' ')
 			encoded += '/';
-		else {
+		else 
+		{
 			if(islower(input[i]))
 				input[i] = toupper(input[i]);
-			a  = input[i];
-			it = mp.find(a);
+			temp  = input[i];
+			it = mp.find(temp);
 			encoded += it->second + " ";
 		}
 	}
 	return encoded;
 }
-string morseToWord(string input, std::map<string,string> mp, map<string,string>::iterator it) {
+
+string morseToWord(string input, const *map<string,string> mp, const *map<string,string>::iterator it) 
+{
 	input += " ";
-	string decoded = "",a = "";
-	for(int i = 0; i < input.length(); i++) {
-		if(input[i] != ' ' && input[i] != '/') {
-			a += input[i];
+	string decoded = "",temp = "";
+	for(int i = 0; i < input.length(); i++) 
+	{
+		if(input[i] != ' ' && input[i] != '/') 
+		{
+			temp += input[i];
 		}
-		else if(input[i] == ' ') {
-			it = mp.find(a);
+		else if(input[i] == ' ') 
+		{
+			it = mp.find(temp);
 			decoded += it->second;	
-			a = "";
+			temp = "";
 		}
 		else if(input[i] == '/')
 			decoded += " ";
