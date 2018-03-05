8baf1f68097ba4b27619e304b442c1b5e5eb71f4
mansiag <mansi.29ag@gmail.com>
2018-03-05 08:16:08 +0530

    changes

--- code/online_challenges/src/project_euler/problem_020/problem_020.py
+++ code/online_challenges/src/project_euler/problem_020/problem_020.py
@@ -1,8 +1,10 @@
 factorial = 1
 for i in range(100):
 	factorial *= (i + 1)
-sum = 0
+digit_sum = 0
+
 while factorial > 0:
-	sum += factorial % 10
+	digit_sum += factorial % 10
 	factorial = factorial // 10
-print(sum)
+
+print(digit_sum)
