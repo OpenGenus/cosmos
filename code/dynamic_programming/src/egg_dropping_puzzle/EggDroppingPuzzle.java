fc665bd3b4a9f7c51eb99271d1a4380a3f9ff37d
Nida <24700264+Nidakhan786@users.noreply.github.com>
2018-03-06 19:35:42 +0530

    Update EggDroppingPuzzle.java

--- code/dynamic_programming/src/egg_dropping_puzzle/EggDroppingPuzzle.java
+++ code/dynamic_programming/src/egg_dropping_puzzle/EggDroppingPuzzle.java
@@ -12,10 +12,10 @@ public class EggDroppingPuzzle
 	private static int minTrials(int a, int b)
 	{
 	    
-		int eggFloor[][] = new int[a+1][b+1];
+		int eggFloor[][] = new int[a + 1][b + 1];
 		int result, x;
 		
-		for (int i = 1; i <= a; i++)
+		for (int i = 1; i <= a; ++i)
 		{
 		    eggFloor[i][0] = 0;   // Zero trial for zero floor.
 		    eggFloor[i][1] = 1;   // One trial for one floor 
@@ -23,18 +23,18 @@ public class EggDroppingPuzzle
 		
 	        // j trials for only 1 egg
 		
-                for (int j = 1; j <= b; j++)
+                for (int j = 1; j <= b; ++j)
 			 eggFloor[1][j] = j;
 		
 	
-   		 for (int i = 2; i <= a; i++)
+   		 for (int i = 2; i <= a; ++i)
 		  {
-			for (int j = 2; j <= b; j++)
+			for (int j = 2; j <= b; ++j)
 			{
 				eggFloor[i][j] = Integer.MAX_VALUE;
-				for (x = 1; x <= j; x++)
+				for (x = 1; x <= j; ++x)
 				{   // get worst case from:case in which egg break (eggFloor[i-1][x-1]) and  case in which egg does not break (eggFloor[i][j-x]).
-					result = 1 + Math.max(eggFloor[i-1][x-1], eggFloor[i][j-x]);  
+					result = 1 + Math.max(eggFloor[i - 1][x - 1], eggFloor[i][j - x]);  
           
 					//choose min of all values for particular x
         				 if (result < eggFloor[i][j])
@@ -48,17 +48,16 @@ public class EggDroppingPuzzle
 		
 	//testing the program
 	public static void main(String args[])
-	{
-	     int a,b;
+	{   
 	Scanner	sc = new Scanner(System.in);
 	System.out.println("Enter no. of eggs");
 	
-		a = Integer.parseInt(sc.nextLine());
+		int a = Integer.parseInt(sc.nextLine());
 		System.out.println("Enter no. of floors");
 		
-		b = Integer.parseInt(sc.nextLine());
+		int b = Integer.parseInt(sc.nextLine());
               //result outputs min no. of trials in worst case for a eggs and b floors
-              int result = minTrials(a, b);
-	      System.out.println("Minimum number of attempts needed in Worst case with a eggs and b floor are: " + result); 
+                int result = minTrials(a, b);
+	        System.out.println("Minimum number of attempts needed in Worst case with a eggs and b floor are: " + result); 
 	}
 }
