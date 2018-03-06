db365acafd450d44af992a12207283f299c8816f
Nida <24700264+Nidakhan786@users.noreply.github.com>
2018-03-06 22:26:02 +0530

    Update RodCutting.java

--- code/dynamic_programming/src/rod_cutting/RodCutting.java
+++ code/dynamic_programming/src/rod_cutting/RodCutting.java
@@ -2,19 +2,20 @@
   Returns the best obtainable price for a rod of length a and
    price[] as prices of different pieces */
 
-public class RodCutting {
-	
+public class RodCutting
+{
 	private static int cutRod(int price[] , int a)
 	{
-		int val[] = new int[a + 1];
+         	int val[] = new int[a + 1];
 		val[0] = 0;
-
 		for (int i = 1; i <= a; ++i)
 		{
 			int max_val = Integer.MIN_VALUE;
 			for (int j = 0; j < i; ++j)
+			{
 				max_val = Math.max(max_val,price[j] + val[i-j-1]);
                                 val[i] = max_val;
+			}
 		}
 
 		return val[a];
@@ -23,9 +24,8 @@ public class RodCutting {
 	//Testing Program 
 	public static void main(String args[])
 	{
-		int arr[] = new int[] {1, 5, 17, 18, 21};
+		int arr[] =  {1, 5, 17, 18, 21};
 		int size = arr.length;
-		System.out.println("Maximum Obtainable Value is " +
-							cutRod(arr, size));
+		System.out.println("Maximum Obtainable Value is " + cutRod(arr, size));
 	}
 }
