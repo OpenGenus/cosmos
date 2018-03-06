2d56a8231e50f3f7e899badd48d72e372f68cb0c
Nida <24700264+Nidakhan786@users.noreply.github.com>
2018-03-06 18:35:23 +0530

    Update Biggest_of_n_numbers.java

--- code/unclassified/src/biggest_of_n_numbers/Biggest_of_n_numbers.java
+++ code/unclassified/src/biggest_of_n_numbers/Biggest_of_n_numbers.java
@@ -1,22 +1,23 @@
 import java.util.Scanner;
 
-public class Biggest_of_n_numbers
-  {
-  public static void main(String argn[]){
-    Scanner data = new Scanner(System.in);
-    int n, i, temp, max;
-    System.out.print("Enter numbers of elements");
-    n = data.nextInt();
-    System.out.println("Enter " +n+ " numbers");
-    max = data.nextInt();
-    for (i = 1; i < n; ++i) 
-{ 
-    temp = data.nextInt();
-    if(temp < max)
-    continue;
-    else
-    max = temp;
-}
-   System.out.println("Maximum is "+max);
-}
-}
+public class BiggestOfNNumbers
+{
+       public static void main(String[] args)
+       {
+            Scanner data = new Scanner(System.in);
+            System.out.print("Enter numbers of elements");
+            int n = data.nextInt();
+            System.out.println("Enter " +n+ " numbers");
+            int max = data.nextInt();
+         
+            for (int i = 1; i < n; ++i)
+            { 
+                  int temp = data.nextInt();
+                  if(temp < max)
+                  continue;
+                  else
+                  max = temp;
+            }
+              System.out.println("Maximum is "+max);
+        }
+ }
