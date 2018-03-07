50cb63b727c6b75dd9c78996364833433bf732fe
Nida <24700264+Nidakhan786@users.noreply.github.com>
2018-03-07 20:11:38 +0530

    Update EggDroppingPuzzle.java

--- code/dynamic_programming/src/egg_dropping_puzzle/EggDroppingPuzzle.java
+++ code/dynamic_programming/src/egg_dropping_puzzle/EggDroppingPuzzle.java
@@ -35,7 +35,7 @@ public class EggDroppingPuzzle {
 		
 	//testing the program
    	 public static void main(String args[]) {   
-       		Scanner	sc = new Scanner(System.in);
+       		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter no. of eggs");
 	
 		int a = Integer.parseInt(sc.nextLine());
