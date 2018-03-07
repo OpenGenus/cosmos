1b0cee041514f39f1a654949906afae3dd3b580b
Shreya Anand <iashreya@hotmail.com>
2018-03-07 20:22:14 +0530

    Update PalindromeInASinglyLinkedList.c

--- code/data_structures/src/list/singly_linked_list/operations/PalindromeInASinglyLinkedList.c
+++ code/data_structures/src/list/singly_linked_list/operations/PalindromeInASinglyLinkedList.c
@@ -4,23 +4,20 @@
 struct node
 {
     int  info;
-    struct node*  link;
+    struct node* link;
 };
-
 //Function for creating the linked list
-
-    struct node* create(struct node*  start)
+struct node* create(struct node*  start) 
 {
     int proceed = 1;
-    struct node*  temp, *ptr;
-
+    struct node* temp, *ptr;
     while(proceed == 1)
     {
         temp = (struct node *)malloc(sizeof(struct node));
         temp->link = NULL;
         puts("Enter info") ;
         scanf("%d", &temp->info);
-       if(start == NULL)
+        if(start == NULL)
         {
             start = temp;
             ptr = temp;
@@ -36,7 +33,6 @@ struct node
 
     return(start);
 }
-
 //Function to check if the list forms a palindrome or not
 struct node* check(struct node *ptr_bck, struct node *ptr_fwd, int *f)
 {
@@ -47,12 +43,13 @@ struct node* check(struct node *ptr_bck, struct node *ptr_fwd, int *f)
 
     if(ptr_bck->info != ptr_fwd->info)
         *f = 0 ;
-
+    
     return(ptr_fwd->link);
 }
 
 int main(void)
-{   struct node *start = NULL;
+{   
+    struct node *start = NULL;
     int flag = 1, *f = &flag;
 
     start = create(start);
