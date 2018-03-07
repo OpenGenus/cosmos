338e5a8429d9357843afbf86ef1dd443e062c1f9
Garvit Kothari <31393258+Garvit-k@users.noreply.github.com>
2018-03-07 22:03:19 +0530

    Update leap_year.java

--- code/unclassified/src/leap_year/leap_year.java
+++ code/unclassified/src/leap_year/leap_year.java
@@ -1,24 +1,19 @@
-public class LeapYear {
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+class LeapYear {
 	public static boolean isLeapYear(int year) {
-		if(year % 4 == 0)
-			return true;
-		if(year % 400 == 0)
-			return true;
-		if(year % 100 == 0)
-			return false;
-		
-		return false;
+		return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 	}
 	public static void main(String []args) {
 		Scanner in = new Scanner(new InputStreamReader(System.in));
-		System.out.println("Enter starting year");
+		System.out.println("Enter the starting year: ");
 		int startyear = in.nextInt();
-		System.out.println("Enter ending year");
+		System.out.println("Enter the ending year: ");
 		int endyear = in.nextInt();
-		for(int i = startyear; i <= endyear; ++i) {
+		for(int i = startyear; i <= endyear; ++i)
 			if(isLeapYear(i))
 				System.out.println(i);
-		}
 		in.close();
 	}
 }
