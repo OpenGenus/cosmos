accf55f797dd6a6403893f7465b75eed6733e563
Garvit Kothari <31393258+Garvit-k@users.noreply.github.com>
2018-03-07 21:57:23 +0530

    Updated requested changes

--- code/mathematical_algorithms/src/log_of_factorial/log_of_factorial.java
+++ code/mathematical_algorithms/src/log_of_factorial/log_of_factorial.java
@@ -1,5 +1,6 @@
 import java.io.InputStreamReader;
 import java.util.Scanner;
+
 class LogOfFactorial {
 	public static double logOfFactorial(int num) {
 		double ans = 0;
@@ -10,9 +11,9 @@ class LogOfFactorial {
 	}
 	public static void main(String []args) {
 		Scanner in = new Scanner(new InputStreamReader(System.in));
-		System.out.println("Enter a number");
+		System.out.println("Enter a number: ");
 		int num = in.nextInt();
-		System.out.println("Log of Factorial of "+num+" is "+logOfFactorial(num));
+		System.out.println("Log of Factorial of " + num + " is " + logOfFactorial(num));
 		in.close();
 	}
 }
