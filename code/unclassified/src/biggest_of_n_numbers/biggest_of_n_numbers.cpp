efed0956f4fbf9b344980b32b7520a2e690d4247
tbhaxor <tbhaxor@gmail.com>
2018-03-08 22:49:31 +0530

    modified files

--- code/unclassified/src/biggest_of_n_numbers/biggest_of_n_numbers.cpp
+++ code/unclassified/src/biggest_of_n_numbers/biggest_of_n_numbers.cpp
@@ -3,22 +3,15 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-using namespace std;
 
 int main() {
-    int n;
-    vector<int> arr;
-    cout<<"enter number of numbers : ";
-    cin>>n;
-    for(int i = 0 ; i < n ; i++) {
-        int a;
-        cin>>a;
-        arr.push_back(a);
+    std::vector<int> arr;
+    std::cout << "Keep entering numbers (EOF to stop): ";
+    for (int num; std::cin >> num;) {
+        arr.push_back(num);
     }
 
     sort(arr.begin(), arr.end());
 
-    cout<<"biggest number : "<<arr[n-1];
-
-    return 0;
-}   
+    std::cout<<"biggest number : "<<arr.back();
+}  
