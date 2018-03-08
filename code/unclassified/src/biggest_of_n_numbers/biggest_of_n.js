b0ee579c84261fc06ed5728ed6813ccb3b7311e2
tbhaxor <tbhaxor@gmail.com>
2018-03-08 22:30:44 +0530

    biggest of n in javascript

---
+++ code/unclassified/src/biggest_of_n_numbers/biggest_of_n.js
@@ -0,0 +1,8 @@
+// Part of cosmos from opengenus foundation
+
+const biggestOfN = array => {
+    return Math.max.apply(null, array)
+}
+
+console.log(biggestOfN([10, 0, -1, 100, 20]))
+console.log(biggestOfN([9090, 0, -100, 1, 20]))
