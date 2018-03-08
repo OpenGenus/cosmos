b2ba6608ea27fc9711b34177a48c455b96fa2d6c
tbhaxor <tbhaxor@gmail.com>
2018-03-08 22:39:32 +0530

    leap year in javascript

---
+++ code/unclassified/src/leap_year/leap_year.js
@@ -0,0 +1,14 @@
+// Part of cosmos from opengenus foundation
+
+const isLeep = year =>{
+    return (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+}
+
+var startYear = 1990
+var endYear = 2020
+
+for(i = startYear; i<= endYear; i++) {
+    if(isLeep(i)) {
+        console.log(i)
+    }
+}
