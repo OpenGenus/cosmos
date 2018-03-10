6d1559b97da992ad4fa69959e1089e99b96601e1
tbhaxor <tbhaxor@gmail.com>
2018-03-10 07:41:25 +0530

    prime numbers of n in javascript

---
+++ code/mathematical_algorithms/src/prime_numbers_of_n/prime_numbers_of_n.js
@@ -0,0 +1,28 @@
+function primeFactors(number)
+{
+    let factors = [];
+    while (number % 2 == 0) {
+        factors.push(2)
+        number = number / 2;
+    }
+
+    for (let i = 3; i <= Math.sqrt(number); i += 2)
+    {
+        while (number % i == 0) {
+            factors.push(i)
+            number = number / i;
+        }
+    }
+
+    if (number > 2)
+    {
+        factors.push(number)
+    }
+
+    return factors;
+}
+
+// test cases
+console.log(primeFactors(10))    // [2, 5]
+console.log(primeFactors(100))   // [2, 2, 5, 5]
+console.log(primeFactors(300))  // [2, 2, 3, 5, 5]
