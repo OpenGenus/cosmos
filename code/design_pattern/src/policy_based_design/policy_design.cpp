340b6a610751dea85f5e4e09fa666c3ae7379103
n-is <073bex422.nischal@pcampus.edu.np>
2018-03-15 21:46:29 +0545

    Fix the indentations and follow documenting rules.

--- code/design_pattern/src/policy_based_design/policy_design.cpp
+++ code/design_pattern/src/policy_based_design/policy_design.cpp
@@ -1,5 +1,5 @@
 /*
- * policy_design.cpp
+* policy_design.cpp
 *
 * Created: 3/15/2018 1:14:41 AM
 *  Author: n-is
@@ -11,44 +11,44 @@
 class Classical
 {
 public:
-        Classical(int a) { }
+    Classical(int a) { }
 
-        void solve() {
-                std::cout << "The differential equation was solved by the "
-                                "classical method." << std::endl;
-        }
+    void solve() {
+        std::cout << "The differential equation was solved by the classical" <<
+            " method." << std::endl;
+    }
 };
 
 class Laplace
 {
 public:
-        void solve() {
-                std::cout << "The differential equation was solved by the "
-                                "Laplace Transform method." << std::endl;
-        }
+    void solve() {
+        std::cout << "The differential equation was solved by the Laplace" <<
+            " Transform method." << std::endl;
+    }
 };
 
 template <class Method>
 class DifferentialEquaton
 {
 private:
-        Method m_;
+    Method m_;
 public:
-        DifferentialEquaton(const Method & m = Method()):
-        m_(m) { }
+    DifferentialEquaton(const Method & m = Method()):
+    m_(m) { }
 
-        void solve() {
-                m_.solve();
-        }
+    void solve() {
+        m_.solve();
+    }
 };
 
 int main()
 {
-        DifferentialEquaton<Laplace> eqn1;
-        DifferentialEquaton<Classical> eqn2(Classical(1));
+    DifferentialEquaton<Laplace> eqn1;
+    DifferentialEquaton<Classical> eqn2(Classical(1));
 
-        eqn1.solve();
-        eqn2.solve();
+    eqn1.solve();
+    eqn2.solve();
 
-        return 0;
+    return 0;
 }
