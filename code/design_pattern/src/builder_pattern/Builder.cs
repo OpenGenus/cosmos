0cd3e14ed7d38849f1fce3ce7417ad6184c1af28
Chandler Davidson <chandlermdavidson@gmail.com>
2018-03-15 19:01:14 -0500

    Finished Builder Pattern

--- code/design_pattern/src/builder_pattern/Builder.cs
+++ code/design_pattern/src/builder_pattern/Builder.cs
@@ -5,22 +5,25 @@ namespace SandBox
 {
 	/// <summary>
 	/// Acts as the "Director" class.
+    /// 
 	/// An intermediary between the customer (main) and the manufacturer.
+    /// Unaware of the intracacies of building each product.
 	/// </summary>
 	class Dealer
 	{
 		/// <summary>
-		/// Orders a vehicle (generic) from a Manufacturer.
+		/// Orders a vehicle from a Manufacturer.
 		/// </summary>
-		/// <param name="manufacturer">The manufacturer in which to order from</param>
-		public void Order(Manufacturer manufacturer)
+		/// <param name="manufacturer">The manufacturer to order from</param>
+        public Vehicle Order(Manufacturer manufacturer)
 		{
-			manufacturer.BuildVehicle();
+			return manufacturer.BuildVehicle();
 		}
 	}
 
 	/// <summary>
 	/// Template class for each Manufacturer to follow.
+    /// Allows for easy extensibility, while maintaining consistency.
 	/// </summary>
 	abstract class Manufacturer
 	{
@@ -29,31 +32,81 @@ namespace SandBox
 		public abstract Vehicle BuildVehicle();
 	}
 
-	class PriusManufacturer : Manufacturer
+    /// <summary>
+    /// 1 of 3.
+    /// An example of one of the many possible builder classes that extend the 
+    /// abstract class Manufacturer.
+    /// </summary>
+	class MotorCycleManufacturer : Manufacturer
 	{
-		private string ManufacturerName = "Prius";
-		public PriusManufacturer()
+        public string ManufacturerName = "MotorCycle";
+		public MotorCycleManufacturer()
 		{
 		}
 
 		public override Vehicle BuildVehicle()
 		{
 			var vehicle = new Vehicle(ManufacturerName);
-			vehicle.Parts.Add("Battery", "SuperPoweredRecharging");
-			vehicle.Parts.Add("Brakes", "Regenative");
+            vehicle.Parts.Add("Body", "Bike");
+			vehicle.Parts.Add("Handlebars", "Stock");
+			vehicle.Parts.Add("Wheels", "Two-Wheeled");
 
 			return vehicle;
 		}
 	}
 
-	class TeslaManufacturer : Manufacturer
-	{
+	/// <summary>
+	/// 2 of 3.
+	/// An example of one of the many possible builder classes that extend the 
+    /// abstract class Manufacturer.
+	/// </summary>
+	class SUVManufacturer : Manufacturer
+    {
+        private string ManufacturerName = "SUV";
+        public SUVManufacturer()
+        {
+        }
 
-	}
+        public override Vehicle BuildVehicle()
+        {
+            var vehicle = new Vehicle(ManufacturerName);
+            vehicle.Parts.Add("Body", "Large-Box");
+            vehicle.Parts.Add("Steering Wheel", "Stock");
+            vehicle.Parts.Add("Wheels", "Four-Wheeled");
+
+            return vehicle;
+        }
+    }
 
 	/// <summary>
-	/// Acts as item being built.
-	/// Each manufacturer could add different parts.
+	/// 3 of 3.
+	/// An example of one of the many possible builder classes that extend the 
+    /// abstract class Manufacturer.
+	/// </summary>
+	class ConvertibleManufacturer : Manufacturer
+    {
+        private string ManufacturerName = "Convertible";
+        public ConvertibleManufacturer()
+        {
+        }
+
+        public override Vehicle BuildVehicle()
+        {
+			var vehicle = new Vehicle(ManufacturerName);
+			vehicle.Parts.Add("Body", "Small-Sleek");
+			vehicle.Parts.Add("Steering Wheel", "Stock");
+			vehicle.Parts.Add("Wheels", "Four-Wheeled");
+            vehicle.Parts.Add("Roof", "Soft-Top");
+
+			return vehicle;
+        }
+    }
+
+	/// <summary>
+	/// Acts as the item being built.
+    /// 
+    /// Each item starts generic, then the manufacturer makes it unique. The
+    /// process is abstracted from the item.
 	/// </summary>
 	class Vehicle
 	{
@@ -67,11 +120,11 @@ namespace SandBox
 
 		public void Show()
 		{
-			Console.WriteLine("\n------------------------------");
-			Console.WriteLine("Manufacturer: {0}", Make);
+			Console.WriteLine("\n---------------------------");
+			Console.WriteLine("Vehicle Type: {0}", Make);
 
 			foreach (var part in Parts)
-				Console.WriteLine(" {0} : {1}", part.Key, part.Value);
+				Console.WriteLine("  {0} : {1}", part.Key, part.Value);
 		}
 	}
 
@@ -79,7 +132,17 @@ namespace SandBox
 	{
 		static void Main(string[] args)
 		{
-			
+            var MotorCycleManufacturer = new MotorCycleManufacturer();
+			var motorCycle = MotorCycleManufacturer.BuildVehicle();
+			motorCycle.Show();
+
+			var SUVManufacturer = new SUVManufacturer();
+			var suv = SUVManufacturer.BuildVehicle();
+			suv.Show();
+
+			var ConvertibleManufacturer = new ConvertibleManufacturer();
+            var convertible = ConvertibleManufacturer.BuildVehicle();
+            convertible.Show();
 		}
 	}
 }
