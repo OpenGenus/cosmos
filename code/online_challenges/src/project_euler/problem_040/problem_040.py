195ad587cb4edc1655f5b3b15c22f507622deeb0
Adeen Shukla <adeens357@gmail.com>
2018-03-16 19:31:48 +0530

    Add comments to problem_040.py in project_euler/problem_040

--- code/online_challenges/src/project_euler/problem_040/problem_040.py
+++ code/online_challenges/src/project_euler/problem_040/problem_040.py
@@ -1,14 +1,17 @@
-nums = ""
-i = 0
+# Made by adeen-s
+# Part of cosmos by OpenGenus
 
-while len(nums) < 1000002:
-    nums = nums + str(i)
+nums = "" # The string containing the sequence of numbers
+i = 0 # Used for iteration
+
+while len(nums) < 1000002: # Loop to add the sequence of numbers to the list
+    nums = nums + str(i) 
     i = i + 1
 
-answer = 1
-i = 1
+answer = 1 # Initialising answer which will store final computed answer
+i = 1 # Reset value for use in next loop
 
-while i <= 1000000:
+while i <= 1000000: # Loop to generate the answer
     answer = answer * int(nums[i])
     i = i * 10
     
