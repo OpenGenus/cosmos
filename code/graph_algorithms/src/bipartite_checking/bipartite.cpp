079c495fa71d427d87ed1439aeb5900c9943ca78
NotAPenguin <michiel.vandeginste@gmail.com>
2018-03-16 16:54:41 +0100

    stying changes + return type of bipartite()
    return type of bipartite() should be bool

--- code/graph_algorithms/src/bipartite_checking/bipartite.cpp
+++ code/graph_algorithms/src/bipartite_checking/bipartite.cpp
@@ -3,53 +3,55 @@
 
 using namespace std;
 
-int bipartite(int g[][10],int s,int n)
+bool bipartite(int g[][10],int s,int n)
 {
-  int *col=new int [n];
+  int *col = new int[n];
   
-  for(int i=0;i<n;i++)
-  col[i]=-1;
+  for(int i = 0; i < n; i++)
+      col[i] = -1;
   
   queue<int> q;
   
-  col[s]=1;
+  col[s] = 1;
   q.push(s);
   
   while(!q.empty())
   {
-    int u=q.front();
+    int u = q.front();
     q.pop();
    
-    if(g[u][u]==1)
-    return 0;
+    if(g[u][u] == 1)
+        return false;
      
-    for(int v=0;v<n;v++)
+    for(int v = 0; v < n; v++)
     {
-      if(g[u][v] && col[v]==-1)
-      {col[v]=1-col[u];
-      q.push(v);
+      if(g[u][v] && col[v] == -1)
+      {
+        col[v] = 1 - col[u];
+        q.push(v);
       }
-      else if(g[u][v]==1 && col[u]==col[v])
-      return 0;
+      else if(g[u][v] == 1 && col[u] == col[v])
+          return false;
     }
+    
    }
    
-   return 1;
+   return true;
  }   
     
       
 int main()
 {
-  int g[10][10],n;
+  int g[10][10], n;
   
-  cin>>n;
+  cin >> n;
   
-  for(int i=0;i<n;i++)
-  for(int j=0;j<n;j++)
-  cin>>g[i][j];
+  for(int i = 0; i < n; i++)
+      for(int j = 0; j < n; j++)
+          cin >> g[i][j];
    
-  int x=bipartite(g,0,n);
+  bool x = bipartite(g, 0, n);
   
- cout<<x<<endl;
+ cout << x << endl;
  return 0; 
 }
