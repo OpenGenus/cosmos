7f6c01302d1863d8e7e064b6f6a46768a4496f5a
Danny Henderson <dannyhenderson@Dannys-MacBook-Pro.local>
2018-03-18 13:51:46 -0600

    Added suggested fixes and test file

--- code/bit_manipulation/src/addition_using_bits/addition_using_bits.cpp
+++ code/bit_manipulation/src/addition_using_bits/addition_using_bits.cpp
@@ -5,13 +5,12 @@ int bitwiseAddition(int n, int m)
 {
     while (m != 0)
     {
-        int i = n & m;  // Variable i keeps track of bits that carry over
-        n = n ^ m;      // This adds up the individual bits
-        m = i << 1;     // Shift carry over bits so they can be added 
+        int carry = n & m;  // Variable carry keeps track of bits that carry over
+        n = n ^ m;          // This adds up the individual bits
+        m = carry << 1;     // Shift carry over bits so they can be added 
     }
     return n;
 }
-
 
 int bitwiseAdditionRecursive(int n, int m)
 {
@@ -19,15 +18,8 @@ int bitwiseAdditionRecursive(int n, int m)
         return n;
     else
     {
-        int i = n & m;
-        return bitwiseAdditionRecursive(n ^ m, i << 1);
+        int carry = n & m;
+        return bitwiseAdditionRecursive(n ^ m, carry << 1);
     }
 }
 
-int main()
-{
-    int n = 10;
-    int m = 5;
-    std::cout << bitwiseAddition(n, m) << "\n"; 
-    std::cout << bitwiseAdditionRecursive(n, m) << "\n";
-}
