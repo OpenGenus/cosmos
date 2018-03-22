4ae07e6792556195df51b3b4b6bdcecf7a95888c
Gurkirat Singh <tbhaxor@gmail.com>
2018-03-22 20:50:36 +0000

    Update jaccard.py

--- code/unclassified/src/jaccard_similarity/jaccard.py
+++ code/unclassified/src/jaccard_similarity/jaccard.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 
-from math import *
-
-
 def jaccard_similarity(x, y):
 
     intersection_cardinality = len(set.intersection(*[set(x), set(y)]))
@@ -10,4 +7,4 @@ def jaccard_similarity(x, y):
     return intersection_cardinality / float(union_cardinality)
 
 
-print jaccard_similarity([0, 1, 2, 5, 6], [0, 2, 3, 5, 7, 9])
+print(jaccard_similarity([0, 1, 2, 5, 6], [0, 2, 3, 5, 7, 9]))
