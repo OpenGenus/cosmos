da19b7aa6900f992d35753fb20e0f9d1f5bf2026
Gurkirat Singh <tbhaxor@gmail.com>
2018-03-22 21:12:40 +0000

    Update palindrome.py

--- code/string_algorithms/src/palindrome_checker/palindrome.py
+++ code/string_algorithms/src/palindrome_checker/palindrome.py
@@ -1,6 +1,4 @@
-# Part of Cosmos by OpenGenus Foundation
 def isPalindromeRecursive(string):
-    print(string)
     if len(string) == 2 or len(string) == 1:
         return True
     if string[0] != string[len(string) - 1]:
@@ -21,16 +19,16 @@ def isPalindromeIterative(string):
             return False
     return True
 
+if __name__ == "__main__":
+    print(isPalindromeRecursive("alpha")) # should output false
+    print(isPalindromeRecursive("racecar"))  # should output true
+    print(isPalindromeRecursive("abba"))  # should output true
 
-if __name__ == 'main':
-    print isPalindromeRecursive("alpha")  # should output false
-    print isPalindromeRecursive("racecar")  # should output true
-    print isPalindromeRecursive("abba")  # should output true
+    print(isPalindromeReverse("alpha"))  # should output false
+    print(isPalindromeReverse("racecar"))  # should output true
+    print(isPalindromeReverse("abba"))  # should output true
 
-    print isPalindromeReverse("alpha")  # should output false
-    print isPalindromeReverse("racecar")  # should output true
-    print isPalindromeReverse("abba")  # should output true
-
-    print isPalindromeIterative("alpha")  # should output false
-    print isPalindromeIterative("racecar")  # should output true
-    print isPalindromeIterative("abba")  # should output true
+    print(isPalindromeIterative("alpha"))  # should output false
+    print(isPalindromeIterative("racecar"))  # should output true
+    print(isPalindromeIterative("abba")) # should output true
+    
