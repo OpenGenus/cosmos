a468ce6fb671f21d75580e1822e0022ee15aa3d7
Gurkirat Singh <tbhaxor@gmail.com>
2018-03-22 21:21:28 +0000

    Update kmp.py

--- code/string_algorithms/src/kmp_algorithm/kmp.py
+++ code/string_algorithms/src/kmp_algorithm/kmp.py
@@ -1,8 +1,3 @@
-# Part of Cosmos by OpenGenus Foundation 
-
-# the Knuth-Morris-Pratt string searching algorithm (or KMP algorithm) 
-
-# first step is to generate lps array i.e. longest prefix suffix array
 def getLPS(pattern):
 	m = len(pattern)
 	lps = [0]*(m+1)
@@ -24,13 +19,11 @@ lps = getLPS(pattern)
 i=0; j=0;
 n=len(text); m=len(pattern)
 
-while i<n:
+while i<n-1:
 	while j>=0 and text[i] != pattern[j]:
 		j = lps[j]
 	i+=1; j+=1;
 	
 	if j == m:
 		print("Match found at index: " + str(i-j))
-		j = lps[j]
-
-
+j = lps[j]
