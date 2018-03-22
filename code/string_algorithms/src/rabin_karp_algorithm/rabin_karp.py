e5ebcc4623f66f67acbf60063881234d250d0cba
Gurkirat Singh <tbhaxor@gmail.com>
2018-03-22 20:56:16 +0000

    Update rabin_karp.py

--- code/string_algorithms/src/rabin_karp_algorithm/rabin_karp.py
+++ code/string_algorithms/src/rabin_karp_algorithm/rabin_karp.py
@@ -14,22 +14,22 @@ def search(pat, txt, q):
     t = 0
     h = 1
 
-    for i in xrange(M - 1):
+    for i in range(M - 1):
         h = (h * d) % q
 
-    for i in xrange(M):
+    for i in range(M):
         p = (d * p + ord(pat[i])) % q
         t = (d * t + ord(txt[i])) % q
 
-    for i in xrange(N - M + 1):
+    for i in range(N - M + 1):
         if p == t:
-            for j in xrange(M):
+            for j in range(M):
                 if txt[i + j] != pat[j]:
                     break
 
             j += 1
             if j == M:
-                print "Pattern found at index " + str(i)
+                print("Pattern found at index " + str(i))
 
         if i < N - M:
             t = (d * (t - ord(txt[i]) * h) + ord(txt[i + M])) % q
