145ad20287a663ffcd49b778e9cabf32f3124726
Gurkirat Singh <tbhaxor@gmail.com>
2018-03-23 04:57:31 +0000

    Update smallest_digit_in_number.cs

--- code/mathematical_algorithms/src/smallest_digit_in_number/smallest_digit_in_number.cs
+++ code/mathematical_algorithms/src/smallest_digit_in_number/smallest_digit_in_number.cs
@@ -9,7 +9,7 @@ namespace SmallestNumber
             int min = n % 10;
             n /= 10;
             
-            while(n!=0)
+            while(n != 0)
             {
                 if(n % 10 < min)
                 {
