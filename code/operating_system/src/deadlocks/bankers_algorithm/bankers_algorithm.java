6ffea1a808f049ca9f72d4c4052affe4ec0a7d30
Nida <24700264+Nidakhan786@users.noreply.github.com>
2018-03-23 20:55:41 +0530

    Update bankers_algorithm.java

--- code/operating_system/src/deadlocks/bankers_algorithm/bankers_algorithm.java
+++ code/operating_system/src/deadlocks/bankers_algorithm/bankers_algorithm.java
@@ -32,7 +32,7 @@ public class BankersAlgorithm {
 		System.out.println("Enter allocation matrix -->");
 		for (int i = 0; i < processes; i++) {
 			System.out.println("Row " + (i + 1) + " (P" + i + "): ");
-			for (int j = 0; j < resources; j++) {
+			for (int j = 0; j < resources; ++j) {
 				allocated_resources[i][j] = scanner.nextInt(); // allocation matrix
 			}
 		}
@@ -41,14 +41,14 @@ public class BankersAlgorithm {
 		System.out.println("Enter max matrix -->");
 		for (int i = 0; i < processes; i++) {
 			System.out.println("Row " + (i + 1) + " (P" + i + "): ");
-			for (int j = 0; j < resources; j++) {
+			for (int j = 0; j < resources; ++j) {
 				max[i][j] = scanner.nextInt(); // max matrix
 			}
 		}
 
 		// Get available matrix input
 		System.out.println("Enter available matrix -->");
-		for (int j = 0; j < resources; j++)
+		for (int j = 0; j < resources; ++j)
 			available_resources[0][j] = scanner.nextInt(); // available matrix
 		
 		scanner.close();
@@ -62,19 +62,19 @@ public class BankersAlgorithm {
 		printArray(calc_need());
 	}
 	static void printArray(int[][] array) {
-		for (int i = 0; i < array.length; i++) {
+		for (int i = 0; i < array.length; ++i) {
 			System.out.println("");
-			for (int j = 0; j < array[0].length; j++) {
+			for (int j = 0; j < array[0].length; ++j) {
 				System.out.print(array[i][j] + " ");
 			}
 		}
 		System.out.println("");
 	}
 
-	//
+	
 	private static int[][] calc_need() {
-		for (int i = 0; i < processes; i++)
-			for (int j = 0; j < resources; j++)
+		for (int i = 0; i < processes; ++i)
+			for (int j = 0; j < resources; ++j)
 				// calculating need matrix
 				need[i][j] = max[i][j] - allocated_resources[i][j];
 		return need;
@@ -82,10 +82,10 @@ public class BankersAlgorithm {
 
 	private static boolean check_allocatable(int i) {
 		// checking if all resources for ith process can be allocated
-		for (int j = 0; j < resources; j++)
+		for (int j = 0; j < resources; ++j)
 			if (available_resources[0][j] < need[i][j])
-			return false;
-		return true;
+				return false;
+			return true;
 	}
 
 	public static void checkSafety() {
@@ -93,13 +93,12 @@ public class BankersAlgorithm {
 		calc_need();
 		boolean done[] = new boolean[processes];
 		int j = 0;
-
 		while (j < processes) { // until all process allocated
-		boolean allocated = false;
-		for (int i = 0; i < processes; i++)
+			boolean allocated = false;
+		for (int i = 0; i < processes; ++i)
 			if (!done[i] && check_allocatable(i)) { // trying to allocate
 
-				for (int k = 0; k < resources; k++)
+				for (int k = 0; k < resources; ++k)
 					available_resources[0][k] = available_resources[0][k]
 						- need[i][k] + max[i][k];
 				System.out.println("Allocated process : " + i);
