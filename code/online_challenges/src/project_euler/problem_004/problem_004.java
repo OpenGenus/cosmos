98ae0772785f0ba4aba5ed83a174ec01bcd23ce5
Sneha Raina <32961290+SnehaR26@users.noreply.github.com>
2018-03-24 02:14:03 +0530

    problem_004.java for project euler

---
+++ code/online_challenges/src/project_euler/problem_004/problem_004.java
@@ -0,0 +1,23 @@
+public class problem_004 {
+      public static void main( String[] args) {
+                int prod, temp, sum, rem, lar = 0;
+                // Nested loop to iterate for every three digit number
+                for(int i = 100; i <= 999; ++i) {
+                    for(int j = 100; j <= 999; ++j) {
+                               prod = i * j;
+                               temp = prod;
+                               sum = 0;
+                               // Loop to reverse the number 
+                               while (temp > 0) { 
+                                    rem = temp % 10;
+                                    sum = (sum * 10) + rem;
+                                    temp /= 10;
+                                }
+                              // Statement to check the palindrome condition and store the largest value
+                              if ((prod == sum) && (prod > lar))
+                                    lar = prod;
+                  }
+               }
+               System.out.println("The largest palindrome made from the product of two 3-digit numbers is " + lar);
+        }   
+ }
