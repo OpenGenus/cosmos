a8c151c7bd66ff5b0ed243fdd88db3b4d8807b06
shobhit <jshobhit141@gmail.com>
2018-03-24 22:59:14 +0000

    indentend

--- code/randomized_algorithms/src/randomized_quick_sort/randomized_quicksort.cpp
+++ code/randomized_algorithms/src/randomized_quick_sort/randomized_quicksort.cpp
@@ -9,38 +9,39 @@ int part(int arr[], int low, int high);
 
 int main() 
 {
-	srand(time(NULL));
-    int arr[10] = {-25, 119, 32, 54, 623, 20, -2, -4, 8, 11}; // unsorted list
-    quicksort(arr, 0, 9);
-    for(int i=0; i < 10; i++) 
-    	std::cout << arr[i] << " ";
-    std::cout << std::endl;
+        srand(time(NULL));
+        int arr[10] = {-25, 119, 32, 54, 623, 20, -2, -4, 8, 11}; // unsorted list
+        quicksort(arr, 0, 9);
+            for(int i=0; i < 10; i++) 
+                std::cout << arr[i] << " ";
+        std::cout << std::endl;
 }
 void quicksort(int arr[], int low, int high) 
 {
-	if(low < high)
-	{
-    int pi = part(arr, low, high);
+        if(low < high)
+	    {
+            int pi = part(arr, low, high);
 
-    quicksort(arr, low, pi-1);
-    quicksort(arr, pi+1, high);
-	}
+            quicksort(arr, low, pi-1);
+            quicksort(arr, pi+1, high);
+	    }
 }
 // sort a sub array in increasing order
 int part(int arr[], int low, int high)
 {
-	int pivotIndex = low + rand() % (high - low + 1);
-	int pivot = arr[pivotIndex];
-	int l = low - 1;
-	std::swap(arr[pivotIndex], arr[high]);
-	for(int i = low; i < high; i++) 
-	{
-		if(arr[i] < pivot) 
-		{
-			l++;
-			std::swap(arr[l],arr[i]);
-		}
-	}
+        int pivotIndex = low + rand() % (high - low + 1);
+	    int pivot = arr[pivotIndex];
+	    int l = low - 1;
+        std::swap(arr[pivotIndex], arr[high]);
+        
+	    for(int i = low; i < high; i++) 
+        {
+            if(arr[i] < pivot) 
+		    {
+                l++;
+                std::swap(arr[l],arr[i]);
+		    }
+        }
 	std::swap(arr[l+1],arr[high]);
 	return l+1;
 }
