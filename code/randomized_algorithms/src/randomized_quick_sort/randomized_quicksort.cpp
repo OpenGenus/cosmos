6aad931ec100e6668942a13141dbffae4329ed8d
Shobhit <jshobhit141@gmail.com>
2018-03-31 05:57:39 +0000

    modified to use vectors instead of normal array

--- code/randomized_algorithms/src/randomized_quick_sort/randomized_quicksort.cpp
+++ code/randomized_algorithms/src/randomized_quick_sort/randomized_quicksort.cpp
@@ -3,52 +3,54 @@
 #include <algorithm>
 #include <ctime>
 #include <iostream>
+#include <vector>
 
-void quicksort(int arr[], int i, int n);
-int part(int arr[], int low, int high);
+void quicksort(std::vector<int> &vect, int low, int high);
+int part(std::vector<int> &vect, int low, int high);
 
 int main() 
 {
-        srand(time(NULL));
-        int arr[10] = {-25, 119, 32, 54, 623, 20, -2, -4, 8, 11};
-        
-        // sorts the array in increasing order
-        quicksort(arr, 0, 9);
-        
-        for(int i=0; i < 10; i++) 
-       	    std::cout << arr[i] << " ";
-            
-        std::cout << '\n';
-        return 0;
+    srand(time(NULL));
+    
+    std::vector<int> val = {-25, 119, 32, 54, 623, 20, -2, -4, 8, 11};
+    
+    // sorts the array in increasing order
+    quicksort(val, 0, 10);
+    
+    for(int i=0; i < 10; i++) 
+        std::cout << val[i] << " ";
+    
+    std::cout << '\n';
+    return 0;
 }
-void quicksort(int arr[], int low, int high) 
+void quicksort(std::vector<int> &vect, int low, int high) 
 {
     if(low < high)
     {
-         int pi = part(arr, low, high);
-
-         quicksort(arr, low, pi-1);
-         quicksort(arr, pi+1, high);
+        int pi = part(vect, low, high);
+        
+        quicksort(vect, low, pi-1);
+        quicksort(vect, pi+1, high);
     }
 }
-// sort a sub array in increasing order
-int part(int arr[], int low, int high)
+// sort a sub array partially in increasing order
+int part(std::vector<int> &vect, int low, int high)
 {
-     int pivotIndex = low + (rand() % (high - low + 1));
-     int pivot = arr[pivotIndex];
-     int l = low - 1;
-        
-     std::swap(arr[pivotIndex], arr[high]);
-        
-    for(int i = low; i < high; i++) 
+    int pivotIndex = low + (rand() % (high - low + 1));
+    int pivot = vect[pivotIndex];
+    int l = low - 1;
+    
+    std::swap(vect[pivotIndex], vect[high]);
+    
+    for(int i = low; i < high; ++i) 
     {
-	    if(arr[i] < pivot) 
-	    {
-		    ++l
-             std::swap(arr[l],arr[i]);
-          }
+        if(vect[i] < pivot) 
+        {
+            ++l;
+            std::swap(vect[l], vect[i]);
+        }
     }    
-        std::swap(arr[l+1],arr[high]);
-        
+    std::swap(vect[l+1], vect[high]);
+    
     return l+1;
 }
