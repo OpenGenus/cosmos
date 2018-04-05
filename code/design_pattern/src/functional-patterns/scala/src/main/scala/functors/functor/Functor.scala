f6a4c1a2e67c4282ea8aa5817612bc2198a6dc36
robertbutacu <robert.butacu@yahoo.com>
2018-04-05 17:09:28 +0300

    functor

--- code/design_pattern/src/functional-patterns/scala/src/main/scala/functors/functor/Functor.scala
+++ code/design_pattern/src/functional-patterns/scala/src/main/scala/functors/functor/Functor.scala
@@ -1,4 +1,18 @@
 package functors.functor
 
+import scala.language.higherKinds
+
+/**
+  * ADT which aspires to be a functor must preserve 2 laws:
+  *   1. fmap over the identity functor of the ADT should coincide with the original ADT
+  *     fmap(identity, adt) === adt
+  *
+  *   2. The composition of 2 functions and mapping the resulting function over a functor
+  *     should be the same as mapping the first function over the functor and then the second one.
+  *     fmap(f compose g, adt) === fmap(f, fmap(g, adt))
+  *
+  * Some functors include: List, Set, Map, Option, etc.
+  */
 trait Functor[F[_]] {
+  def fmap[A, B](fA: F[A])(f: A => B): F[B]
 }
