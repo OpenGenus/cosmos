b641c367baa555a06a9269c4ed3dd0653355ee01
Ernest <iattempt.net@gmail.com>
2018-04-09 21:30:40 +0800

    Fix string algorithms category. Squashed commit of the following:
    commit bd7fdd2910a79b47fcfd15ae9d77608c83ef5d93
    Author: Ernest <iattempt.net@gmail.com>
    Date:   Mon Apr 9 21:13:41 2018 +0800
    
        Fix wrong coding style: add/remove, spaces/indentations and etc.

--- code/string_algorithms/src/trie_pattern_search/trie.cpp
+++ code/string_algorithms/src/trie_pattern_search/trie.cpp
@@ -6,97 +6,109 @@
 #define alphas 26
 using namespace std;
 
-typedef struct Node{
+typedef struct Node
+{
     Node *child[alphas];
     int leaf;
 }trieNode;
 
-trieNode* getNode(){
+trieNode* getNode()
+{
     trieNode *t = new trieNode;
-    if(t){
-        for(int i=0;i<alphas;i++)
+    if (t)
+    {
+        for (int i = 0; i < alphas; i++)
             t->child[i] = NULL;
         t->leaf = 0;
     }
     return t;
 }
 
-void insert(trieNode *root, string s){
+void insert(trieNode *root, string s)
+{
     trieNode *head = root;
     int len = s.length();
-    for(int i=0;i<len;i++){
-        if(head->child[s[i]-'a']==NULL){
-            head->child[s[i]-'a'] = getNode();
-        }
-        head = head->child[s[i]-'a'];
+    for (int i = 0; i < len; i++)
+    {
+        if (head->child[s[i] - 'a'] == NULL)
+            head->child[s[i] - 'a'] = getNode();
+        head = head->child[s[i] - 'a'];
     }
     head->leaf = 1;
 }
 
-void walk(trieNode *root){
+void walk(trieNode *root)
+{
     trieNode *head = root;
-    for(int i=0;i<alphas;i++){
-        if(head->child[i]!=NULL){
-            cout<<char(i+'a')<<" ";
+    for (int i = 0; i < alphas; i++)
+        if (head->child[i] != NULL)
+        {
+            cout << char(i + 'a') << " ";
             walk(head->child[i]);
         }
-    }
 }
 
-bool noChildren(trieNode *root){
+bool noChildren(trieNode *root)
+{
     trieNode *head = root;
-    for(int i=0;i<26;i++)
-        if(head->child[i])
+    for (int i = 0; i < 26; i++)
+        if (head->child[i])
             return false;
     return true;
 }
 
-bool notLeaf(trieNode *root){
-    if(root->leaf == 1)
+bool notLeaf(trieNode *root)
+{
+    if (root->leaf == 1)
         return false;
     return true;
 }
 
-bool deletes(trieNode *root, string s, int l,int len){
-    if(root){
-        if(l==len){
+bool deletes(trieNode *root, string s, int l, int len)
+{
+    if (root)
+    {
+        if (l == len)
+        {
             root->leaf = 0;
-            if(noChildren(root)){
+            if (noChildren(root))
                 return true;
-            }
             return false;
         }
-        else{
-            int index = s[l]-'a';
-            if(deletes(root->child[index],s,l+1,len)){
+        else
+        {
+            int index = s[l] - 'a';
+            if (deletes(root->child[index], s, l + 1, len))
+            {
                 free(root->child[index]);
                 root->child[index] = NULL;
-                return (notLeaf(root) && noChildren(root));
+                return notLeaf(root) && noChildren(root);
             }
         }
     }
     return false;
 }
 
-int main(){
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     vector<string> str;
     string s;
 
-    for(int i=0;i<n;i++){
-      cin>>s;
-      str.push_back(s);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> s;
+        str.push_back(s);
     }
 
     trieNode *root = getNode();
-    for(int i=0;i<n;i++){
-        insert(root,str[i]);
-    }
+    for (int i = 0; i < n; i++)
+        insert(root, str[i]);
     walk(root);
-    cout<<endl;
-    cin>>s;
-    deletes(root,s,0,n);
+    cout << endl;
+    cin >> s;
+    deletes(root, s, 0, n);
     walk(root);
     return 0;
 }
