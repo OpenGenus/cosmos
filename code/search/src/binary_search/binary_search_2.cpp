259044fde3b4e5c6cbce6abb9ba62659858d1c13
Ernest <iattempt.net@gmail.com>
2018-04-09 21:24:16 +0800

    Fix search category. Squashed commit of the following:
    commit d8c79b1bf429aed0865a3fcceffeba553a11c102
    Author: Ernest <iattempt.net@gmail.com>
    Date:   Mon Apr 9 20:59:45 2018 +0800
    
        Fix wrong coding style: add/remove, spaces/indentations and etc.

--- code/search/src/binary_search/binary_search_2.cpp
+++ code/search/src/binary_search/binary_search_2.cpp
@@ -9,56 +9,59 @@ using namespace std;
 
 /* Utility functions */
 
-void fill(vector<int> &v) {
-    for(std::size_t i=0;i<v.size();i++) {
-        v[i]=rand() % 100;
-    }
+void fill(vector<int> &v)
+{
+    for (std::size_t i = 0; i < v.size(); i++)
+        v[i] = rand() % 100;
 }
 
-void printarr(vector<int> &v) {
-    for(std::size_t i=0;i<v.size();i++) {
+void printarr(vector<int> &v)
+{
+    for (std::size_t i = 0; i < v.size(); i++)
         cout << v[i] << " ";
-    }
     cout << endl;
 }
 
 /* Binary search with fewer comparisons */
-int binary_search(vector<int> &v, int key) {
+int binary_search(vector<int> &v, int key)
+{
     int m;
-    
-    int l=0,r=v.size();
 
-    while(r-l >1) {
-        m=l+(r-l)/2;
+    int l = 0, r = v.size();
 
-        if( v[m] > key) 
-            r=m;
-        else 
-            l=m;
+    while (r - l > 1)
+    {
+        m = l + (r - l) / 2;
+
+        if (v[m] > key)
+            r = m;
+        else
+            l = m;
     }
-    
-    if ( v[l] == key) 
+
+    if (v[l] == key)
         return l;
-    else 
+    else
         return -1;
 }
 
 /* Driver program */
-int main() {
+int main()
+{
     int size;
     cout << "Set array size:";
     cin >> size;
-    
+
     vector<int> v(size);
     fill(v);
 
     cout << "Array (sorted) : ";
-    sort(v.begin(),v.end());    
+    sort(v.begin(), v.end());
     printarr(v);
-    
+
     cout << "Search for (input search key) : ";
     int key;
-    cin >> key;   
-    
-    cout << "Found " << key << " at index "<< binary_search(v,key) << endl;
+    cin >> key;
+
+    cout << "Found " << key << " at index " << binary_search(v, key) << endl;
 }
