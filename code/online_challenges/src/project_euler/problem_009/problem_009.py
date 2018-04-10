10f6fe71f14d463b3f1baa0c1387d2bf6c1c5f96
parulagg27 <parulagg27@gmail.com>
2018-04-10 21:40:45 +0530

    Made required changes

--- code/online_challenges/src/project_euler/problem_009/problem_009.py
+++ code/online_challenges/src/project_euler/problem_009/problem_009.py
@@ -1,11 +1,11 @@
 def main():
-	sum_total = 1000
+    sum_total = 1000
 
-	for c in range (sum_total):
-		for b in range (c):
-			for a in range (b):
-				if (a + b + c == sum_total) and (a**2 + b**2 == c**2):
-					print (a*b*c)
+    for c in range (sum_total):
+        for b in range (c):
+            for a in range (b):
+                if (a + b + c == sum_total) and (a**2 + b**2 == c**2):
+                    print (a * b * c)
 
 if __name__ == "__main__":
-	main()
+    main()
