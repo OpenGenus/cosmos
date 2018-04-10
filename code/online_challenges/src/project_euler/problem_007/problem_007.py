d687c87e0b3f1fcdfdb8311f12ab8064c8585859
parulagg27 <parulagg27@gmail.com>
2018-04-10 21:34:34 +0530

    Made required changes, replaced tabs with spaces, added new line at EOF

--- code/online_challenges/src/project_euler/problem_007/problem_007.py
+++ code/online_challenges/src/project_euler/problem_007/problem_007.py
@@ -1,15 +1,14 @@
 def main():
-	n = 10001
-	x = 2
-	list_of_primes = []
-	
-	while (len(list_of_primes) < n):
-		if all (x % prime for prime in list_of_primes):
-			list_of_primes.append(x)
-		x += 1
+    n = 10001
+    x = 2
+    list_of_primes = []
 
-	print (list_of_primes[-1])
+    while (len(list_of_primes) < n):
+        if all (x % prime for prime in list_of_primes):
+            list_of_primes.append(x)
+        x += 1
 
+    print (list_of_primes[-1])
 
 if __name__ == "__main__":
-	main()
+    main()
