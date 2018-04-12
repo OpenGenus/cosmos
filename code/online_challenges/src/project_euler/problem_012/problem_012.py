4743cdea2bcc5245bcbbe5cfab46f25aa1b9a6a6
parulagg27 <parulagg27@gmail.com>
2018-04-12 09:47:12 +0530

    Project euler problem12 in python

---
+++ code/online_challenges/src/project_euler/problem_012/problem_012.py
@@ -0,0 +1,23 @@
+import math
+
+def main(): 
+    i = 1
+    triangle_number = 0
+    divisor_count = 0
+
+    while True:
+        triangle_number += i
+        i += 1
+        divisor_count = 0
+
+        for z in range(1, int(math.sqrt(triangle_number))):
+            if triangle_number % z == 0:
+                divisor_count += 2
+
+        if divisor_count > 499:
+            break
+
+    print (triangle_number)
+
+if __name__ == "__main__":
+    main()
