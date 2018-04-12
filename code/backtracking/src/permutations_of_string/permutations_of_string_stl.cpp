9d8423fccdf7ce28e91056482fad72baaf13ce01
Ernest <iattempt.net@gmail.com>
2018-04-12 20:58:35 +0800

    Fix backtracking category

--- code/backtracking/src/permutations_of_string/permutations_of_string_stl.cpp
+++ code/backtracking/src/permutations_of_string/permutations_of_string_stl.cpp
@@ -3,12 +3,13 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main() {
-	string s;
-	cin >> s;
-	sort(s.begin(), s.end());
-	do {
-		cout << s << endl;
-	} while (next_permutation(s.begin(), s.end()));
-	return 0;
+int main()
+{
+    string s;
+    cin >> s;
+    sort(s.begin(), s.end());
+    do
+        cout << s << endl;
+    while (next_permutation(s.begin(), s.end()));
+    return 0;
 }
