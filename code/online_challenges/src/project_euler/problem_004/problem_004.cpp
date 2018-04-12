e6b33dbc1b9b2a0c68f31de6558fcb701d9c29f8
Ernest <iattempt.net@gmail.com>
2018-04-12 21:11:35 +0800

    Fix online_challenges category

--- code/online_challenges/src/project_euler/problem_004/problem_004.cpp
+++ code/online_challenges/src/project_euler/problem_004/problem_004.cpp
@@ -4,21 +4,16 @@
 int main()
 {
     int l = 0;
-    
+
     for (int i = 100; i <= 999; ++i)
-    {
         for (int j = 100; j <= 999; ++j)
         {
             int c = i * j;
             std::string cS = std::to_string(c);
 
-            if ((cS == std::string{ cS.rbegin(), cS.rend() }) && (c > l)
-            )
-            {
+            if ((cS == std::string{ cS.rbegin(), cS.rend() }) && (c > l))
                 l = c;
-            }
         }
-    }
-    
+
     std::cout << l << "\n";
 }
--- code/online_challenges/src/project_euler/problem_007/problem_007.cpp
+++ code/online_challenges/src/project_euler/problem_007/problem_007.cpp
@@ -9,22 +9,14 @@ std::vector<long long int> primesUpto(size_t limit) // Function that implements 
     primesBoolArray[0] = primesBoolArray[1] = false;
     size_t sqrtLimit = std::sqrt(limit) + 1;
     for (size_t i = 0; i < sqrtLimit; ++i)
-    {
         if (primesBoolArray[i])
-        {
             for (size_t j = (2 * i); j < limit; j += i)
-            {
                 primesBoolArray[j] = false;
-            }
-        }
-    }
+
     for (size_t i = 0; i < primesBoolArray.size(); ++i)
-    {
         if (primesBoolArray[i])
-        {
             primesUptoLimit.push_back(i);
-        }
-    }
+
     return primesUptoLimit;
 }
 
