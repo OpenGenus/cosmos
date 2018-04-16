fb7c3030a9f41e13e2ca6b13f752eabd8f25ae09
erobynreid <reider@umich.edu>
2018-04-16 13:53:31 -0400

    Added functionality to queue.py

--- code/data_structures/src/queue/queue/queue.py
+++ code/data_structures/src/queue/queue/queue.py
@@ -4,10 +4,8 @@ import array
 class Queue:
 	def __init__(self, size_max):
 		self.max = size_max
-		self.head = 0
-		self.tail = 0
 		self.size = 0
-		self.data = array.array('i', range(size_max))
+		self.data = []
 
 	def empty(self):
 		return self.size == 0
@@ -19,30 +17,34 @@ class Queue:
 		if self.size == self.max:
 			print "Queue full"
 			return False
-		self.data[self.tail] = x
+		self.data.insert(0, x)
 		self.size += 1
-		self.tail += 1
-		if self.tail == self.max:
-			self.tail = 0
 		return True
 
 	def dequeue(self):
 		if self.size == 0:
 			return None
-		x = self.data[self.head]
+		x = self.data.pop()
 		self.size -= 1
-		self.head += 1
-		if self.head == self.max:
-			self.head = 0
 		return x
+
+	def display(self):
+		if self.size == 0:
+			print "Queue is empty"
+		else:
+			out = ""
+			for ele in self.data:
+				out = out + " " + str(ele)
+			print(out[::-1])
+		return
 
 print "Enter the size of Queue"
 n = int(raw_input())
 q = Queue(n)
 while True:
-	print
 	print "Press E to enqueue an element"
 	print "Press D to dequeue an element"
+	print "Press P to display all elements of the queue"
 	print "Press X to exit"
 	opt = raw_input().strip()
 	if opt == "E":
@@ -59,5 +61,7 @@ while True:
 			print "Queue is empty"
 		else:
 			print "Element is", ele
+	if opt == "P":
+		q.display()
 	if opt == "X":
 		break
