03b5e27662a1781d5f2080f7186125f175ea52f1
Samy <syrazhd@gmail.com>
2018-04-21 18:16:07 +0200

    Comments added

--- code/unclassified/src/DiamondSquare.java
+++ code/unclassified/src/DiamondSquare.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 
+/**
+ * @author Samy Metadjer
+ * Diamond Square algorithm is a method allowing to generate
+ * heightmaps for computer graphics by using a 2D Grid.
+ */
 public class DiamondSquare{
     public static void main(String[] args){
         double[][] matrix;
@@ -19,6 +24,10 @@ public class DiamondSquare{
         }
     }
 
+    /**
+     * Generates random value for each cell of the given array.
+     * @param matrix the array to be manipulated
+     */
     public static void diamondSquare(double[][] matrix){
         int height = matrix.length;
         int boundary = 40;
@@ -26,6 +35,10 @@ public class DiamondSquare{
         int id;
         int shift;
 
+        /*
+        ** Set a random value to each corner of the matrix (Seeds values).
+        ** It's the first square step of the algorithm
+         */
         matrix[0][0] = (Math.random() * (boundary - (-boundary) )) + (-boundary);
         matrix[0][height-1] = (Math.random() * (boundary - (-boundary) )) + (-boundary);
         matrix[height-1][0] = (Math.random() * (boundary - (-boundary) )) + (-boundary);
@@ -33,7 +46,7 @@ public class DiamondSquare{
 
         while (i > 1) {
             id = i / 2;
-            for (int xIndex = id; xIndex < height; xIndex += i) { /* Beginning of the Diamond Step */
+            for (int xIndex = id; xIndex < height; xIndex += i) { // Beginning of the Diamond Step
                 for (int yIndex = id; yIndex < height; yIndex += i) {
                     double moyenne = (matrix[xIndex - id][yIndex - id]
                             + matrix[xIndex - id][yIndex + id]
@@ -42,7 +55,7 @@ public class DiamondSquare{
                     matrix[xIndex][yIndex] = moyenne + (Math.random() * (height - (-height) )) + (-height);
                 }
             }
-            for (int xIndex = 0; xIndex < height; xIndex += id) { /* Beginning of the Square Step */
+            for (int xIndex = 0; xIndex < height; xIndex += id) { // Beginning of the Square Step
                 if (xIndex % i == 0) {
                     shift = id;
                 } else {
