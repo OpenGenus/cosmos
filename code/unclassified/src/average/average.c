61176ffe30bece03d6ece15045fdf0154257abcb
Christian Bender <christianbender89@web.de>
2018-04-22 22:37:51 +0200

    some changes

--- code/unclassified/src/average/average.c
+++ code/unclassified/src/average/average.c
@@ -1,9 +1,12 @@
 // Part of Cosmos by OpenGenus Foundation
 // Code written by Adeen Shukla (adeen-s)
-#include<stdio.h>
+#include <stdio.h>
 
-int main() {
-	int n, sum = 0, tmp = 0, i;
+int 
+main() 
+{
+	int n, tmp = 0, i;
+    double sum = 0.0;
     
     printf("Enter the total number of inputs : ");
     scanf("%d", &n);
@@ -14,6 +17,7 @@ int main() {
 	   sum += tmp;		
     }
 
-    float avg = (float) sum / n;
-    printf("\nAverage = %f\n", avg);
+    printf("\nAverage = %2.2f\n", (sum / n));
+
+    return (0);
 }
