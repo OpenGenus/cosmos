3b2e4f31d361d1c89d6ca65ea94815af77e5b0a3
Christian Bender <christianbender89@web.de>
2018-04-22 22:54:23 +0200

    small changes

--- code/unclassified/src/magic_square/magic_square.py
+++ code/unclassified/src/magic_square/magic_square.py
@@ -1,8 +1,7 @@
 # MAgic Square generation for odd numbered squares
 import numpy as np
 
-print "Enter the dimension. Note that it should be odd : "
-N = input()
+N = input("Enter the dimension. Note that it should be odd : ")
 magic_square = np.zeros((N, N), dtype=int)
 
 n = 1
@@ -11,10 +10,10 @@ i, j = 0, N // 2
 while n <= N**2:
     magic_square[i, j] = n
     n += 1
-    newi, newj = (i - 1) % N, (j + 1) % N
-    if magic_square[newi, newj]:
+    new_i, new_j = (i - 1) % N, (j + 1) % N
+    if magic_square[new_i, new_j]:
         i += 1
     else:
-        i, j = newi, newj
+        i, j = new_i, new_j
 
 print(magic_square)
