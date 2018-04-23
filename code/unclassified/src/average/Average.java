be97960d61a91917ba885d98459e49c41b000026
Christian Bender <christianbender89@web.de>
2018-04-23 15:12:43 +0200

    changed the code as requested

--- code/unclassified/src/average/Average.java
+++ code/unclassified/src/average/Average.java
@@ -7,31 +7,30 @@ class Average
     * @param n number of elements.
     * @return the average calculated as sum(arr)/n
     */
-    static double avrg(Vector<Integer> arr)
+    static double getAverage(ArrayList<Integer> arr)
     {
-        if (arr.isEmpty()) return 0.0;
+        if (arr.isEmpty()) {
+            return 0.0;
+        } 
         double sum = 0;
-        double avg = 0.0;
-        final int SIZE = arr.size();
-        for(int i = 0;i < SIZE; i++)
+        for(int value : arr)
         {
-            sum += arr.get(i);
+            sum += value;
         }
         
-        return sum / SIZE;
+        return sum / arr.size();
     }
     public static void main(String args[])
     {
         int n;
         // int[] arr = new int[1000];
-        Vector<Integer> arr = new Vector<Integer>();
+        ArrayList<Integer> arr = new ArrayList<Integer>();
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter elements in array (-1)exit ");
         
         while ((n = scan.nextInt()) != -1)
             arr.add(n);
 
-        double avg = avrg(arr);
-        System.out.printf("Average = %2.2f\n",avg);
+        System.out.printf("Average = %2.2f\n",getAverage(arr));
     }
 }
