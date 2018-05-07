1361a8368763bb0ff6d015b8399f84773e5f14ae
Pier Paolo Ramon <ramonpierre@gmail.com>
2018-05-07 23:34:46 +0200

    [JS] Use .reduce to get the sum in 'average'

--- code/unclassified/src/average/average.js
+++ code/unclassified/src/average/average.js
@@ -1,14 +1,15 @@
 /* Part of Cosmos by OpenGenus Foundation */
 
 // finds the average of an array of numbers.
-//Using map function
+// Using reduce function
 function getAverage(numbers){
-    var sum = 0;
-    numbers.map(function(number){
-		sum += number;
-	})
+    var sum = numbers.reduce(function(sum, number) {
+        return sum += number;
+    }, 0);
+
     return (sum / numbers.length);
 }
 
 nums = [10,20,30,40,50];
+
 console.log(getAverage(nums));
