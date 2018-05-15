8bacfa37718211be96453c55cb924357c4f19c7f
Christian Bender <christianbender89@web.de>
2018-05-15 21:20:26 +0200

    changed formatting

--- code/mathematical_algorithms/src/count_digits/count_digits.c
+++ code/mathematical_algorithms/src/count_digits/count_digits.c
@@ -2,22 +2,24 @@
 // Part of Cosmos by OpenGenus Foundation
 // Code written by Adeen Shukla (adeen-s)
 
-int countDigits(unsigned long n) {
+int 
+countDigits(unsigned long n) {
 	if(n == 0) {
-		return 1;
+		return (1);
 	}
 	int count = 0;
 	while(n != 0) {
 		count++;
 		n /= 10;
-    	}
-	return count;
+    }
+	return (count);
 }
 
-int main(){
+int 
+main(){
 	unsigned long n;
 	printf("\nEnter a number\n");	
 	scanf("%lu", &n);
 	printf("\nThe number of digits is --> %d\n", countDigits(n));
-	return 0;
+	return (0);
 }
