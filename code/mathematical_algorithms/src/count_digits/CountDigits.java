d5b8967c8d28ca412e48203583f411c033abe04b
Christian Bender <christianbender89@web.de>
2018-05-15 21:26:24 +0200

    added spaces

--- code/mathematical_algorithms/src/count_digits/CountDigits.java
+++ code/mathematical_algorithms/src/count_digits/CountDigits.java
@@ -1,6 +1,6 @@
 /* Part of Cosmos by OpenGenus Foundation */
 public class CountDigits {
-    
+
     public static void main(String args[]) {
         System.out.println(countDigits(0)); // => 1
         System.out.println(countDigits(2)); // => 1
@@ -9,11 +9,11 @@ public class CountDigits {
     
     public static int countDigits(int n)
     {
-        if(n == 0) 
+        if (n == 0) 
             return 1;
     	
         int count = 0;
-        while(n != 0) {
+        while (n != 0) {
             count++;
             n /= 10;
         }
