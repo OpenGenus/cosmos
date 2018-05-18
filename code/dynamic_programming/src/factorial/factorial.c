41851e126865377f1989bdee1c8ee4123d563938
Deepak Yadav <deepyadav442@gmail.com>
2018-05-18 17:32:23 +0530

    Update factorial.c

--- code/dynamic_programming/src/factorial/factorial.c
+++ code/dynamic_programming/src/factorial/factorial.c
@@ -1,21 +1,13 @@
 #include <stdio.h>
-
-int
-factorial(int n)
+int main()
 {
-	if (n == 0 || n == 1)
-		return (1);
-	else
-		return (n * factorial(n - 1));
-}
-
-int
-main()
-{
-	int n;
+	int number;
 	printf("Enter a Whole Number: ");
-	scanf("%d", &n);
-	printf("%d! = %d\n", n, factorial(n));
-
+	scanf("%d",&number);
+    	int result[100001]={0};
+   	result[0]=1;
+  	for(int i=1;i<=number;i++)
+        	result[i]=i*result[i-1];
+	printf("%d! = %d\n", number, result[number]);
 	return (0);
 }
