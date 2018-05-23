98f1122a2608e8c7769e88208a1f826d9f1bc678
PANKAJ SHARMA <sharmapankaj090@gmail.com>
2018-05-23 10:40:31 -0700

    Update deque.c
    Formatted According to style rules . Global Variables are not removed as they'll make the code untidy .

--- code/data_structures/src/queue/double_ended_queue/deque.c
+++ code/data_structures/src/queue/double_ended_queue/deque.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <conio.h> 
 #define MAX 10 
+
 int deque[MAX];
 int left =-1 ;
 int right = -1 ;
-void input_deque(void);
-void output_deque(void); 
-void insert_left(void);
-void insert_right(void); 
-void delete_left(void); 
-void delete_right(void); 
+
+void inputdeque(void);
+void outputdeque(void); 
+void insertleft(void);
+void insertright(void); 
+void deleteleft(void); 
+void deleteright(void); 
 void display(void); 
 
-int main() 
+int main( ) 
 { 
 	int option; 
 	printf("\n *****MAIN MENU*****"); 
@@ -20,16 +22,17 @@ int main()
 	printf("\n 2.Output restricted deque"); 
 	printf("\n Enter your option : "); 
 	scanf("%d",&option); 
-	switch(option) 
+	
+	switch (option) 
 	{  
-		case 1:   input_deque();   
+		case 1:   inputdeque();   
 			    	break;  
-		case 2:   output_deque();   
+		case 2:   outputdeque();   
 					break; 
 	} return 0;
 } 
 
-void input_deque() 
+void inputdeque( ) 
 { 
 	int option;
 	do 
@@ -43,65 +46,67 @@ void input_deque()
 		printf("\n Enter your option : ");  
 		
 		scanf("%d",&option);  
-		switch(option)  
+		switch (option)  
 		{   
-			case 1:    insert_right();    
+			case 1:    insertright();    
 						break;   
-			case 2:    delete_left();    
+			case 2:    deleteleft();    
 						break;   
-			case 3:    delete_right();    
+			case 3:    deleteright();    
 						break;   
 			case 4:    display();    
 						break;  
 		}
 		 
-	}while(option!=5);
+	}while (option!=5);
 } 
 
-void output_deque() 
+void outputdeque( ) 
 { 
 	int option; 
 	do 
 	{  
-	printf("\n\n OUTPUT RESTRICTED DEQUE");  
-	printf("\n 1.Insert at right");  
-	printf("\n 2.Insert at left");  
-	printf("\n 3.Delete from left");  
-	printf("\n 4.Display");  
-	printf("\n 5.Quit");  
-	printf("\n Enter your option : ");  
-	scanf("%d",&option);  
-	switch(option)  
-		{   
-		case 1:    insert_right();
-		  	    	break;   
-		case 2:    insert_left();    
-					break;   
-		case 3:    delete_left(); 
-		   			break;   
-		case 4:    display();    
-					break;  
-		}
-	}while(option!=5); 
+		printf("\n\n OUTPUT RESTRICTED DEQUE");  
+		printf("\n 1.Insert at right");  
+		printf("\n 2.Insert at left");  
+		printf("\n 3.Delete from left");  
+		printf("\n 4.Display");  
+		printf("\n 5.Quit");  
+		printf("\n Enter your option : ");  
+	
+		scanf("%d",&option);  
+		switch(option)  
+			{   
+				case 1:    insertright();
+				  	    	break;   
+				case 2:    insertleft();    
+							break;   
+				case 3:    deleteleft(); 
+				   			break;   
+				case 4:    display();    
+							break;  
+			}
+	}while (option!=5); 
 } 
-void insert_right() 
+
+void insertright( ) 
 { 
 	int val; 
 	printf("\n Enter the value to be added:"); 
 	scanf("%d", &val); 
-	if( (left == 0 && right == MAX-1 ) || (left == right+1) )
+	if ( (left == 0 && right == MAX-1 ) || (left == right+1) )
 	{  
 		printf("\n OVERFLOW");
 	    return; 
 	}
-	if( left == -1) // Queue is Empty Inititally
+	if (left == -1) // Queue is Empty Inititally
 	{  
 		left = 0;  
 		right = 0;
     } 
 	else 
 	{
-  		if(right == MAX-1) 							//right is at last position of queue    
+  		if (right == MAX-1) 							//right is at last position of queue    
 			right = 0;
 	  	else   
 	  		right = right+1; 
@@ -109,7 +114,7 @@ void insert_right()
 	deque[right] = val ; 
 } 
 
-void insert_left() 
+void insertleft( ) 
 { 
 	int val;
 	 printf("\n Enter the value to be added:"); 
@@ -135,15 +140,17 @@ void insert_left()
 	deque[left] = val; 
 }
  
-void delete_left() 
+void deleteleft( ) 
 { 
 	if ( left == -1 ) 
 	{  
 		printf("\n UNDERFLOW");  
 		return ; 
 	} 
+
 	printf("\n The deleted element is : %d", deque[left]);
-	if(left == right) 										/*Queue has only one element */ 
+
+	if (left == right) 										/*Queue has only one element */ 
 	{  
 		left = -1 ;  
 		right = -1 ; 
@@ -156,30 +163,32 @@ void delete_left()
 		left = left+1; 
 	} 
 } 	
-void delete_right() 
+
+void deleteright() 
 { 
 	if ( left == -1 ) 
 	{  
 		printf("\n UNDERFLOW");
 		return ; 
 	} 
+	
 	printf("\n The element deleted is : %d", deque[right]); 
 	
-	if(left == right) /*queue has only one element*/ 
+	if (left == right) /*queue has only one element*/ 
 	{  
 		left = -1 ;  
 		right = -1 ; 
 	} 
 	else 
 	{  
-		if(right == 0)   
+		if (right == 0)   
 			right = MAX - 1 ;  
 		else   
 			right = right - 1 ; 
 	} 
 } 
 
-void display() 
+void display( ) 
 { 
 	int front = left, rear = right;
 	if ( front == -1 ) 
@@ -188,9 +197,9 @@ void display()
 		 return; 
 	} 
 	printf("\n The elements of the queue are : ");
-	if(front <= rear ) 
+	if (front <= rear ) 
 	{  
-		while(front <= rear)  
+		while (front <= rear)  
 		{   
 			printf("%d ",deque[front]);   
 			front++;  
@@ -198,13 +207,13 @@ void display()
 	} 
 	else 
 	{  
-		while ( front <= MAX - 1)  
+		while (front <= MAX - 1)  
 		{   
 			printf("%d ", deque[front]);   
 			front++;  
 		}  
 		front = 0;  
-		while(front <= rear)  
+		while (front <= rear)  
 		{   
 			printf("%d ",deque[front]);   
 			front++;  
