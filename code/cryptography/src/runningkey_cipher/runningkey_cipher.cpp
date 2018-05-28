880d64887010e9e39521f1bacfac264e69d36f41
John <39648915+Technist@users.noreply.github.com>
2018-05-28 17:13:21 +0700

    Create runningkey_cipher.cpp

---
+++ code/cryptography/src/runningkey_cipher/runningkey_cipher.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string plaintext = "DEFENDTHEEASTWALLOFTHECASTLE";
+    std::string key = "HOWDOESTHEDUCKKNOWTHATSAIDVI";
+    std::string ciphertext;
+    
+    for (int i = 0; i < plaintext.size(); i++)
+    {
+        if (isalpha(plaintext[i]) && isalpha(key[i]))
+        {
+            int plaintextIndex = toupper(plaintext[i]) - 'A';
+            int keyIndex = toupper(key[i]) - 'A';
+            
+            int resultIndex = (plaintextIndex + keyIndex) % 26;
+            
+            ciphertext += char(resultIndex + 'A');
+        }
+    }
+    
+    std::cout << ciphertext << '\n';    //  prints 'KSBHBHLALIDMVGKYZKYAHXUAAWGM'
+}
