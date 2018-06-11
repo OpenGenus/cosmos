b43c438b5b12d4531ae751423d1c331a5812df8b
Vasu Dev <33747133+DeVasu@users.noreply.github.com>
2018-06-11 22:52:41 +0530

    Algorithm for Inserting Elements at the Tail

---
+++ code/data_structures/src/list/singly_linked_list/operations/insertion/insertion_at_tail.cpp
@@ -0,0 +1,66 @@
+/*
+  C++ program to add elements at the Tail of a Singly Linked List
+  Part of Cosmos by OpenGenus Foundation
+*/
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+
+struct node{
+             int data;
+             node *next;
+
+            };
+
+void printList(node *head)
+{
+    cout<<"This is your current List\n";
+    while(head!=NULL)
+    {
+        cout<<head->data<<" => ";
+        head=head->next;
+    }
+    cout<<"NULL\n";
+}
+
+void insertAtTail(node **head,int data)
+{
+    node *newptr = new node;
+    newptr->data=data;
+    newptr->next=NULL;
+
+    node *dummy = *head;                    //dummy variables is used to traverse to the end of the List
+
+    while(dummy->next!=NULL) dummy=dummy->next; //traversing to the last Element of the List
+    dummy->next = newptr;                       //pointing the last Element to the new node
+    return;
+}
+
+int main()
+{
+    node *head = new node;
+    int choice(0),first(1),data;            //first is to check if its the first item of the List
+    cout<<"List is currently empty. Press 1 to add an Element\n";
+    cin>>choice;
+    while(choice)
+    {
+        if(first==1){
+            cout<<"\nEnter the Data Element\n";
+            cin>>head->data;
+            head->next=NULL;
+            first=0;
+        }
+       // printList(head);
+        else{
+            cout<<"\nEnter the Data Element\n";
+            cin>>data;
+            insertAtTail(&head,data);
+        }
+       cout<<"Do you want to add more Elements at the TAIL?(0 for NO, 1 for YES)";
+       cin>>choice;
+    }
+    printList(head);
+}
+
