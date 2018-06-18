37e530ecca647e5fcacdb90ae5e00587d7b7e5c3
Yash Shah <yashah1234@gmail.com>
2018-06-18 05:52:10 +0400

    Encapsulated AVLTree in a single class

--- code/data_structures/src/tree/binary_tree/avl_tree/avl_tree.py
+++ code/data_structures/src/tree/binary_tree/avl_tree/avl_tree.py
@@ -1,156 +1,217 @@
-class Node():
-	def __init__(self, d = 0):
-		self.key = d
-		self.height = 1
+#!/usr/bin/python
+"""
+	AVL Tree in Python
+	18.06.18
+	Yash Shah(https://github.com/yashshah1)
+"""
+
+class Node(object):
+	def __init__(self, key):
 		self.left = None
 		self.right = None
-def height(node):
-	if node is None:
-		return 0
-	return node.height
+		self.key = key
 
-
-def right_rotate(node):
-	x = node.left
-	T2 = x.right
-
-	x.right = node
-	node.left = T2
-	
-	node.height = max(height(node.left), height(node.right)) + 1
-	x.height = max(height(x.left), height(x.right)) + 1
-
-	return x
-
-def left_rotate(node):
-	y = node.right
-	T2 = y.left
-
-	y.left = node
-	node.right = T2
-
-	node.height = max(height(node.left), height(node.right)) + 1
-	y.height = max(height(y.left), height(y.right)) + 1
-
-	return y
-
-def get_balance(node):
-	if node is None:
-		return 0
-	return height(node.left) - height(node.right)
+class AVLTree(object):
+	def __init__(self, key = None):
+		self.node = None 
+		self.height = -1  
+		self.balance = 0; 
 		
-def insert(node, key):
-	if node is None:
-		return Node(key)
-
-	if key < node.key:
-		node.left = insert(node.left, key)
-	elif key > node.key:
-		node.right = insert(node.right, key)
-	else: 
-		return node
-
-	node.height = 1 + max(height(node.left), height(node.right))
+		if key: 
+			self.insert(i)
 	
-	balance = get_balance(node)
-
-	if balance > 1 and key < node.left.key:
-		return right_rotate(node)
-		
-	if balance < -1 and key > node.right.key:
-		return left_rotate(node)
-
-	if balance > 1 and key > node.left.key:
-	
-		node.left = left_rotate(node.left)
-		return right_rotate(node)
-
-	if balance < -1 and key < node.right.key:
-		node.right = right_rotate(node.right)
-		return left_rotate(node)
-	return node
- 
-def min_value_node(node):
-	current = node
-
-	while current.left is not None:
-	   current = current.left
-
-	return current
-
-def delete_node(root, key):
-	if root is None:
-		return root
-
-	if key < root.key:
-		root.left = delete_node(root.left, key)
-
-	elif key > root.key:
-		root.right = delete_node(root.right, key)
-
-	else:
-		if root.left is None or root.right is None:
-			temp = None
-			if temp == root.left:
-				temp = root.right
-			else:
-				temp = root.left
-				
-			if temp is None:
-				temp = root
-				root = null
-			else:
-				root = temp 
+	def height(self):
+		if self.node:
+			return self.node.height
 		else:
-			temp = min_value_node(root.right)
-			root.key = temp.key
-			root.right = delete_node(root.right, temp.key)
-
-	if root is None:
-		return root
-
-	root.height = max(height(root.left), height(root.right)) + 1
-
-	balance = get_balance(root)
-
-	if balance > 1 and get_balance(root.left) >= 0:
-		return right_rotate(root)
-
-	if balance > 1 and get_balance(root.left) < 0:
-		root.left = left_rotate(root.left)
-		return right_rotate(root)
-
-	if balance < -1 and get_balance(root.right) <= 0:
-		return left_rotate(root)
-
-	if balance < -1 and get_balance(root.right) > 0:
-		root.right = right_rotate(root.right)
-		return left_rotate(root)
-	return root
-
-
-def pre_order(node):
-	if node is not None:
-		print str(node.key),
-		pre_order(node.left)
-		pre_order(node.right)
+			return 0
+	def is_leaf(self):
+		if self.height == 0:
+			return 0
+		else:
+			return 1
+			
+	def rrotate(self):
+		"""
+		Right-rotates the tree
+		"""
+		node_1 = self.node 
+		node_2 = self.node.left.node 
+		node_3 = node_2.right.node 
 		
-def main():
-	print "Enter numbers (space seperated) to be added to the AVL Tree: ",
-	arr = map(int, raw_input().strip().split())
+		self.node = node_2
+		node_2.right.node = node_1
+		node_1.left.node = node_3
 	
-	tree = Node(arr[0])
-	for i in arr[1::]:
-		tree = insert(tree, i)
- 	print "pre_order traversal of constructed tree is : ",
-	pre_order(tree)
-	print
-	tree = delete_node(tree, arr[0])
-	print
-	print "pre_order traversal after deletion of " + str(arr[0]) + ": ",
-	pre_order(tree)
+	def lrotate(self):
+		"""
+		Left-rotates the tree
+		"""
+		node_1 = self.node 
+		node_2 = self.node.right.node 
+		node_3 = node_2.left.node 
+		
+		self.node = node_2 
+		node_2.left.node = node_1 
+		node_1.right.node = node_3
 	
-if __name__ == "__main__":
-	main()
+	def balance_the_tree(self):
+		"""
+		The actual "AVL" stuff of rebalancing.
+		refer to: https://www.geeksforgeeks.org/avl-tree-set-2-deletion/
+		"""
+		
+		# key inserted. Let's check if we're balanced
+		self.update_heights(False)
+		self.update_balances(False)
+		
+		while self.balance < -1 or self.balance > 1: 
+			if self.balance > 1:
+				if self.node.left.balance < 0:  
+					self.node.left.lrotate() # II
+					self.update_heights()
+					self.update_balances()
+				self.rrotate() # I
+				self.update_heights()
+				self.update_balances()
+				
+			if self.balance < -1:
+				if self.node.right.balance > 0:  
+					self.node.right.rrotate() # we're in case III
+					self.update_heights()
+					self.update_balances()
+				self.lrotate() # IV
+				self.update_heights()
+				self.update_balances()
+			
+	def insert(self, key):
+		#First do normal BST, according to key
+		if not self.node:
+			self.node = Node(key)
+			self.node.left = AVLTree()
+			self.node.right = AVLTree()
+			
+		elif key < self.node.key:
+			self.node.left.insert(key)
+		else:
+			self.node.right.insert(key)
+			
+		
+		self.balance_the_tree()
 
-  
+
+	def delete(self, key):
+		if self.node: #check for a node
+			if self.node.key == key: #If key is root
+				if (not self.node.left.node) and (not self.node.right.node):
+					self.node = None #No children, YAY, remove root
+				elif not self.node.left.node:
+					self.node = self.node.right.node #If left child, make it the right
+				elif not self.node.right.node:
+					self.node = self.node.left.node
+				
+				# both children present. Find logical successor
+				else:  
+					replacement = self.min_value_node(self.node.right.node) #read func_doc
+					if replacement:  
+						self.node.key = replacement.key 						
+						self.node.right.delete(replacement.key)
+				self.balance_the_tree()  
+				
+			elif key < self.node.key: #key is in left tree
+				self.node.left.delete(key)  
+			else: #key is in right tree
+				self.node.right.delete(key)
+			
+			self.balance_the_tree()
+	
+			
+	def update_heights(self, recurse = True):
+		if self.node: 
+			if recurse: 
+				if self.node.left: 
+					self.node.left.update_heights()
+				if self.node.right:
+					self.node.right.update_heights()
+			
+			self.height = max(self.node.left.height, self.node.right.height) + 1 
+		else: 
+			self.height = -1 
+			
+	def update_balances(self, recurse = True):
+		if self.node:
+			if recurse: 
+				if self.node.left:
+					self.node.left.update_balances()
+				if self.node.right:
+					self.node.right.update_balances()
+
+			self.balance = self.node.left.height - self.node.right.height 
+		else: 
+			self.balance = 0 
+
+
+	def check_balanced(self):
+		if not self == None or not self.node:
+			return True
+		
+		self.update_heights()
+		self.update_balances()
+		if abs(self.balance) < 2:
+			if self.node.left.check_balanced() and self.node.right.check_balanced():
+				return True
+		return False
+		
+	def inorder_traverse(self):
+		if self.node:
+			self.node.left.inorder_traverse()
+			print self.node.key, 
+			self.node.right.inorder_traverse()
+			
+	def min_value_node(self, node):
+		"""
+		Gets the minimum value node 
+		"""
+		if node: #safety check
+			while node.left:
+				if not node.left.node:
+					return node 
+				else: 
+					node = node.left.node  
+		return node 
+
+#Driver:
+if __name__ == "__main__":
+	my_tree = AVLTree()
+	l = [7, 5, 2, 6, 3, 4, 1, 8, 9, 0]
+	print "Adding [7, 5, 2, 6, 3, 4, 1, 8, 9, 0]"
+	for i in l:
+		my_tree.insert(i)
+	my_tree.inorder_traverse()
+	print
+	print
+	
+	my_tree.delete(3)
+	print "Delete 3"
+	my_tree.inorder_traverse()
+	print
+	print
+	print "Delete 4"
+	my_tree.delete(4)
+	my_tree.inorder_traverse()
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
