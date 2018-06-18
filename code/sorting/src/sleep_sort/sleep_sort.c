08cb1935305fc5ec3fa0343aafd7b6f8814f3211
Ritwik Sharma <ritwik.1204@gmail.com>
2018-06-18 22:20:10 +0530

    Add macro for windows.h

--- code/sorting/src/sleep_sort/sleep_sort.c
+++ code/sorting/src/sleep_sort/sleep_sort.c
@@ -1,9 +1,10 @@
 /* Part of Cosmos by OpenGenus Foundation */
 
 #include <stdio.h>
-//Uncomment when using in your system
-//#include <windows.h>
-//#include <process.h>
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+    #include <windows.h>
+    #include <process.h>
+#endif
 
 void routine(void *a)
 {
