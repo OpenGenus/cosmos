143d27da7facf30fa42439104508befe47734ad8
Bharat Arya <arya.bharat@rocketmail.com>
2018-06-26 23:35:57 +0530

    Update solution.cpp

--- code/online_challenges/src/Hacker Rank/Algo_DP_Maxsubarray/solution.cpp
+++ code/online_challenges/src/Hacker Rank/Algo_DP_Maxsubarray/solution.cpp
@@ -1,54 +1,44 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
+#include<bits/stdc++.h>
 using namespace std;
 
-
 int main() 
-{
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int n;
-        cin>>n;
-        vector <int> arr(n);
-        int flag=0;
-        for(int i=0;i<n;i++)
-            {
-            cin>>arr[i];
-            if(arr[i]>=0)
+    {  //t is the number of test case
+        int t;
+        cin>>t;
+        while(t--){
+            //n is the size of array
+            int n;
+            cin>>n;
+            //arr is vector of size n
+            vector <int> arr(n);
+            //This Algo is Kaden algorithm 
+            //Also known as Minimum Sum Subarray
+            int flag=0;
+            for(int i=0;i<n;i++){
+                cin>>arr[i];
+                if(arr[i]>=0)
                 flag++;
             }
-        if(flag==0)
-        {
+            if(flag==0){
             sort(arr.begin(),arr.end());
             cout<<arr[n-1]<<" "<<arr[n-1]<<endl;
             continue;
-        }
+            }
         
-        int sum,ans,ans1;
-        sum=ans=ans1=0;
-        for(int i=0;i<n;i++)
-        {
-            if(sum+arr[i]>0)
-                sum=sum+arr[i];
-            else
-                sum=0;
-            if(arr[i]>=0)
-                ans1=ans1+arr[i];
-            
-            ans=max(ans,sum);
-            
+            int sum,ans,ans1;
+            sum=ans=ans1=0;
+            for(int i=0;i<n;i++){
+                if(sum+arr[i]>0)
+                    sum=sum+arr[i];
+                else
+                    sum=0;
+                if(arr[i]>=0)
+                    ans1=ans1+arr[i];
+                
+                ans=max(ans,sum);
         }
-        
         
         cout<<ans<<" "<<ans1<<endl;
-        
-    }
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    }  
     return 0;
 }
-
