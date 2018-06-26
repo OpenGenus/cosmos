3c908e9135f52f6d8a42c88460a6cb2fdb68bf16
Bharat Arya <arya.bharat@rocketmail.com>
2018-06-27 00:07:36 +0530

    Update Solution.cpp

--- code/online_challenges/src/Hacker Rank/DS_Array_Manipulation/Solution.cpp
+++ code/online_challenges/src/Hacker Rank/DS_Array_Manipulation/Solution.cpp
@@ -1,35 +1,28 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
+
 using namespace std;
+long a[400009];
 
-
-int main()
-{
-     unsigned long long n,t;
-    cin>>n>>t;
-    vector <unsigned long long> v(n,0);
-    unsigned long long  maxi=0;
-    for( unsigned long long i=0;i<t;i++)
-    {
-        unsigned long long a,b,k;
-        cin>>a>>b>>k;
-        v[a-1]+=k;
-        if(b<=n)
-            v[b]-=k;
-       // cout<<endl;
-        
+int main() {
+    int n;
+    int m;
+    cin >> n >> m;
+    vector < pair < int, int > > v;
+    for(int a0 = 0; a0 < m; a0++){
+        int a;
+        int b;
+        int k;
+        cin >> a >> b >> k;
+        v.push_back(make_pair(a, k));
+        v.push_back(make_pair(b+1, -1 * k));
     }
-    for(unsigned long long j=1;j<n;j++)
-        v[j]=v[j-1]+v[j];
-      for(unsigned long long j=0;j<n;j++)
-        {
-        maxi=max(maxi,v[j]);
-      }
-    
-    cout<<maxi;
-    /* Enintter your code here. Read input from STDIN. Print output to STDOUT */   
+    long mx = 0, sum = 0;
+    sort(v.begin(), v.end());
+    for(int i=0 ; i<2*m; i++) {
+        sum += v[i].second;
+        mx = max(mx, sum);
+    }
+        
+    cout<<mx<<endl;
     return 0;
 }
