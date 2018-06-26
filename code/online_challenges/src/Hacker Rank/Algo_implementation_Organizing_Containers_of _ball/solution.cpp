6a16414bea0d6411d26312e27dc5d70d302c1aff
Bharat Arya <arya.bharat@rocketmail.com>
2018-06-26 23:42:22 +0530

    Update solution.cpp

--- code/online_challenges/src/Hacker Rank/Algo_implementation_Organizing_Containers_of _ball/solution.cpp
+++ code/online_challenges/src/Hacker Rank/Algo_implementation_Organizing_Containers_of _ball/solution.cpp
@@ -1,47 +1,41 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
+#include<bits/stdc++.h>
 using namespace std;
-
-
 int main() 
 {
+    //t is the number of test case
     int t;
     cin>>t;
     while(t--)
     {
-        int n;
+        int n;//size of metrix n * n
         cin>>n;
         vector <int> a(n);
         vector <int> b(n);
-        for(int i=0;i<n;i++)
-        {
+        for(int i=0;i<n;i++){
             int sum=0;
-            for(int j=0;j<n;j++)
-            {
-                int temp;cin>>temp;
+            for(int j=0;j<n;j++){
+                int temp;
+                cin>>temp;
                 a[j]=a[j]+temp;
                 sum=sum+temp;
             }    
             b[i]=sum;
         }
-            sort(a.begin(),a.end());
-    sort(b.begin(),b.end());
-    int flag=0;
-    for(int i=0;i<n;i++)
-    {
-        if(a[i]!=b[i])
-            {flag++;break;}
+        sort(a.begin(),a.end());//used Sort function of C++ STL
+        sort(b.begin(),b.end());
+        int flag=0;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]!=b[i]){
+                flag++;
+                break;
+            }
+        }
+        if(flag==0)
+            cout<<"Possible"<<endl;
+        else
+            cout<<"Impossible"<<endl;
     }
-    if(flag==0)
-        cout<<"Possible"<<endl;
-    else
-        cout<<"Impossible"<<endl;
-    }
-
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    /* Complexity =  O(T*(N^2))*/   
     return 0;
 }
-
