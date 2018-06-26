bc99dbf6bdc20eae001b4d36ca0c0a2882e874dc
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2018-06-26 11:11:30 +0530

    Update search_element_python.py
    Using Node adapters as per suggestion and applying `is not` and `is` keywords in necessary places. Also added testing utility

--- code/data_structures/src/list/singly_linked_list/operations/find/search_element_python.py
+++ code/data_structures/src/list/singly_linked_list/operations/find/search_element_python.py
@@ -18,18 +18,26 @@ class Node:
         ''' Node representation as required'''
         return (self.data)
 
+class NodeAdapter1(Node):
+    def __bool__(self):
+        return False
 
+class NodeAdapter2(Node):
+    def __bool__(self):
+        return True
+    
 class SinglyLinkedList:
-    def __init__(self):
+    def __init__(self,NodeType):
         self.head = None
+        self._NodeType = NodeType
 
     def append(self, data):
         '''
         Inserts New data at the ending of the Linked List
         Takes O(n) time
         '''
-        new_node = Node(data=data)
-        if not self.head:
+        new_node = _NodeType(data = data)
+        if self.head is None:
             self.head = new_node
             return
 
@@ -64,10 +72,32 @@ class SinglyLinkedList:
         '''
         all_nodes = []
         current_node = self.head
-        while current_node:
+        while current_node is not None:
             all_nodes.append(str(current_node.data))
             current_node = current_node.next
         if len(all_nodes) > 0:
             return ' -> '.join(all_nodes)
         else:
             return ""
+
+def main():
+    lst1 = SinglyLinkedList(Node)
+    lst1.append(10)
+    lst1.append(20)
+    lst1.append(30)
+    lst1.find(10)
+
+    lst2 = SinglyLinkedList(NodeAdapter1)
+    lst2.append(10)
+    lst2.append(20)
+    lst2.append(30)
+    lst2.find(20)
+
+    lst3 = SinglyLinkedList(NodeAdapter2)
+    lst3.append(10)
+    lst3.append(20)
+    lst3.append(30)
+    lst3.find(30)
+
+if __name__ == "__main__":
+    main()
