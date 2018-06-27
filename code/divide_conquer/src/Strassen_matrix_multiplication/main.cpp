ea4fe5ed30e2b294ce1e9fa7cad9960df2da9724
Jayakrishna Sahit <jayakrishna.sahit@iitgn.ac.in>
2018-06-27 17:04:23 +0530

    Update main.cpp

--- code/divide_conquer/src/Strassen_matrix_multiplication/main.cpp
+++ code/divide_conquer/src/Strassen_matrix_multiplication/main.cpp
@@ -11,18 +11,16 @@
 #include <iostream>
 #include <random>
 #include <vector>
-using namesapce std;
-using namespace std::chrono;
 
-vector<vector<int>> generator(int m, int n){
+std::vector<std::vector<int>> generator(int m, int n) {
     std::mt19937 rng;
     rng.seed(std::random_device()());
     std::uniform_int_distribution<std::mt19937::result_type> dist6(0,1);
-    vector<vector<int>> ans;
-    for( int i = 0; i < m; i++){
-        vector<int> row;
-        for(int j = 0;j < n ; j++){  
-            if (i+j % 5 != 5) {
+    std::vector<std::vector<int>> ans;
+    for (int i = 0;i < m;i++) {
+        std::vector<int> row;
+        for (int j = 0; j < n; j++) {  
+            if ((i + j) % 5 != 5) {
                 row.push_back(dist6(rng));
             }
             else {
@@ -35,16 +33,14 @@ vector<vector<int>> generator(int m, int n){
 }
 
 
-int f(const vector<vector<int>>& a){
+int f(const std::vector<std::vector<int>>& a) {
     return a[0][0];
 }
-vector<vector<int>> retriever( const vector<vector<int>>& matrix, int x, int y){
-    vector<vector<int>> ans;
-    for (int i = 0; i < x; i++ )
-    {
-        vector<int> row;
-        for (int j = 0; j < y; j++ )
-        {
+std::vector<std::vector<int>> retriever(const std::vector<std::vector<int>>& matrix, int x, int y) {
+    std::vector<std::vector<int>> ans;
+    for (int i = 0; i < x; i++) {
+        std::vector<int> row;
+        for (int j = 0; j < y; j++) {
             row.push_back(matrix[i][j]);
         }
         ans.push_back(row);
@@ -53,36 +49,34 @@ vector<vector<int>> retriever( const vector<vector<int>>& matrix, int x, int y){
 }
 
 
-vector<vector<int>> converter(const vector<vector<int>>& matrix){
-    auto no_of_rows = matrix.size();
-    auto no_of_cols = matrix[0].size();
-    vector<vector<int>>::size_type t = 1;
-    vector<vector<int>>::size_type p;
-    if(x > y){
-        p = no_of_rows;
+std::vector<std::vector<int>> converter(const std::vector<std::vector<int>>& matrix) {
+    auto noOfRows = matrix.size();
+    auto noOfCols = matrix[0].size();
+    std::vector<std::vector<int>>::size_type t = 1;
+    std::vector<std::vector<int>>::size_type p;
+    if(x > y) {
+        p = noOfRows;
     }
-    else{
-        p = no_of_cols;
+    else {
+        p = noOfCols;
     }
-    while( t < p){
-        t = t*2;
+    while (t < p) {
+        t *= 2;
     }
     p = t;
     int flag = 0;
-    vector<vector<int>> ans;
-    for ( std::vector<std::vector<int>>::size_type i = 0; i < p; i++ )
-    {
-        if(i >= no_of_rows){
+    std::vector<std::vector<int>> ans;
+    for (vector<vector<int>>::size_type i = 0; i < p; i++) {
+        if (i >= noOfRows) {
             flag = 1;
         }
-        vector<int> row;
-        for ( std::vector<int>::size_type j = 0; j < p; j++ )
-        {
-            if(flag == 1){
+        std::vector<int> row;
+        for (std::vector<int>::size_type j = 0; j < p; j++) {
+            if (flag == 1) {
                 row.push_back(0);
             }
             else{
-                if(j >= no_of_cols){
+                if (j >= noOfCols) {
                     row.push_back(0);
                 }
                 else{
@@ -95,24 +89,20 @@ vector<vector<int>> converter(const vector<vector<int>>& matrix){
     return ans;
 }
 
-void print(const vector<vector<int>>& grid){
-    for ( std::vector<std::vector<int>>::size_type i = 0; i < grid.size(); i++ )
-    {
-        for ( std::vector<int>::size_type j = 0; j < grid[i].size(); j++ )
-        {
+void print(const std::vector<std::vector<int>>& grid) {
+    for (std::vector<std::vector<int>>::size_type i = 0; i < grid.size(); i++) {
+        for (std::vector<int>::size_type j = 0; j < grid[i].size(); j++) {
             std::cout << grid[i][j] << ' ';
         }
         std::cout << std::endl;
     }
 }
 
-vector<vector<int>> add(const vector<vector<int>>& a, const vector<vector<int>>& b){
-    vector<vector<int>> ans;
-    for ( std::vector<std::vector<int>>::size_type i = 0; i < a.size(); i++ )
-    {
+std::vector<std::vector<int>> add(const std::vector<std::vector<int>>& a, const std::vector<std::vector<int>>& b) {
+    std::vector<std::vector<int>> ans;
+    for (std::vector<std::vector<int>>::size_type i = 0; i < a.size(); i++) {
         vector<int> row;
-        for ( std::vector<int>::size_type j = 0; j < a[i].size(); j++ )
-        {
+        for (std::vector<int>::size_type j = 0; j < a[i].size(); j++) {
             int temp = a[i][j] + b[i][j];
             row.push_back(temp);
         }
@@ -121,13 +111,11 @@ vector<vector<int>> add(const vector<vector<int>>& a, const vector<vector<int>>&
     return ans;
 }
 
-vector<vector<int>> subtract(const vector<vector<int>>& a, const vector<vector<int>>& b){
-    vector<vector<int>> ans;
-    for ( std::vector<std::vector<int>>::size_type i = 0; i < a.size(); i++ )
-    {
+std::vector<std::vector<int>> subtract(const std::vector<std::vector<int>>& a, const std::vector<std::vector<int>>& b) {
+    std::vector<std::vector<int>> ans;
+    for (std::vector<std::vector<int>>::size_type i = 0; i < a.size(); i++) {
         vector<int> row;
-        for ( std::vector<int>::size_type j = 0; j < a[i].size(); j++ )
-        {
+        for (std::vector<int>::size_type j = 0; j < a[i].size(); j++) {
             int temp = a[i][j] - b[i][j];
             row.push_back(temp);
         }
@@ -138,17 +126,14 @@ vector<vector<int>> subtract(const vector<vector<int>>& a, const vector<vector<i
 
 
 
-vector<vector<int>> naive_multi(const vector<vector<int>>& a, const vector<vector<int>>& b){
+std::vector<std::vector<int>> naive_multi(const std::vector<std::vector<int>>& a, const std::vector<std::vector<int>>& b) {
     int s = 0;
-    vector<vector<int>> ans;
-    for ( std::vector<std::vector<int>>::size_type i = 0; i < a.size(); i++ )
-    {
+    std::vector<std::vector<int>> ans;
+    for (std::vector<std::vector<int>>::size_type i = 0; i < a.size(); i++) {
         vector<int> row;
-        for ( std::vector<int>::size_type j = 0; j < b[i].size(); j++ )
-        {
+        for (std::vector<int>::size_type j = 0; j < b[i].size(); j++) {
             s = 0;
-            for ( std::vector<int>::size_type k = 0; k < a[i].size(); k++ )
-            {
+            for (std::vector<int>::size_type k = 0; k < a[i].size(); k++) {
                 s += a[i][k] * b[k][j];
             }
             row.push_back(s);
@@ -159,11 +144,11 @@ vector<vector<int>> naive_multi(const vector<vector<int>>& a, const vector<vecto
 }
 
 
-vector<vector<int>> strassen(const vector<vector<int>>& m1, const vector<vector<int>>& m2){
+std::vector<std::vector<int>> strassen(const std::vector<std::vector<int>>& m1, const std::vector<std::vector<int>>& m2) {
     std::vector<std::vector<int>>::size_type s1 = m1.size();
     std::vector<std::vector<int>>::size_type s2 = m2.size();
-    if ( s1 > 2 && s2 > 2) {
-        vector<vector<int>> a,b,c,d,e,f,g,h,ans;
+    if (s1 > 2 && s2 > 2) {
+        std::vector<std::vector<int>> a,b,c,d,e,f,g,h,ans;
         for (std::vector<std::vector<int>>::size_type i = 0; i < m1.size() / 2; i++) {
             vector<int> row;
             for (std::vector<int>::size_type j = 0; j < m1[i].size() / 2; j++) {
@@ -222,7 +207,7 @@ vector<vector<int>> strassen(const vector<vector<int>>& m1, const vector<vector<
         }
 
 
-        vector<vector<int>> p1, p2, p3, p4, p5, p6, p7;
+        std::vector<std::vector<int>> p1, p2, p3, p4, p5, p6, p7;
         p1 = strassen(a, subtract(f, h));
         p2 = strassen(add(a, b), h);
         p3 = strassen(add(c, d), e);
@@ -230,7 +215,7 @@ vector<vector<int>> strassen(const vector<vector<int>>& m1, const vector<vector<
         p5 = strassen(add(a, d), add(e, h));
         p6 = strassen(subtract(b, d), add(g, h));
         p7 = strassen(subtract(a, c), add(e, f));
-        vector<vector<int>> c1, c2, c3, c4;
+        std::vector<std::vector<int>> c1, c2, c3, c4;
         c1 = subtract(add(add(p5, p4), p6), p2);
         c2 = add(p1, p2);
         c3 = add(p3, p4);
@@ -240,27 +225,27 @@ vector<vector<int>> strassen(const vector<vector<int>>& m1, const vector<vector<
 
         int flag1, flag2;
         for (std::vector<std::vector<int>>::size_type i = 0; i < m1.size(); i++) {
-            vector<int> row;
-            if( i < m1.size()/2){
+            std::vector<int> row;
+            if (i < m1.size() / 2) {
                 flag1 = 0;
             }
-            else{
+            else {
                 flag1 = 1;
             }
             for (std::vector<int>::size_type j = 0; j < m2[i].size(); j++) {
-                if( j < m2[i].size()/2){
-                    if( flag1 == 0){
+                if ( j < m2[i].size()/2) {
+                    if ( flag1 == 0) {
                         row.push_back(c1[i][j]);
                     }
-                    else{
+                    else {
                         row.push_back(c3[i-m1.size()/2][j]);
                     }
                 }
-                else{
-                    if( flag1 == 0){
+                else {
+                    if ( flag1 == 0) {
                         row.push_back(c2[i][j-m2[i].size()/2]);
                     }
-                    else{
+                    else {
                         row.push_back(c4[i-m1.size()/2][j-m2[i].size()/2]);
                     }
                 }
@@ -270,91 +255,91 @@ vector<vector<int>> strassen(const vector<vector<int>>& m1, const vector<vector<
         return ans;
     }
     else{
-        vector<vector<int>> v;
+        std::vector<std::vector<int>> v;
         v = naive_multi(m1, m2);
         return v;
     }
 }
 
-vector<vector<int>> fast_multi(const vector<vector<int>>& m1, const vector<vector<int>>& m2){
-    vector<int> ranges, ranges_c, ind;
-    vector<vector<int>> ans;
-    for( vector<vector<int>>::size_type i = 0; i < m1.size(); i++){
+std::vector<std::vector<int>> fast_multi(const std::vector<std::vector<int>>& m1, const std::vector<std::vector<int>>& m2){
+    std::vector<int> ranges, ranges_c, ind;
+    std::vector<std::vector<int>> ans;
+    for(std::vector<std::vector<int>>::size_type i = 0; i < m1.size(); i++){
         int a = 0;
         int b = 0;
-        for( vector<vector<int>>::size_type j = 0; j < m1[i].size(); j++){
-            if( m1[j][i] != 0){
+        for(std::vector<std::vector<int>>::size_type j = 0; j < m1[i].size(); j++){
+            if (m1[j][i] != 0) {
                 a += 1;
             }
-            if( m2[i][j] != 0){
+            if (m2[i][j] != 0) {
                 b+= 1;
             }
         }
         ranges.push_back(a*b);
         ranges_c.push_back(a*b);
     }
-    sort( ranges.begin(), ranges.end());
+    sort(ranges.begin(), ranges.end());
     int comp, index;
-    for( vector<vector<int>>::size_type i = 0; i < m1.size(); i++){
+    for (std::vector<std::vector<int>>::size_type i = 0; i < m1.size(); i++) {
         int s = 0;
-        for(vector<vector<int>>::size_type j = 0; j <= i; j++){
+        for (std::vector<std::vector<int>>::size_type j = 0; j <= i; j++) {
             s += ranges[j];
         }
         s += (m1.size()-i-1)*m1.size()*m1.size();
-        if(i == 0){
+        if (i == 0) {
             comp = s;
             index = 1;
         }
-        else{
-            if(s < comp){
+        else {
+            if (s < comp) {
                 comp = s;
                 index = i+1;
             }
         }
     }
-    for( int g = 0; g < index; g++){
-        for( vector<vector<int>>::size_type i = 0; i < m1.size(); i++){
-            if(ranges_c[i] == ranges[g]){
+    for ( int g = 0; g < index; g++) {
+        for (std::vector<std::vector<int>>::size_type i = 0; i < m1.size(); i++) {
+            if (ranges_c[i] == ranges[g]) {
                 ind.push_back(i);
                 break;
             }
         }
     }
-    for( vector<vector<int>>::size_type i = 0; i < m1.size(); i++ ){
+    for (std::vector<vector<int>>::size_type i = 0; i < m1.size(); i++ ) {
         int flag;
         int flag_search = 0;
-        for( vector<int>::size_type j = 0; j < ind.size(); j++ ){
-            if( i == ind[j]){
+        for (std::vector<int>::size_type j = 0; j < ind.size(); j++ ) {
+            if (i == ind[j]) {
                 flag_search = 1;
                 break;
             }
         }
-        vector<vector<int>> row, col;
-        for( vector<int>::size_type j = 0; j < ind.size(); j++ ){
-            vector<int> temp1, temp2;
+        std::vector<std::vector<int>> row, col;
+        for (std::vector<int>::size_type j = 0; j < ind.size(); j++) {
+            std::vector<int> temp1, temp2;
             temp1.push_back(m1[j][i]);
             temp2.push_back(m2[i][j]);
             col.push_back(temp1);
             row.push_back(temp2);
         }
-        if( i == 0){
-            if(flag_search == 1){
+        if (i == 0) {
+            if (flag_search == 1) {
                 ans = naive_multi(col, row);
             }
-            else{
-                vector<vector<int>> row_c, col_c;
+            else {
+                std::vector<std::vector<int>> row_c, col_c;
                 row_c = converter(row);
                 col_c = converter(col);
                 ans = retriever(strassen(col_c, row_c), m1.size(), m1.size());
             }
         }
-        else{
-            vector<vector<int>> v1, v2;
-            if(flag_search == 1){
+        else {
+            std::vector<std::vector<int>> v1, v2;
+            if (flag_search == 1) {
                 v1 = naive_multi(col, row);
             }
-            else{
-                vector<vector<int>> row_c, col_c;
+            else {
+                std::vector<std::vector<int>> row_c, col_c;
                 row_c = converter(row);
                 col_c = converter(col);
                 v1 = retriever(strassen(col_c, row_c), m1.size(), m1.size());
@@ -367,42 +352,39 @@ vector<vector<int>> fast_multi(const vector<vector<int>>& m1, const vector<vecto
 
 
 int main() {
-    vector<vector<int>> matrix1;
-    vector<vector<int>> matrix2;
-    vector<vector<int>> v1, v2, v3;
-    int i_1, j_1;
-    int i_2, j_2;
-
-    cout << "enter the dimensions of matrix1\n";
-    cin >> i_1 >> j_1;
-    cout << '\n';
-    cout << "enter the dimensions of matrix2\n";
-    cin >> i_2 >> j_2;
-    cout << '\n';
+    std::vector<vector<int>> matrix1, matrix2, v1, v2, v3;
+    int i1, j1;
+    int i2, j2;
+    std::cout << "enter the dimensions of matrix1\n";
+    std::cin >> i1 >> j1;
+    std::cout << '\n';
+    std::cout << "enter the dimensions of matrix2\n";
+    std::cin >> i2 >> j2;
+    std::cout << '\n';
     int x,y;
-    for(x = 0; x< i_1; x++){
-        vector<int> row;
-        for(y = 0; y < j_1; y++){
+    for (x = 0; x< i1; x++) {
+        std::vector<int> row;
+        for (y = 0; y < j1; y++) {
             int temp;
-            cout << "enter the " << x+1 << " , " << y+1 << " element for matrix1 - ";
-            cin >> temp;
+            std::cout << "enter the " << x+1 << " , " << y+1 << " element for matrix1 - ";
+            std::cin >> temp;
             row.push_back(temp);
         }
         matrix1.push_back(row);
     }
-    for(x = 0; x< i_2; x++){
-        vector<int> row;
-        for(y = 0; y < j_2; y++){
+    for (x = 0; x< i2; x++) {
+        std::vector<int> row;
+        for (y = 0; y < j2; y++) {
             int temp;
-            cout << "enter the " << x+1 << " , " << y+1 << " element for matrix2 - ";
-            cin >> temp;
+            std::cout << "enter the " << x+1 << " , " << y+1 << " element for matrix2 - ";
+            std::cin >> temp;
             row.push_back(temp);
         }
         matrix2.push_back(row);
     }
     //print(matrix1);
 
-    vector<vector<int>> m1, m2, m3, m4;
+    std::vector<std::vector<int>> m1, m2, m3, m4;
     m1 = converter(matrix1);
     m2 = converter(matrix2);
     //print(m1);
@@ -414,28 +396,28 @@ int main() {
     m4 = generator(1024, 1024);
     */
 
-    auto start = high_resolution_clock::now();
-    v1 = retriever(naive_multi(m1, m2), i_1 , j_2);
-    auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(start - stop);
+    auto start = std::chrono::high_resolution_clock::now();
+    v1 = retriever(naive_multi(m1, m2), i1 , j2);
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<microseconds>(start - stop);
 
-    cout << "tiime taken by function" << duration.count() << endl;
+    std::cout << "tiime taken by function" << duration.count() << endl;
     //print(v1);
 
 
 
-    auto start_2 = high_resolution_clock::now();
-    v2 = retriever(strassen(m1, m2), i_1, j_2);
-    auto stop_2 = high_resolution_clock::now();
-    auto duration_2 = duration_cast<microseconds>(start_2 - stop_2);
+    auto start2 = std::chrono::high_resolution_clock::now();
+    v2 = retriever(strassen(m1, m2), i1, j2);
+    auto stop2 = std::chrono::high_resolution_clock::now();
+    auto duration2 = std::chrono::duration_cast<microseconds>(start2 - stop2);
 
-    cout << "tiime taken by function" << duration_2.count() << endl;
+    std::cout << "tiime taken by function" << duration2.count() << endl;
 
     /*
-    auto start_3 = high_resolution_clock::now();
+    auto start_3 = std::chrono::high_resolution_clock::now();
     v3 = retriever(fast_multi(m1, m2), i_1, j_2);
-    auto stop_3 = high_resolution_clock::now();
-    auto duration_3 = duration_cast<microseconds>(start_3 - stop_3);
+    auto stop_3 = std::chrono::high_resolution_clock::now();
+    auto duration_3 = std::chrono::duration_cast<microseconds>(start_3 - stop_3);
 
 
     cout << "tiime taken by function" << duration_3.count() << endl;
