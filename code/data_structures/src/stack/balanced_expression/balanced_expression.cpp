7ad2c03f2bb47c3e3d9203a57868efe6a5ec3c20
bahl24 <36095178+bahl24@users.noreply.github.com>
2018-06-30 09:23:24 +0530

    Update balanced_expression.cpp

--- code/data_structures/src/stack/balanced_expression/balanced_expression.cpp
+++ code/data_structures/src/stack/balanced_expression/balanced_expression.cpp
@@ -27,44 +27,44 @@ int main()
 {
     /* The code takes a string str as input from stdin and prints "YES" 
     if str is a balanced expression else it prints "NO".*/
-        stack<char> s;
-        string str;
-        cin>>str;
-        int no = 0;
-        for(int i = 0; i < str.length(); i++)
-        {
-            char now = str[i];
-            if(isOpen(now))
-                s.push(now);
-            else
-            {
-                char popped;
-                if(s.size() > 0)
-                {
-                    popped = s.top();
-                    s.pop();
-                }
-                else
-                {
-                    no = 1;
-                    break;
-                }
-                if(isBalanced(popped, now))
-                    continue;
-                else
-                {
-                    no = 1;
-                    break;
-                }
-            }
-        }
-        if(s.size() != 0)
-            no = 1;
-        if(no)
-            cout<<"NO\n";
+    stack<char> s;
+    string str;
+    cin >> str;
+    int no = 0;
+    for(int i = 0; i < str.length(); i++)
+    {
+        char now = str[i];
+        if(isOpen(now))
+            s.push(now);
         else
         {
-            cout<<"YES\n";
+            char popped;
+            if(s.size() > 0)
+            {
+                popped = s.top();
+                s.pop();
+            }
+            else
+            {
+                no = 1;
+                break;
+            }
+            if(isBalanced(popped, now))
+                continue;
+            else
+            {
+                no = 1;
+                break;
+            }
         }
-    return 0;
+    }
+    if(!s.empty())
+        no = 1;
+    if(no)
+        cout<<"NO\n";
+    else
+    {
+        cout<<"YES\n";
+    }
+return 0;
 }
