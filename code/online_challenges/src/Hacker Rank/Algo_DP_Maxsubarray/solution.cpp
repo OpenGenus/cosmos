84867ee5bee40b0eb3394a10064abdc81bd2a68d
Bharat Arya <arya.bharat@rocketmail.com>
2018-07-01 02:01:23 +0530

    Update solution.cpp
    changes asked Done

--- code/online_challenges/src/Hacker Rank/Algo_DP_Maxsubarray/solution.cpp
+++ code/online_challenges/src/Hacker Rank/Algo_DP_Maxsubarray/solution.cpp
@@ -1,44 +1,56 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int main() 
-    {  //t is the number of test case
-        int t;
-        cin>>t;
-        while(t--){
-            //n is the size of array
-            int n;
-            cin>>n;
-            //arr is vector of size n
-            vector <int> arr(n);
-            //This Algo is Kaden algorithm 
-            //Also known as Minimum Sum Subarray
-            int flag=0;
-            for(int i=0;i<n;i++){
-                cin>>arr[i];
-                if(arr[i]>=0)
-                flag++;
-            }
-            if(flag==0){
-            sort(arr.begin(),arr.end());
-            cout<<arr[n-1]<<" "<<arr[n-1]<<endl;
-            continue;
-            }
-        
+int main()
+{
+        int t;             //t is the number of test case
+        cin >> t;
+        while(t--)
+        {
+            int n;             //n is the size of input array
             int sum,ans,ans1;
-            sum=ans=ans1=0;
-            for(int i=0;i<n;i++){
-                if(sum+arr[i]>0)
-                    sum=sum+arr[i];
-                else
-                    sum=0;
-                if(arr[i]>=0)
-                    ans1=ans1+arr[i];
-                
-                ans=max(ans,sum);
-        }
-        
-        cout<<ans<<" "<<ans1<<endl;
-    }  
+            sum = ans = ans1 = 0;
+
+            int flag = 0;
+
+            cin >> n;
+            vector <int> arr(n);
+
+            for(int i = 0; i < n; i++)
+                {
+                    cin >> arr[i];
+                    if(arr[i] >= 0)
+                    
+                    flag++;
+                }
+
+            if (flag == 0)
+                {
+                    sort (arr.begin(), arr.end());
+                    
+                    cout << arr[n-1] << " " << arr[n-1] <<"\n";
+                    continue;
+                }
+
+            int sum,ans,ans1;
+            sum = ans = ans1 = 0;
+
+            for(int i = 0; i < n; i++)
+                {
+                    if((sum + arr[i]) > 0)
+                        sum = sum + arr[i];
+                    else
+                        sum = 0;
+                    if(arr[i] >= 0)
+                        ans1 = ans1 + arr[i];
+
+                    ans = max(ans,sum);
+                }
+
+        cout << ans << " " << ans1 <<endl;
+    }
+
     return 0;
 }
