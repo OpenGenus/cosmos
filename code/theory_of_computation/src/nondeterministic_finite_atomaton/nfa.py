bf74ad45aeeac3cc11e942ee9ecafe1237f1f5d8
meettaraviya <meet11061997@gmail.com>
2018-07-06 11:27:29 +0530

    Added support for epsilon/lambda/null moves

--- code/theory_of_computation/src/nondeterministic_finite_atomaton/nfa.py
+++ code/theory_of_computation/src/nondeterministic_finite_atomaton/nfa.py
@@ -1,8 +1,9 @@
 from dfa import DFA # located at code/theory_of_computation/src/deterministic_finite_automaton/dfa.py
 
+EPSILON = ''
+
 class NFA(object):
 	"""Class for Non-deterministic Finite Atomata"""
-	DEAD = -1
 	def __init__(self, transitions, start, accepting):
 		
 		self.start = start
@@ -10,12 +11,22 @@ class NFA(object):
 
 		self.transitions = transitions
 
-	def accepts(self, word, _start=None):
+	def accepts(self, word, _start=None, _marked=set()):
 
 		if _start is None:
 			curr_state = self.start
 		else:
 			curr_state = _start
+
+		if EPSILON in self.transitions[curr_state] and curr_state not in _marked:
+
+			new_marked = _marked.copy()
+			new_marked.add(curr_state)
+
+			for next_state in self.transitions[curr_state][EPSILON]:
+
+				if self.accepts(word, _start=next_state, _marked=new_marked):
+					return True
 
 		if len(word) == 0:
 			return curr_state in self.accepting
@@ -24,9 +35,11 @@ class NFA(object):
 			try:
 				choices = self.transitions[curr_state][word[0]]
 
+				new_marked = set()
+
 				for choice in choices:
 
-					if self.accepts(word[1:], _start=choice):
+					if self.accepts(word[1:], _start=choice, _marked=new_marked):
 						return True
 
 				return False
@@ -35,15 +48,85 @@ class NFA(object):
 				print("WARNING: Missing entry in transition assumed leading to dead state")
 				return False
 
-	def determinize(self):
+	def remove_epsilon(self):
 
-		states = set(self.transitions.keys())
+		epsilon_closure = {}
+		changed_closures = set()
+
+		for state, state_transitions in self.transitions.items():
+
+			epsilon_closure[state] = set(state_transitions.get(EPSILON, [])+[state])
+			changed_closures.add(state)
+
+
+		while len(changed_closures) > 0:
+
+			new_changed_closures = set()
+
+			for state, state_eps_transitions in epsilon_closure.items():
+
+				for next_state in state_eps_transitions.intersection(changed_closures):
+					
+					next_state_eps_transitions = epsilon_closure[next_state]
+					
+					if not next_state_eps_transitions.issubset(state_eps_transitions):
+						new_changed_closures.add(state)
+						state_eps_transitions.update(next_state_eps_transitions)
+
+			changed_closures = new_changed_closures
+
+		new_transitions = {}
+
+		for state in self.transitions.keys():
+
+			state_actions = set()
+			for eps_state in epsilon_closure[state]:
+				state_actions.update(self.transitions[eps_state].keys())
+
+			state_actions.discard(EPSILON)
+			new_state_transitions = dict((action, set()) for action in state_actions)
+
+			for eps_state in epsilon_closure[state]:
+
+				for action, next_states in self.transitions[eps_state].items():
+
+					if action != EPSILON:
+
+						for next_state in next_states:
+
+							new_state_transitions[action].update(epsilon_closure[next_state])
+					
+
+			new_transitions[state] = new_state_transitions
+
+		new_accepting = set()
+		for acc_state in self.accepting:
+			new_accepting.update(epsilon_closure[acc_state])
+		
+		if not epsilon_closure[self.start].isdisjoint(self.accepting):
+			new_accepting.add(self.start)
+
+		return NFA(
+				transitions=new_transitions,
+				start=self.start,
+				accepting=new_accepting
+			)
+
+
+	def determinize(self, remove_epsilon=True):
+
+		if remove_epsilon:
+			base_nfa = self.remove_epsilon()
+		else:
+			base_nfa = self
+
 
 		actions = set()
-		for state, out_transitions in self.transitions.items():
+
+		for out_transitions in base_nfa.transitions.values():
 			actions.update(out_transitions.keys())
 
-		dfa_start = frozenset([self.start])
+		dfa_start = frozenset([base_nfa.start])
 		dfa_states_left = set([dfa_start])
 		mappings = {dfa_start: 1}
 
@@ -56,7 +139,7 @@ class NFA(object):
 				dfa_next_state = set()
 				for nfa_state in dfa_state:
 					try:
-						dfa_next_state.update(self.transitions[nfa_state][action])
+						dfa_next_state.update(base_nfa.transitions[nfa_state][action])
 					except KeyError:
 						pass
 				dfa_next_state = frozenset(dfa_next_state)
@@ -65,7 +148,7 @@ class NFA(object):
 					dfa_states_left.add(dfa_next_state)
 				dfa_transitions[mappings[dfa_state]][action] = mappings[dfa_next_state]
 
-		dfa_accepting = [mappings[dfa_state] for dfa_state in mappings.keys() if not dfa_state.isdisjoint(self.accepting)]
+		dfa_accepting = [mappings[dfa_state] for dfa_state in mappings.keys() if not dfa_state.isdisjoint(base_nfa.accepting)]
 
 		return DFA(
 				transitions=dfa_transitions,
@@ -76,36 +159,47 @@ class NFA(object):
 
 def main():
 
-	print("Automata that accepts strings with 'a' as 3rd character from last")
+	print("Automata that accepts strings with 'a' in 2nd, 3rd or 4th character from last")
 
-	nfa = NFA(
+	nfa_with_epsilon = NFA(
 			transitions={
 				1: {'a': [1, 2], 'b': [1]},
-				2: {'a': [3], 'b': [3]},
-				3: {'a': [4], 'b': [4]},
-				4: {},
+				2: {'a': [3], 'b': [3], '': [3]},
+				3: {'a': [4], 'b': [4], '': [4]},
+				4: {'a': [5], 'b': [5]},
+				5: {},
 			},
 			start=1,
-			accepting=[4]
+			accepting=[5]
 		)
 
-	words = ["a", "aa", "aaa", "abab", "abaaab"]
 
+	words = ["a", "b", "ab", "bb", "baa", "aba", "abba", "bbba"]
 	print()
-	print("Tests for NFA:-")
+	print("Tests for NFA with epsilon:-")
 
 	for word in words:
-		print("{} accepted? {}".format(word, nfa.accepts(word)))
+		print("'{}' accepted? {}".format(word, nfa_with_epsilon.accepts(word)))
+
+	nfa_wo_epsilon = nfa_with_epsilon.remove_epsilon()
+	
+	print()
+	print("Tests for NFA without epsilon:-")
+
+	for word in words:
+		print("'{}' accepted? {}".format(word, nfa_wo_epsilon.accepts(word)))
+
 
 	print()
-	print("Tests for DFA, obtained by determinizing NFA:-")
-	dfa = nfa.determinize()
+	print("Tests for DFA, obtained by determinizing NFA without epsilon:-")
+	dfa = nfa_wo_epsilon.determinize(remove_epsilon=False)
 
 	for word in words:
 		print("'{}' accepted? {}".format(word, dfa.accepts(word)))
 
 	print()
-	print("Number of states in NFA: ", len(nfa.transitions))
+	print("Number of states in NFA with epsilon: ", len(nfa_with_epsilon.transitions))
+	print("Number of states in NFA without epsilon: ", len(nfa_wo_epsilon.transitions))
 	print("Number of states in DFA: ", len(dfa.transitions))
 
 
