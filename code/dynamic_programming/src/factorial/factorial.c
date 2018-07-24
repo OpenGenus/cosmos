83599f8386a93a352bb8f5635be88b3d197f9680
Deepak Yadav <deepyadav442@gmail.com>
2018-07-24 10:50:31 +0530

    Update factorial.c

--- code/dynamic_programming/src/factorial/factorial.c
+++ code/dynamic_programming/src/factorial/factorial.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 int main()
 {
 	int number;
 	printf("Enter a Whole Number: ");
 	scanf("%d",&number);
-    	int result[100001]={0};
+    int *result=(int*)malloc(number*(sizeof(int)));
+    memset(result,0,sizeof(int)*number);
    	result[0]=1;
   	for(int i=1;i<=number;i++)
         	result[i]=i*result[i-1];
 	printf("%d! = %d\n", number, result[number]);
+	free(result);
 	return (0);
 }
