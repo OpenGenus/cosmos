47206b38039d4239eaba56f13c93ea11af39c39e
Bharat Arya <arya.bharat@rocketmail.com>
2018-07-24 15:18:39 +0530

    Update Algo_DP_Maxsubarray.cpp

--- code/online_challenges/src/Hacker Rank/Algo_DP_Maxsubarray.cpp
+++ code/online_challenges/src/Hacker Rank/Algo_DP_Maxsubarray.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-using namespace std;
 
 int main()
 {
         int t;             //t is the number of test case
-        cin >> t;
+        std::cin >> t;
         while(t--)
         {
             int n;             //n is the size of input array
@@ -15,26 +14,26 @@ int main()
 
             int flag = 0;
 
-            cin >> n;
-            vector <int> arr(n);
+            std::cin >> n;
+            std::vector <int> arr(n);
 
             for(int i = 0; i < n; i++)
                 {
-                    cin >> arr[i];
+                    std::cin >> arr[i];
                     if(arr[i] >= 0)
-                    
+
                     flag++;
                 }
 
             if (flag == 0)
                 {
                     sort (arr.begin(), arr.end());
-                    
-                    cout << arr[n-1] << " " << arr[n-1] <<"\n";
+
+                    std::cout << arr[n-1] << " " << arr[n-1] <<"\n";
                     continue;
                 }
 
-            int sum,ans,ans1;
+
             sum = ans = ans1 = 0;
 
             for(int i = 0; i < n; i++)
@@ -46,10 +45,10 @@ int main()
                     if(arr[i] >= 0)
                         ans1 = ans1 + arr[i];
 
-                    ans = max(ans,sum);
+                    ans = std::max(ans,sum);
                 }
 
-        cout << ans << " " << ans1 <<endl;
+        std::cout << ans << " " << ans1 << "\n";
     }
 
     return 0;
