94e3ee75dde6c9ccf5a349bfe88afedab5bf714a
Bharat Arya <arya.bharat@rocketmail.com>
2018-07-24 15:22:50 +0530

    Update DS_Array_Manipulation.cpp

--- code/online_challenges/src/Hacker Rank/DS_Array_Manipulation.cpp
+++ code/online_challenges/src/Hacker Rank/DS_Array_Manipulation.cpp
@@ -1,35 +1,34 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-using namespace std;
 
 int main()
 {
     int n;
     int m;
 
-    cin >> n >> m;
-    vector < pair < int, int > > v;
+    std::cin >> n >> m;
+    std::vector < std::pair < int, int > > v;
 
     for(int a0 = 0; a0 < m; a0++)
     {
         int a;
         int b;
         int k;
-        cin >> a >> b >> k;
-        v.push_back(make_pair(a, k));
-        v.push_back(make_pair(b+1, -1 * k));
+        std::cin >> a >> b >> k;
+        v.push_back(std::make_pair(a, k));
+        v.push_back(std::make_pair(b + 1, -1 * k));
     }
 
     long mx = 0, sum = 0;
     sort(v.begin(), v.end());
 
-    for(int i=0 ; i<2*m; i++)
+    for(int i = 0  ; i < 2*m; i++)
     {
         sum += v[i].second;
-        mx = max(mx, sum);
+        mx = std::max(mx, sum);
     }
 
-    cout<<mx<<endl;
+    std::cout << mx << "\n";
     return 0;
 }
