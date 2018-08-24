bf0e4a447afe6fe4169f4b8af4e6a869670df4d3
Carlos Almendros <calmendrosc@gmail.com>
2018-08-24 07:18:09 +0200

    Corrections on variable name misspellings for Knapsack CPP algorithm

--- code/dynamic_programming/src/knapsack/knapsack.cpp
+++ code/dynamic_programming/src/knapsack/knapsack.cpp
@@ -14,18 +14,18 @@ typedef pair<int, int> pii;
 #define mp make_pair
 
 //cosmos: knapsack 0-1
-int knapsack(int value[], int weight[], int n_items, int maixumWeight)
+int knapsack(int value[], int weight[], int n_items, int maximumWeight)
 {
-    int dp[n_items + 1][maixumWeight + 1];
+    int dp[n_items + 1][maximumWeight + 1];
 
-    for (int w = 0; w <= maixumWeight; w++)
+    for (int w = 0; w <= maximumWeight; w++)
         dp[0][w] = 0;
 
     for (int i = 0; i <= n_items; i++)
         dp[i][0] = 0;
 
     for (int i = 1; i <= n_items; i++)
-        for (int w = 1; w <= maixumWeight; w++)
+        for (int w = 1; w <= maximumWeight; w++)
         {
             if (weight[i - 1] <= w)
                 dp[i][w] = max(value[i - 1] + dp[i - 1][w - weight[i - 1]], dp[i - 1][w]);
@@ -33,7 +33,7 @@ int knapsack(int value[], int weight[], int n_items, int maixumWeight)
                 dp[i][w] = dp[i - 1][w];
         }
 
-    return dp[n_items][maixumWeight];
+    return dp[n_items][maximumWeight];
 }
 
 int main()
@@ -43,9 +43,9 @@ int main()
 
     int value[] = {12, 1000, 30, 10, 1000};
     int weight[] = {19, 120, 20, 1, 120};
-    int n_tems = 5;
-    int maixumWeight = 40;
+    int n_items = 5;
+    int maximumWeight = 40;
 
-    cout << knapsack(value, weight, n_tems, maixumWeight) << endl;    //values of the items, weights, number of items and the maximum weight
+    cout << knapsack(value, weight, n_items, maximumWeight) << endl;    //values of the items, weights, number of items and the maximum weight
     return 0;
 }
