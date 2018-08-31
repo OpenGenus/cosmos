2d334f3caef02a189f57cd45ae98b4f5fd98f4fb
srinidhi <kss682@gmail.com>
2018-08-31 15:15:02 +0530

    solves issue #3488

--- code/online_challenges/src/project_euler/problem_036/problem_036.py
+++ code/online_challenges/src/project_euler/problem_036/problem_036.py
@@ -1,26 +1,41 @@
+
 def base_check(a, y):
-    ans = ''
-    while a > 0:
-        ans += str(a % y)
-        a = a // y
-    return ans == ans[::-1]
+	ans =0
+	while a!=0:
+		ans=ans*10+a % y
+		a = a // y
+    
+	
+	
+	return ans 
 
 
-def palindrome(x, base):
-    x = str(x)
-    if x != x[::-1]:
-        return False
-    else:
-        x = int(x)
-        x = base_check(x, base)
-        x = (str(x))
-        return x == x[::-1]
+def palindrome(x):
+	reverse=0
+	num=x
+	while x!=0:
+		reverse=reverse*10+x%10
+		x=x//10
+	if reverse==num:
+		return True
+	else:
+		return False
 
+
+	
 
 number = 1000000
 base = 2
 answer = 0
 for i in range(1, number + 1):
-    if palindrome(i, base):
-        answer += i
+	if palindrome(i) and i%base!=0:
+		binary=base_check(i,base)
+		if palindrome(binary):
+			answer+=i
+
+
 print(answer)
+
+
+
+
