8bf378b38bdffea45f2583cfd066d6baf46b916f
Hritik Tayade <34279258+braceritchie@users.noreply.github.com>
2018-09-07 18:38:08 +0530

    Updated fcfs.java according to coding styles
    - Better indentation
    - Braces according to coding styles

--- code/operating_system/src/scheduling/first_come_first_serve/fcfs.java
+++ code/operating_system/src/scheduling/first_come_first_serve/fcfs.java
@@ -1,9 +1,7 @@
 import java.io.*; 
-class FCFS 
-{ 
+class FCFS { 
  
-	public static void main(String args[]) throws Exception 
-	{ 
+	public static void main(String args[]) throws Exception { 
  		int n,at[],bt[],wt[],tat[]; 
 		float AWT=0; 
 		InputStreamReader isr=new InputStreamReader(System.in); 
@@ -16,33 +14,35 @@ class FCFS
 		bt=new int[n];
 		at=new int[n];
   
-		System.out.println(“Enter Burst time for each process\n”); 
-		for(int i=0;i<n;i++) 
-		{ 
+		System.out.println(“Enter Burst time for each process\n”);
+		
+		for(int i=0;i<n;i++) { 
 			System.out.println(“Process[“+(i+1)+"]"); 
 			bt[i]=Integer.parseInt(br.readLine()); 
 		} 
+						    
 		System.out.println(“\n\nEnter Around Time”); 
-		for(int i=0;i<n;i++) 
-		{ 
+						    
+		for(int i=0;i<n;i++) { 
 			System.out.println(“Process[”+i+"]"); 
 			at[i]=Integer.parseInt(br.readLine()); 
-		} 
+		}
+						    
 		System.out.println(“\n”); 
 		wt[0]=0; 
-		for(int i=1;i<n;i++) 
-		{ 
+		for(int i=1;i<n;i++) { 
 			wt[i]=wt[i-1]+bt[i-1]; 
 			wt[i]=wt[i]-at[i]; 
 		} 
-		for(int i=0;i<n;i++) 
-		{ 
+						    
+		for(int i=0;i<n;i++) { 
 			tat[i]=wt[i]+bt[i]; 
 			awt=awt+wt[i]; 
 		} 
-		System.out.println(”  PROCESS\t\tBURST-TIME\tWAITING-TIME\tTURN AROUND-TIME\n“); 
-		for(int i=0;i<n;i++) 
-		{
+						    
+		System.out.println(”  PROCESS\t\tBURST-TIME\tWAITING-TIME\tTURN AROUND-TIME\n“);
+						    
+		for(int i=0;i<n;i++) {
 			System.out.println(”    “+ i + ”\t\t“+bt[i]+”\t“+wt[i]+”\t“+tat[i]);
 		} 
 		awt=awt/n; 
