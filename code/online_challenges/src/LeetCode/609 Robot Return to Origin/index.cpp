accfcd2f03b0d5938e150a3022ba71935510a6df
Brian Phair <bsphair@gmail.com>
2018-09-16 21:24:16 -0400

    Added comments

--- code/online_challenges/src/LeetCode/609 Robot Return to Origin/index.cpp
+++ code/online_challenges/src/LeetCode/609 Robot Return to Origin/index.cpp
@@ -3,7 +3,7 @@ public:
     bool judgeCircle(string moves) {
         int up = 0, down = 0, left = 0, right = 0, count = 0;
         
-        while(count < moves.length()){
+        while(count < moves.length()){      //loop through the entire input string
             
             if(moves[count] == 'U'){
                 up++;
@@ -21,8 +21,10 @@ public:
             count++;
         }
 
-
-        if(up == down && left == right){
+        /*For the robot to be in the original position, the 'up' must equal the 'down' 
+          and the 'right' must equal the 'left'.
+        */
+        if(up == down && left == right){        
             return true;
         }
         
