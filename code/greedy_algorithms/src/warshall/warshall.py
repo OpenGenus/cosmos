5b951c56ca5d0576eb20843c98c6f3e1f2a85f46
Rithvik Kasarla <rithvikrk719@gmail.com>
2018-09-26 09:00:14 -0400

    warshall.py

---
+++ code/greedy_algorithms/src/warshall/warshall.py
@@ -0,0 +1,12 @@
+def warshall(a):
+    assert (len(row) == len(a) for row in a)
+    n = len(a)
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                a[i][j] = a[i][j] or (a[i][k] and a[k][j])
+    return a
+ 
+ 
+num = input("Enter num:")
+warshall(num)
