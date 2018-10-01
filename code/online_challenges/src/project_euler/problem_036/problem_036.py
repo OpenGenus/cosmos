e50e5c762bcfafdb1e4762efad54b84e1149e94e
Srinidhi Krishna <30746998+kss682@users.noreply.github.com>
2018-10-01 18:56:36 +0530

    Update problem_036.py
    changes made in the coding style

--- code/online_challenges/src/project_euler/problem_036/problem_036.py
+++ code/online_challenges/src/project_euler/problem_036/problem_036.py
@@ -1,39 +1,30 @@
 
 def base_check(a, y):
-	ans =0
+	ans = 0
 	while a!=0:
-		ans=ans*10+a % y
+		ans = ans * 10 + a  %  y
 		a = a // y
-    
-	
-	
 	return ans 
 
-
 def palindrome(x):
-	reverse=0
-	num=x
-	while x!=0:
-		reverse=reverse*10+x%10
-		x=x//10
+	reverse = 0
+	num = x
+	while x ! = 0:
+		reverse = reverse * 10 + x % 10
+		x = x // 10
 	if reverse==num:
 		return True
 	else:
 		return False
 
-
-	
-
 number = 1000000
 base = 2
 answer = 0
 for i in range(1, number + 1):
-	if palindrome(i) and i%base!=0:
-		binary=base_check(i,base)
+	if palindrome(i) and i % base ! = 0:
+		binary = base_check(i,base)
 		if palindrome(binary):
-			answer+=i
-
-
+			answer + = i
 print(answer)
 
 
