76c5c0e58f7418900acae42d6dcf9c71d70bc2b0
Vaibhav <vk530873@gmail.com>
2018-10-02 20:50:09 +0530

    Create balanced_paranthesis.cpp

---
+++ code/data_structures/src/stack/balanced_expression/balanced_paranthesis.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool checkBalanced(string s) {
+	if(s.length() % 2 !=0) return false;
+	stack <char> st;
+	for(int i=0;i<s.length();i++) {
+		if(s[i] == '{' || s[i] == '[' || s[i] == '(') {
+			st.push(s[i]);
+		}
+		else {
+			char temp = st.top();
+			if((s[i] == '}' && temp == '{') || (s[i] == ']' && temp == '[') || (s[i] == ')' && temp == '(')) {
+				st.pop();
+			}
+			else {
+				return false;
+			}
+		}
+	}
+	return st.empty();
+}
+
+int main()
+{
+	string s ;
+	cin>>s;
+	bool res = checkBalanced(s);
+	if(res) {
+		cout<<"Expression is balanced";
+	}
+	else {
+		cout<<"Expression is not balanced";
+	}
+	return 0;
+}
