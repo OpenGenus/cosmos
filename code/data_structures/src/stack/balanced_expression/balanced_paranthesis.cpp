dcbf31b89211f8046fb957dab3c5adf4d2417781
Vaibhav <vk530873@gmail.com>
2018-10-02 23:32:31 +0530

    Update balanced_paranthesis.cpp

--- code/data_structures/src/stack/balanced_expression/balanced_paranthesis.cpp
+++ code/data_structures/src/stack/balanced_expression/balanced_paranthesis.cpp
@@ -3,20 +3,18 @@
 using namespace std;
 
 bool checkBalanced(string s) {
-	if(s.length() % 2 !=0) return false;
+	if (s.length() % 2 !=0) return false;
 	stack <char> st;
-	for(int i=0;i<s.length();i++) {
-		if(s[i] == '{' || s[i] == '[' || s[i] == '(') {
+	for (int i=0;i<s.length();i++) {
+		if (s[i] == '{' || s[i] == '[' || s[i] == '(') {
 			st.push(s[i]);
 		}
 		else {
 			char temp = st.top();
-			if((s[i] == '}' && temp == '{') || (s[i] == ']' && temp == '[') || (s[i] == ')' && temp == '(')) {
-				st.pop();
-			}
-			else {
-				return false;
-			}
+			if (s[i] == '}' && temp == '{') st.pop();
+			else if (s[i] == ']' && temp == '[') st.pop();
+			else if (s[i] == ')' && temp == '(') st.pop();
+			else return false;
 		}
 	}
 	return st.empty();
@@ -24,10 +22,10 @@ bool checkBalanced(string s) {
 
 int main()
 {
-	string s ;
+	string s;
 	cin>>s;
 	bool res = checkBalanced(s);
-	if(res) {
+	if (res) {
 		cout<<"Expression is balanced";
 	}
 	else {
