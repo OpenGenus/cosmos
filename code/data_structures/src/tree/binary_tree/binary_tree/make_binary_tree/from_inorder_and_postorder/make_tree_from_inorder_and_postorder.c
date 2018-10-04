36075d79747d2cbdb8a493a11e914fe57424e8b4
Vaibhav <vk530873@gmail.com>
2018-10-04 11:25:36 +0530

    Update make_tree_from_inorder_and_postorder.c

--- code/data_structures/src/tree/binary_tree/binary_tree/make_binary_tree/from_inorder_and_postorder/make_tree_from_inorder_and_postorder.c
+++ code/data_structures/src/tree/binary_tree/binary_tree/make_binary_tree/from_inorder_and_postorder/make_tree_from_inorder_and_postorder.c
@@ -11,24 +11,22 @@ btree *makeTree(int *in , int *post , int start , int end , int *index) {
 	if (start > end) return NULL;
 
 	btree *nn = (btree*)malloc(sizeof(btree));
-	nn->data = post[*index];
+	nn -> data = post[*index];
 	int i;
 	for (i=start;i <= end;i++) {
-		if (post[*index] == in[i]) {
-			break;
-		}
+		if (post[*index] == in[i]) break;
 	}
 	(*index)--;
-	nn->right = makeTree(in,post,i+1,end,index);
-	nn->left = makeTree(in,post,start,i-1,index);
+	nn -> right = makeTree(in, post, i+1, end, index);
+	nn -> left = makeTree(in, post, start, i-1, index);
 	return nn;
 }
 
 void printPostOrderTree(btree *root) {
 	if(root == NULL) return;
-	printPostOrderTree(root->left);
-	printPostOrderTree(root->right);
-	printf("%d ",root->data);
+	printPostOrderTree(root -> left);
+	printPostOrderTree(root -> right);
+	printf("%d ",root -> data);
 }
 
 int main()
@@ -36,7 +34,7 @@ int main()
 	int post[] = {4,5,2,6,7,3,1};
 	int in[] = {4,2,5,1,6,3,7};
 	int index = 6;
-	btree *root = makeTree (in,post,0,6,&index);
+	btree *root = makeTree (in, post, 0, 6, &index);
 
 	printPostOrderTree(root);
 	return 0;
