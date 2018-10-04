24ba69e2b0efff685ea5b4e7df95fadc5b6d8c26
sanath sunakd <sanathsunkad07@gmail.com>
2018-10-04 22:14:08 +0530

    Add files via upload

---
+++ code/artificial_intelligence/normalEqn.m
@@ -0,0 +1,23 @@
+function [theta] = normalEqn(X, y)
+%NORMALEQN Computes the closed-form solution to linear regression 
+%   NORMALEQN(X,y) computes the closed-form solution to linear 
+%   regression using the normal equations.
+
+theta = zeros(size(X, 2), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the code to compute the closed form solution
+%               to linear regression and put the result in theta.
+%
+
+% ---------------------- Sample Solution ----------------------
+
+theta = pinv(X' * X) * X' * y;
+
+
+% -------------------------------------------------------------
+
+
+% ============================================================
+
+end
