bbc844f5e4bd883dc2161aebd82c348e0cd23e15
Vaibhav <vk530873@gmail.com>
2018-10-06 10:25:05 +0530

    Update balanced_paranthesis.cpp

--- code/data_structures/src/stack/balanced_expression/balanced_paranthesis.cpp
+++ code/data_structures/src/stack/balanced_expression/balanced_paranthesis.cpp
@@ -5,7 +5,7 @@ using namespace std;
 bool checkBalanced(string s) {
 	if (s.length() % 2 != 0) return false;
 	stack <char> st;
-	for (int i=0;i < s.length();i++) {
+	for (int i = 0; i < s.length(); i++) {
 		if (s[i] == '{' || s[i] == '[' || s[i] == '(') {
 			st.push(s[i]);
 		}
