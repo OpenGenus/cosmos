4c4855f6aaf6e5f988387d53291778565946c92a
Vitorvgc <vitorvgc07@gmail.com>
2018-10-08 01:55:26 -0300

    Add C++ solution of project euler 023

---
+++ code/online_challenges/src/project_euler/problem_023/problem_023.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int sumProperDivisors(int n)
+{
+    int sum = 0;
+    for (int i = 1; i*i <= n; ++i)
+        if (n % i == 0) {
+            sum += i;
+            if(n / i != i)
+                sum += n / i;
+        }
+    return sum - n;
+}
+
+void findAbundantNumbers(int limit, std::vector<int> &numbers)
+{
+    for (int i = 1; i <= limit; ++i)
+        if (sumProperDivisors(i) > i)
+            numbers.push_back(i);
+}
+
+bool isSumOfAbundantNumbers(int n, const std::vector<int> &numbers)
+{
+    for (int i = 0; i < numbers.size() && numbers[i] < n; ++i)
+        if (std::binary_search(numbers.begin(), numbers.end(), n - numbers[i]))
+            return true;
+    return false;
+}
+
+int main()
+{
+    int sum = 0, max = 28123;
+    std::vector<int> numbers;
+    findAbundantNumbers(max, numbers);
+    for (int i = 1; i <= max; ++i)
+        if (!isSumOfAbundantNumbers(i, numbers))
+            sum += i;
+
+    std::cout << sum << std::endl;
+    return 0;
+}
