f7ce10ae04234b13cf42514e6496e33988746c88
Sai Lakshmi <42446464+sailakshmi17@users.noreply.github.com>
2018-10-16 14:52:12 +0530

    Updated priority_queue.c
    removed header <conio.h>,global variable and fixed indentation errors

--- code/data_structures/src/queue/queue/priority_queue.c
+++ code/data_structures/src/queue/queue/priority_queue.c
@@ -1,89 +1,89 @@
 #include<stdio.h>
-#include<conio.h>
-#define max 50
 
 int head=0,tail=0;
-int queue[2][max];
+int queue[2][50];
 
-void enqueue();
-void dequeue();
-void display();
+void enqueue(int);
+void dequeue(int);
+void display(int);
 
-int main(){
- int n;
- while(1)
- { 
-  printf("enter 1 to enqueue\n 2 to dequeue\n 3 to display\n 4 to exit");
-  scanf("%d",&n);
-  switch(n)
-  {
-   case 1 :enqueue();
-           break;
-   case 2 :dequeue();
-           break;
-   case 3 :display();
-           break;
-   case 4 :exit(1);
-   default:printf("enter valid number");
-  }
- } 
- return(0);
+int main()
+{
+    int n;
+    int max=50; 
+    while (1)
+    { 
+        printf("enter 1 to enqueue\n 2 to dequeue\n 3 to display\n 4 to exit");
+        scanf("%d",&n);
+        switch (n) {
+        case 1 :
+                enqueue(max);
+                break;
+        case 2 :
+                dequeue(max);
+                break;
+        case 3 :
+                display();
+                break;
+        case 4 :
+                exit(1);
+        default:
+                printf("enter valid number");
+        }
+    } 
+    return (0);
 }
 
-void enqueue(){
- int value,priority,x,i,j;
- printf("enter the value and its priority");
- scanf("%d%d",&value,&priority);
- if(tail==max)
- {
-  printf("queue is full");
- }
- else if(tail<max)
- {
-  queue[0][tail]=value;
-  queue[1][tail[=priority;
-  for(i=tail;i!=0;i--)
-  {
-   if(queue[1][i]<queue[1][i-1])
-   {
-    for(j=0;j<2;j++)
-    {
-     x=queue[j][i];
-     queue[j][i]=queue[j][i-1];
-     queue[j][i-1]=x;
+void enqueue(int max)
+{
+    int value,priority,x,i,j;
+    printf("enter the value and its priority");
+    scanf("%d%d",&value,&priority);
+    if (tail==max) {
+        printf("queue is full");
     }
-   }
-  }
-  tail++;
- }
+    else if (tail<max) {
+        queue[0][tail]=value;
+        queue[1][tail[=priority;
+        for (i=tail;i!=0;i--)
+        {
+              if (queue[1][i]<queue[1][i-1]) {
+                       for (j=0;j<2;j++)
+                       {
+                       x=queue[j][i];
+                       queue[j][i]=queue[j][i-1];
+                       queue[j][i-1]=x;
+                       }
+              }
+        }
+        tail++;
+    }
 }
 
-void dequeue(){
- if(tail==0)
- {
-  printf("queue is empty");
- }
- else
- {
- int i;
- printf("the element removed is %d",queue[0][head]);
- head++;
- }
+void dequeue(int max)
+{
+    if (tail==0 || head==max) {
+        printf("queue is empty");
+    }
+    else {
+        int i;
+        printf("the element removed is %d",queue[0][head]);
+        head++;
+    }
 }
 
-void display(){
- int i;
- if(tail==0)
- {
-  printf("queue is empty");
- }
- else
- {
-  for(i=head;i<tail;i++)
-  {
-   printf("%d",queue[0][i]);
-  }
- }
+void display()
+{
+    int i;
+    if (tail==0) {
+        printf("queue is empty");
+    }
+    else {
+        for (i=head;i<tail;i++)
+        {
+              printf("%d",queue[0][i]);
+        }
+    }
 }
 
 
