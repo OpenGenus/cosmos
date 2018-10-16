acaf4e4e80e968447ad59dc2aefd6257d8816a8d
Tuba Ali <Tuba Ali>
2018-10-17 00:01:34 +0530

    Add Tower_of_Hanoi folder

---
+++ code/unclassified/src/Tower_of_Hanoi/TowerOfHanoi.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class FunWithRecursion {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		toh("a","b","c",3);
+	}
+
+	private static void toh(String src, String dest, String helper, int i) {
+		if(i==0){
+			return;
+		}
+		toh(src,helper,dest,i-1);
+		System.out.println("Move "+ i +" from "+src+" to "+dest);
+		toh(helper,dest,src,i-1);
+	}
+}
