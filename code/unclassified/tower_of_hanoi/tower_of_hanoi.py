e45eeb5ec5f8707d5469ef252b34d0f911fc8aae
Flavian <flavian.feugueur@epitech.eu>
2018-10-16 12:08:30 +0200

    issue #3810 add tower of hanoi algorithm

---
+++ code/unclassified/tower_of_hanoi/tower_of_hanoi.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# source, middle, and target represent the number of rod
+
+import sys
+
+def tower_hanoi(nb, source, target, middle):
+	if nb is 1:
+		print("move", nb, "from", source, "to", target)
+	else:
+		tower_hanoi(nb - 1, source, middle, target)
+		print("move", nb, "from", source, "to", target)
+		tower_hanoi(nb - 1, middle, target, source)
+
+if len(sys.argv) != 2:
+	print("Usage: python3 tower_of_hanoi.py <nb of discs>")
+	sys.exit(1)
+nb = int(sys.argv[1])
+source = 1
+middle = 2
+target = 3
+tower_hanoi(nb, source, target, middle)
