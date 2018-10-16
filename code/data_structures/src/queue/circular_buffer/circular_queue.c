11322092f9ad10f408ce779fdaee1f34db627222
Sai Lakshmi <42446464+sailakshmi17@users.noreply.github.com>
2018-10-16 14:34:58 +0530

    Updated circular_queue.c
    removed header <conio.h>,global variable and  fixed errors by following c guide

--- code/data_structures/src/queue/circular_buffer/circular_queue.c
+++ code/data_structures/src/queue/circular_buffer/circular_queue.c
@@ -1,101 +1,97 @@
 #include<stdio.h>
-#include<conio.h>
-# define max 5  //size of circular queue can be any value,for time being i kept it as 5
-int head=0;tail=0;
-int queue[max];
 
-void enqueue();
-void dequeue();
-void display();
+int queue[5];
+int head=0,tail=0;
 
-int main(){
- int n;
- while(1)
- {
- printf("enter i to enqueue\n 2 to dequeue \n 3 to display");
- scanf("%d",&x);
- switch(x)
- {
-   case 1 :enqueue();
-           break;
-   case 2 :dequeue();
-           break;
-   case 3 :display();
-           break;  
-   default:printf("enter valid input");        
- }
- }
- return(0);
+void enqueue(int);
+void dequeue(int);
+void display(int);
+
+int main()
+{
+     int x;
+     int max=5;
+     while (1)
+     {
+         printf("enter i to enqueue\n 2 to dequeue \n 3 to display");
+         scanf("%d",&x);
+         switch (x) {
+         case 1 :
+                 enqueue(max);
+                 break;
+         case 2 :
+                 dequeue(max);
+                 break;
+         case 3 :
+                 display(max);
+                 break;  
+         default:
+                 printf("enter valid input");        
+         }
+     }
+     return (0);
 }
 
 
-void enqueue(){
- int val;
- printf("enter a value");
- scanf("%d",&val);
- if(tail!=head-1 && tail<max-1)
- {
-   queue[tail]=val;
-   tail++;
- }
- else if(head!=0 && tail==max-1)
- {
-   queue[tail]=max;
-   tail=0;
- }
- else
- {
-  printf("queue is full");
- }
+void enqueue(int max)
+{
+     int val;
+     printf("enter a value");
+     scanf("%d",&val);
+     if (tail!=head-1 && tail<max-1) {
+           queue[tail]=val;
+           tail++;
+     }
+     else if (head!=0 && tail==max-1) {
+           queue[tail]=max;
+           tail=0;
+     }
+     else {
+           printf("queue is full");
+     }
 }
 
 
 
-void dequeue(){
-  if(tail==0)
-  {
-   printf("queue is empty");
-  }
-  else
-  {
-   printf("the element is %d",queue[head]); 
-  }
-  if(head==max-1)
-  {
-   head=0; 
-  }
-  else
-  {
-   head++;
-  }
+void dequeue(int max)
+{
+      if (tail==0) {
+           printf("queue is empty");
+      }
+      else {
+           printf("the element is %d",queue[head]); 
+      }
+      if (head==max-1) {
+           head=0; 
+      }
+      else {
+           head++;
+      }
 }
 
-void display(){
- if(tail==0)
- {
-  printf("queue is empty");
- }
- else
- {
-   int i;
-   i=head;
-   if(head>tail)
-   {
-       for(;i!=max;i++)
-       {
-         printf("%d",queue[i]);
-       }
-       for(i=0;i<tail;i++)
-       {
-         printf("%d",queue[i]);
-       }
-   }
-   else if(head<tail)
-   {
-       for(i=head;i<tail;i++)
-       {
-         printf("%d",queue[i]);
-       }
-   }
- }
+void display(int max)
+{
+      if (tail==0) {
+           printf("queue is empty");
+      }
+      else {
+           int i;
+           i=head;
+           if (head>tail) {
+                     for (;i!=max;i++)
+                     {
+                         printf("%d",queue[i]);
+                     }
+                     for (i=0;i<tail;i++)
+                     {
+                         printf("%d",queue[i]);
+                     }
+           }
+           else if (head<tail) {
+                     for (i=head;i<tail;i++)
+                     {
+                         printf("%d",queue[i]);
+                     }
+           }
+      }
 }
