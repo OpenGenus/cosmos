98a0ffb35000ae9bc3727115e69b2151c64e3f0e
Sai Lakshmi <42446464+sailakshmi17@users.noreply.github.com>
2018-10-16 14:08:28 +0530

    Updated towers_of_hanoi.c
    removed the header <conio.h>,fixed indentation errors and included forward declaration

--- code/unclassified/src/towers_of_hanoi.c
+++ code/unclassified/src/towers_of_hanoi.c
@@ -1,28 +1,29 @@
 #include<stdio.h>
-#include<conio.h>
 
-int main(){
- int n;
- char source,intermediate,destination; //three towers 
- printf("enter number of disks");
- scanf("%d",&n);
- source='s';
- intermediate='i';
- destination='d';
- toh(n,source,destination,intermediate);
- return(0);
+int toh(int,int,int,int);
+
+int main()
+{
+     int n;
+     char source,intermediate,destination; /*three towers*/ 
+     printf("enter number of disks");
+     scanf("%d",&n);
+     source='s';
+     intermediate='i';
+     destination='d';
+     toh(n,source,destination,intermediate);
+     return (0);
 }
 
-int toh(int n,char s,char d,char i){
- if(n==1)
- {
-  printf("move %d disk from %c to %c \n",n,s,d);
- }
- else
- {
-  toh(n-1,s,i,d);
-  printf("move %d disk from %c to %c \n",n,s,d);
-  toh(n-1,i,d,s);
- }
- return(0);
+int toh(int n, char s, char d, char i)
+{
+     if (n==1){
+         printf("move %d disk from %c to %c \n",n,s,d);
+     }
+     else{
+         toh(n-1,s,i,d);
+         printf("move %d disk from %c to %c \n",n,s,d);
+         toh(n-1,i,d,s);
+     }
+     return(0);
 }
