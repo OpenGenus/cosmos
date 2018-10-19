6a8d23995555e49c80aa857fa019eaed856e47a5
Arnav Borborah <arnavborborah11@gmail.com>
2018-10-19 17:33:18 -0400

    Use idiomatic Rust

--- code/search/src/linear_search/linear_search.rs
+++ code/search/src/linear_search/linear_search.rs
@@ -1,19 +1,18 @@
-fn search(vec: &[i32], n: i32) -> i32 {
+fn search(vec: &[i32], n: i32) -> Option<i32> {
     for i in 0..vec.len() {
         if vec[i] == n {
-            return i as i32; // Element found return index
+            return Some(i as i32); // Element found
         }
     }
-    return -1; // If element not found return -1
+    None
 }
 
 fn main() {
     let arr = vec![1, 3, 45, 56, 8, 21, 7, 69, 12];
     let num = 56;
     let index =  search(&arr, num);
-    if index != -1 {
-        println!("Found {}, at index {}", num, index);
-    }else {
-        println!("{} not found!", num)
+    match index {
+        None => println!("{} not found!", num),
+        Some(i) => println!("Found {}, at index {}", num, i)
     }
 }
