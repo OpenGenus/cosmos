2ef4c4e6e56a77a675e1afa1cadb3dd280937b74
Gustavo Castellanos <gustavoaca1997@gmail.com>
2018-10-21 16:09:19 -0400

    Add LinkedList.hs
    Linked List in Haskell

---
+++ code/data_structures/src/linked_list/LinkedList.hs
@@ -0,0 +1,41 @@
+module LinkedList where
+import Data.Maybe
+{-| A node for a linked list of type `a`-}
+data Node a = Node {
+    value :: a, -- Value of type `a` in node
+    next :: LinkedList a -- Next node
+}
+
+{-| A linked list with nodes of type a -}
+type LinkedList a = Maybe (Node a)
+
+instance (Show a) => Show (Node a) where
+    show node = 
+        (show (value node) ++ " -> " ++
+            case (next node) of
+                Nothing -> "None"
+                Just node' -> show node')
+
+{-| Given a value, create a new node (singleton). -}
+newNode :: a -> Node a
+newNode a = Node {
+    value = a,
+    next = Nothing
+}
+
+{-| Given a linked list and a node, return a new list with the node in its head. -}
+addNode :: Node a -> LinkedList a -> LinkedList a
+addNode node list = Just $ Node {
+    value = value node,
+    next = list
+}
+
+{-| Given a linked list and a value, create a new node for it with this value. -}
+addValue :: a -> LinkedList a -> LinkedList a
+addValue x list = addNode (newNode x) list
+
+{-| Function that returns sum of all values -}
+sumLinkedList :: (Num a) => a -> LinkedList a -> a
+sumLinkedList total Nothing = total
+sumLinkedList x (Just node) =
+    sumLinkedList (x + value node) (next node)
