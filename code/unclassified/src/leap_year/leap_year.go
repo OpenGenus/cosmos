aac2d97ecf056019a881731c4fabd4d1e1cebfb1
Ivan Fonseca <ivanfon@riseup.net>
2018-10-21 09:49:19 -0400

    Fix code formatting

--- code/unclassified/src/leap_year/leap_year.go
+++ code/unclassified/src/leap_year/leap_year.go
@@ -3,41 +3,41 @@ package main
 import "fmt"
 
 func isLeapyear(year int) bool {
-  if year % 400 == 0 {
-    return true
-  }
-  if year % 100 == 0 {
-    return false
-  }
-  if year % 4 == 0 {
-    return true
-  }
+	if year%400 == 0 {
+		return true
+	}
+	if year%100 == 0 {
+		return false
+	}
+	if year%4 == 0 {
+		return true
+	}
 
-  return false
+	return false
 }
 
 func leapYearsInRange(min, max int) []int {
-  var res []int
-  for i := min; i <= max; i++ {
-    if isLeapyear(i) {
-      res = append(res, i)
-    }
-  }
+	var res []int
+	for i := min; i <= max; i++ {
+		if isLeapyear(i) {
+			res = append(res, i)
+		}
+	}
 
-  return res
+	return res
 }
 
 func main() {
-  res := leapYearsInRange(2010, 2030)
-  for _, year := range res {
-    fmt.Println(year)
-  }
-  /*
-   * Output:
-   * 2012
-   * 2016
-   * 2020
-   * 2024
-   * 2028
-   */
+	res := leapYearsInRange(2010, 2030)
+	for _, year := range res {
+		fmt.Println(year)
+	}
+	/*
+	 * Output:
+	 * 2012
+	 * 2016
+	 * 2020
+	 * 2024
+	 * 2028
+	 */
 }
