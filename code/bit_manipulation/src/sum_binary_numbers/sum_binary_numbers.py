c67fe44ac0e1048413909d74a1535ba3b1ecc794
Shay Elmualem <wowshay@gmail.com>
2018-10-24 00:23:06 +0300

    Moved examples into a main() function

--- code/bit_manipulation/src/sum_binary_numbers/sum_binary_numbers.py
+++ code/bit_manipulation/src/sum_binary_numbers/sum_binary_numbers.py
@@ -8,8 +8,13 @@ def sum_binaries(b1: str, b2: str) -> str:
     return bin(int(b1, 2) + int(b2, 2)).lstrip('0b')
 
 
-sum_binaries('10', '1000')
-# Output: 1010
+def main():
+    sum_binaries('10', '1000')
+    # Output: 1010
 
-sum_binaries('1111', '10100')
-# Output: 100011
+    sum_binaries('1111', '10100')
+    # Output: 100011
+
+
+if __name__ == '__main__':
+    main()
