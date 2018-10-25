47cd7ce7a0a375c89ce1c2ad842993c9d8ed41e3
Matheus <matheus.henrique@maisresultado.com.br>
2018-10-25 15:22:13 -0300

    New recursive memoization fibonacci search

--- code/search/src/fibonacci_search/fibonacci_search.py
+++ code/search/src/fibonacci_search/fibonacci_search.py
@@ -1,36 +1,12 @@
-'''
-Part of Cosmos by OpenGenus Foundation
-'''
+memo = {}
 
 
-def fibonacci_search(arr, x):
-    length = len(arr)
-    fib_m2 = 0
-    fib_m1 = 1
-    fib_m = fib_m2 + fib_m1
-
-    while fib_m < length:
-        fib_m2 = fib_m1
-        fib_m1 = fib_m
-        fib_m = fib_m2 + fib_m1
-
-    offset = -1
-    while fib_m > 1:
-        i = min(offset + fib_m2, length - 1)
-
-        if arr[i] < x:
-            fib_m = fib_m1
-            fib_m1 = fib_m2
-            fib_m2 = fib_m - fib_m1
-            offset = i
-        elif arr[i] > x:
-            fib_m = fib_m2
-            fib_m1 = fib_m1 - fib_m2
-            fib_m2 = fib_m - fib_m1
-        else:
-            return i
-
-    if fib_m1 and offset < length - 1 and arr[offset + 1] == x:
-        return offset + 1
-
-    return -1
+def fib(n):
+    if n in memo:
+        return memo[n]
+    if n <= 2:
+        f = 1
+    else:
+        f = fib(n - 1) + fib(n - 2)
+    memo[n] = f
+    return f
