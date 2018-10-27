807c1aca87f00eff1c6ef6ee861fdc2fdcb02a26
Matheus Guimaraes <matheusguimr@gmail.com>
2018-10-27 14:51:38 -0300

    Add Laplacian filter in Python

--- code/filters/src/laplacian_filter/laplacian_filter.py
+++ code/filters/src/laplacian_filter/laplacian_filter.py
@@ -4,17 +4,19 @@ Part of Cosmos by OpenGenus Foundation
 Author: Matheus Guimaraes (@matheusguimr)
 """
 
+
 import cv2
 import numpy as np
+from matplotlib import pyplot as ppl
 
 
-def set_kernel(type_kernel):
+def set_kernel(type_temp):
     laplacian_kernel = 0
-    if type_kernel == 3:
+    if type_temp == 3:
         laplacian_kernel = np.array([[1, 1, 1],
                                      [1, -8, 1],
                                      [1, 1, 1]])
-    elif type_kernel == 5:
+    elif type_temp == 5:
         laplacian_kernel = np.array([[-1, -1, -1, -1, -1],
                                      [-1, -1, -1, -1, -1],
                                      [-1, -1, 24, -1, -1],
@@ -23,25 +25,31 @@ def set_kernel(type_kernel):
     return laplacian_kernel
 
 
-def get_laplacian_filter(gray_image, matrix_type):
-    borders = int(matrix_type / 2)
-    laplacian_kernel = set_kernel(matrix_type)
-    laplacian_image = np.zeros(gray_image.shape, dtype=np.int16)
-    for i in range(borders, gray_image.shape[0] - borders):
-        for j in range(borders, gray_image.shape[1] - borders):
-            kernel = gray_image[i - borders:i + borders + 1, j - borders:j + borders + 1] * laplacian_kernel
+def get_laplacian_filter(gray, matrix_type):
+    bd = int(matrix_type / 2)
+    matrix = set_kernel(matrix_type)
+    laplacian_image = np.zeros(gray.shape, dtype=np.int16)
+    for i in range(bd, gray.shape[0] - bd):
+        for j in range(bd, gray.shape[1] - bd):
+            kernel = gray[i - bd:i + bd + 1, j - bd:j + bd + 1] * matrix
             laplacian_image[i, j] = np.sum(kernel)
     return laplacian_image
 
 
 if __name__ == '__main__':
+    # Read image
     img = cv2.imread('lena.jpg')
+    # Convert image for gray scale
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    laplacian_filter_3x3 = get_laplacian_filter(gray_img, 3)
-    laplacian_filter_5x5 = get_laplacian_filter(gray_img, 5)
+    # Pass image to function
+    filter_3x3 = get_laplacian_filter(gray_img, 3)
+    filter_5x5 = get_laplacian_filter(gray_img, 5)
 
-    cv2.imshow('Original image', img)
-    cv2.imshow('Laplacian filter with 3x3 mask', laplacian_filter_3x3)
-    cv2.imshow('Laplacian filter with 5x5 mask', laplacian_filter_5x5)
-    cv2.waitKey(0)
+    # Show the images
+    fig = ppl.figure()
+    ppl.subplot(1, 3, 1), ppl.imshow(gray_img, 'gray')
+    ppl.subplot(1, 3, 2), ppl.imshow(filter_3x3, 'gray')
+    ppl.subplot(1, 3, 3), ppl.imshow(filter_3x3, 'gray')
+    ppl.show()
+
