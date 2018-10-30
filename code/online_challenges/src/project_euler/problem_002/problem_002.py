9d901fae122e92e06f78ac2a375badc47abde6b7
Leonardo Fernando Arruda Gomes <leonardo.fernando@maisresultado.com.br>
2018-10-30 15:31:35 -0300

    Exercice 2

--- code/online_challenges/src/project_euler/problem_002/problem_002.py
+++ code/online_challenges/src/project_euler/problem_002/problem_002.py
@@ -1,20 +1,18 @@
 def main():
     # Variables to keep track of Fibonacci numbers
-    p2 = 0
-    p1 = 0
-    current = 1
-    total = 0
-
-    while current <= 4000000:
-        # Add even fibonacci numbers
-        total += current if current % 2 == 0 else 0
-
-        # Update fibonacci numbers
-        p2 = p1
-        p1 = current
-        current = p2 + p1
-
-    print(total)
+    prev = 1
+    forw = 1
+    sum = 0
+    isFib = True
+    while isFib:
+        value = prev + forw
+        prev = forw
+        forw = value
+        if value > 4000000:
+            isFib = False
+        if value % 2 == 0 and isFib:
+            sum += value
+    print(sum)
 
 if __name__ == '__main__':
     main()
