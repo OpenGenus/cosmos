6e2e3eef5714fb1a6f2ac5d6e08de890a5cf3ab8
Juan Martin Enriquez <juanenriquez@gmail.com>
2018-10-30 22:45:57 -0300

    Fixes of style and ES6 compliance

--- code/dynamic_programming/src/edit_distance/edit_distance.js
+++ code/dynamic_programming/src/edit_distance/edit_distance.js
@@ -1,9 +1,9 @@
 // dynamic programming | edit distance | Javascript
 // Part of Cosmos by OpenGenus Foundation
 
-editDistance = function(str1, str2) {
-    var lenStr1 = str1.length;
-    var lenStr2 = str2.length;
+editDistance = (str1, str2) => {
+    const lenStr1 = str1.length;
+    const lenStr2 = str2.length;
     
 
     if (lenStr1 == 0) {
@@ -14,29 +14,26 @@ editDistance = function(str1, str2) {
         return lenStr1;
     }
 
-    var distanceVectorInit = [];
-    var distanceVectorFinal = [];
-    for(var i=0; i<lenStr1+1; i++) {
-       distanceVectorInit.push(0);
-       distanceVectorFinal.push(0);
-    }
+    let distanceVectorInit = [];
+    let distanceVectorFinal = [];
 
-    for(var i=0; i<lenStr1+1; i++) {
-        distanceVectorInit[i] = i;
+    for(var i=0; i<=lenStr1; i++) {
+       distanceVectorInit.push(i);
+       distanceVectorFinal.push(0);
     }
 
     for(var i=0; i<lenStr2; i++) {
         distanceVectorFinal[0] = i+1;
         // use formula to fill in the rest of the row
-        for (var j = 0; j < lenStr1; j++) {
-            var substitutionCost = 0;
+        for (let j = 0; j < lenStr1; j++) {
+            let substitutionCost = 0;
             if (str1[j] == str2[i]) {
                 substitutionCost = distanceVectorInit[j];
             } else {
                 substitutionCost = distanceVectorInit[j] + 1
             }
 
-            distanceVectorFinal[j+1] = Math.min(distanceVectorInit[j+1] + 1, distanceVectorFinal[j] + 1, substitutionCost);
+            distanceVectorFinal[j+1] = Math.min(distanceVectorInit[j + 1] + 1, distanceVectorFinal[j] + 1, substitutionCost);
         }
 
         distanceVectorInit = distanceVectorFinal.slice(0);
@@ -45,4 +42,4 @@ editDistance = function(str1, str2) {
     return distanceVectorFinal[lenStr1];
 }
 
-console.log(editDistance("saturday", "sunday")); //Should be equal to 3
+console.log(editDistance('saturday', 'sunday')); //Should be equal to 3
