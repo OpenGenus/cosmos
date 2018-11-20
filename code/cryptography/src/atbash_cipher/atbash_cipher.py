b323f346b6866fbfeae1b6150be1a4351aad6c10
Shlok Gilda <shlok@ost.com>
2018-11-20 12:47:13 +0530

    Added comments.

--- code/cryptography/src/atbash_cipher/atbash_cipher.py
+++ code/cryptography/src/atbash_cipher/atbash_cipher.py
@@ -1,13 +1,17 @@
 # Programmer: Shlok Gilda
 # Updated: 18/1/2018
 # Part of Cosmos by OpenGenus Foundation
-def atbash(plain):
-    cipher = list(plain)
-    for i, c in enumerate(plain):
+
+"""
+Function to encode/decode given string.
+"""
+def cypher(plaintext):
+    cipher = list(plaintext)
+    for i, c in enumerate(plaintext):
         if ord(c) in range(ord('a'), ord('z') + 1):
             cipher[i] = chr(ord('a') + ord('z') - ord(c))
     return ''.join(cipher)
 
 if __name__ == '__main__':
     import sys
-    print atbash(sys.argv[1])
+    print cypher(sys.argv[1])
