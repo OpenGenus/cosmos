2e5c3fbcedc2eb880a781ae0656dddce186c25ab
Shlok Gilda <shlok@ost.com>
2018-11-21 22:08:36 +0530

    Python solution for Project Euler Problem 23.

---
+++ code/online_challenges/src/project_euler/problem_023/problem_023.py
@@ -0,0 +1,25 @@
+def compute():
+	LIMIT = 28124
+	divisorsum = [0] * LIMIT
+	for i in range(1, LIMIT):
+		for j in range(i * 2, LIMIT, i):
+			divisorsum[j] += i
+	abundantnums = [i for (i, x) in enumerate(divisorsum) if x > i]
+
+	expressible = [False] * LIMIT
+	for i in abundantnums:
+		for j in abundantnums:
+			if i + j < LIMIT:
+				expressible[i + j] = True
+			else:
+				break
+
+	ans = 0
+	for (i, x) in enumerate(expressible):
+		if not x:
+			ans += i
+	return str(ans)
+
+
+if __name__ == "__main__":
+	print(compute())
