d3d73a5ea53b9c3c4f4d9b10690699d2d878e9d1
anupamdas104 <anupamdas104@gmail.com>
2018-12-02 16:34:10 -0500

    Added the code to find the nth ugly number in C++ using dynamic programming

--- code/mathematical_algorithms/src/ugly_number/ugly_number.cpp
+++ code/mathematical_algorithms/src/ugly_number/ugly_number.cpp
@@ -19,30 +19,30 @@ unsigned int ugly_number(unsigned int input)
 	
 	//loops through until the intened input to get the ugly number
     for(unsigned int i = 1; i < input; i++)
-	{		
+    {		
         nextUglyNumber = min(nextMultipleOf2, min(nextMultipleOf3, nextMultipleOf5));
-		
+
         uglyNumbers[i] = nextUglyNumber;
-		
+
 		//If the ugly number is same as any of the multiples, icrements the index for that multiple and updates the next multiple
         if(nextUglyNumber == nextMultipleOf2)
-		{
+        {
             indexForNextMultipleOf2 += 1;
             nextMultipleOf2 = uglyNumbers[indexForNextMultipleOf2]*2;
         }
         if(nextUglyNumber == nextMultipleOf3)
-		{
+        {
             indexForNextMultipleOf3 += 1;
             nextMultipleOf3 = uglyNumbers[indexForNextMultipleOf3]*3;
         }
         if(nextUglyNumber == nextMultipleOf5)
-		{
+        {
             indexForNextMultipleOf5 += 1;
             nextMultipleOf5 = uglyNumbers[indexForNextMultipleOf5]*5;
         }
     }
 	
-	//returns the nth ugly number
+    //returns the nth ugly number
     return nextUglyNumber;
 }
 
