a0fef2d82384188c932feca52b57238dfc27723b
Your Name <you@example.com>
2019-02-23 10:20:17 -0800

    added spaces around operators

--- code/mathematical_algorithms/src/tribonacci_numbers/tribonacci_numbers.js
+++ code/mathematical_algorithms/src/tribonacci_numbers/tribonacci_numbers.js
@@ -8,7 +8,7 @@ function tribonacci(n) {
   else if (n == 2) {
     return 1;
   }
-  return tribonacci(n-3) + tribonacci(n-2) + tribonacci(n-1);
+  return tribonacci(n - 3) + tribonacci(n - 2) + tribonacci(n - 1);
 }
 
 tribonacci(15); //1705
