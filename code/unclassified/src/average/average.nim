0a2806b8079c5ed828ae77f550f0ecc907825fa7
Jay Shah <Jay9596@users.noreply.github.com>
2019-02-24 17:58:11 -0500

    Update - Computes average - return float - Add isMainModule

--- code/unclassified/src/average/average.nim
+++ code/unclassified/src/average/average.nim
@@ -1,7 +1,8 @@
-func average(arr: seq[int]): int =
+func average(arr: seq[int]): float =
     for i in arr:
-      result += i
-  
-# echo average(@[1,2,3,4,5])
-# echo @[1,2,3,4,79,3,6,59,92].average
-  
+        result += i.float()
+    result / arr.len().float()
+
+when isMainModule:
+    echo average(@[1, 2, 3, 4, 5])
+    echo @[1, 2, 3, 4, 79, 3, 6, 59, 92].average
