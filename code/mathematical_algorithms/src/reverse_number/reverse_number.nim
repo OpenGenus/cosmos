916ef131820ca7dab6a62f809246a758d0df05af
Jay Shah <Jay9596@users.noreply.github.com>
2019-02-24 19:03:42 -0500

    Update: - Add isMainModule - Remove test func

--- code/mathematical_algorithms/src/reverse_number/reverse_number.nim
+++ code/mathematical_algorithms/src/reverse_number/reverse_number.nim
@@ -1,16 +1,13 @@
 func reverse(number: int): int =
     var n = number.abs()
     while n > 0:
-      let digit = n mod 10
-      result = (result * 10) + digit
-      n = n div 10
-  
+        let digit = n mod 10
+        result = (result * 10) + digit
+        n = n div 10
     if number < 0:
-      result *= -1
-  
-func test() =
+        result *= -1
+
+when isMainModule:
     let num = 12345
     assert((reverse num.reverse) == num)
-  
-# Test
-# test()
+
