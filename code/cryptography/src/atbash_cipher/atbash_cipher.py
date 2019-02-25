1fe62edb878a6d110f26ef5affc92ac69461905d
Shlok Gilda <shlok@ost.com>
2019-02-25 13:57:22 +0530

    Updated formatting as suggested by black.

--- code/cryptography/src/atbash_cipher/atbash_cipher.py
+++ code/cryptography/src/atbash_cipher/atbash_cipher.py
@@ -1,17 +1,15 @@
-# Programmer: Shlok Gilda
-# Updated: 18/1/2018
-# Part of Cosmos by OpenGenus Foundation
-
-"""
-Function to encode/decode given string.
-"""
 def cypher(plaintext):
+    """
+    Function to encode/decode given string.
+    """
     cipher = list(plaintext)
     for i, c in enumerate(plaintext):
-        if ord(c) in range(ord('a'), ord('z') + 1):
-            cipher[i] = chr(ord('a') + ord('z') - ord(c))
-    return ''.join(cipher)
+        if ord(c) in range(ord("a"), ord("z") + 1):
+            cipher[i] = chr(ord("a") + ord("z") - ord(c))
+    return "".join(cipher)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import sys
+
     print cypher(sys.argv[1])
