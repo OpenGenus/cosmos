6fb4801e6bb94589e9d02bc9c268663fd5e81960
Vaibhav <vk530873@gmail.com>
2019-02-25 23:43:34 +0530

    Update balanced_expression.cpp

--- code/data_structures/src/stack/balanced_expression/balanced_expression.cpp
+++ code/data_structures/src/stack/balanced_expression/balanced_expression.cpp
@@ -6,9 +6,8 @@ bool checkBalanced(string s) {
 	if (s.length() % 2 != 0) return false;
 	stack <char> st;
 	for (int i = 0; i < s.length(); i++) {
-		if (s[i] == '{' || s[i] == '[' || s[i] == '(') {
+		if (s[i] == '{' || s[i] == '[' || s[i] == '(') 
 			st.push(s[i]);
-		}
 		else {
 			char temp = st.top();
 			if (s[i] == '}' && temp == '{') st.pop();
