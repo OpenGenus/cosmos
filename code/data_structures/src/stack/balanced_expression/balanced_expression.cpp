5a62ee83d71343943c354674e0840a90c54da493
Arnav Borborah <arnavborborah11@gmail.com>
2019-02-26 17:54:09 +0530

    Update code/data_structures/src/stack/balanced_expression/balanced_expression.cpp
    Co-Authored-By: ervaibhavkumar <vk530873@gmail.com>

--- code/data_structures/src/stack/balanced_expression/balanced_expression.cpp
+++ code/data_structures/src/stack/balanced_expression/balanced_expression.cpp
@@ -3,7 +3,7 @@
 
 bool checkBalanced(string s) {
 	if (s.length() % 2 != 0) return false;
-	stack <char> st;
+	std::stack <char> st;
 	for (int i = 0; i < s.length(); i++) {
 		if (s[i] == '{' || s[i] == '[' || s[i] == '(') 
 			st.push(s[i]);
