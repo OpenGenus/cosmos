fb234e7bd9176538441b14553ba420c89aa180ec
Bhupesh-V <varshneybhupesh@gmail.com>
2019-02-26 17:27:25 +0530

    review changes done

--- code/unclassified/src/optimized_fibonacci/optimized_fibonacci.cpp
+++ code/unclassified/src/optimized_fibonacci/optimized_fibonacci.cpp
@@ -1,13 +1,15 @@
 /* Fibonacci Series implemented using Memoization */
 #include <iostream>
-using namespace std;
 
 long long fibonacci (int n) {
-
     static long long fib[100] = {}; // initialises the array with all elements as 0
-    fib[1] = 0; fib[2] = 1;
-	if (n == 1 || n == 2) return fib[n];
-	else if (fib[n] != 0) return fib[n];
+    fib[1] = 0; 
+    fib[2] = 1;
+	
+	if (n == 1 || n == 2) 
+		return fib[n];
+	else if (fib[n] != 0) 
+		return fib[n];
 	else {
 	    fib[n] = fibonacci (n - 1) + fibonacci (n - 2);
 	    return fib[n];
@@ -17,10 +19,10 @@ long long fibonacci (int n) {
 int main () {
 	
 	int n;
-	cout << "Enter number of terms: ";
-	cin >> n;
+	std::cout << "Enter number of terms: ";
+	std::cin >> n;
 	for (int i = 1; i <= n; i++) 
-		cout << fibonacci (i) << endl; 	
+		std::cout << fibonacci (i) << std::endl; 	
 
 	return 0;
 }
