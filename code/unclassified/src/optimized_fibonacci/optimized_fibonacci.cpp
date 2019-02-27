7e2eafd688844bab02e2ddc713c1b5b1a8b0ff0d
Arnav Borborah <arnavborborah11@gmail.com>
2019-02-27 06:33:14 -0500

    Remove extra whitespace/lines

--- code/unclassified/src/optimized_fibonacci/optimized_fibonacci.cpp
+++ code/unclassified/src/optimized_fibonacci/optimized_fibonacci.cpp
@@ -3,12 +3,12 @@
 
 long long fibonacci (int n) {
     static long long fib[100] = {}; // initialises the array with all elements as 0
-    fib[1] = 0; 
+    fib[1] = 0;
     fib[2] = 1;
-	
-    if (n == 1 || n == 2) 
+
+    if (n == 1 || n == 2)
 	return fib[n];
-    else if (fib[n] != 0) 
+    else if (fib[n] != 0)
 	return fib[n];
     else {
 	fib[n] = fibonacci (n - 1) + fibonacci (n - 2);
@@ -17,12 +17,9 @@ long long fibonacci (int n) {
 }
 
 int main () {
-	
 	int n;
 	std::cout << "Enter number of terms: ";
 	std::cin >> n;
-	for (int i = 1; i <= n; i++) 
-		std::cout << fibonacci (i) << std::endl; 	
-
-	return 0;
+	for (int i = 1; i <= n; i++)
+		std::cout << fibonacci (i) << std::endl;
 }
