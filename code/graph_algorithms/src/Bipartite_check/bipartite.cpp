371079a9faec3f74f3399aa3a56d8ca3e6544704
vaibhav Pathak <46650456+quantum08@users.noreply.github.com>
2019-03-04 23:52:15 +0530

    Update bipartite.cpp
    1.)  updating header File .
    
    2.) removing extra spacing
    
    3.) Better  declaration of variable

--- code/graph_algorithms/src/Bipartite_check/bipartite.cpp
+++ code/graph_algorithms/src/Bipartite_check/bipartite.cpp
@@ -1,28 +1,26 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<queue>
 using namespace std;
 int main()
 {
-    long int n,m,u,v,c,flag=0;
-
+    long int vertex,edge,u,v,c,flag=0;
     // taking n ->number of vertex  and m -> number of edges
-    cin>>n>>m;
-
+    cin>>vertex>>edge;
     // adj is 2-D vector to store edges between vertex
-    vector<vector<long int > >adj(n,vector<long int>());
-
-    // dis is use to store color of vertex
-    vector<long int> dis(n,-1);
+    vector<vector<long int > >adj(vertex,vector<long int>());
+    // color is use to store color of vertex
+    vector<long int> color(vertex,-1);
     queue<long int>q;
-
     //for number of edges times
-    for(int i=0;i<m;i++)
+    for(int i=0;i<edge;i++)
     {
         cin>>u>>v;
         adj[u-1].push_back(v-1);
         adj[v-1].push_back(u-1);
     }
     u=0;
-    dis[u]=0;
+    color[u]=0;
     q.push(u);
     while(!q.empty())
     {
@@ -31,12 +29,12 @@ int main()
         for(auto i=adj[c].begin();i!=adj[c].end();i++)
         {
             // updating color of vertex
-            if(dis[*i]==-1 )
+            if(color[*i]==-1 )
             {
                 q.push(*i);
-                dis[*i]=1-dis[c];
+                color[*i]=1-color[c];
             }
-            else if(dis[*i]==dis[c])  // checking for bipartite graph
+            else if(color[*i]==color[c])  // checking for bipartite graph
             {
                 flag=1;
             }
