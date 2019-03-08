f524a1689fe2df5c0fc1f09359c072581d9587bc
vaibhav Pathak <46650456+quantum08@users.noreply.github.com>
2019-03-08 23:40:51 +0530

    Update bipartite.cpp

--- code/graph_algorithms/src/Bipartite_check/bipartite.cpp
+++ code/graph_algorithms/src/Bipartite_check/bipartite.cpp
@@ -1,21 +1,20 @@
 #include<iostream>
 #include<vector>
 #include<queue>
-using namespace std;
 int main()
 {
     long int vertex,edge,u,v,c,flag=0;
     // taking n ->number of vertex  and m -> number of edges
-    cin>>vertex>>edge;
+    std::cin>>vertex>>edge;
     // adj is 2-D vector to store edges between vertex
-    vector<vector<long int > >adj(vertex,vector<long int>());
+    std::vector<std::vector<long int > >adj(vertex,std::vector<long int>());
     // color is use to store color of vertex
-    vector<long int> color(vertex,-1);
-    queue<long int>q;
+    std::vector<long int> color(vertex,-1);
+    std::queue<long int>q;
     //for number of edges times
     for(int i=0;i<edge;i++)
     {
-        cin>>u>>v;
+        std::cin>>u>>v;
         adj[u-1].push_back(v-1);
         adj[v-1].push_back(u-1);
     }
@@ -41,7 +40,7 @@ int main()
         }
     }
     if(flag==0)
-        cout<<"Graph is bipartite";
+        std::cout<<"Graph is bipartite";
     else
-        cout<<"Graph is not bipartite";
+        std::cout<<"Graph is not bipartite";
 }
