64b41aa994c222d1df7c29f4ad09b1e77fb3b960
Bhupesh-V <varshneybhupesh@gmail.com>
2019-03-09 12:45:37 +0530

    minor changes

--- code/data_structures/src/queue/priority_queue/priority_queue.cpp
+++ code/data_structures/src/queue/priority_queue/priority_queue.cpp
@@ -13,95 +13,103 @@ struct node {
 };
 
 class PriorityQueue {
-    private:
-        node *front;
-    public:
-        PriorityQueue() {
-            front = nullptr;
-        }
-        // Insert into Priority Queue
-        void add(int item, int priority) {
-            node *tmp, *q;
-            tmp = new node;
-            tmp->info = item;
-            tmp->priority = priority;
-            if (front == nullptr || priority < front->priority){
-                tmp -> link = front;
-                front = tmp;
-            }
-            else {
-                q = front;
-                while (q->link != nullptr && q->link -> priority <= priority)
-                    q = q->link;
+private:
+    node *front;
+public:
+    PriorityQueue() {
+        front = nullptr;
+    }
 
-                tmp->link = q->link;
-                q->link = tmp;
+    // Add an element into Priority Queue according to its priority
+    void add(int item, int priority) {
+        node *tmp, *q;
+        tmp = new node;
+        tmp->info = item;
+        tmp->priority = priority;
+        if (front == nullptr || priority < front->priority){
+            tmp -> link = front;
+            front = tmp;
+        }
+        else {
+            q = front;
+            while (q->link != nullptr && q->link -> priority <= priority)
+                q = q->link;
+
+            tmp->link = q->link;
+            q->link = tmp;
+        }
+    }
+    
+    // Removes an element with highest priority from Priority Queue 
+    void remove() {
+        node *tmp;
+        if(front == nullptr)
+            throw true;
+        else {
+            tmp = front;
+            std::cout << "Removed item is: " << tmp->info << "\n";
+            front = front->link;
+            delete tmp;
+        }
+    }
+    
+    // Print all the elements in the Priority Queue
+    void display() {
+        node *ptr;
+        ptr = front;
+        if (front == nullptr)
+            std::cout << "Queue is empty\n";
+        else{	
+            std::cout << "Queue is :\n";
+            std::cout << "Priority Item\n";
+            while(ptr != nullptr){
+                std::cout << ptr->priority << "\t\t" << ptr->info << "\n";
+                ptr = ptr->link;
             }
         }
-        
-        // Delete from Priority Queue
-        void remove() {
-            node *tmp;
-            if(front == nullptr)
-                std::cout << "Queue Underflow\n";
-            else {
-                tmp = front;
-                std::cout << "Removed item is: " << tmp->info << "\n";
-                front = front->link;
-                free(tmp);
-            }
-        }
-        
-        // Print Priority Queue
-        void display() {
-            node *ptr;
-            ptr = front;
-            if (front == nullptr)
-                std::cout << "Queue is empty\n";
-            else{	
-                std::cout << "Queue is :\n";
-                std::cout << "Priority Item\n";
-                while(ptr != nullptr){
-                    std::cout << ptr->priority << "\t\t" << ptr->info << "\n";
-                    ptr = ptr->link;
-                }
-            }
-        }
+    }
 };
 
 int main() {
 
     int choice, item, priority;
     PriorityQueue pq; 
-
-    do{
-        std::cout << "1.Insert\n";
-        std::cout << "2.Delete\n";
-        std::cout << "3.Display\n";
-        std::cout << "4.Quit\n";
-        std::cout << "Enter your choice : "; 
-        std::cin >> choice;
-        switch(choice){
-        case 1:
-            std::cout << "Input the item value : ";
-            std::cin >> item;
-            std::cout << "Enter its priority : ";
-            std::cin >> priority;
-            pq.add(item, priority);
-            break;
-        case 2:
-            pq.remove();
-            break;
-        case 3:
-            pq.display();
-            break;
-        case 4:
-            break;
-        default :
-            std::cout << "Wrong Choice !!!\n";
+    
+    try {
+            do{
+            std::cout << "1.Insert\n";
+            std::cout << "2.Delete\n";
+            std::cout << "3.Display\n";
+            std::cout << "4.Quit\n";
+            std::cout << "Enter your choice : "; 
+            std::cin >> choice;
+            switch(choice) {
+                case 1:
+                    std::cout << "Input the item value : ";
+                    std::cin >> item;
+                    std::cout << "Enter its priority : ";
+                    std::cin >> priority;
+                    pq.add(item, priority);
+                    break;
+                case 2:
+                    pq.remove();
+                    break;
+                case 3:
+                    pq.display();
+                    break;
+                case 4:
+                    break;
+                default :
+                    std::cout << "Wrong Choice !!!\n";
+            }
+        } while(choice != 4);
+    }
+    catch(bool test){
+        if (test){
+            std::cout << "\tQueue Underflow !!\n";
+            exit(0);
         }
     }
-    while(choice != 4);
-
+    
     return 0;
 }
