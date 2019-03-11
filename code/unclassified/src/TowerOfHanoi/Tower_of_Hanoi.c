9c1f599ffd1c4129e0e6e9019dc10d05d0d6e4f6
GAURAV PONKIYA <39650633+GP1999@users.noreply.github.com>
2019-03-11 22:06:43 +0530

    Add files via upload

---
+++ code/unclassified/src/TowerOfHanoi/Tower_of_Hanoi.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void TowerOfHanoi(int n, char from_rod, char to_rod, char aux_rod);
+int
+main()
+{
+    int n;
+    char source,destination,auxi;
+    printf("Enter no. of disk:");
+    scanf("%d",&n);
+    printf("Enter source rode name:");
+    scanf(" %c",&source);
+    printf("Enter Destination rode name:");
+    scanf(" %c",&destination);
+    printf("Enter Auxi. rode name:");
+    scanf(" %c",&auxi);
+    TowerOfHanoi(n,source ,destination,auxi);
+	return 0;
+}
+
+void
+TowerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
+{
+	if (n == 1) {
+		printf("\n Move disk 1 from rod %c to rod %c", from_rod, to_rod);
+		return ;
+	}
+	TowerOfHanoi(n-1, from_rod, aux_rod, to_rod);
+	printf("\n Move disk %d from rod %c to rod %c", n, from_rod, to_rod);
+	TowerOfHanoi(n-1, aux_rod, to_rod, from_rod);
+}
