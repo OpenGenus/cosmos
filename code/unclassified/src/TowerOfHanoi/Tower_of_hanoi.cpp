004c0bf11da831120727a8e1436978f46edd26f3
GAURAV PONKIYA <39650633+GP1999@users.noreply.github.com>
2019-03-11 21:47:13 +0530

    Create Tower_of_hanoi.cpp

---
+++ code/unclassified/src/TowerOfHanoi/Tower_of_hanoi.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+void TowerOfHanoi(int n, char from_rod, char to_rod, char aux_rod);
+int
+main()
+{
+    int n;
+    char source,destination,auxi;
+    cout<<"Enter no. of disk:";
+    cin>>n;
+    cout<<"Enter source rode name:";
+    cin>>source;
+    cout<<"Enter Destination rode name:";
+    cin>>destination;
+    cout<<"Enter Auxi. rode name:";
+    cin>>auxi;
+   TowerOfHanoi(n,source ,destination,auxi);
+	return 0;
+}
+
+void
+TowerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
+{
+	if (n == 1) {
+		cout<<"Move disk 1 from rod "<<from_rod<<" to rod " <<to_rod<<endl;
+		return;
+	}
+	TowerOfHanoi(n-1, from_rod, aux_rod, to_rod);
+	cout<<"Move disk "<<n<< " from rod "<<from_rod<<" to rod "<<to_rod<<endl;
+	TowerOfHanoi(n-1, aux_rod, to_rod, from_rod);
+}
