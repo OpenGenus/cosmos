8851774238ada1f3a9d7086579fa64f5d12589f5
GAURAV PONKIYA <39650633+GP1999@users.noreply.github.com>
2019-03-11 21:54:26 +0530

    Update Tower_of_hanoi.cpp

--- code/unclassified/src/TowerOfHanoi/Tower_of_hanoi.cpp
+++ code/unclassified/src/TowerOfHanoi/Tower_of_hanoi.cpp
@@ -1,3 +1,15 @@
+//Algorithm of tower of Hanoi
+//START
+// Hanoi(disk_no, source, dest, aux)
+//   IF disk == 1, THEN
+//      move disk from source to dest             
+//   ELSE
+//      Hanoi(disk_no - 1, source, aux, dest)   : Step 1
+//      move disk from source to dest           : Step 2
+//      Hanoi(disk_no - 1, aux, dest, source)   :  Step 3
+//   END IF   
+//END Procedure
+//STOP
 #include<iostream>
 using namespace std;
 void TowerOfHanoi(int n, char from_rod, char to_rod, char aux_rod);
@@ -14,18 +26,18 @@ main()
     cin>>destination;
     cout<<"Enter Auxi. rode name:";
     cin>>auxi;
-   TowerOfHanoi(n,source ,destination,auxi);
-	return 0;
+    TowerOfHanoi(n,source ,destination,auxi);
+   return 0;
 }
 
 void
 TowerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
 {
-	if (n == 1) {
-		cout<<"Move disk 1 from rod "<<from_rod<<" to rod " <<to_rod<<endl;
-		return;
+    if (n == 1) {
+	    cout<<"Move disk 1 from rod "<<from_rod<<" to rod " <<to_rod<<endl;
+	    return;
 	}
-	TowerOfHanoi(n-1, from_rod, aux_rod, to_rod);
-	cout<<"Move disk "<<n<< " from rod "<<from_rod<<" to rod "<<to_rod<<endl;
-	TowerOfHanoi(n-1, aux_rod, to_rod, from_rod);
+    TowerOfHanoi(n-1, from_rod, aux_rod, to_rod);
+    cout<<"Move disk "<<n<< " from rod "<<from_rod<<" to rod "<<to_rod<<endl;
+    TowerOfHanoi(n-1, aux_rod, to_rod, from_rod);
 }
