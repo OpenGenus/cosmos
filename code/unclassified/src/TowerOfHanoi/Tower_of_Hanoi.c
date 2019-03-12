b664556ecb44178857286aa92ce1ff592a827721
GAURAV PONKIYA <39650633+GP1999@users.noreply.github.com>
2019-03-12 23:45:21 +0530

    Update Tower_of_Hanoi.c

--- code/unclassified/src/TowerOfHanoi/Tower_of_Hanoi.c
+++ code/unclassified/src/TowerOfHanoi/Tower_of_Hanoi.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-void TowerOfHanoi(int n, char from_rod, char to_rod, char aux_rod);
+void TowerOfHanoi (int n, char from_rod, char to_rod, char aux_rod);
 int
 main()
 {
@@ -17,13 +17,13 @@ main()
 	return 0;
 }
 void
-TowerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
+TowerOfHanoi (int n, char from_rod, char to_rod, char aux_rod)
 {
 	if (n == 1) {
 		printf("\n Move disk 1 from rod %c to rod %c", from_rod, to_rod);
 		return ;
 	}
-	TowerOfHanoi(n-1, from_rod, aux_rod, to_rod);
+	TowerOfHanoi (n-1, from_rod, aux_rod, to_rod);
 	printf("\n Move disk %d from rod %c to rod %c", n, from_rod, to_rod);
-	TowerOfHanoi(n-1, aux_rod, to_rod, from_rod);
+	TowerOfHanoi (n-1, aux_rod, to_rod, from_rod);
 }
