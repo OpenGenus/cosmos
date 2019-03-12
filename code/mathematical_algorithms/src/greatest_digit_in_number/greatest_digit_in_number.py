00f2081a4c737615d7025850693fef7f992d0c74
u-wut-m8 <sumanmit.padhi@outlook.com>
2019-03-12 17:40:50 +0530

    Add Python one liner to find greatest digit in a number.

--- code/mathematical_algorithms/src/greatest_digit_in_number/greatest_digit_in_number.py
+++ code/mathematical_algorithms/src/greatest_digit_in_number/greatest_digit_in_number.py
@@ -1,17 +1,2 @@
 #!/usr/bin/python3
-
-
-def greatest_digit(n):
-    max_digit = int(n % 10)
-
-    while n:
-        if max_digit < n % 10:
-            max_digit = n % 10
-        else:
-            max_digit = max_digit
-        n /= 10
-    return max_digit
-
-
-user_number = int(input("enter number: "))
-print("greatest digit is :\n", int(greatest_digit(user_number)))
+print(int(max([*input("Enter number: ")])))
