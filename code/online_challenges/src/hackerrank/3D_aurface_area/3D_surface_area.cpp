d51fe21472a7d191d85957c57303ae2670227e5b
Bharat Arya <arya.bharat@rocketmail.com>
2019-03-19 19:00:52 +0530

    Update 3D_surface_area.cpp

--- code/online_challenges/src/hackerrank/3D_aurface_area/3D_surface_area.cpp
+++ code/online_challenges/src/hackerrank/3D_aurface_area/3D_surface_area.cpp
@@ -1,49 +1,52 @@
 #include < iostream >
 
-  int main() {
+int main()
+{
     int h, w;
     int a[110][110];
     std::cin >> h >> w;
     for (int i = 0; i < h; ++i) {
-      for (int j = 0; j < w; ++j)
-        std::cin >> a[i][j];
+        for (int j = 0; j < w; ++j)
+            std::cin >> a[i][j];
     }
     int sum = 0;
     for (int i = 0; i < h; ++i) {
-      if (i == 0) {
-        for (int j = 0; j < w; ++j) {
-          int temp = 0;
-          if (j == 0) {
-            temp = (a[i][j] - 1) * 4 + 6;
-            sum = sum + temp;
-          } else {
-            temp = (a[i][j] - 1) * 4 + 6;
-            int d = std::min(a[i][j - 1], a[i][j]);
-            temp = temp - (d * 2);
-            sum = sum + temp;
-          }
+        if (i == 0) {
+            for (int j = 0; j < w; ++j) {
+                int temp = 0;
+                if (j == 0) {
+                    temp = (a[i][j] - 1) * 4 + 6;
+                    sum = sum + temp;
+                }
+                else {
+                    temp = (a[i][j] - 1) * 4 + 6;
+                    int d = std::min(a[i][j - 1], a[i][j]);
+                    temp = temp - (d * 2);
+                    sum = sum + temp;
+                }
+            }
         }
-      } else {
-        for (int j = 0; j < w; ++j) {
-          int temp = 0;
-          if (j == 0) {
-            temp = (a[i][j] - 1) * 4 + 6;
-            int d = std::min(a[i][j], a[i - 1][j]);
-            temp = temp - (d * 2);
-            sum = sum + temp;
-          } else {
-            temp = (a[i][j] - 1) * 4 + 6;
-            int d = std::min(a[i][j - 1], a[i][j]);
-            temp = temp - (d * 2);
-            d = std::min(a[i][j], a[i - 1][j]);
-            temp = temp - (d * 2);
-            sum = sum + temp;
-          }
+        else {
+            for (int j = 0; j < w; ++j) {
+                int temp = 0;
+                if (j == 0) {
+                    temp = (a[i][j] - 1) * 4 + 6;
+                    int d = std::min(a[i][j], a[i - 1][j]);
+                    temp = temp - (d * 2);
+                    sum = sum + temp;
+                }
+                else {
+                    temp = (a[i][j] - 1) * 4 + 6;
+                    int d = std::min(a[i][j - 1], a[i][j]);
+                    temp = temp - (d * 2);
+                    d = std::min(a[i][j], a[i - 1][j]);
+                    temp = temp - (d * 2);
+                    sum = sum + temp;
+                }
+            }
         }
-      }
-
     }
     std::cout << sum;
 
     return 0;
-  }
+}
