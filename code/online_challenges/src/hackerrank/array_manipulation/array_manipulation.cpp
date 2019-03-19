c7adcce1bf0d28f74fdeeddb4e2dfb4d0efefe5b
Bharat Arya <arya.bharat@rocketmail.com>
2019-03-19 19:02:58 +0530

    Update array_manipulation.cpp

--- code/online_challenges/src/hackerrank/array_manipulation/array_manipulation.cpp
+++ code/online_challenges/src/hackerrank/array_manipulation/array_manipulation.cpp
@@ -2,30 +2,31 @@
 #include <vector>
 #include <algorithm>
 
-int main() {
-  int n;
-  int m;
-
-  std::cin >> n >> m;
-  std::vector < std::pair < int, int > > v;
-
-  for (int a0 = 0; a0 < m; a0++) {
+int main()
+{
+    int n;
+    int m;
     int a;
     int b;
     int k;
-    std::cin >> a >> b >> k;
-    v.push_back(std::make_pair(a, k));
-    v.push_back(std::make_pair(b + 1, -1 * k));
-  }
 
-  long mx = 0, sum = 0;
-  sort(v.begin(), v.end());
+    std::cin >> n >> m;
+    std::vector<std::pair<int, int> > v;
 
-  for (int i = 0; i < 2 * m; i++) {
-    sum += v[i].second;
-    mx = std::max(mx, sum);
-  }
+    for (int a0 = 0; a0 < m; ++a0) {
+        std::cin >> a >> b >> k;
+        v.push_back(std::make_pair(a, k));
+        v.push_back(std::make_pair(b + 1, -1 * k));
+    }
 
-  std::cout << mx << "\n";
-  return 0;
+    long mx = 0, sum = 0;
+    sort(v.begin(), v.end());
+
+    for (int i = 0; i < 2 * m; ++i) {
+        sum += v[i].second;
+        mx = std::max(mx, sum);
+    }
+
+    std::cout << mx << "\n";
+    return 0;
 }
