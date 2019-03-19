1941064d9d3c038a5c955a5e10a3a6e9f836ccd5
Bharat Arya <arya.bharat@rocketmail.com>
2019-03-19 19:03:53 +0530

    Update bigger_is_greater.cpp

--- code/online_challenges/src/hackerrank/bigger_is_greater/bigger_is_greater.cpp
+++ code/online_challenges/src/hackerrank/bigger_is_greater/bigger_is_greater.cpp
@@ -6,14 +6,13 @@ int main()
     int t;
     std::cin >> t;
 
-    while(t--)
-    {
+    while (t--) {
         std::string s;
         std::cin >> s;
-        bool ans = next_permutation(s.begin(), s.end());        // do permutation and store it in string ans
-        if(ans == false)         // if there is no permutation possible original and ans will be same
-            std::cout <<"no answer \n";
+        bool ans = next_permutation(s.begin(), s.end());         // do permutation and store it in string ans
+        if (ans == false)        // if there is no permutation possible original and ans will be same
+            std::cout << "no answer \n";
         else
-            std::cout << s << "\n";        // else print ans
+            std::cout << s << "\n";         // else print ans
     }
 }
