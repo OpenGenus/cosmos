cbb9d3fc0082bb99121a754873e0e825c128d86a
Bharat Arya <arya.bharat@rocketmail.com>
2019-03-19 21:07:35 +0530

    Update 3D_surface_area.cpp

--- code/online_challenges/src/hackerrank/3D_aurface_area/3D_surface_area.cpp
+++ code/online_challenges/src/hackerrank/3D_aurface_area/3D_surface_area.cpp
@@ -22,7 +22,7 @@ int main()
                     temp = (a[i][j] - 1) * 4 + 6;
                     int d = std::min(a[i][j - 1], a[i][j]);
                     temp = temp - (d * 2);
-                    sum = sum + temp;
+                    sum  += temp;
                 }
             }
         }
