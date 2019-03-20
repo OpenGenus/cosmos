4c56e360093d891659ea20fd8bd8621754819f82
Arunkumar0258 <everythinghere007@gmail.com>
2019-03-20 13:45:42 +0530

    basic structure added

---
+++ code/data_structures/src/list/singly_linked_list/singly_list.c
@@ -0,0 +1,8 @@
+/* This project aims to demonstrate various operations on linked lists. It uses
+a clean command line interface and provides a baseline to make linked list
+applications in C. There are two files. The C file containing main() function and
+invoking functions. The link.h header file where all functions are implemented.
+*/
+
+#include <unistd.h>     // for sleep() function
+#include "link.h"       // custom header file to implement functions
