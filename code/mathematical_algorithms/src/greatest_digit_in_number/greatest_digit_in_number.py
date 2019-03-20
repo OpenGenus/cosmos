035b18f1eab32244216e92a4929788c7924e4a60
u-wut-m8 <sumanmit.padhi@outlook.com>
2019-03-21 01:48:48 +0530

    Update Python implementation of program to find greatest digit in a number.

--- code/mathematical_algorithms/src/greatest_digit_in_number/greatest_digit_in_number.py
+++ code/mathematical_algorithms/src/greatest_digit_in_number/greatest_digit_in_number.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
-print(int(max([*input("Enter number: ")])))
+
+
+def greatest_digit(n):
+    max_digit = int(n % 10)
+
+    while n:
+        if max_digit < n % 10:
+            max_digit = n % 10
+        else:
+            max_digit = max_digit
+        n /= 10
+    return max_digit
+
+
+def greatest_digits_with_builtins(num):
+    print("Greatest digit is: {}".format(int(max([*str(num)]))))
+
+
+user_number = int(input("enter number: "))
+print("greatest digit is :\n", int(greatest_digit(user_number)))
+greatest_digits_with_builtins(user_number)
