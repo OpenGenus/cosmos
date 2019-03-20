2590ea1d2e8ec99a3fb2a6af6453fe51813b430e
Arunkumar0258 <everythinghere007@gmail.com>
2019-03-21 03:12:35 +0530

    insert operations added

--- code/data_structures/src/list/singly_linked_list/menu_interface/singly_list.c
+++ code/data_structures/src/list/singly_linked_list/menu_interface/singly_list.c
@@ -23,3 +23,64 @@ int main()
 		c = menu();
 	}while(c!=17);
 }
+
+void print()
+{
+	printf("1. Insert at beginning\n");
+	printf("2. Insert at end\n");
+	printf("3. Insert at a particular location\n");
+	printf("4. Insert after a particular value\n");
+	printf("5. Delete from beginning\n");
+	printf("6. Delete from end\n");
+	printf("7. Delete a particular node\n");
+	printf("8. Delete a particular value\n");
+	printf("9. Replace a value at a particular node\n");
+	printf("10. Replace a particular value in the list\n");
+	printf("11. Delete all duplicate values and sort\n");
+	printf("12. Display\n");
+	printf("13. Reverse\n");
+	printf("14. Sort(Ascending)\n");
+	printf("15. Write to File\n");
+	printf("16. Read from a file\n");
+	printf("17. Quit\n");
+}
+
+int menu()
+{
+	int n,a,b;
+	char *filename = malloc(MAX_LENGTH*sizeof(char));
+	clear();
+	usleep(500000);
+
+	print();
+
+	printf("Select the operation you want to perform (17 to Quit): ");
+	scanf("%d",&n);
+
+		switch(n)
+		{
+			case 1:
+					printf("Enter the number you want to insert: ");
+					scanf("%d",&a);
+					insert_beg(a);
+					break;
+			case 2:
+					printf("Enter the number you want to insert: ");
+					scanf("%d",&a);
+					insert_end(a);
+					break;
+			case 3:
+					printf("Enter the number you want to insert: ");
+					scanf("%d",&a);
+					printf("Enter the location in which you want to insert: ");
+					scanf("%d",&b);
+					insert_at_loc(a,b);
+					break;
+			case 4:
+					printf("Enter the number you want to insert: ");
+					scanf("%d",&a);
+					printf("Enter the value after which to insert: ");
+					scanf("%d",&b);
+					insert_after_value(a,b);
+					break;
+      
