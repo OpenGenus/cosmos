e77f6d6ee3633b8848c08fe228500966285a3b61
Arunkumar0258 <everythinghere007@gmail.com>
2019-03-21 15:04:28 +0530

    revised code v1

--- code/data_structures/src/list/singly_linked_list/menu_interface/singly_list.c
+++ code/data_structures/src/list/singly_linked_list/menu_interface/singly_list.c
@@ -4,8 +4,31 @@ applications in C. There are two files. The C file containing main() function an
 invoking functions. The link.h header file where all functions are implemented.
 */
 
-#include <unistd.h>     // for sleep() function
-#include "link.h"       // custom header file to implement functions
+#include <unistd.h>     /* for sleep() function */
+#include "link.h"       /* custom header file to implement functions */
+
+/* For clearing screen in Windows and UNIX-based OS */
+#ifdef _WIN32
+	#define CLEAR "cls"
+#else 		/* In any other OS */
+	#define CLEAR "clear"
+#endif
+
+/* For cross-platform delay function for better UI transition */
+#ifdef _WIN32
+	#include <Windows.h>
+#else
+	#include <unistd.h>
+#endif
+
+void msleep(int ms)
+{
+#ifdef _WIN32
+    Sleep(ms);
+#else
+    usleep(ms * 1000);
+#endif
+}
 
 void print();
 void clear();
@@ -16,7 +39,7 @@ int main()
 	int c;
 	printf("WELCOME TO THE LINKED LIST APPLICATION\n");
 	clear();
-	usleep(500000);
+	msleep(500);
 
 	do
 	{
@@ -47,104 +70,102 @@ void print()
 
 int menu()
 {
-	int n,a,b;
-	char *filename = malloc(MAX_LENGTH*sizeof(char));
 	clear();
-	usleep(500000);
+	msleep(500);		/* for delay effect 500ms */
 
 	print();
 
+	int n,a,b;
+	char *filename = malloc(MAX_LENGTH*sizeof(char));		/* For writing and reading linked list data from file */
 	printf("Select the operation you want to perform (17 to Quit): ");
 	scanf("%d",&n);
 
-		switch(n)
-		{
-			case 1:
-					printf("Enter the number you want to insert: ");
-					scanf("%d",&a);
-					insert_beg(a);
-					break;
-			case 2:
-					printf("Enter the number you want to insert: ");
-					scanf("%d",&a);
-					insert_end(a);
-					break;
-			case 3:
-					printf("Enter the number you want to insert: ");
-					scanf("%d",&a);
-					printf("Enter the location in which you want to insert: ");
-					scanf("%d",&b);
-					insert_at_loc(a,b);
-					break;
-			case 4:
-					printf("Enter the number you want to insert: ");
-					scanf("%d",&a);
-					printf("Enter the value after which to insert: ");
-					scanf("%d",&b);
-					insert_after_value(a,b);
-					break;
-      case 5:
-				delete_beg();
-				break;
-		case 6:
-				delete_end();
-				break;
-		case 7:
-				printf("Enter the location of node to delete: ");
+	switch(n)
+	{
+		case 1:
+				printf("Enter the number you want to insert: ");
 				scanf("%d",&a);
-				delete_node(a);
+				insert_beg(a);
 				break;
-		case 8:
-				printf("Enter the value you want to delete: ");
+		case 2:
+				printf("Enter the number you want to insert: ");
 				scanf("%d",&a);
-				delete_value(a);
+				insert_end(a);
 				break;
-    case 9:
-					printf("Enter the location of node you want to replace: ");
-					scanf("%d",&b);
-					printf("Enter the new number: ");
-					scanf("%d",&a);
-					replace_node(a,b);
-					break;
-			case 10:
-					printf("Enter the value you want to replace: ");
-					scanf("%d",&a);
-					printf("Enter the new number: ");
-					scanf("%d",&b);
-					replace_value(b,a);
-					break;
-			case 11:
-					delete_dup_vals();
-					break;
-			case 12:
-					display();
-					usleep(5000000);
-					break;
-      case 13:
-					reverse();
-					break;
-			case 14:
-					sortlist();
-					break;
-			case 15:
-					printf("Enter the name of file to write to: ");
-					scanf("%s",filename);
-					writefile(filename);
-					break;
-			case 16:
-					listfile();
-					printf("Enter the name of file to read from: ");
-					scanf("%s",filename);
-					readfile(filename);
-					break;
-			case 17:
-					return n;
-					break;
-		}
+		case 3:
+				printf("Enter the number you want to insert: ");
+				scanf("%d",&a);
+				printf("Enter the location in which you want to insert: ");
+				scanf("%d",&b);
+				insert_at_loc(a,b);
+				break;
+		case 4:
+				printf("Enter the number you want to insert: ");
+				scanf("%d",&a);
+				printf("Enter the value after which to insert: ");
+				scanf("%d",&b);
+				insert_after_value(a,b);
+				break;
+    case 5:
+			delete_beg();
+			break;
+	case 6:
+			delete_end();
+			break;
+	case 7:
+			printf("Enter the location of node to delete: ");
+			scanf("%d",&a);
+			delete_node(a);
+			break;
+	case 8:
+			printf("Enter the value you want to delete: ");
+			scanf("%d",&a);
+			delete_value(a);
+			break;
+  case 9:
+				printf("Enter the location of node you want to replace: ");
+				scanf("%d",&b);
+				printf("Enter the new number: ");
+				scanf("%d",&a);
+				replace_node(a,b);
+				break;
+		case 10:
+				printf("Enter the value you want to replace: ");
+				scanf("%d",&a);
+				printf("Enter the new number: ");
+				scanf("%d",&b);
+				replace_value(b,a);
+				break;
+		case 11:
+				delete_dup_vals();
+				break;
+		case 12:
+				display();
+				msleep(5000);
+				break;
+    case 13:
+				reverse();
+				break;
+		case 14:
+				sortlist();
+				break;
+		case 15:
+				printf("Enter the name of file to write to: ");
+				scanf("%s",filename);
+				writefile(filename);
+				break;
+		case 16:
+				listfile();
+				printf("Enter the name of file to read from: ");
+				scanf("%s",filename);
+				readfile(filename);
+				break;
+		case 17:
+				return n;
+	}
 }
 
 void clear(void)
 {
-	printf("\033[2J");
-	printf("\033[%d;%dH",0,0);
+	system(CLEAR);		/* Cross-platform clear screen */
 }
