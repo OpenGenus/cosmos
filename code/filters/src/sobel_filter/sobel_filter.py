bdde553c49f01ceb723fe8c605e328e4791c53df
Matheus Guimarães <matheusguimr@gmail.com>
2019-03-21 14:11:47 -0300

    Optimize library imports in 'sobel_filter.py'

--- code/filters/src/sobel_filter/sobel_filter.py
+++ code/filters/src/sobel_filter/sobel_filter.py
@@ -1,59 +1,56 @@
 """
-Sobel filter algorithm
+Laplacian filter algorithm
 Part of Cosmos by OpenGenus Foundation
-Author: Matheus Guimaraes (@matheusguimr)
+Author: Matheus Guimaraes (@matheustguimaraes)
 """
 
 
-import cv2
-import math as mt
-import numpy as np
-from matplotlib import pyplot as ppl
+from cv2 import imread, cvtColor, COLOR_BGR2GRAY
+from matplotlib.pyplot import figure, subplot, imshow, show
+from numpy import array, zeros, sum, int16
 
 
-def set_kernel(direction):
-    sobel_kernel = 0
-    if direction:
-        sobel_kernel = np.array([[-1, 0, 1],
-                                 [-2, 0, 2],
-                                 [-1, 0, 1]])
-    elif not direction:
-        sobel_kernel = np.array([[-1, -2, -1],
-                                 [0, 0, 0],
-                                 [1, 2, 1]])
-    return sobel_kernel
+def set_kernel(type_temp):
+    laplacian_kernel = 0
+    if type_temp == 3:
+        laplacian_kernel = array([[1, 1, 1], [1, -8, 1], [1, 1, 1]])
+    elif type_temp == 5:
+        laplacian_kernel = array(
+            [
+                [-1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1],
+                [-1, -1, 24, -1, -1],
+                [-1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1],
+            ]
+        )
+    return laplacian_kernel
 
 
-def get_sobel_filter(gray):
-    bd = int(np.ndim(gray) / 2)
-    kernel_x = set_kernel(False)
-    kernel_y = set_kernel(True)
-    sob_x = np.zeros(gray.shape, dtype=np.int16)
-    sob_y = np.zeros(gray.shape, dtype=np.int16)
-    sob_conv = np.zeros(gray.shape, dtype=np.int16)
+def get_laplacian_filter(gray, matrix_type):
+    bd = int(matrix_type / 2)
+    matrix = set_kernel(matrix_type)
+    laplacian_image = zeros(gray.shape, dtype=int16)
     for i in range(bd, gray.shape[0] - bd):
         for j in range(bd, gray.shape[1] - bd):
-            conv_x = gray[i - bd:i + bd + 1, j - bd:j + bd + 1] * kernel_x
-            conv_y = gray[i - bd:i + bd + 1, j - bd:j + bd + 1] * kernel_y
-            sob_x[i][j] = np.sum(conv_x)
-            sob_y[i][j] = np.sum(conv_y)
-            sob_conv[i][j] = mt.sqrt(pow(sob_y[i][j], 2) + pow(sob_x[i][j], 2))
-    return sob_conv, sob_y, sob_x
+            kernel = gray[i - bd: i + bd + 1, j - bd: j + bd + 1] * matrix
+            laplacian_image[i, j] = sum(kernel)
+    return laplacian_image
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Read image
-    img = cv2.imread('lena.jpg')
+    img = imread("lena.jpg")
     # Convert image for gray scale
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray_img = cvtColor(img, COLOR_BGR2GRAY)
 
     # Pass image to function
-    sobel, sobel_vertical, sobel_horizontal = get_sobel_filter(gray_img)
+    filter_3x3 = get_laplacian_filter(gray_img, 3)
+    filter_5x5 = get_laplacian_filter(gray_img, 5)
 
     # Show the images
-    fig = ppl.figure()
-    ppl.subplot(1, 3, 1), ppl.imshow(sobel_horizontal, 'gray')
-    ppl.subplot(1, 3, 2), ppl.imshow(sobel_vertical, 'gray')
-    ppl.subplot(1, 3, 3), ppl.imshow(sobel, 'gray')
-    ppl.show()
-
+    fig = figure()
+    subplot(1, 3, 1), imshow(gray_img, "gray")
+    subplot(1, 3, 2), imshow(filter_3x3, "gray")
+    subplot(1, 3, 3), imshow(filter_3x3, "gray")
+    show()
