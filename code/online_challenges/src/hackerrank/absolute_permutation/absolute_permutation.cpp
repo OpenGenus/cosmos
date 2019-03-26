c49a1ce2d13e014cc2c6be6b5e151fd6ab39669c
warned101 <astitvagupta123@gmail.com>
2019-03-26 15:31:04 +0530

    Absolute Permutation

---
+++ code/online_challenges/src/hackerrank/absolute_permutation/absolute_permutation.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+int main(){
+    int t;
+    std::cin >> t;
+    for(int a0 = 0; a0 < t; ++a0){
+        int n, k;
+        std::cin >> n >> k;
+        
+        int temp = k;
+        
+        if(k == 0){
+            for(int i = 1; i <= n; ++i)
+                std::cout << i << " ";
+        }
+        
+        else if(n % (2 * k) == 0){
+            for(int i = 1; i <= n; i++){
+                std::cout << i + temp << " ";
+                if(i % k == 0)
+                    temp = temp*-1;
+            }
+        }
+        else
+            std::cout << "-1";
+        std::cout << std::endl; 
+    }
+    return 0;
+}
