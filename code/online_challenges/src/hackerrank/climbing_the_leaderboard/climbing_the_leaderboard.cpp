4750d8f9545ddacfd2e16eaf8ebcbfda35daa1d1
warned101 <astitvagupta123@gmail.com>
2019-03-27 13:58:02 +0530

    Added Climbing the Leaderboard

---
+++ code/online_challenges/src/hackerrank/climbing_the_leaderboard/climbing_the_leaderboard.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stack> 
+
+int main() {
+    unsigned long n, m, i, tmp;
+    
+    std::cin >> n;
+    
+    std::stack<unsigned long> scores;
+    
+    for (i = 0; i < n; ++i) {
+        std::cin >> tmp;
+        if (scores.empty() || scores.top() != tmp) 
+            scores.push(tmp);
+    }
+    
+    std::cin >> m;
+
+    for (i = 0; i < m; ++i) {
+        std::cin >> tmp;
+        while (!scores.empty() && tmp >= scores.top()) 
+            scores.pop();
+        std::cout << (scores.size() + 1) << "\n";
+    }
+    return 0;
+}
