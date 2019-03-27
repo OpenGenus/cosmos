5d6687b932f6d8c5306016052bb0ab22de5547e8
Abhishek Paharva <500053865@stu.upes.ac.in>
2019-03-27 18:10:07 +0530

    Update JAIN.cpp

--- code/online_challenges/src/codechef/JAIN/JAIN.cpp
+++ code/online_challenges/src/codechef/JAIN/JAIN.cpp
@@ -1,49 +1,50 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <list>
+#include <stack>
+#include <string>
+#include <map>
+#include <vector>
+#include <set>
+#include <utility>
 
-#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL);  cout.tie(NULL)
-#define rint register int
-#define ull unsigned long long
-
-using namespace std;
-
-map<char,int>mp;
+std::map<char,int>g_mp;
 
 void build ()
 {
-    mp.insert(make_pair('a',16));
-    mp.insert(make_pair('e',8));
-    mp.insert(make_pair('i',4));
-    mp.insert(make_pair('o',2));
-    mp.insert(make_pair('u',1));
+    g_mp.insert(std::make_pair('a',16));
+    g_mp.insert(std::make_pair('e',8));
+    g_mp.insert(std::make_pair('i',4));
+    g_mp.insert(std::make_pair('o',2));
+    g_mp.insert(std::make_pair('u',1));
 }
 
 int main()
 {
-    fastio;
     build();
 
-    rint t;
-    cin >> t;
+    int t;
+    std::cin >> t;
 
     while (t--)
     {
-        rint n;
-        cin >> n;
+        int n;
+        std::cin >> n;
 
-        string a[n];
-        for (rint i = 0; i < n; ++i)
-            cin >> a[i];
+        std::string a[n];
+        for (int i = 0; i < n; ++i)
+            std::cin >> a[i];
 
-        vector<short>value;
-        set<short>s;
-        vector<ull>count(33,0);
+        std::vector<short>value;
+        std::set<short>s;
+        std::vector<long long>count(33,0);
 
-        for (rint i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
         {
-            rint x = 0;
-            for(rint j = 0; j < a[i].size(); ++j)
+            int x = 0;
+            for(int j = 0; j < a[i].size(); ++j)
             {
-                x = x | mp[a[i][j]];
+                x = x | g_mp[a[i][j]];
             }
             if (s.find(x) == s.end())
             {   
@@ -51,20 +52,23 @@ int main()
                 count[x] = 1;
                 s.insert(x);
             }
-            else count[x] = count[x] + 1;
+            else
+            {
+                count[x]++;
+            }
         }
 
-        ull ans = 0;
+        long long ans = 0;
 
-        for (rint i = 0; i < value.size(); ++i)
+        for (int i = 0; i < value.size(); ++i)
         {
-            for (rint j = i + 1; j < value.size(); ++j)
+            for (int j = i + 1; j < value.size(); ++j)
                 if ((value[i] | value[j]) == 31)
                     ans = ans + (count[value[i]] * count[value[j]]);
         }
 
         ans = ans + ((count[31] * (count[31] - 1)) / 2);
-        cout << ans << "\n";
+        std::cout << ans << "\n";
     }
 
     return 0;
