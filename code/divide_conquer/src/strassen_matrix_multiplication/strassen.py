c4736db4a78b4358bb187dad211baac73c661ba8
yedhink <yedhin1998@gmail.com>
2019-03-29 20:23:07 +0530

    fix #4002 : ported python2 strassen to python3 and added test case

--- code/divide_conquer/src/strassen_matrix_multiplication/strassen.py
+++ code/divide_conquer/src/strassen_matrix_multiplication/strassen.py
@@ -1,129 +1,109 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May 29 13:36:32 2018
+def default_matrix_multiplication(a, b):
+    """
+    Only for 2x2 matrices
+    """
+    if len(a) != 2 or len(a[0]) != 2 or len(b) != 2 or len(b[0]) != 2:
+        raise Exception("Matrices should be 2x2!")
+    new_matrix = [
+        [a[0][0] * b[0][0] + a[0][1] * b[1][0], a[0][0] * b[0][1] + a[0][1] * b[1][1]],
+        [a[1][0] * b[0][0] + a[1][1] * b[1][0], a[1][0] * b[0][1] + a[1][1] * b[1][1]],
+    ]
 
-@author: Sahit
-"""
-
-import numpy as np 
-
-
-def Correction(matrix_1, matrix_2):
-    r = max(matrix_1.shape[0], matrix_1.shape[1], matrix_2.shape[0], matrix_2.shape[1])
-    i = 1
-    while i < r:
-        i = i*2
-        
-    return i
-    '''    
-    result = np.zeros((i,i))
-    for i in xrange(matrix.shape[0]):
-        for j in xrange(matrix.shape[1]):
-            result[i][j] = matrix[i][j]
-    return result
-    '''
-def Modification(matrix, p):
-    result = np.zeros((p,p))
-    for i in xrange(matrix.shape[0]):
-        for j in xrange(matrix.shape[1]):
-            result[i][j] = matrix[i][j]
-    return result
-    
-    
-    
-    
-def Extraction(matrix, x, y):
-    result = np.zeros((x,y))
-    for i in xrange(x):
-        for j in xrange(y):
-            result[i][j] = matrix[i][j]
-            
-    return result
-    
-
-def Strassen(matrix_1, matrix_2):
-    r = matrix_1.shape[0]/2
-    if r == 0:
-        return matrix_1.dot(matrix_2)
-    else:
-        
-        a = matrix_1[:r, :r]
-        b = matrix_1[:r, r:]
-        c = matrix_1[r:, :r]
-        d = matrix_1[r:, r:]
-        
-        e = matrix_2[:r, :r]
-        f = matrix_2[:r, r:]
-        g = matrix_2[r:, :r]
-        h = matrix_2[r:, r:]
-        
-        
-        p1 = Strassen(a, f-h)
-        p2 = Strassen(a+b, h)
-        p3 = Strassen(c+d, e)
-        p4 = Strassen(d, g-e)
-        p5 = Strassen(a+d, e+h)
-        p6 = Strassen(b-d, g+h)
-        p7 = Strassen(a-c, e+f)
-    
-        s1 = p5+p4+p6-p2
-        s2 = p1+p2
-        s3 = p3+p4
-        s4 = p1+p5-p3-p7
-        
-        
-        result = np.zeros((r*2, r*2))
-        
-        for i in xrange(r):
-            for j in xrange(r):
-                result[i][j] = s1[i][j]
-                
-        for i in xrange(r):
-            for j in xrange(r):
-                result[i][r+j] = s2[i][j]
-                
-        for i in xrange(r):
-            for j in xrange(r):
-                result[r+i][j] = s3[i][j]
-           
-        for i in xrange(r):
-            for j in xrange(r):
-                result[r+i][r+j] = s4[i][j]
-                
-        return result
-
-print 'enter the dimensions of the first matrix'
-r1 = map(int, raw_input().split())
-x1, y1 = r1[0], r1[1]
-print 'enter the dimensions of the second matrix'
-r2 = map(int, raw_input().split())
-x2, y2 = r2[0], r2[1]
+    return new_matrix
 
 
-m1 = []
-for i in xrange(x1):
-    row = []
-    for j in xrange(y1):
-        print 'enter the ', i+1, j+1, ' element of the first matrix'
-        row.append(input())
-    m1.append(row)
-    
-m2 = []
-for i in xrange(x2):
-    row = []
-    for j in xrange(y2):
-        print 'enter the ', i+1, j+1, ' element of the first matrix'
-        row.append(input())
-    m2.append(row)
-    
-
-matrix1 = np.array(m1)
-matrix2 = np.array(m2)
-
-f = Correction(matrix1, matrix2)
+def matrix_addition(matrix_a, matrix_b):
+    # print(matrix_a)
+    return [
+        [matrix_a[row][col] + matrix_b[row][col] for col in range(len(matrix_a[row]))]
+        for row in range(len(matrix_a))
+    ]
 
 
+def matrix_subtraction(matrix_a, matrix_b):
+    return [
+        [matrix_a[row][col] - matrix_b[row][col] for col in range(len(matrix_a[row]))]
+        for row in range(len(matrix_a))
+    ]
 
-answer = Extraction(Strassen(Modification(matrix1,f), Modification(matrix2,f)), x1, y2)
-print answer
+
+def split_matrix(a):
+    """
+    Given a matrix, return the TOP_LEFT, TOP_RIGHT,
+    BOT_LEFT and BOT_RIGHT quadrant
+    """
+    if len(a) % 2 != 0 or len(a[0]) % 2 != 0:
+        raise Exception("Odd matrices are not supported!")
+
+    matrix_length = len(a)
+    mid = matrix_length // 2
+    top_left = [[a[i][j] for j in range(mid)] for i in range(mid)]
+    bot_left = [[a[i][j] for j in range(mid)] for i in range(mid, matrix_length)]
+
+    top_right = [[a[i][j] for j in range(mid, matrix_length)] for i in range(mid)]
+    bot_right = [
+        [a[i][j] for j in range(mid, matrix_length)] for i in range(mid, matrix_length)
+    ]
+
+    return top_left, top_right, bot_left, bot_right
+
+
+def get_matrix_dimensions(matrix):
+    return len(matrix), len(matrix[0])
+
+
+def strassen(matrix_a, matrix_b):
+    """
+    Recursive function to calculate the product of
+    two matrices, using the Strassen Algorithm.
+    Currently only works for matrices of even length (2x2, 4x4, 6x6...etc)
+    """
+    if get_matrix_dimensions(matrix_a) != get_matrix_dimensions(matrix_b):
+        raise Exception(
+            "Both matrices are not the same dimension!"
+            + "\nMatrix A:{} \nMatrix B:{}".format(matrix_a, matrix_b)
+        )
+    if get_matrix_dimensions(matrix_a) == (2, 2):
+        return default_matrix_multiplication(matrix_a, matrix_b)
+
+    A, B, C, D = split_matrix(matrix_a)
+    E, F, G, H = split_matrix(matrix_b)
+
+    p1 = strassen(A, matrix_subtraction(F, H))
+    p2 = strassen(matrix_addition(A, B), H)
+    p3 = strassen(matrix_addition(C, D), E)
+    p4 = strassen(D, matrix_subtraction(G, E))
+    p5 = strassen(matrix_addition(A, D), matrix_addition(E, H))
+    p6 = strassen(matrix_subtraction(B, D), matrix_addition(G, H))
+    p7 = strassen(matrix_subtraction(A, C), matrix_addition(E, F))
+
+    top_left = matrix_addition(matrix_subtraction(matrix_addition(p5, p4), p2), p6)
+    top_right = matrix_addition(p1, p2)
+    bot_left = matrix_addition(p3, p4)
+    bot_right = matrix_subtraction(matrix_subtraction(matrix_addition(p1, p5), p3), p7)
+
+    # construct the new matrix from our 4 quadrants
+    new_matrix = []
+    for i in range(len(top_right)):
+        new_matrix.append(top_left[i] + top_right[i])
+    for i in range(len(bot_right)):
+        new_matrix.append(bot_left[i] + bot_right[i])
+    return new_matrix
+
+
+def main():
+    rows_cols = int(input("Enter square matrix rows = "))
+    print("\nInput First Matrix")
+    first_matrix = [[int(x) for x in input().split()] for i in range(rows_cols)]
+    print("\nInput Second Matrix")
+    second_matrix = [[int(x) for x in input().split()] for i in range(rows_cols)]
+
+    print("\nMultiplied Matrix")
+    for row in strassen(first_matrix, second_matrix):
+        for col in row:
+            print(col, end="\t")
+        print()
+
+
+if __name__ == "__main__":
+    main()
