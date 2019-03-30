e30694c0b97ce18ced230a95ac58180cff107524
msknkh <muskankhandelwal369@gmail.com>
2019-03-31 01:30:39 +0530

    Extension Change

--- code/operating_system/src/processCreation/Processes.c
+++ code/operating_system/src/processCreation/Processes.c
@@ -1,7 +1,7 @@
 #include<unistd.h>
 #include <stdio.h>
-#include <sys/types.h> 
-#include <sys/wait.h> 
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <stdlib.h>
 #include <stdbool.h>
 int main(void)
