395a1cfc38a3bd90c74bfd9c8ca69ec138c6f732
Astitva Gupta <astitvagupta123@gmail.com>
2019-04-03 19:33:00 +0530

    Matched Coding Style

--- code/online_challenges/src/hackerrank/absolute_permutation/absolute_permutation.cpp
+++ code/online_challenges/src/hackerrank/absolute_permutation/absolute_permutation.cpp
@@ -3,21 +3,21 @@
 int main(){
     int t;
     std::cin >> t;
-    for(int a0 = 0; a0 < t; ++a0){
+    for(int i = 0; i < t; ++i){
         int n, k;
         std::cin >> n >> k;
         
         int temp = k;
         
         if(k == 0){
-            for(int i = 1; i <= n; ++i)
-                std::cout << i << " ";
+            for(int j = 1; j <= n; ++j)
+                std::cout << j << " ";
         }
         
         else if(n % (2 * k) == 0){
-            for(int i = 1; i <= n; i++){
-                std::cout << i + temp << " ";
-                if(i % k == 0)
+            for(int j = 1; j <= n; ++j){
+                std::cout << j + temp << " ";
+                if(j % k == 0)
                     temp = temp*-1;
             }
         }
