d80325e49515feb8aebd6712348c74175f5f3409
Astitva Gupta <astitvagupta123@gmail.com>
2019-04-05 07:51:56 +0530

    Bug fixes

--- code/online_challenges/src/hackerrank/absolute_permutation/absolute_permutation.cpp
+++ code/online_challenges/src/hackerrank/absolute_permutation/absolute_permutation.cpp
@@ -3,27 +3,31 @@
 int main(){
     int t;
     std::cin >> t;
-    for(int i = 0; i < t; ++i){
+    for (int i = 0; i < t; ++i)
+    {
         int n, k;
         std::cin >> n >> k;
-        
+
         int temp = k;
-        
-        if(k == 0){
-            for(int j = 1; j <= n; ++j)
+
+        if(k == 0)
+        {
+            for (int j = 1; j <= n; ++j)
                 std::cout << j << " ";
         }
-        
-        else if(n % (2 * k) == 0){
-            for(int j = 1; j <= n; ++j){
+
+        else if (n % (2 * k) == 0)
+        {
+            for (int j = 1; j <= n; ++j)
+            {
                 std::cout << j + temp << " ";
-                if(j % k == 0)
+                if (j % k == 0)
                     temp = temp*-1;
             }
         }
         else
             std::cout << "-1";
-        std::cout << std::endl; 
+        std::cout << "\n";
     }
     return 0;
 }
