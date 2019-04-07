32f70fc9a4417a3790728243af758f2c9be00091
vaibhav Pathak <46650456+quantum08@users.noreply.github.com>
2019-04-07 14:13:30 +0530

    Update bipartite.cpp
    Spacing after conditional statement and naming variable

--- code/graph_algorithms/src/Bipartite_check/bipartite.cpp
+++ code/graph_algorithms/src/Bipartite_check/bipartite.cpp
@@ -3,17 +3,17 @@
 #include <queue>
 int main()
 {
-    long int vertex , edge , u , v , c ;
-    bool flag=false;
+    long int vertex , edge , u , v , counter ;
+    bool flag = false;
     // taking n ->number of vertex  and m -> number of edges
     std::cin >> vertex >> edge;
     // adj is 2-D vector to store edges between vertex
     std::vector < std::vector < long int > > adj(vertex , std::vector <long int> ());
     // color is use to store color of vertex
-    std::vector <long int> color(vertex , -1);
-    std::queue <long int> q;
-    //for number of edges times
-    for(int i=0;i < edge; ++i)
+    std::vector < long int > color(vertex , -1);
+    std::queue < long int > q;
+    // for number of edges times
+    for (int i=0;i < edge; ++i)
     {
         std::cin >> u >> v;
         adj[u-1].push_back(v-1);
@@ -22,25 +22,25 @@ int main()
     u=0;
     color[u]=0;
     q.push(u);
-    while(!q.empty())
+    while (!q.empty())
     {
-        c=q.front();
+        counter=q.front();
         q.pop();
-        for(auto i=adj[c].begin();i!=adj[c].end(); ++i)
+        for (auto i=adj[counter].begin();i!=adj[counter].end(); ++i)
         {
             // updating color of vertex
-            if(color[*i]==-1 )
+            if (color[*i]==-1)
             {
                 q.push(*i);
-                color[*i]=1-color[c];
+                color[*i]=1-color[counter];
             }
-            else if(color[*i]==color[c])  // checking for bipartite graph
+            else if (color[*i]==color[counter])
             {
                 flag=true;
             }
         }
     }
-    if( !flag )
+    if ( !flag )
         std::cout << "Graph is bipartite" << "\n" ;
     else
         std::cout << "Graph is not bipartite" << "\n" ;
