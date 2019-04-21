835e9ff5fe4745d985bb3f489bb7549cd0cc8ce7
yedhink <yedhin1998@gmail.com>
2019-04-21 21:47:21 +0530

    format using black

--- scripts/stats.py
+++ scripts/stats.py
@@ -6,18 +6,18 @@ import pathlib
 import argparse
 
 
-Path = collections.namedtuple('Entry', ('suffix', 'group', 'name'))
+Path = collections.namedtuple("Entry", ("suffix", "group", "name"))
 paths = [
     Path(
-        suffix=path.suffix.lstrip('.').lower(),
-        group=path.parts[-3].replace('-', ' ').replace('_', ' '),
-        name=path.parts[-2].replace('-', ' ').replace('_', ' '),
+        suffix=path.suffix.lstrip(".").lower(),
+        group=path.parts[-3].replace("-", " ").replace("_", " "),
+        name=path.parts[-2].replace("-", " ").replace("_", " "),
     )
-    for path in pathlib.Path(__file__).parents[1].glob('code/*/*/*')
-    if path.suffix and path.suffix.lower() != '.md'
+    for path in pathlib.Path(__file__).parents[1].glob("code/*/*/*")
+    if path.suffix and path.suffix.lower() != ".md"
 ]
 suffixes = {path.suffix for path in paths}
-suffixes = sorted(suffixes - {'', 'md'})
+suffixes = sorted(suffixes - {"", "md"})
 
 name_max_len = max(max(len(path.group), len(path.name)) for path in paths)
 suffix_max_len = max(len(path.suffix) for path in paths)
@@ -28,17 +28,19 @@ last_group = None
 sort_key = lambda path: (path.group, path.name)  # noqa
 paths = sorted(paths, key=sort_key)
 
+
 def generate_txt():
-    global paths,suffixes,name_max_len,suffix_max_len,totals,group_stats,last_group,sort_key
+    global paths, suffixes, name_max_len, suffix_max_len, totals, group_stats, last_group, sort_key
+
     def print_group_stats():
         if group_stats:
-            print(' ' * name_max_len, end='  ')
+            print(" " * name_max_len, end="  ")
             for suffix in suffixes:
-                print('-' * (suffix_max_len + 1), end=' ')
+                print("-" * (suffix_max_len + 1), end=" ")
             print()
-            print(' ' * name_max_len, end='  ')
+            print(" " * name_max_len, end="  ")
             for suffix in suffixes:
-                print(str(group_stats[suffix]).rjust(suffix_max_len), end='  ')
+                print(str(group_stats[suffix]).rjust(suffix_max_len), end="  ")
             print()
             print()
 
@@ -46,49 +48,53 @@ def generate_txt():
         if group != last_group:
             print_group_stats()
             print()
-            print(group.upper().ljust(name_max_len), end='  ')
+            print(group.upper().ljust(name_max_len), end="  ")
             for suffix in suffixes:
-                print(suffix.rjust(suffix_max_len), end='  ')
+                print(suffix.rjust(suffix_max_len), end="  ")
             print()
-            print('-' * name_max_len, end='- ')
+            print("-" * name_max_len, end="- ")
             for suffix in suffixes:
-                print('-' * (suffix_max_len + 1), end=' ')
+                print("-" * (suffix_max_len + 1), end=" ")
             print()
             last_group = group
             group_stats = collections.defaultdict(int)
 
-        print(name.ljust(name_max_len), end='  ')
+        print(name.ljust(name_max_len), end="  ")
         alg_suffixes = {p.suffix for p in g}
         for suffix in suffixes:
             if suffix in alg_suffixes:
                 totals[suffix] += 1
                 group_stats[suffix] += 1
-                value = 'x'
+                value = "x"
             else:
-                value = '.'
-            print(value.rjust(suffix_max_len), end='  ')
+                value = "."
+            print(value.rjust(suffix_max_len), end="  ")
         print()
 
     print_group_stats()
 
     print()
-    print('=' * name_max_len, end='= ')
+    print("=" * name_max_len, end="= ")
     for suffix in suffixes:
-        print('=' * (suffix_max_len + 1), end=' ')
+        print("=" * (suffix_max_len + 1), end=" ")
     print()
-    print('TOTALS:'.rjust(name_max_len), end='  ')
+    print("TOTALS:".rjust(name_max_len), end="  ")
     for suffix in suffixes:
-        print(str(totals[suffix]).rjust(suffix_max_len), end='  ')
+        print(str(totals[suffix]).rjust(suffix_max_len), end="  ")
     print()
 
 
 def generate_markdown():
-    global paths,suffixes,name_max_len,suffix_max_len,totals,group_stats,last_group,sort_key
+    global paths, suffixes, name_max_len, suffix_max_len, totals, group_stats, last_group, sort_key
+
     def print_group_stats():
         if group_stats:
-            print("| |",end='')
+            print("| |", end="")
             for suffix in suffixes:
-                print(" {} |".format(str(group_stats[suffix]).rjust(suffix_max_len)), end='')
+                print(
+                    " {} |".format(str(group_stats[suffix]).rjust(suffix_max_len)),
+                    end="",
+                )
             print()
             print()
 
@@ -97,53 +103,59 @@ def generate_markdown():
             print_group_stats()
             print()
             print("# {}".format(group.upper().ljust(name_max_len)))
-            print("| {} | ".format(group.upper().ljust(name_max_len)), end='  ')
+            print("| {} | ".format(group.upper().ljust(name_max_len)), end="  ")
             for suffix in suffixes:
-                print("{} | ".format(suffix.rjust(suffix_max_len)), end='  ')
+                print("{} | ".format(suffix.rjust(suffix_max_len)), end="  ")
             print()
-            print('| ',end="")
-            print('-' * name_max_len, end='- | ')
+            print("| ", end="")
+            print("-" * name_max_len, end="- | ")
             for suffix in suffixes:
-                print('-' * (suffix_max_len + 1), end=' | ')
+                print("-" * (suffix_max_len + 1), end=" | ")
             print()
             last_group = group
             group_stats = collections.defaultdict(int)
 
-        print("| {} |".format(name), end='  ')
+        print("| {} |".format(name), end="  ")
         alg_suffixes = {p.suffix for p in g}
         for suffix in suffixes:
             if suffix in alg_suffixes:
                 totals[suffix] += 1
                 group_stats[suffix] += 1
-                value = 'x'
+                value = "x"
             else:
-                value = '.'
-            print(" {} | ".format(value), end='  ')
+                value = "."
+            print(" {} | ".format(value), end="  ")
         print()
 
     print()
     print()
     tot_str = "TOTALS"
-    print('| {} |'.format(tot_str), end='  ')
+    print("| {} |".format(tot_str), end="  ")
     for suffix in suffixes:
-        print(" {} |".format(str(totals[suffix]).rjust(suffix_max_len)), end='  ')
+        print(" {} |".format(str(totals[suffix]).rjust(suffix_max_len)), end="  ")
     print()
-    print('| ',end="")
-    print('-' * len(tot_str), end=' | ')
+    print("| ", end="")
+    print("-" * len(tot_str), end=" | ")
     for suffix in suffixes:
-        print('-' * (suffix_max_len + 1), end=' | ')
+        print("-" * (suffix_max_len + 1), end=" | ")
     print()
-
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Get statstics in txt or markdown. Make sure you are in root of cosmos repo before running')
-    parser.add_argument('-f','--format',help='Ouput can be \"txt\" for text file and \"md\" for markdown or \"all\" for all formats',default='all')
+    parser = argparse.ArgumentParser(
+        description="Get statstics in txt or markdown. Make sure you are in root of cosmos repo before running"
+    )
+    parser.add_argument(
+        "-f",
+        "--format",
+        help='Ouput can be "txt" for text file and "md" for markdown or "all" for all formats',
+        default="all",
+    )
     results = parser.parse_args()
     choice = results.format
-    if choice == 'txt':
+    if choice == "txt":
         generate_txt()
-    elif choice == 'md':
+    elif choice == "md":
         generate_markdown()
     else:
         generate_txt()
