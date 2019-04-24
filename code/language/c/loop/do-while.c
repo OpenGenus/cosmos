d953de3f35fb62354903a7578708711c3ad54146
shreyarastogi <41367451+shreyarastogi@users.noreply.github.com>
2019-04-24 12:39:38 +0530

    Update do-while.c

--- code/language/c/loop/do-while.c
+++ code/language/c/loop/do-while.c
@@ -1,1 +1,18 @@
+// do-while loop
+//C program to print sum of first 5 natural numbers using do..while loop
 
+#include <stdio.h>
+int main ()
+{
+ int sum = 0, i = 1;  //initialization of counter variable i
+ do{
+   sum = sum+i;
+   i++;  //increment of counter variable
+  }while(i <= 5);  //coondition of do while
+
+ printf("sum of first 5 natural numbers = %d",sum);
+ return 0;
+}  //end of program
+
+output:
+sum of first 5 natural numbers = 15
