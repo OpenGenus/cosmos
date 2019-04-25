bf52e878bb793bedf99779783a66d181740e2d11
warned101 <astitvagupta123@gmail.com>
2019-04-26 01:03:36 +0530

    Fixed coding style

--- code/sorting/src/shaker_sort/shaker_sort.cpp
+++ code/sorting/src/shaker_sort/shaker_sort.cpp
@@ -7,9 +7,7 @@
 #include <vector>
 #include <algorithm>
 
-using namespace std;
-
-void shaker_sort(vector<int> &v)
+void shaker_sort(std::vector<int> &v)
 {
     bool swap = false;
     for (unsigned i = 0; i < v.size() / 2; ++i)
@@ -22,7 +20,7 @@ void shaker_sort(vector<int> &v)
                 v[j + 1] = tmp;
                 swap = true;
             }
-        for (unsigned j = v.size() - 2 - i; j > i; j--)
+        for (unsigned j = v.size() - 2 - i; j > i; --j)
             if (v[j] < v[j - 1])
             {
                 int tmp = v[j];
@@ -39,24 +37,26 @@ void shaker_sort(vector<int> &v)
 int main()
 {
     int size = 0;
-    cout << "Enter size of array: ";
-    cin >> size;
-    vector<int> source_array;
+    std::cout << "Enter size of array: ";
+    std::cin >> size;
+    std::vector<int> source_array;
 
     for (int i = 0; i < size; ++i)
         source_array.push_back(rand() % 100 + 1);
 
-    cout << "Source array is: ";
+    std::cout << "Source array is: ";
+
     for (int j : source_array)
-        cout << j << " ";
-    cout << endl;
+        std::cout << j << " ";
+    std::cout << "\n";
 
     shaker_sort(source_array);
 
-    cout << "Sorted array is: ";
+    std::cout << "Sorted array is: ";
+
     for (int j : source_array)
-        cout << j << " ";
-    cout << endl;
+        std::cout << j << " ";
+    std::cout << "\n";
 
     return 0;
 }
