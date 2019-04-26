35c096b7083ca8534ca3e631b34ffbdd5c65d375
yedhink <yedhin1998@gmail.com>
2019-04-26 06:38:28 +0530

    add default value for lang not in extension map and add missing lang names

--- scripts/stats.py
+++ scripts/stats.py
@@ -27,70 +27,74 @@ avoid_dirs = ["project", "test", "img", "image", "images"]
 paths = []
 original_paths = []
 for path in pathlib.Path(__file__).parents[1].glob("code/**/**/*"):
-    if (path.suffix
-            and not any(elem in list(path.parts) for elem in avoid_dirs)
-            and path.suffix.lower() not in avoid_extensions):
+    if (
+        path.suffix
+        and not any(elem in list(path.parts) for elem in avoid_dirs)
+        and path.suffix.lower() not in avoid_extensions
+    ):
         original_paths.append(path.parts)
         paths.append(
             Path(
                 suffix=path.suffix.lstrip(".").lower(),
                 group=path.parts[1].replace("-", " ").replace("_", " "),
                 name=path.parts[-2].replace("-", " ").replace("_", " "),
-            ))
+            )
+        )
 
 suffixes = {path.suffix for path in paths}
 suffixes = sorted(suffixes - set(avoid_extensions))
 extension_map = {
-    'adb': 'Ada',
-    'bf': 'BF',
-    'c': 'C',
-    'clj': 'Clojure',
-    'cpp': 'C++',
-    'cr': 'CRiSP',
-    'cs': 'C#',
-    'elm': 'ELM',
-    'erl': 'Erlang',
-    'ex': 'Euphoria',
-    'exs': 'EXS',
-    'f': 'Fortran',
-    'fs': 'Visual F#',
-    'go': 'Golang',
-    'h': 'C',
-    'hpp': 'C++',
-    'hs': 'Haskell',
-    'htm': 'HTML',
-    'html': 'HTML',
-    'ipynb': 'Jupyter Notebook',
-    'java': 'Java',
-    'jl': 'Julia',
-    'js': 'JavaScript',
-    'kt': 'Kotlin',
-    'lua': 'Lua',
-    'm': 'Objective-C',
-    'ml': 'ML',
-    'nim': 'Nim',
-    'pde': 'Processing Development Environment',
-    'php': 'PHP',
-    'pl': 'Perl',
-    'purs': 'PureScript',
-    'py': 'Python',
-    'rb': 'Ruby',
-    're': 'Reason',
-    'rkt': 'Racket',
-    'rs': 'Rust',
-    'ruby': 'Ruby',
-    'sc': 'SuperCollider',
-    'scala': 'Scala',
-    'sh': 'Shell Script',
-    'sml': 'Simple Macro Language',
-    'swift': 'Swift',
-    'ts': 'TypeScript',
-    'vb': 'Visual Basic',
+    "adb": "Ada",
+    "bf": "BrainFuck",
+    "c": "C",
+    "clj": "Clojure",
+    "cpp": "C++",
+    "cr": "CRiSP",
+    "cs": "C#",
+    "elm": "ELM",
+    "erl": "Erlang",
+    "ex": "Euphoria",
+    "exs": "Elixir",
+    "f": "Fortran",
+    "fs": "Visual F#",
+    "go": "Golang",
+    "h": "C",
+    "hpp": "C++",
+    "hs": "Haskell",
+    "htm": "HTML",
+    "html": "HTML",
+    "ipynb": "Jupyter Notebook",
+    "java": "Java",
+    "jl": "Julia",
+    "js": "JavaScript",
+    "kt": "Kotlin",
+    "lua": "Lua",
+    "m": "Objective-C",
+    "ml": "ML",
+    "nim": "Nim",
+    "pde": "Processing Development Environment",
+    "php": "PHP",
+    "pl": "Perl",
+    "purs": "PureScript",
+    "py": "Python",
+    "rb": "Ruby",
+    "re": "Reason",
+    "rkt": "Racket",
+    "rs": "Rust",
+    "ruby": "Ruby",
+    "sc": "SuperCollider",
+    "scala": "Scala",
+    "sh": "Shell Script",
+    "sml": "Simple Macro Language",
+    "swift": "Swift",
+    "ts": "TypeScript",
+    "vb": "Visual Basic",
 }
 suffix_with_lang_name = set()
 for ext in suffixes:
-    suffix_with_lang_name.add(extension_map[ext])
-suffixes = list(suffix_with_lang_name)
+    lang = extension_map.get(ext, ext.upper())
+    suffix_with_lang_name.add(lang)
+suffixes = list(sorted(suffix_with_lang_name))
 
 name_max_len = max(max(len(path.group), len(path.name)) for path in paths)
 suffix_max_len = max(len(suffix) for suffix in suffixes)
@@ -105,8 +109,11 @@ paths = sorted(paths, key=sort_key)
 def generate_txt():
     global paths, suffixes, name_max_len, suffix_max_len, totals, group_stats, last_group, sort_key
 
-    print("This is the progress list of Cosmos | Updated on: {}".format(
-        datetime.today().strftime('%Y-%m-%d')))
+    print(
+        "This is the progress list of Cosmos | Updated on: {}".format(
+            datetime.today().strftime("%Y-%m-%d")
+        )
+    )
     print()
 
     def print_group_stats():
@@ -165,8 +172,10 @@ def generate_markdown():
     global paths, suffixes, name_max_len, suffix_max_len, totals, group_stats, last_group, sort_key
 
     print(
-        "This is the progress list of [Cosmos](https://github.com/OpenGenus/cosmos/) | Updated on: {}"
-        .format(datetime.today().strftime('%Y-%m-%d')))
+        "This is the progress list of [Cosmos](https://github.com/OpenGenus/cosmos/) | Updated on: {}".format(
+            datetime.today().strftime("%Y-%m-%d")
+        )
+    )
     print()
 
     def print_group_stats():
@@ -174,8 +183,7 @@ def generate_markdown():
             print("| |", end="")
             for suffix in suffixes:
                 print(
-                    " {} |".format(
-                        str(group_stats[suffix]).rjust(suffix_max_len)),
+                    " {} |".format(str(group_stats[suffix]).rjust(suffix_max_len)),
                     end="",
                 )
             print()
@@ -187,10 +195,11 @@ def generate_markdown():
             print()
             category_dir = group.replace(" ", "_")
             print(
-                "# [{}](https://github.com/OpenGenus/cosmos/tree/master/code/{})"
-                .format(group.upper(), category_dir))
-            print(
-                "| {} | ".format(group.upper().ljust(name_max_len)), end="  ")
+                "# [{}](https://github.com/OpenGenus/cosmos/tree/master/code/{})".format(
+                    group.upper(), category_dir
+                )
+            )
+            print("| {} | ".format(group.upper().ljust(name_max_len)), end="  ")
             for suffix in suffixes:
                 print("{} | ".format(suffix.rjust(suffix_max_len)), end="  ")
             print()
@@ -207,9 +216,8 @@ def generate_markdown():
         for x in original_paths:
             if x[-2] == original_name:
                 name_path = "https://github.com/OpenGenus/cosmos/tree/master/{}".format(
-                    "".join([
-                        y + "/" for pos, y in enumerate(x) if pos != len(x) - 1
-                    ]))
+                    "".join([y + "/" for pos, y in enumerate(x) if pos != len(x) - 1])
+                )
                 break
 
         print("| [{}]({}) |".format(name, name_path), end="  ")
@@ -229,9 +237,7 @@ def generate_markdown():
     tot_str = "TOTALS"
     print("| {} |".format(tot_str), end="  ")
     for suffix in suffixes:
-        print(
-            " {} |".format(str(totals[suffix]).rjust(suffix_max_len)),
-            end="  ")
+        print(" {} |".format(str(totals[suffix]).rjust(suffix_max_len)), end="  ")
     print()
     print("| ", end="")
     print("-" * len(tot_str), end=" | ")
@@ -242,14 +248,12 @@ def generate_markdown():
 
 def main():
     parser = argparse.ArgumentParser(
-        description=
-        "Get statstics in txt or markdown. Make sure you are in root of cosmos repo before running"
+        description="Get statstics in txt or markdown. Make sure you are in root of cosmos repo before running"
     )
     parser.add_argument(
         "-f",
         "--format",
-        help=
-        'Ouput can be "txt" for text file and "md" for markdown or "all" for all formats',
+        help='Ouput can be "txt" for text file and "md" for markdown or "all" for all formats',
         default="all",
     )
     results = parser.parse_args()
