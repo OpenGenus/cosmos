c0085ea6aa3f8ef002137b820290e74594a03554
Shreya Gupta <shreyagupta3011@gmail.com>
2019-04-28 10:25:09 +0530

    Update example.cpp

--- code/languages/C++/flow-control/example.cpp
+++ code/languages/C++/flow-control/example.cpp
@@ -1,18 +1,15 @@
-#include <iostream>
+#include <iostream>   
 
-using namespace std;    
-
-int 
-main()
+int main()
 {
     int students;
     int grade;
-    cin >> students;
+    std::cin >> students;
     for (int i = 1; i <= students; i++) 
     {
         int state = 0;
-        cin >> grade;
-        if ( grade > 85 )
+        std::cin >> grade;
+        if (grade > 85)
                 state = 1;
         else if (grade < 85 && grade > 55)
                 state = 2;
@@ -21,13 +18,13 @@ main()
         
         switch (state) {
         case 1:
-            cout << "Great job!!\n";
+            std::cout << "Great job!!\n";
             break;
         case 2:
-            cout << "Good job\n";
+            std::cout << "Good job\n";
             break;
         case 3:
-            cout << "Average\n";
+            std::cout << "Average\n";
         }   
     
      }
