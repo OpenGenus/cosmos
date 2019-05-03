c13657529b557f8b5da1d8338b75b0e5b9e8d81e
Utkarsh Sharma <flamingice2801@gmail.com>
2019-05-04 01:31:02 +0530

    Create remove_leaves.cpp
    This removes all the Leaf Nodes from the tree.

---
+++ code/data_structures/src/tree/binary_tree/binary_tree/remove_leaves/remove_leaves.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *construct(vector<int> &datalist)
+{
+    Node *root = NULL;
+    vector<Node *> newlist;
+
+    for (int i = 0; i < datalist.size(); i++)
+    {
+        if (datalist[i] == -1)
+        {
+            newlist.pop_back();
+        }
+
+        else
+        {
+            Node *newnode = new Node(datalist[i]);
+            if (newlist.size() == 0)
+            {
+                root = newnode;
+            }
+
+            else
+            {
+                Node *top_of_stack = newlist.back();
+                if (top_of_stack->left == NULL)
+                {
+                    top_of_stack->left = newnode;
+                }
+
+                else
+                {
+                    top_of_stack->right = newnode;
+                }
+            }
+            newlist.push_back(newnode);
+        }
+    }
+    return (root);
+}
+
+Node *removeLeavesTheBestWay(Node *root)
+{
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    if (root->left == NULL && root->right == NULL)
+    {
+        delete root;
+        return NULL;
+    }
+    root->left = removeLeavesTheBestWay(root->left);
+    root->right = removeLeavesTheBestWay(root->right);
+    return root;
+}
+
+display(Node *root)
+{
+    if (root->left != NULL && root->right != NULL)
+    {
+        cout << root->left->data << "<-" << root->data << "->" << root->right->data << endl;
+        display(root->left);
+        display(root->right);
+    }
+    else if (root->left != NULL)
+    {
+        cout << root->left->data << "<-" << root->data << "." << endl;
+        display(root->left);
+    }
+    else if (root->right != NULL)
+    {
+        cout << "." << root->data << "->" << root->right->data << endl;
+        display(root->right);
+    }
+    else
+    {
+        cout << "." << root->data << "." << endl;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    vector<int> list{50, 25, 12, -1, 37, 30, -1, 40, -1, -1, -1, 75, 62, 60, -1, 70, -1, -1, 87, -1, -1, -1};
+    Node *root = construct(list);
+    display(root);
+    cout << "Remove Leaves" << endl;
+    removeLeavesTheBestWay(root);
+    display(root);
+}
