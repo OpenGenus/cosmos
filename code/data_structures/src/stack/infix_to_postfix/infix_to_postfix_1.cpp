738775a03643dcb93301cd387cac83461dbb87aa
Piyush Chaudhari <piyushrc26@gmail.com>
2019-05-07 23:23:33 +0530

    Update infix_to_postfix_1.cpp

--- code/data_structures/src/stack/infix_to_postfix/infix_to_postfix_1.cpp
+++ code/data_structures/src/stack/infix_to_postfix/infix_to_postfix_1.cpp
@@ -3,10 +3,8 @@
 
 class InfixToPostfix
 {
-
 public:
-
-    InfixToPostfix(const std::string &expression) : expression_(expression) { }
+    InfixToPostfix(const std::string &expression) : expression_(expression) {}
 
     int getPrecedenceOfOperators(char);
 
@@ -19,11 +17,11 @@ private:
 
 int InfixToPostfix::getPrecedenceOfOperators(char ch)
 {
-    if(ch == '+' || ch == '-')
+    if (ch == '+' || ch == '-')
         return 1;
-    if(ch == '*' || ch == '/')
+    if (ch == '*' || ch == '/')
         return 2;
-    if(ch == '^')
+    if (ch == '^')
         return 3;
     else
         return 0;
@@ -34,34 +32,34 @@ std::string InfixToPostfix::convertInfixToPostfix()
     std::stack <char> stack1;
     std::string infixToPostfixExp = "";
     int i = 0;
-    while( expression_[i] != '\0')
+    while (expression_[i] != '\0')
     {
         //if scanned character is open bracket push it on stack
-        if(expression_[i] == '(' || expression_[i] == '[' || expression_[i] == '{')
+        if (expression_[i] == '(' || expression_[i] == '[' || expression_[i] == '{')
             stack1.push(expression_[i]);
 
             //if scanned character is opened bracket pop all literals from stack till matching open bracket gets poped
-        else if(expression_[i] == ')' || expression_[i] == ']' || expression_[i] == '}')
+        else if (expression_[i] == ')' || expression_[i] == ']' || expression_[i] == '}')
         {
-            if(expression_[i] == ')')
+            if (expression_[i] == ')')
             {
-                while( stack1.top() != '(')
+                while (stack1.top() != '(')
                 {
                     infixToPostfixExp = infixToPostfixExp + stack1.top();
                     stack1.pop();
                 }
             }
-            if(expression_[i] == ']')
+            if (expression_[i] == ']')
             {
-                while( stack1.top() != '[')
+                while (stack1.top() != '[')
                 {
                     infixToPostfixExp = infixToPostfixExp + stack1.top();
                     stack1.pop();
                 }
             }
-            if(expression_[i] == '}')
+            if (expression_[i] == '}')
             {
-                while( stack1.top() != '{')
+                while (stack1.top() != '{')
                 {
                     infixToPostfixExp = infixToPostfixExp + stack1.top();
                     stack1.pop();
@@ -70,10 +68,10 @@ std::string InfixToPostfix::convertInfixToPostfix()
             stack1.pop();
         }
             //if scanned character is operator
-        else if(expression_[i] == '+' || expression_[i] == '-' || expression_[i] == '*' || expression_[i] == '/' || expression_[i] == '^')
+        else if (expression_[i] == '+' || expression_[i] == '-' || expression_[i] == '*' || expression_[i] == '/' || expression_[i] == '^')
         {
             //very first operator of expression is to be pushed on stack
-            if(stack1.empty()) {
+            if (stack1.empty()) {
                 stack1.push(expression_[i]);
             }
             else{
@@ -83,7 +81,7 @@ std::string InfixToPostfix::convertInfixToPostfix()
                  * final postifix expression, on other side if precedence order of instack operator is less than i
                  * coming operator, push incoming operator on stack.
                  */
-                if(getPrecedenceOfOperators(stack1.top()) >= getPrecedenceOfOperators(expression_[i]))
+                if (getPrecedenceOfOperators(stack1.top()) >= getPrecedenceOfOperators(expression_[i]))
                 {
                     infixToPostfixExp = infixToPostfixExp + stack1.top();
                     stack1.pop();
@@ -104,9 +102,9 @@ std::string InfixToPostfix::convertInfixToPostfix()
     }
 
     //poping out all remainig operator literals & adding to final postfix expression
-    if(!stack1.empty())
+    if (!stack1.empty())
     {
-        while( !stack1.empty())
+        while (!stack1.empty())
         {
             infixToPostfixExp = infixToPostfixExp + stack1.top();
             stack1.pop();
