8b0d2d246f82436a9020db86aa1676001ddd4dec
Robot Jelly <36916536+robotjellyzone@users.noreply.github.com>
2019-05-09 20:33:44 +0530

    infixConversion files in java

--- code/data_structures/src/stack/infix_to_prefix/infixConversion.java
+++ code/data_structures/src/stack/infix_to_prefix/infixConversion.java
@@ -1,13 +1,13 @@
-    // To include java library 
-    import java.util.*; 
+// To include java library 
+import java.util.*; 
 
-    class infixPrefix
-    { 
+class infixPrefix
+{ 
     /** 
       * This method checks if given character is 
       * an operator or not. 
       * @param c character input
-    */  	
+      */  	
     static boolean isOperator(char c) 
     { 
     	return (!(c >= 'a' && c <= 'z') && 
@@ -38,19 +38,14 @@
     static String infixToPrefix(String infix) 
     { 
     	// stack for operators. 
-    	Stack<Character> operators = new Stack<Character>(); 
-
-    	// stack for operands. 
-    	Stack<String> operands = new Stack<String>(); 
-
-    	for (int i = 0; i < infix.length(); i++) 
-    	{ 
-
-    		// If current character is an 
+    	Stack<Character> operators = new Stack<Character>();
+        // stack for operands. 
+    	Stack<String> operands = new Stack<String>();
+        for (int i = 0; i < infix.length(); i++){    
+            // If current character is an 
     		// opening bracket, then 
     		// push into the operators stack. 
-    		if (infix.charAt(i) == '(') 
-    		{ 
+    		if (infix.charAt(i) == '('){ 
     			operators.push(infix.charAt(i)); 
     		} 
 
@@ -60,13 +55,9 @@
     		// in operands stack until 
     		// matching opening bracket is 
     		// not found. 
-    		else if (infix.charAt(i) == ')') 
-    		{ 
-    			while (!operators.empty() && 
-    				operators.peek() != '(') 
-    				{ 
-
-    				// operand 1 
+    		else if (infix.charAt(i) == ')'){ 
+    			while (!operators.empty() && operators.peek() != '('){
+                    // operand 1 
     				String op1 = operands.peek(); 
     				operands.pop(); 
 
@@ -84,17 +75,14 @@
     				String tmp = op + op2 + op1; 
     				operands.push(tmp); 
     			} 
-
-    			// Pop opening bracket 
+                // Pop opening bracket 
     			// from stack. 
     			operators.pop(); 
     		} 
-
-    		// If current character is an 
+             // If current character is an 
     		// operand then push it into 
     		// operands stack. 
-    		else if (!isOperator(infix.charAt(i))) 
-    		{ 
+    		else if (!isOperator(infix.charAt(i))){ 
     			operands.push(infix.charAt(i) + ""); 
     		} 
 
@@ -104,14 +92,9 @@
     		// high priority operators from 
     		// operators stack and pushing 
     		// result in operands stack. 
-    		else
-    		{ 
-    			while (!operators.empty() && 
-    				getPriority(infix.charAt(i)) <= 
-    					getPriority(operators.peek())) 
-    				{ 
-
-    				String op1 = operands.peek(); 
+    		else{ 
+    			while (!operators.empty() && getPriority(infix.charAt(i)) <= getPriority(operators.peek())){ 
+                    String op1 = operands.peek(); 
     				operands.pop(); 
 
     				String op2 = operands.peek(); 
@@ -122,9 +105,8 @@
 
     				String tmp = op + op2 + op1; 
     				operands.push(tmp); 
-    			} 
-
-    			operators.push(infix.charAt(i)); 
+    			}
+                operators.push(infix.charAt(i)); 
     		} 
     	} 
 
@@ -132,8 +114,7 @@
     	// stack until it is empty and 
     	// operation in add result of 
     	// each pop operands stack. 
-    	while (!operators.empty()) 
-    	{ 
+    	while (!operators.empty()){ 
     		String op1 = operands.peek(); 
     		operands.pop(); 
 
@@ -146,8 +127,7 @@
     		String tmp = op + op2 + op1; 
     		operands.push(tmp); 
     	} 
-
-    	// Final prefix expression is 
+         // Final prefix expression is 
     	// present in operands stack. 
     	return operands.peek(); 
     } 
@@ -156,16 +136,15 @@
 
     //Driver Code
     public class infixConversion{
-    public static void main(String args[]) 
-        { infixPrefix g = new infixPrefix();
-         // Using Scanner for Getting Input from User 
+        public static void main(String args[]){
+            infixPrefix g = new infixPrefix();
+            // Using Scanner for Getting Input from User 
             Scanner in = new Scanner(System.in); 
-      
             String s = in.nextLine(); 	
-    	//String s = "(A-B/C)*(A/K-L)"; 
-    	System.out.println(g.infixToPrefix(s)); 
-         } 
+    	    //String s = "(A-B/C)*(A/K-L)"; 
+    	    System.out.println(g.infixToPrefix(s)); 
+     } 
     }
 
-    //Input - (A-B/C)*(A/K-L)
-    // Output -  *-A/BC-/AKL
+//Input - (A-B/C)*(A/K-L)
+// Output -  *-A/BC-/AKL
