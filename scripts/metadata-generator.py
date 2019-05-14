6178656ab4733da642434d9da07ca34a24980a17
yedhink <yedhin1998@gmail.com>
2019-05-14 19:51:48 +0530

    format using black

--- scripts/metadata-generator.py
+++ scripts/metadata-generator.py
@@ -23,20 +23,23 @@ avoid_dirs = ["project", "test", "img", "image", "images"]
 paths = []
 original_paths = collections.defaultdict(str)
 for path in pathlib.Path(__file__).parents[1].glob("code/**/**/*"):
-    if (path.suffix
-            and not any(elem in list(path.parts) for elem in avoid_dirs)
-            and path.suffix.lower() not in avoid_extensions):
+    if (
+        path.suffix
+        and not any(elem in list(path.parts) for elem in avoid_dirs)
+        and path.suffix.lower() not in avoid_extensions
+    ):
         original_paths[path.parts[-2]] = "/".join(path.parts[:-2])
         paths.append(
             Path(
                 suffix=path.suffix.lstrip(".").lower(),
                 group=path.parts[1].replace("-", " ").replace("_", " "),
                 name=path.parts[-2].replace("-", " ").replace("_", " "),
-            ))
+            )
+        )
 
 metadata = collections.defaultdict(str)
 for each in paths:
-    x = each[-1].replace(' ', '_').rstrip()
+    x = each[-1].replace(" ", "_").rstrip()
     metadata_path = "metadata/{}".format(x)
     pathlib.Path(metadata_path).mkdir(parents=True, exist_ok=True)
     filename = pathlib.Path("{}/data.json".format(metadata_path))
