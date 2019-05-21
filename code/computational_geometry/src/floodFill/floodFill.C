ebb88ca572635011fa6139a6fe5d47bbd982d3c6
robotjellyzone <robotjelly.zone@gmail.com>
2019-05-21 09:01:43 +0530

    adding c file of flood fill algo

--- code/computational_geometry/src/floodFill/floodFill.C
+++ code/computational_geometry/src/floodFill/floodFill.C
@@ -1,44 +1,44 @@
-#include<stdio.h>
-#include<graphics.h>
+#include <stdio.h>
+#include <graphics.h>
 
-void floodFill(int x, int y, int old, int newcol)
+void floodFill (int x, int y, int old, int newcol)
 {
     int current;
 
     // To get color pixel
-    current = getpixel(x, y);
+    current = getpixel (x, y);
 
     // check current pixel is old_color or not
-    if(current == old)
+    if (current == old)
     {
-        delay(5);
+        delay (5);
 
 	    // put new pixel with new color
-	    putpixel(x, y, newcol);
+	    putpixel (x, y, newcol);
 
 	    // recursive call for bottom pixel fill
-	    floodFill(x + 1, y, old, newcol);
+	    floodFill (x + 1, y, old, newcol);
 
 	    // recursive call for top pixel fill
-	    floodFill(x - 1, y, old, newcol);
+	    floodFill (x - 1, y, old, newcol);
 
 	    // recursive call for right pixel fill
-	    floodFill(x, y + 1, old, newcol);
+	    floodFill (x, y + 1, old, newcol);
 
 	    // recursive call for left pixel fill
-	    floodFill(x, y - 1, old, newcol);
+	    floodFill (x, y - 1, old, newcol);
 
 	    // recursive call for bottom-right corner pixel fill
-        floodFill(x + 1, y + 1, old, newcol);
+        floodFill (x + 1, y + 1, old, newcol);
 
 	    // recursive call for top-right corner pixel fill
-        floodFill(x - 1, y + 1, old, newcol);
+        floodFill (x - 1, y + 1, old, newcol);
 
 	    // recursive call for bottom-left corner pixel fill
-        floodFill(x + 1, y - 1, old, newcol);
+        floodFill (x + 1, y - 1, old, newcol);
 
 	    // recursive call for top-left corner pixel fill
-        floodFill(x - 1, y - 1, old, newcol);
+        floodFill (x - 1, y - 1, old, newcol);
     }
 }
 
@@ -47,26 +47,26 @@ void main()
     int gd = DETECT, gm;
     int left, top, right, bottom;
 
-    printf("Enter the left point: ");
-    scanf("%d", & left);
+    printf ("Enter the left point: ");
+    scanf ("%d", & left);
 
-    printf("Enter the top point: ");
-    scanf("%d", & top);
+    printf ("Enter the top point: ");
+    scanf ("%d", & top);
 
-    printf("Enter the right point: ");
-    scanf("%d", & right);
+    printf ("Enter the right point: ");
+    scanf ("%d", & right);
 
-    printf("Enter the bottom point: ");
+    printf ("Enter the bottom point: ");
     scanf("%d", & bottom);
 
-    initgraph(& gd, & gm, "C:\\TURBOC3\\BGI");
+    initgraph (& gd, & gm, "C:\\TURBOC3\\BGI");
 
-    rectangle(left, top, right, bottom);
+    rectangle (left, top, right, bottom);
 
-    floodFill(left + 1, top + 1, 0, 15);
+    floodFill (left + 1, top + 1, 0, 15);
 
-    getch();
-    closegraph();
+    getch ();
+    closegraph ();
 }
 
 // Sample input - https://imgur.com/css9qIj
