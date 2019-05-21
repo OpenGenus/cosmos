fcd560d5774ecad58df7fcfe30c55601a1ab47c4
robotjellyzone <robotjelly.zone@gmail.com>
2019-05-21 09:02:52 +0530

    adding c++ file of flood fill algo

--- code/computational_geometry/src/floodFill/floodFill.cpp
+++ code/computational_geometry/src/floodFill/floodFill.cpp
@@ -1,7 +1,7 @@
-#include<stdio.h>
-#include<graphics.h>
+#include <stdio.h>
+#include <graphics.h>
 
-void floodFill(int x, int y, int old, int newcol)
+void floodFill (int x, int y, int old, int newcol)
 {
     int current;
 
@@ -14,31 +14,31 @@ void floodFill(int x, int y, int old, int newcol)
         delay(5);
 
 	    // put new pixel with new color
-	    putpixel(x, y, newcol);
+	    putpixel (x, y, newcol);
 
 	    // recursive call for bottom pixel fill
-	    floodFill(x + 1, y, old, newcol);
+	    floodFill (x + 1, y, old, newcol);
 
 	    // recursive call for top pixel fill
-	    floodFill(x - 1, y, old, newcol);
+	    floodFill (x - 1, y, old, newcol);
 
 	    // recursive call for right pixel fill
-	    floodFill(x, y + 1, old, newcol);
+	    floodFill (x, y + 1, old, newcol);
 
 	    // recursive call for left pixel fill
-	    floodFill(x, y - 1, old, newcol);
+	    floodFill (x, y - 1, old, newcol);
 
 	    // recursive call for bottom-right corner pixel fill
-        floodFill(x + 1, y + 1, old, newcol);
+        floodFill (x + 1, y + 1, old, newcol);
 
 	    // recursive call for top-right corner pixel fill
-        floodFill(x - 1, y + 1, old, newcol);
+        floodFill (x - 1, y + 1, old, newcol);
 
 	    // recursive call for bottom-left corner pixel fill
-        floodFill(x + 1, y - 1, old, newcol);
+        floodFill (x + 1, y - 1, old, newcol);
 
 	    // recursive call for top-left corner pixel fill
-        floodFill(x - 1, y - 1, old, newcol);
+        floodFill (x - 1, y - 1, old, newcol);
     }
 }
 
@@ -50,23 +50,23 @@ void main()
     cout << "Enter the left point: ";
     cin >> left;
 
-    cout << ("Enter the top point: ");
+    cout << "Enter the top point: ";
     cin >> top;
 
-    cout << ("Enter the right point: ");
+    cout << "Enter the right point: ";
     cin >> right;
 
     cout << "Enter the bottom point: ";
     cin >> bottom;
 
-    initgraph(& gd, & gm, "C:\\TURBOC3\\BGI");
+    initgraph (& gd, & gm, "C:\\TURBOC3\\BGI");
 
-    rectangle(left, top, right, bottom);
+    rectangle (left, top, right, bottom);
 
-    floodFill(left + 1, top + 1, 0, 15);
+    floodFill (left + 1, top + 1, 0, 15);
 
-    getch();
-    closegraph();
+    getch ();
+    closegraph ();
 }
 
 // Sample input - https://imgur.com/css9qIj
