b037ccc2b12bbcbd2e8a89f7d574bfca05fd3c45
msknkh <muskankhandelwal369@gmail.com>
2019-05-23 01:40:56 +0530

    Revert "Edit Dining Philosopher Code"
    This reverts commit 86bdfe05b90370e5e262981ae1495509f0c02e8d.

--- code/operating_system/src/concurrency/dining_philosophers/dining_philosophers.c
+++ code/operating_system/src/concurrency/dining_philosophers/dining_philosophers.c
@@ -1,91 +1,57 @@
-#include <pthread.h> 
-#include <semaphore.h> 
+/* Dining Philosophers Problem*/
 #include <stdio.h>
-#include<unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
 
-#define N 6 
-#define THINKING 2 
-#define HUNGRY 1 
-#define EATING 0 
-#define LEFT (phnum + (N-1)) % N 
-#define RIGHT (phnum + 1) % N 
+void *func(int n);
 
-int state[N]; 
-int phil[N] = { 0, 1, 2, 3, 4, 5 }; 
+pthread_t philosopher[5];
+pthread_mutex_t chopstick[5];
 
-sem_t mutex; 
-sem_t S[N]; 
+int main()
+{
+	int i, k;
+	void *msg;
+	for (i = 1; i <= 5; i++) {
+		k = pthread_mutex_init(&chopstick[i], NULL);
+		if (k == -1) {
+			printf("\n Mutex initialization failed");
+			exit(1);
+		}
+	}
+	for (i = 1; i <= 5; i++) {
+		k = pthread_create(&philosopher[i], NULL, (void *) func, (int *) i);
+		if (k != 0) {
+			printf("\n Thread creation error \n");
+			exit(1);
+		}
+	}
+	for (i = 1; i <= 5; i++) {
+		k = pthread_join(philosopher[i], &msg);
+		if (k != 0) {
+			printf("\n Thread join failed \n");
+			exit(1);
+		}
+	}
+	for (i = 1; i <= 5; i++) {
+		k = pthread_mutex_destroy(&chopstick[i]);
+		if (k != 0) {
+			printf("\n Mutex Destroyed \n");
+			exit(1);
+		}
+	}
+	return 0;
+}
 
-void test(int phnum){ 
-   if (state[phnum] == HUNGRY && state[LEFT] != EATING && state[RIGHT] != EATING){ 
-      // state that eating 
-      state[phnum] = EATING;
-      sleep(2); 
-      printf("Phil %d using chopstick %d and %d\n", phnum + 1, LEFT + 1, phnum + 1); 
-      printf("Phil %d is Eating\n", phnum + 1); 
-      sem_post(&S[phnum]); 
-   } 
-} 
-
-// take up chopsticks 
-void take_fork(int phnum){ 
-   sem_wait(&mutex); 
-
-   state[phnum] = HUNGRY; 
-
-   printf("Phil %d is Hungry\n", phnum + 1); 
-
-   test(phnum); 
-   sem_post(&mutex); 
-
-
-   sem_wait(&S[phnum]); 
-
-   sleep(1); 
-} 
-
-// put down chopsticks 
-void put_fork(int phnum){ 
-   sem_wait(&mutex); 
-
-   state[phnum] = THINKING; 
-   
-   printf("Phil %d putting chopstick %d and %d down\n", 
-      phnum + 1, LEFT + 1, phnum + 1); 
-   printf("Phil %d is thinking\n", phnum + 1); 
-
-   test(LEFT); 
-   test(RIGHT); 
-   sem_post(&mutex); 
-} 
-
-void* philospher(void* num){ 
-   int* i = num; 
-   sleep(1); 
-   take_fork(*i); 
-   sleep(0); 
-   put_fork(*i);
-   printf("\n");  
-} 
-
-int main(){ 
-   int i; 
-   pthread_t thread_id[N]; 
-
-   // initialize the semaphores 
-   sem_init(&mutex, 0, 1); 
-
-   for (i = 0; i < N; i++) 
-      sem_init(&S[i], 0, 0); 
-
-   for (i = 0; i < N; i++) { 
-      // create philosopher processes 
-      pthread_create(&thread_id[i], NULL, philospher, &phil[i]); 
-      printf("Phil %d is thinking\n", i + 1); 
-   } 
-   printf("\n");
-   for (i = 0; i < N; i++) 
-      pthread_join(thread_id[i], NULL);
-   printf("No one left to eat\n");
-   return 0; 
+void *func(int n)
+{
+	printf("\nPhilosopher %d is thinking", n);
+	pthread_mutex_lock(&chopstick[n]);
+	pthread_mutex_lock(&chopstick[(n + 1) % 5]);
+	printf("\nPhilosopher %d is eating", n);
+	sleep(3);
+	pthread_mutex_unlock(&chopstick[n]);
+	pthread_mutex_unlock(&chopstick[(n + 1) % 5]);
+	printf("\nPhilosopher %d Finished eating", n);
 }
