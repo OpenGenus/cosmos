39cae6148b91977ed485d39cb044b89399749a9c
shreyarastogi <41367451+shreyarastogi@users.noreply.github.com>
2019-05-24 11:52:02 +0530

    Update do-while.c

--- code/language/c/loop/do-while.c
+++ code/language/c/loop/do-while.c
@@ -1,12 +1,11 @@
 /* do-while loop*/
 /*C program to print sum of first 5 natural numbers using do..while loop*/
-/*do- while loop is a loop for performing repetetive tasks. In this loop the body of the loop is executed*/
+/*do- while loop is a loop for performing repetitive tasks. In this loop the body of the loop is executed*/
 /*atleast once, and then the test condition is checked. If it evaluates to true, the loop continues to run otherwise it is exited.*/
 
 #include <stdio.h>
 
-int 
-main() 
+int main() 
 {
   int sum = 0, i = 1; /*initialization of counter variable i*/
   do {
