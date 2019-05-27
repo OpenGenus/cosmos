be67dc32716161edbddaef2da41ea0281d588f27
saddhu1005 <saddhu1005@gmail.com>
2019-05-27 08:39:29 +0530

    fixed bogosort implicit declaration error

--- code/sorting/src/bogo_sort/bogo_sort.c
+++ code/sorting/src/bogo_sort/bogo_sort.c
@@ -6,21 +6,21 @@
 */
 
 #include <stdio.h>    
-#include <stdlib.h>    
+#include <stdlib.h>   
 
-int
-main()    
+void bogosort(int *a, int n);
+
+int main()    
 {        
     int num[10]={1, 4, 7, 5, 9, 2, 6, 3, 8, 0};        
     int i;            
     bogosort(num, 10);        
     printf("The array after sorting is:");        
     for (i = 0;i < 10;i++) {            
-        printf("%d\n", num[i]);        
+        printf("%d ", num[i]);        
     } printf("\n");    
 }         
-int
-is_sorted(int *a, int n)    
+int is_sorted(int *a, int n)    
 {        
     while ( --n >= 1 ) {        
         if ( a[n] < a[n-1] ) 
@@ -28,8 +28,7 @@ is_sorted(int *a, int n)
     } 
     return (1);    
 }    
-void
-shuffle(int *a, int n)    
+void shuffle(int *a, int n)    
 {        
     int i, t, temp;        
     for (i = 0;i < n;i++) {            
@@ -39,8 +38,7 @@ shuffle(int *a, int n)
         a[temp] = t;        
     }    
 }    
-void
-bogosort(int *a, int n)    
+void bogosort(int *a, int n)    
 {        
     while ( !is_sorted(a, n) )             
     shuffle(a, n);    
