7efa6adca7f1dc4311f44f7f10d47b0d9a060f15
msknkh <muskankhandelwal369@gmail.com>
2019-05-30 14:15:20 +0530

    Edits

--- code/computational_geometry/src/ellipse_drawing/Ellipse.cpp
+++ code/computational_geometry/src/ellipse_drawing/Ellipse.cpp
@@ -14,15 +14,17 @@ void ellipseSymm(int x, int y, int c1, int c2)
 void myEllipse(int a, int b, int c1, int c2)
 {
     setcolor(BLUE);
-    line(300, 0, 300, 479);
+    line(c1, 0, c1, 479);
     setcolor(RED);
-    line(0, 240, 639, 240);
+    line(0, c2, 639, c2);
 
+    // x = a and y = 0 are the first coordinates of the ellipse that are drawn on the screen
     int x = a;
     int y = 0;
+    // d is the decision parameter that determines the next point to be chosen for drawing
     int d = (a * a) - ((1/4) * (b * b)) - (a * b * b);
     ellipseSymm(x, y, c1, c2);
-    
+
     while ((b * b * x) >= (a * a * y))
     {
         if (d < 0)
@@ -38,10 +40,10 @@ void myEllipse(int a, int b, int c1, int c2)
     }
 
     d = (b * b * (x - 1) * (x - 1)) + (a * a * (y + 0.5) * (y + 0.5)) - (a * a * b * b);
-    
+
     while (x >= 0)
     {
-        if (d < 0) 
+        if (d < 0)
         {
             d += (b * b * (3 - (2 * x))) + (2 * a * a * (1 + y));
             y++;
@@ -49,7 +51,7 @@ void myEllipse(int a, int b, int c1, int c2)
 
         else
             d += (b * b * (3 - (2 * x)) );
-        
+
 	x--;
         ellipseSymm(x, y, c1, c2);
     }
@@ -66,8 +68,11 @@ int main()
     cout << "Enter value of minor axis" << endl;
     cin >> b;
 
-    c1 = 300;
-    c2 = 240;
+    cout << "Enter the x coordinate of the centre of the ellipse" << endl;
+    cin >> c1;
+
+    cout << "Enter the y coordinate of the centre of the ellipse" << endl;
+    cin >> c2;
 
     myEllipse(a, b, c1, c2);
 
