0eaa4004e70f1f57af91857bfa941f08c876d292
Mudit <40176751+mudit458@users.noreply.github.com>
2019-10-01 14:47:05 +0530

    Reformated file xor_swap.py
    Formatting for xor_swap.py

--- code/bit_manipulation/src/xor_swap/xor_swap.py
+++ code/bit_manipulation/src/xor_swap/xor_swap.py
@@ -1,17 +1,19 @@
-# Part of Cosmos by OpenGenus Foundation 
+'''
+ Part of Cosmos by OpenGenus Foundation 
 
-# Swaps two given numbers making use of xor
+ Swaps two given numbers making use of xor
 
-# Works for both python 2 and python 3
+ Works for both python 2 and python 3
+'''
+def xor_swap(a, b):
+    a = b ^ a
+    b = a ^ b
+    a = b ^ a
+    return a, b
 
-def xorswap(n,m):
-    n=m^n
-    m=n^m
-    n=m^n
-    return n,m
 
-n=10
-m=15
-print ("Earlier A was equal to ",n," and B was equal to ",m)
-n,m=xorswap(n,m)
-print ("Now A is equal to ",n," and B is equal to ",m)
+n = 10
+m = 15
+print("Earlier A was equal to ", n, " and B was equal to ", m)
+n, m = xorswap(n, m)
+print("Now A is equal to ", n, " and B is equal to ", m)
