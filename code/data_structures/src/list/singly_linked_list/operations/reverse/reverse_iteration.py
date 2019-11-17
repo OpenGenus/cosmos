0f9a935b95c23fde8e821f0c75ecc16e97600541
shubhamkumar27 <shubhamkumar8180323@gmail.com>
2019-11-17 10:16:45 +0530

    Reverse linked list using iteration

--- code/data_structures/src/list/singly_linked_list/operations/reverse/reverse_iteration.py
+++ code/data_structures/src/list/singly_linked_list/operations/reverse/reverse_iteration.py
@@ -1,25 +1,26 @@
-class Node():
+class Node:
     def __init__(self, value):
         self.data = value
         self.next = None
 
-class Linkedlist():
+
+class Linkedlist:
     def __init__(self):
         self.head = None
 
-    def insert_end(self,value):
+    def insert_end(self, value):
         node = Node(value)
-        if(self.head==None):
+        if self.head == None:
             self.head = node
         else:
             cur = self.head
-            while(cur.next):
+            while cur.next:
                 cur = cur.next
             cur.next = node
 
-    def insert_head(self,value):
+    def insert_head(self, value):
         node = Node(value)
-        if(self.head==None):
+        if self.head == None:
             self.head = node
         else:
             node.next = self.head
@@ -29,25 +30,25 @@ class Linkedlist():
         if self.head == None:
             return None
         cur = self.head
-        print('Elements are :', end=' ')
-        while(cur):
-            print(cur.data,end=' ')
+        print("Elements are :", end=" ")
+        while cur:
+            print(cur.data, end=" ")
             cur = cur.next
         print()
 
-
-########################################################################
+    ########################################################################
     def reverse(self):
         if self.head == None or self.head.next == None:
             return
         head = self.head
         tail = None
-        while(head):
+        while head:
             temp = head.next
             head.next = tail
             tail = head
             head = temp
         self.head = tail
+
 
 #################################################################3
 
@@ -59,8 +60,8 @@ lis.insert_head(4)
 lis.insert_end(5)
 lis.insert_end(6)
 lis.insert_end(7)
-print('Given list is : ',end="")
+print("Given list is : ", end="")
 lis.traverse()
 lis.reverse()
-print('Reversed list is : ',end="")
+print("Reversed list is : ", end="")
 lis.traverse()
