09eb36e1e3ec74e15e92a77f3640289aa108c429
Ayush7-BIT <59275479+Ayush7-BIT@users.noreply.github.com>
2020-03-02 23:23:46 +0530

    Updated 2sum.java
    First contribution: "https://github.com/OpenGenus/cosmos/edit/master/code/mathematical_algorithms/src/2sum/2sum.java" [Pull request](link to GitHub pull request on Cosmos)

--- code/mathematical_algorithms/src/2sum/2sum.java
+++ code/mathematical_algorithms/src/2sum/2sum.java
@@ -1,12 +1,12 @@
 // Part of Cosmos by OpenGenus Foundation
 
 public class two_sum {
-    private static int[] two_sum(int[] arr, int target) {
-        if (arr == null || arr.length < 2) return new int[]{0, 0};
+    private static int[] two_sum(int[] arr, int target) {//function decleration
+        if (arr == null || arr.length < 2) return new int[]{0, 0};//Condition to be checked
 
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < arr.length; i++) {
-            if (map.containsKey(arr[i])) {
+        HashMap<Integer, Integer> map = new HashMap<>();//Map Interface
+        for (int i = 0; i < arr.length; i++) {//Loop till total number of arr elements
+            if (map.containsKey(arr[i])) {//Checking condition for a definite key
                 return new int[]{map.get(arr[i]), i};
             } else {
                 map.put(target - arr[i], i);
@@ -14,12 +14,12 @@ public class two_sum {
         }
         return new int[]{0, 0};
     }
+//Driver's code
+    public static void main(String[] args) {//Main function
+        int[] arr1 = {3, 5, 7, 0, -3, -2, -3};//Initialization of arr1
+        int[] arr2 = {3, 5, 0, -3, -2, -3};//Initialization of arr2
 
-    public static void main(String[] args) {
-        int[] arr1 = {3, 5, 7, 0, -3, -2, -3};
-        int[] arr2 = {3, 5, 0, -3, -2, -3};
-
-        System.out.println(Arrays.toString(two_sum(arr1, 4)));
-        System.out.println(Arrays.toString(two_sum(arr2, 4)));
+        System.out.println(Arrays.toString(two_sum(arr1, 4)));//Output condititon
+        System.out.println(Arrays.toString(two_sum(arr2, 4)));//Output condititon
     }
 }
