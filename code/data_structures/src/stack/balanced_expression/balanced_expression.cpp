117376ce22fe08051b9f5f4821b37d8f4f1c6ae9
nitashadhingra <nitashadhingra612@gmail.com>
2020-03-07 01:34:23 +0530

    First Contribution (add comments)

--- code/data_structures/src/stack/balanced_expression/balanced_expression.cpp
+++ code/data_structures/src/stack/balanced_expression/balanced_expression.cpp
@@ -1,17 +1,22 @@
+/* To check if string has balanced brackets */
+
 #include <iostream>
 #include <stack>
 
 bool checkBalanced(string s)
 {
+    // if not in pairs, then not balanced
     if (s.length() % 2 != 0)
         return false;
     std::stack <char> st;
     for (const char: s)
     {
+        //adding opening brackets to stack
         if (s[i] == '{' || s[i] == '[' || s[i] == '(')
             st.push(s[i]);
         else
         {
+            // checking for each closing bracket, if there is an opening bracket in stack
             char temp = st.top();
             if (s[i] == '}' && temp == '{')
                 st.pop();
@@ -35,4 +40,5 @@ int main()
         std::cout << "Expression is balanced";
     else
         std::cout << "Expression is not balanced";
+    return 0;
 }
