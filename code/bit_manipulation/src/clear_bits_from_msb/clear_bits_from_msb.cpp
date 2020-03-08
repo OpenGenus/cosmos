24e3dd3e52792806eba31dd4f38172198a4dc370
apoorvam25 <apoorvam0100@gmail.com>
2020-03-08 12:40:17 +0530

    Added Problems in Bit Masking

---
+++ code/bit_manipulation/src/clear_bits_from_msb/clear_bits_from_msb.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int clearAllBits(int n, int i){
+    int mask = (1 << i) - 1;
+    n = n & mask;
+
+    return n;
+}
+
+int main() {
+	int n, i;
+	cin >> n >> i;
+	cout<< clearAllBits(n, i) <<endl;
+
+	return 0;
+}
+
