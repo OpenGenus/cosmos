2acd92cab0779ae1ef72011900a25370d17900eb
Archita Varshney <architavarshney200@gmail.com>
2020-03-14 17:04:28 +0530

    adding appropriate comments to the code

--- code/divide_conquer/src/quick_sort/quick_sort2.cpp
+++ code/divide_conquer/src/quick_sort/quick_sort2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void swapit(int *x, int *y)
+void swapit(int *x, int *y)         //function to swap two integer values(passed by value)
 {
     int temp;
     temp = *x;
@@ -9,22 +9,23 @@ void swapit(int *x, int *y)
     *y = temp;
 }
 
-int Mpartition(int arr[], int low, int high)
+int Mpartition(int arr[], int low, int high)        
+    //functon to return the position of pivot after arranging smaller elements to its left and larger elements to its right 
 {
-    int i = low - 1;
-    int j = low;
-    for (; j < high; j++)
+    int i = low - 1;    //i points to the last element that was smaller than the pivot while traversing 
+    int j = low;        //j points to the current element while traversing 
+    for (; j < high; j++)       
         if (arr[high] < arr[j])
         {
             i++;
             swapit(&arr[j], &arr[i]);
         }
-    swapit(&arr[high], &arr[i + 1]);
+    swapit(&arr[high], &arr[i + 1]);        //swaps the pivot with the first element sequentially that is greater than the pivot
     return i + 1;
 
 }
 
-void quicksort(int arr[], int low, int high)
+void quicksort(int arr[], int low, int high)        // function which implements divide and conquer
 {
 
     if (low < high)
