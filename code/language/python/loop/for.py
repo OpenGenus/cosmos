ac51b4bd79ffa5efc34a19df54815df2ad999c28
Ratna Priya <ratnpriya1998@gmail.com>
2020-03-18 00:06:08 +0530

    creating for loop in python

---
+++ code/language/python/loop/for.py
@@ -0,0 +1,11 @@
+#for loop in python
+
+## Simple for loop in string
+for x in "cosmos":
+  print(x)
+ 
+ 
+ ## For loop in list
+animals = ["dog", "cat", "rat"]
+for x in animals:
+  print("THe animal is:", x)
