14ec4ce0cefdbad2654263a319cc0ab73b35b191
ritish099 <ritish.rks@gmail.com>
2020-03-18 22:48:38 +0530

    Codechef FCTRL solution in C

---
+++ code/online_challenges/src/codechef/FCTRL/Factorial.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+main()
+{
+    long t, n, s = 1, sum = 0, k;
+    scanf("%ld", &t);
+    while (t--)
+    {
+        scanf("%ld", &n);
+        while (1)
+        {
+            s = s * 5;
+            if (s > n)
+                break;
+            else
+            {
+                k = n / s;
+                sum = sum + k;
+            }
+        }
+        printf("%ld\n", sum);
+        sum = 0;
+        s = 1;
+    }
+}
