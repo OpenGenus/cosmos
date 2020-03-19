4833c0131b56c9057145ff0b65928198e7245215
Raksha <57195964+raksha009@users.noreply.github.com>
2020-03-20 01:38:56 +0530

    Update slab.java

--- code/online_challenges/src/codechef/SLAB/slab.java
+++ code/online_challenges/src/codechef/SLAB/slab.java
@@ -4,51 +4,50 @@ import java.io.*;
 
 class slab
 {
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		Scanner s = new Scanner(System.in);
-		int t = s.nextInt();
-		int a = 12500;
-		int b = 25000;
-		int c = 37500;
-		int d = 50000;
-		int e = 62500;
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        int a = 12500;
+	int b = 25000;
+	int c = 37500;
+	int d = 50000;
+	int e = 62500;
 		
-		while (t-- > 0){
-		    long n = s.nextLong();
-		    long ans = 0;
-		    if (n <= 250000){
-		        System.out.println(n);
-		        
-		    }
-		    else{
-		        if (n >= 250001 && n <=500000){
-		            long div = (long)((n - 250000)*0.05);
-		            System.out.println(n - div);
-		        }
-		        else if (n >= 500001 && n <= 750000){
-		            long div = (long)((n - 500000)*0.1);
-		            System.out.println(n - a-div);
-		        }
-		        else if (n >= 750001 && n <= 1000000){
-		            long div = (long)((n- 750000)*0.15);
-		            System.out.println(n - a-b-div);
-		        }
-		        else if ( n>= 1000001 && n <= 1250000){
-		            long div = (long)((n - 1000000)*0.2);
-		         System.out.println(n - a-b-c-div);   
-		        }
-		        else if ( n>= 1250001 && n <= 1500000){
-		            long div = (long)((n - 1250000)*0.25);
-		            System.out.println(n - a-b-c-d-div); 
-		        }
-		        else if (n> 1500000){
-		            long f  = (long)((n - 1500000)*0.3);
-		            System.out.println(n - a-b-c-d-e-f); 
-		        }
-		    }
+	while (t-- > 0){
+	    long n = s.nextLong();
+	    long ans = 0;
+	    if (n <= 250000){
+	        System.out.println(n);
+	    }
+	    else{
+	        if (n >= 250001 && n <=500000){
+	            long div = (long)((n - 250000)*0.05);
+	            System.out.println(n - div);
+	        }
+	        else if (n >= 500001 && n <= 750000){
+	            long div = (long)((n - 500000)*0.1);
+	            System.out.println(n - a-div);
+	        }
+	        else if (n >= 750001 && n <= 1000000){
+	            long div = (long)((n- 750000)*0.15);
+	            System.out.println(n - a-b-div);
+	        }
+	        else if ( n>= 1000001 && n <= 1250000){
+	            long div = (long)((n - 1000000)*0.2);
+	            System.out.println(n - a-b-c-div);   
+	        }
+	        else if ( n>= 1250001 && n <= 1500000){
+		    long div = (long)((n - 1250000)*0.25);
+		    System.out.println(n - a-b-c-d-div); 
 		}
+	        else if (n> 1500000){
+	            long f  = (long)((n - 1500000)*0.3);
+	            System.out.println(n - a-b-c-d-e-f); 
+	        }
+	    }
 	}
+    }
 }
 
 /*
