0281516fac1901d4e3dc2c58af5d50329743168e
Smriti <smritikpriya@gmail.com>
2020-03-20 18:18:14 +0530

    Update selection_sort.py

--- code/sorting/src/selection_sort/selection_sort.py
+++ code/sorting/src/selection_sort/selection_sort.py
@@ -1,12 +1,18 @@
 # Part of Cosmos by OpenGenus Foundation
 
-
-def selection_sort(array):
+#Python program for selection sort
+def selection_sort(array): 
+    
+    #Traversing one by one through the elements in array(list)
     for i in range(len(array) - 1):
+        
+        #Finding the minimum element in the unsorted array 
         minimumValue = i
         for j in range(i + 1, len(array)):
             if array[j] < array[minimumValue]:
                 minimumValue = j
+                
+        #Swapping the minimum value with the first element of the array
         temp = array[minimumValue]
         array[minimumValue] = array[i]
         array[i] = temp
