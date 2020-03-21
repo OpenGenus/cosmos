d1b13d68de4ca2e0b3e136d955fcc7d9b6708b04
Mehul Kumar Nirala <mehulkumarnirala@iitkgp.ac.in>
2020-03-21 13:24:49 +0530

    Add files via upload

---
+++ code/genetic_algorithms/src/EvolutionarySteering/sketch.js
@@ -0,0 +1,83 @@
+var vehicles = [];
+var food = [];
+var poison = [];
+
+function setup() {
+  var canvas = createCanvas(windowWidth-15, windowHeight-20);
+  canvas.parent('sketch-div');
+  for (var i = 0; i < 50; i++) {
+    var x = random(width);
+    var y = random(height);
+    vehicles[i] = new Vehicle(x, y);
+  }
+
+  for (var i = 0; i < 40; i++) {
+    var x = random(width);
+    var y = random(height);
+    food.push(createVector(x, y));
+  }
+
+  for (var i = 0; i < 20; i++) {
+    var x = random(width);
+    var y = random(height);
+    poison.push(createVector(x, y));
+  }
+}
+
+function mouseDragged() {
+  vehicles.push(new Vehicle(mouseX, mouseY));
+}
+
+function draw() {
+  background(0);
+  fill(250);
+  noStroke();
+  textFont('monospace');
+  textSize(25);
+  text("Evolutionary Steering", 80, 80);
+
+  if (random(1) < 0.1) {
+    var x = random(width);
+    var y = random(height);
+    food.push(createVector(x, y));
+  }
+
+  if (random(1) < 0.01) {
+    var x = random(width);
+    var y = random(height);
+    poison.push(createVector(x, y));
+  }
+
+
+  for (var i = 0; i < food.length; i++) {
+    fill(0, 255, 0);
+    noStroke();
+    ellipse(food[i].x, food[i].y, 4, 4);
+  }
+
+  for (var i = 0; i < poison.length; i++) {
+    fill(255, 0, 0);
+    noStroke();
+    ellipse(poison[i].x, poison[i].y, 4, 4);
+  }
+
+  for (var i = vehicles.length - 1; i >= 0; i--) {
+    vehicles[i].boundaries();
+    vehicles[i].behaviors(food, poison);
+    vehicles[i].update();
+    vehicles[i].display();
+
+    var newVehicle = vehicles[i].clone();
+    if (newVehicle != null) {
+      vehicles.push(newVehicle);
+    }
+
+    if (vehicles[i].dead()) {
+      var x = vehicles[i].position.x;
+      var y = vehicles[i].position.y;
+      food.push(createVector(x, y));
+      vehicles.splice(i, 1);
+    }
+
+  }
+}
