0c5d977c6de56e31ee7bd72d726268c0ed84220d
shivani789 <53372454+shivani789@users.noreply.github.com>
2020-03-21 17:18:24 +0530

    Create FLOW006.cpp

---
+++ code/online_challenges/src/codechef/FLOW006.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+ {
+	int t,n;
+	cin>>t;
+	while(t>0)
+	{
+	    cin>>n;
+	     int sum=0;
+	    while(n!=0)
+	    {
+	     sum = sum + n % 10;
+	    n = n/10;
+	    }
+	    cout<<sum<<endl;
+	    t--;
+	}
+	return 0;
+}
