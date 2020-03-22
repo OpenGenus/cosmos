6d6c7743f0e7bed2e3f306f0570d07a14d9aa71d
abhijeet010304 <abhijeet010304@gmail.com>
2020-03-22 22:22:55 +0530

    added sqrt decomposition code after verification of PR

--- code/square_root_decomposition/src/mos_algorithm/sqrtdecomposition.py
+++ code/square_root_decomposition/src/mos_algorithm/sqrtdecomposition.py
@@ -3,35 +3,31 @@
 #       1->   1 i  - add 1 to the ith element of the array A
 #       2->   2 K  - print the Kth odd number of the array A if it exists, else print -1
 
-import math
-n, q = list(map(int, input().strip().split(' ')))
-array = list(map(int, input().strip().split(' ')))
+def precalculate(n, array):
+    buckets = int(math.ceil(math.sqrt(n)))
+    total_odds = [0 for i in range(buckets)]
+
+    bucket_size = int(math.ceil(n / buckets))
+    for bucket in range(buckets):
+        start = bucket * bucket_size
+        end = min(n, start + bucket_size)
+        for index in range(start, end):
+            if array[index] % 2 != 0:
+                total_odds[bucket] += 1
+    return total_odds,bucket_size
 
 
-buckets = int(math.ceil(math.sqrt(n)))
-total_odds = [0 for i in range(buckets)]
-
-bucket_size = int(math.ceil(n / buckets))
-
-
-
-for bucket in range(buckets):
-    start = bucket * bucket_size
-    end = min(n, start + bucket_size)
-    for index in range(start, end):
-        if array[index]%2 != 0:
-            total_odds[bucket] += 1
-for i in range(q):
-    typ, k = list(map(int, input().strip().split(' ')))
+def query_sol(typ, k,total_odds,bucket_size):
     if typ == 1:
         k -= 1
         array[k] += 1
 
         bucket = k // bucket_size
-        if array[k] % 2 !=0:
+        if array[k] % 2 != 0:
             total_odds[bucket] += 1
         else:
-            total_odds[bucket] +=-1
+            total_odds[bucket] += -1
+
     else:
         cnt = 0
         found_at = -1
@@ -45,11 +41,26 @@ for i in range(q):
 
                 cnt -= count
                 for j in range(start, end):
-                    if array[j] %2 != 0:
+                    if array[j] % 2 != 0:
                         cnt += 1
                         if cnt == k:
                             found_at = j + 1
 
                             break
                 break
-        print(found_at)
+        return found_at
+
+if __name__ == "__main__":
+    import math
+
+    n, q = list(map(int, input().strip().split(" ")))
+    array = list(map(int, input().strip().split(" ")))
+
+    total_odds,bucket_size = precalculate(n, array)
+
+    for i in range(q):
+        typ, k = list(map(int, input().strip().split(" ")))
+        if typ==1:
+            query_sol(typ, k,total_odds,bucket_size)
+        else:
+            print(query_sol(typ, k,total_odds,bucket_size))
