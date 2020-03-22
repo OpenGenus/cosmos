3bfd7127deacea5ed1dba19e703bfaafc169dbe7
Archita Varshney <architavarshney200@gmail.com>
2020-03-22 12:38:58 +0530

    update maxsubarraysum.cpp
    1) remove namespace declaration
    2) convert dynamic array to vector
    3) update variable names using camel case
    4) change to clang format

--- code/unclassified/src/maximum_subarray_sum/maxsubarraysum.cpp
+++ code/unclassified/src/maximum_subarray_sum/maxsubarraysum.cpp
@@ -1,35 +1,41 @@
-//Part of Cosmos by OpenGenus Foundation
+// Part of Cosmos by OpenGenus Foundation
 
 
 // C++ implementation of simple algorithm to find
 // Maximum Subarray Sum in a given array
-//this implementation is done using Kadane's Algorithm which has a time complexity of O(n)
+// this implementation is done using Kadane's Algorithm which has a time complexity of O(n)
 
-#include<bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <vector>
+#include <climits>
 
-int maxSubarraySum(int arr[], int n) {
-	int max_sum_so_far = INT_MIN;   
-	int curr_max = 0;
-	for (int i = 0; i < n; i++) {
-		curr_max = curr_max + arr[i];
-		if (max_sum_so_far < curr_max)
-			max_sum_so_far = curr_max;
-		if (curr_max < 0)     //if the current maximum sum becomes less than 0 then we make it 0
-			curr_max = 0;
-	}
-	return max_sum_so_far;
+int maxSubarraySum(std::vector<int> arr, int n)
+{
+    int maxSumSoFar = INT_MIN;
+    int currMax = 0;
+    for (int element : arr)
+    {
+        currMax = currMax + element;
+        if (maxSumSoFar < currMax)
+            maxSumSoFar = currMax;
+        if (currMax < 0) // if the current maximum sum becomes less than 0 then we make it 0
+            currMax = 0;
+    }
+    return maxSumSoFar;
 }
 
 int main()
 {
-	int n;
-	cin >> n;
-	int* arr = new int[n];
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
-	}
-	int answer = maxSubarraySum(arr, n);
-	cout << "Maximum subarray sum is " << answer;
-	return 0;
-} 
+    std::vector<int> v;
+    int n;
+    std::cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        int x;
+        std::cin >> x;
+        v.push_back(x);
+    }
+    int answer = maxSubarraySum(v, n);
+    std::cout << "Maximum subarray sum is " << answer << '\n';
+    return 0;
+}
