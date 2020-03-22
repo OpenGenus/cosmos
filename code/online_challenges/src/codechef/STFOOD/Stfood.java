5a47e2821dee02ca2909265c843bbee7c28ddf9d
Raksha <57195964+raksha009@users.noreply.github.com>
2020-03-22 21:00:31 +0530

    Update Stfood.java

--- code/online_challenges/src/codechef/STFOOD/Stfood.java
+++ code/online_challenges/src/codechef/STFOOD/Stfood.java
@@ -1,31 +1,28 @@
-import java.util.*;
-import java.lang.*;
 import java.io.*;
+import java.util.*;
 
-class Stfood
-{
-    public static void main (String[] args) throws java.lang.Exception
-    {
+class Stfood {
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-	int t = s.nextInt();
-	while (t-- >0){
-	    int n = s.nextInt();
-	    int maxprofit = 0;
+        int t = s.nextInt();
+        while (t-- > 0) {
+            int n = s.nextInt();
+            int maxprofit = 0;
 
-	    for (int i = 0; i < n ; i++){
-	        int stores = s.nextInt();
-	        int p = s.nextInt();
-	        int v = s.nextInt();
-	        int profit = (p/(stores+1)) * v;
-	        
+            for (int i = 0; i < n; i++) {
+                int stores = s.nextInt();
+                int p = s.nextInt();
+                int v = s.nextInt();
+                int profit = (p / (stores + 1)) * v;
+	       
                 // updating maxprofit
-	        if (maxprofit < profit){
-	            maxprofit = profit;
-	        }
-	    }
-	    
-	    System.out.println(maxprofit);
-	}
+                if (maxprofit < profit) {
+                    maxprofit = profit;
+                }
+            }
+
+            System.out.println(maxprofit);
+        }
     }
 }
 
