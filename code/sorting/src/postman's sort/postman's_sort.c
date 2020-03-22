6216da55b220745edeab6a4278c597b323d1e506
Ritish Singh <54978105+ritish099@users.noreply.github.com>
2020-03-22 12:06:37 +0530

    Update and rename code/sorting/src/Postman_Sort/Postman_Sort.c to code/sorting/src/postman's sort/postman's_sort.c

--- code/sorting/src/postman's sort/postman's_sort.c
+++ code/sorting/src/postman's sort/postman's_sort.c
@@ -2,29 +2,26 @@
 
 #include <stdio.h>
 
-void arrange(int, int);
-int arr[100], arr1[100];
-int i, j, temp, max, count, maxdigits = 0, c = 0;
-
-void main()
+int main() 
 {
     int t1, t2, k, t, n = 1;
-
+    int arr[100], arr1[100];
+    int i, j, temp, max, count, maxdigits = 0, c = 0,i1,j1;
     printf("Enter size of array :");
-    scanf("%d", &count);
+    scanf("%d", & count);
     printf("Enter elements into array :");
-    for (i = 0; i < count; i++)
+    for (i = 0; i < count; ++i) 
     {
         printf("Element - %d : ", i);
-        scanf("%d", &arr[i]);
+        scanf("%d", & arr[i]);
         arr1[i] = arr[i];
     }
 
     //Finding the longest digits in the array and no of elements in that digit
-    for (i = 0; i < count; i++)
+    for (i = 0; i < count; ++i) 
     {
         t = arr[i]; /*first element in t */
-        while (t > 0)
+        while (t > 0) 
         {
             c++; //Counting the no of elements of a digit
             t = t / 10;
@@ -36,13 +33,13 @@ void main()
     while (--maxdigits)
         n = n * 10;
 
-    for (i = 0; i < count; i++)
+    for (i = 0; i < count; ++i) 
     {
         max = arr[i] / n; //Dividing by a particular base
         t = i;
-        for (j = i + 1; j < count; j++)
+        for (j = i + 1; j < count; ++j) 
         {
-            if (max > (arr[j] / n))
+            if (max > (arr[j] / n)) 
             {
                 max = arr[j] / n;
                 t = j;
@@ -55,43 +52,43 @@ void main()
         arr[t] = arr[i];
         arr[i] = temp;
     }
-    while (n >= 1)
+    while (n >= 1) 
     {
-        for (i = 0; i < count;)
+        for (i = 0; i < count;) 
         {
             t1 = arr[i] / n;
-            for (j = i + 1; t1 == (arr[j] / n); j++)
-                ;
-            arrange(i, j);
+            for (j = i + 1; t1 == (arr[j] / n); ++j);
+			j1=j;	//Storing the end position to perform arrangement in the array
+			i1=i;   //Storing the Start position to perform arrangement in the array
+			
+			//Arranging the array
+            for (i1 = i; i1 < j1 - 1; ++i1) 
+            {
+                for (j = i1 + 1; j < j1; ++j) 
+                {
+                    if (arr1[i1] > arr1[j]) 
+                    {
+                        temp = arr1[i1];
+                        arr1[i1] = arr1[j];
+                        arr1[j] = temp;
+                        temp = (arr[i1] % 10);
+                        arr[i1] = (arr[j] % 10);
+                        arr[j] = temp;
+                    }
+                }
+            }
             i = j;
         }
         n = n / 10;
     }
+    // Printing the arranged array
     printf("\nSorted Array (Postman sort) :");
-    for (i = 0; i < count; i++)
+    for (i = 0; i < count; ++i)
         printf("%d ", arr1[i]);
     printf("\n");
+    return 0;
 }
 
-//Arranging the elements
-void arrange(int k, int n)
-{
-    for (i = k; i < n - 1; i++)
-    {
-        for (j = i + 1; j < n; j++)
-        {
-            if (arr1[i] > arr1[j])
-            {
-                temp = arr1[i];
-                arr1[i] = arr1[j];
-                arr1[j] = temp;
-                temp = (arr[i] % 10);
-                arr[i] = (arr[j] % 10);
-                arr[j] = temp;
-            }
-        }
-    }
-}
 /* Enter the Size of the array
 6
 Enter the elements of the array
