ee4b64cc607e22c27d20f81b16d1c9110dd1aa0b
Aman-Codes <54680709+Aman-Codes@users.noreply.github.com>
2020-03-23 11:24:24 +0530

    Update K_Nearest_Neighbours.cpp

--- code/artificial_intelligence/src/k_nearest_neighbours/K_Nearest_Neighbours.cpp
+++ code/artificial_intelligence/src/k_nearest_neighbours/K_Nearest_Neighbours.cpp
@@ -1,70 +1,80 @@
 // Part of Cosmos by OpenGenus
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include<vector>
 
-struct point 
+struct Point
 {
     float w, x, y, z; // w, x, y and z are the values of the dataset
     float distance; // distance will store the distance of dataset point from the unknown test point
     int op; // op will store the class of the point
 };
 
-int main() 
+int main()
 {
-    int i, j, n, k;
-    struct point p;
-    struct point temp;
-    cout << "Number of data points: ";
-    cin >> n;
-    struct point arr[n];
-    cout << "\nEnter the dataset values: \n";
-    cout << "w\tx\ty\tz\top\n";
+    int n, k;
+    Point p;
+    std::cout << "Number of data points: ";
+    std::cin >> n;
+    std::vector <Point> vec;
+    std::cout << "\nEnter the dataset values: \n";
+    std::cout << "w\tx\ty\tz\top\n";
 
     // Taking dataset
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        cin >> arr[i].w >> arr[i].x >> arr[i].y >> arr[i].z >> arr[i].op;
+        float wCoordinate, xCoordinate, yCoordinate, zCoordinate;
+        int opCoordinate;
+        std::cin >> wCoordinate >> xCoordinate >> yCoordinate >> zCoordinate >> opCoordinate;
+        vec.push_back(Point());
+        vec[i].w = wCoordinate;
+        vec[i].x = xCoordinate;
+        vec[i].y = yCoordinate;
+        vec[i].z = zCoordinate;
+        vec[i].op = opCoordinate;
     }
 
-    cout << "\nw\tx\ty\tz\top\n";
+    std::cout << "\nw\tx\ty\tz\top\n";
 
-    for (i = 0; i < n; i++) 
+    for (int i = 0; i < n; i++)
     {
-        cout << fixed << setprecision(2) << arr[i].w << '\t' << arr[i].x << '\t' << arr[i].y << '\t' << arr[i].z << '\t' << arr[i].op << '\n';
+        std::cout << std::fixed << std::setprecision(2) << vec[i].w << '\t' << vec[i].x << '\t' << vec[i].y << '\t' << vec[i].z << '\t' << vec[i].op << '\n';
     }
 
-    cout << "\nEnter the feature values of the unknown point: \nw\tx\ty\tz\n";
-    cin >> p.w >> p.x >> p.y >> p.z;
+    std::cout << "\nEnter the feature values of the unknown point: \nw\tx\ty\tz\n";
+    std::cin >> p.w >> p.x >> p.y >> p.z;
 
-    // Measuring the Euclidean distance
-    for (i = 0; i < n; i++) 
+    float euclideanDistance (Point p, Point v);
+    for (int i = 0; i < n; i++)
     {
-        arr[i].distance = sqrt(((arr[i].w - p.w) * (arr[i].w - p.w)) + ((arr[i].x - p.x) * (arr[i].x - p.x)) + ((arr[i].y - p.y) * (arr[i].y - p.y)) + ((arr[i].z - p.z) * (arr[i].z - p.z)));
+        vec[i].distance = euclideanDistance(p, vec[i]);
     }
 
     // Sorting the training data with respect to distance
-    for (i = 1; i < n; i++)
+    for (int i = 1; i < n; i++)
     {
-        for (j = 0; j < n - i; j++) 
+        for (int j = 0; j < n - i; j++)
         {
-            if (arr[j].distance > arr[j + 1].distance)
+            if (vec[j].distance > vec[j + 1].distance)
             {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                Point temp;
+                temp = vec[j];
+                vec[j] = vec[j + 1];
+                vec[j + 1] = temp;
             }
         }
     }
 
-    cout << "\nw\tx\ty\tz\top\tdistance\n";
-    for (i = 0; i < n; i++)
+    std::cout << "\nw\tx\ty\tz\top\tdistance\n";
+    for (int i = 0; i < n; i++)
     {
-        cout << fixed << setprecision(2) << arr[i].w << '\t' << arr[i].x << '\t' << arr[i].y << '\t' << arr[i].z << '\t' << arr[i].op << '\t' << arr[i].distance << '\n';
+        std::cout << std::fixed << std::setprecision(2) << vec[i].w << '\t' << vec[i].x << '\t' << vec[i].y << '\t' << vec[i].z << '\t' << vec[i].op << '\t' << vec[i].distance << '\n';
     }
 
     // Taking the K nearest neighbors
-    cout << "\nNumber of nearest neighbors(k): ";
-    cin >> k;
+    std::cout << "\nNumber of nearest neighbors(k): ";
+    std::cin >> k;
 
     int freq[1000] = {0};
     int index = 0, maxfreq = 0;
@@ -72,21 +82,28 @@ int main()
     // Creating frequency array of the class of k nearest neighbors
     for (int i = 0; i < k; i++)
     {
-        freq[arr[i].op]++;
+        freq[vec[i].op]++;
     }
 
     // Finding the most frequent occurring class
-    for (int i = 0; i < 1000; i++) 
+    for (int i = 0; i < 1000; i++)
     {
-        if(freq[i] > maxfreq) 
+        if(freq[i] > maxfreq)
         {
             maxfreq = freq[i];
             index = i;
         }
     }
 
-    cout << "The class of unknown point is " << index;
+    std::cout << "The class of the unknown point is " << index;
     return 0;
+}
+
+// Measuring the Euclidean distance
+float euclideanDistance (Point p, Point v) {
+    float result;
+    result = sqrt(((v.w - p.w) * (v.w - p.w)) + ((v.x - p.x) * (v.x - p.x)) + ((v.y - p.y) * (v.y - p.y)) + ((v.z - p.z) * (v.z - p.z)));
+    return result;
 }
 
 // Sample Output
