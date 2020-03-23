2a73bc57cbb2c0494b35a7a4834dc76ea41e09ef
jainvaidehi <54447956+jainvaidehi@users.noreply.github.com>
2020-03-23 17:41:13 -0400

    Update insertion_sort.cs

--- code/sorting/src/insertion_sort/insertion_sort.cs
+++ code/sorting/src/insertion_sort/insertion_sort.cs
@@ -21,10 +21,13 @@ namespace ConsoleApplication1
             }
             insertsort(arr, 5);
             Console.WriteLine("The Sorted Array is :");
+            // A utility loop to print an array of size 5
             for (i = 0; i < 5; i++)
                 Console.WriteLine(arr[i]); 
             Console.ReadLine();
-        }        
+        }  
+         // Function to sort array 
+    // using insertion sort
         static void insertsort(int[] data, int n)
         {
             int i, j;
@@ -32,6 +35,10 @@ namespace ConsoleApplication1
             {
                 int item = data[i];
                 int ins = 0;
+                // Move elements of data[0..i-1], 
+            // that are greater than item, 
+            // to one position ahead of 
+            // their current position 
                 for (j = i - 1; j >= 0 && ins != 1; )
                 {
                     if (item < data[j])
