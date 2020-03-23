9b0a14e8d97b99f17bbfa7e6f8085bbf382ee411
Hari Krishnan U <harikrishnan6336@cet.ac.in>
2020-03-23 11:13:36 +0530

    Update longest_common_subsequence.js

--- code/dynamic_programming/src/longest_common_subsequence/longest_common_subsequence.js
+++ code/dynamic_programming/src/longest_common_subsequence/longest_common_subsequence.js
@@ -1,35 +1,30 @@
 const longestCommonSubsequence = (arr1, arr2) => {
+  let LCS_def = Array.from({ length: arr1.length + 1 }, () =>
+    Array.from({ length: arr2.length + 1 }, () => 0)
+  );
 
-    let LCS_def = Array.from({ length: arr1.length + 1 },
-        () => Array.from({ length: arr2.length + 1 },
-            () => 0));
-    
-    for (let i = 0; i < arr1.length + 1; i++){
-        for (let j = 0; j < arr2.length+1; j++){
-            
-            if (i == 0 || j == 0) {
-                LCS_def[i][j] = 0;
-            }
+  for (let i = 0; i < arr1.length + 1; i++) {
+    for (let j = 0; j < arr2.length + 1; j++) {
+      if (i == 0 || j == 0) {
+        LCS_def[i][j] = 0;
+      }
 
-            // when the last character of both match, increase length of lcs by 1
-            else if (arr1[i - 1] == arr2[j - 1]) {
-                LCS_def[i][j] = LCS_def[i - 1][j - 1] + 1;
-            }
-                
-            /* 
+      // when the last character of both match, increase length of lcs by 1
+      else if (arr1[i - 1] == arr2[j - 1]) {
+        LCS_def[i][j] = LCS_def[i - 1][j - 1] + 1;
+      } else {
+
+      /* 
             when the last character is not same, take maximum obtained by adding 
             one character to one of the subsequences.
             */
-            else {
-                LCS_def[i][j] = Math.max(LCS_def[i - 1][j], LCS_def[i][j - 1])
-            }
-        }
+        LCS_def[i][j] = Math.max(LCS_def[i - 1][j], LCS_def[i][j - 1]);
+      }
     }
-    
-    return  LCS_def[arr1.length][arr2.length];
-    
-}
+  }
 
+  return LCS_def[arr1.length][arr2.length];
+};
 
 // I/P and O/P Examples
 
