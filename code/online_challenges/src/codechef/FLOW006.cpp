8a2eb22287295f2b4ae10445e08ef89d7511ce16
shivani789 <53372454+shivani789@users.noreply.github.com>
2020-03-23 17:19:25 +0530

    Update FLOW006.cpp

--- code/online_challenges/src/codechef/FLOW006.cpp
+++ code/online_challenges/src/codechef/FLOW006.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
+
 using namespace std;
+
 int main()
  {
+	
 	int t,n;
+	
 	//Input number of testcases-->t
 	cin>>t;
+	
 	while(t>0)
 	{
-	    cin>>n;
-		//Initialise sum with 0
-	     int sum=0;
-	    while(n!=0)
-	    {
-	     sum = sum + n % 10;
-	    n = n/10;
-	    }
-	    cout<<sum<<'\n';
-	    t--;
+		cin>>n;
+		//Initialise sum with 
+		int sum=0;
+		
+		while(n!=0)
+		{
+			sum = sum + n % 10;
+			n = n/10;
+		}
+		
+		cout<<sum<<'\n';
+		t--;
+		
 	}
 	return 0;
 }
