7e491c3fea3c71be14b30dcb9821d6a8ec44643f
Hari Krishnan U <harikrishnan6336@cet.ac.in>
2020-03-23 11:20:18 +0530

    Update longest_common_subsequence.c

--- code/dynamic_programming/src/longest_common_subsequence/longest_common_subsequence.c
+++ code/dynamic_programming/src/longest_common_subsequence/longest_common_subsequence.c
@@ -6,37 +6,41 @@ changing the order of the remaining elements.*/
 // Function that computes the longest commmon subsequence between two arrays of numbers
 // Input: First Array, Second Array, Size of First Array, Size of Second Array
 // Output: Longest Common Subsequnce
-int longestCommonSubsequence(int arr1[], int arr2[], int size1, int size2) {
-    
-    // Declaring a 2-D array LCS where LCS[i][j] denotes the longest common subsequence of 
+int longestCommonSubsequence(int arr1[], int arr2[], int size1, int size2)
+{
+    // Declaring a 2-D array LCS where LCS[i][j] denotes the longest common subsequence of
     // the two arrays until the ith element of the first array and the jth element of the second array
     int LCS[size1 + 1][size2 + 1];
-    for (int i = 0; i <= size1; ++i) {
-        for (int j= 0; j <= size2; ++j) {
-            
+    for (int i = 0; i <= size1; ++i)
+    {
+        for (int j = 0; j <= size2; ++j)
+        {
+
             // Scenario 1: putting a 0 if either of the index is 0
-            if (i == 0 || j == 0) {
+            if (i == 0 || j == 0)
+            {
                 LCS[i][j] = 0;
-            }             
-            
+            }
+
             // Scenario 2: ith element of the first array is equal to jth element of array of second array
-            else if (arr1[i - 1] == arr2[j - 1]) {
+            else if (arr1[i - 1] == arr2[j - 1])
+            {
                 LCS[i][j] = LCS[i - 1][j - 1] + 1;
-            }             
-            
+            }
+
             // Scenario 3: ith element of the first array is not equal to jth element of array of second array.
             // In this case we chose the max between LCS[i - 1][j] and LCS[i][j - 1]
-            else {
-                LCS[i][j] = LCS[i - 1][j] > LCS[i][j - 1] ? 
-                            LCS[i - 1][j] : LCS[i][j - 1];
+            else
+            {
+                LCS[i][j] = LCS[i - 1][j] > LCS[i][j - 1] ? LCS[i - 1][j] : LCS[i][j - 1];
             }
         }
     }
     return LCS[size1][size2];
 }
 
-int main() 
-{ 
+int main()
+{
     int size1, size2;
 
     scanf("%d", &size1);
@@ -44,18 +48,19 @@ int main()
 
     int arr1[size1], arr2[size2];
 
-    for (int i = 0; i < size1; ++i) {
+    for (int i = 0; i < size1; ++i)
+    {
         scanf("%d", &arr1[i]);
     }
-    for (int j = 0; j < size2; ++j) {
+    for (int j = 0; j < size2; ++j)
+    {
         scanf("%d", &arr2[j]);
     }
 
     int l = longestCommonSubsequence(arr1, arr2, size1, size2);
     printf("Length of Longest Common Subsequence is: %d\n", l);
-    return 0; 
+    return 0;
 }
-
 
 /*
 Sample Input: 
