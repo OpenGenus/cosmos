c22a87012b99e69731b362ae0cb012e187b8e752
HariniJeyaraman <58400069+HariniJeyaraman@users.noreply.github.com>
2020-03-24 10:52:21 +0530

    updated with 4 spaces

--- code/unclassified/src/unique_number/unique_number.cpp
+++ code/unclassified/src/unique_number/unique_number.cpp
@@ -4,28 +4,28 @@
 #include <vector>
 
 void display_unique(std::vector<int> arr, int n) {
-  sort(arr.begin(), arr.end());
-  for (int i = 0; i < n; i++) {
-    while (i < n - 1 && arr[i] == arr[i + 1])
-      i++;
-    std::cout << arr[i] << " ";
-  }
+    sort(arr.begin(), arr.end());
+    for (int i = 0; i < n; i++) {
+        while (i < n - 1 && arr[i] == arr[i + 1])
+            i++;
+        std::cout << arr[i] << " ";
+    }
 }
 
 int main() {
-  int n, val;
-  std::cout << "Enter the size of array : \n";
-  std::cin >> n;
-  std::vector<int> arr;
-  cout << "Enter the values for the array : \n";
-  for (int i = 0; i < n; i++) {
-    std::cin >> val;
-    arr.push_back(val);
-  }
-  std::cout << "The unique elements are : \n";
-  display_unique(arr, n);
+    int n, val;
+    std::cout << "Enter the size of array : \n";
+    std::cin >> n;
+    std::vector<int> arr;
+    cout << "Enter the values for the array : \n";
+    for (int i = 0; i < n; i++) {
+        std::cin >> val;
+        arr.push_back(val);
+    }
+    std::cout << "The unique elements are : \n";
+    display_unique(arr, n);
 
-  return 0;
+    return 0;
 }
 
 /*Sample Input- Output
