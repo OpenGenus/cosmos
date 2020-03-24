d9bba24832186c757655aa2f361b19d489dbf62b
HariniJeyaraman <58400069+HariniJeyaraman@users.noreply.github.com>
2020-03-25 02:14:25 +0530

    used preincrement and used vector

--- move_zeroes_to_end.cpp
+++ move_zeroes_to_end.cpp
@@ -1,19 +1,20 @@
+#include <cstdlib>
 #include <iostream>
-#include <stdlib.h>
+#include <vector>
 
-void move_zeroes(int arr[], int a[], int n) {
+void move_zeroes(std::vector<int> arr, std::vector<int> a, int n) {
     int k = 0;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; ++i) {
         if (a[i] != 0) {
-            arr[k] = a[i];
+            arr.push_back(a[i]);
             k++;
         }
     }
-    for (int i = k; i < n; i++) {
-        arr[i] = 0;
+    for (int i = k; i < n; ++i) {
+        arr.push_back(0);
     }
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; ++i)
         std::cout << arr[i] << " ";
 }
 
@@ -21,19 +22,22 @@ int main() {
     int n;
     std::cout << "Enter the size of array : \n";
     std::cin >> n;
-    int a[n], arr[n];
+    std::vector<int> a, arr;
+    int val;
     std::cout << "Enter the values for the array : \n";
     for (int i = 0; i < n; i++) {
-        std::cin >> a[i];
+        std::cin >> val;
+        a.push_back(val);
     }
     move_zeroes(arr, a, n);
     return 0;
 }
 
 /*Sample Input-Output
-Enter the size of array : 
+Enter the size of array :
 8
-Enter the values for the array : 
+Enter the values for the array :
 12 0 18 13 0 0 11 0
 12 18 13 11 0 0 0 0
 */
+
