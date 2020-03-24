1485b4c610273c462b7e26bf42ebccb87db7b482
shivani789 <53372454+shivani789@users.noreply.github.com>
2020-03-24 21:02:21 +0530

    Update solve_me_first.cpp

--- code/online_challenges/src/hackerrank/solve_me_first.cpp
+++ code/online_challenges/src/hackerrank/solve_me_first.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
 int solveMeFirst(int a, int b) {
-  int c;
-  c = a + b;
-  return c;
+      int c;
+      c = a + b;
+      return c;
 }
 
 int main() {
-  int num1, num2;
-  int sum;
-  std::cin >> num1 >> num2;
-  sum = solveMeFirst(num1, num2);
-  std::cout << sum;
-  return 0;
+      int num1, num2;
+      int sum;
+      std::cin >> num1 >> num2;
+      sum = solveMeFirst(num1, num2);
+      std::cout << sum;
+      return 0;
 }
