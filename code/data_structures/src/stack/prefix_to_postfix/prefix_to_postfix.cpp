2e855d5ecdde2f2293df473fdef9c9e50980e1ba
Tanushree Aggarwal <47485195+shreetanu@users.noreply.github.com>
2020-03-26 02:49:30 +0530

    Prefix to Postfix in C++

---
+++ code/data_structures/src/stack/prefix_to_postfix/prefix_to_postfix.cpp
@@ -0,0 +1,61 @@
+#include <iostream> 
+#include <stack> 
+#include <string>
+using namespace std; 
+
+// funtion to check if character is operator or not 
+bool isOperator(char c) { 
+switch (c) { 
+case '+': 
+case '-': 
+case '/': 
+case '*': 
+	return true; 
+} 
+return false; 
+} 
+
+// Convert prefix to Postfix expression 
+string PreToPost(string pre_exp) { 
+
+stack<string> s; 
+
+int length = pre_exp.size(); 
+
+ 
+for (int i = length - 1; i >= 0; i--) { 
+
+	// check if symbol is operator 
+	if (isOperator(pre_exp[i])) { 
+
+	// pop two operands from stack 
+	string op1 = s.top(); 
+	        s.pop(); 
+	string op2 = s.top(); 
+	        s.pop(); 
+
+	 
+	string temp = op1 + op2 + pre_exp[i]; 
+
+	// Push string temp back to stack 
+	s.push(temp); 
+	} 
+
+	// if symbol is an operand 
+	else { 
+
+	// push the operand to the stack 
+	s.push(string(1, pre_exp[i])); 
+	} 
+} 
+
+
+return s.top(); 
+} 
+
+int main() { 
+string pre_exp;
+cin>>pre_exp;
+cout << "Postfix : " << PreToPost(pre_exp); 
+return 0; 
+} 
