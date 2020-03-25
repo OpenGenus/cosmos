a2012d40a8676efd5213f3e9ad778aa50ca86382
kuljeet singh <48275443+Kuljeet-123@users.noreply.github.com>
2020-03-25 19:36:49 +0530

    Update LAZERTST.cpp

--- code/online_challenges/src/codechef/LAZERTST/LAZERTST.cpp
+++ code/online_challenges/src/codechef/LAZERTST/LAZERTST.cpp
@@ -1,53 +1,65 @@
-#include<bits/stdc++.h>
+#include<iostream>
 
 using namespace std;
 
 typedef long long ll;
 
-void solve(){
+void solve()
+{
     ll n,m,k,q;
     cin>>n>>m>>k>>q;
     vector<pair<int,int>> qs(q);
     vector<ll> ans(q,0);
-    for (int i = 0; i < q; i++) {
+    for (int i = 0; i < q; i++)
+    {
         cin>>qs[i].first>>qs[i].second;
     }
-    if(k==3){
+    if(k==3)
+    {
         cout<<2<<' ';
-        for (int i = 0; i < q; i++) {
+        for (int i = 0; i < q; i++)
+        {
             cout<<m-1<<' ';
         }
         cout<<endl;
         int h;
         cin>>h;
-        if(h==-1){
+        if(h==-1)
+        {
             exit(1);
         }
-    } else if(k==100){
+    } 
+    else if(k==100)
+    {
         ll gm = 0;
-        for (int i = 0; i < q; i++) {
+        for (int i = 0; i < q; i++)
+        {
             cout<<1<<' '<<qs[i].first<<' '<<qs[i].second<<' '<<(m*i)/10<<endl;
             ll h;
             cin>>h;
-            if(h==-1){
+            if(h==-1)
+            {
                 exit(1);
             }
             gm = max(gm,h);
         }
         cout<<2<<' ';
-        for (int i = 0; i < q; i++) {
+        for (int i = 0; i < q; i++)
+        {
             cout<<gm<<' ';
         }
         cout<<endl;
         int h;
         cin>>h;
-        if(h==-1){
+        if(h==-1)
+        {
             exit(1);
         }
     }
 }
 
-int main(){
+int main()
+{
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     int t;
