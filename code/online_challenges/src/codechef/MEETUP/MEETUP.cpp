f07d95f06d248e8ac02609f1514d366e57a81bef
kuljeet singh <48275443+Kuljeet-123@users.noreply.github.com>
2020-03-25 19:31:23 +0530

    Update MEETUP.cpp

--- code/online_challenges/src/codechef/MEETUP/MEETUP.cpp
+++ code/online_challenges/src/codechef/MEETUP/MEETUP.cpp
@@ -25,13 +25,18 @@ using namespace std;
 #define cerr _cer
 struct _cert
 {
-    template <typename T> _cert& operator << (T) { return *this; }
+    template <typename T> _cert& operator << (T) 
+	{
+	    return *this;
+        }
 };
 _cert _cer;
 #endif
 
-template <typename T> void dprint(T begin, T end) {
-    for (auto i = begin; i != end; i++) {
+template <typename T> void dprint(T begin, T end)
+{
+    for (auto i = begin; i != end; i++)
+    {
 		cerr << (*i) << " ";
     }
     cerr << "\n";
@@ -48,68 +53,83 @@ set<int> eds2[1200];
 string s1[1200];
 string s2[1200];
 
-int rd1() {
+int rd1()
+{
 	scanf(" %s", buf);
 	if (mm1.count(buf))
 		return mm1[buf];
-	else {
+	else
+	{
 		s1[cc1] = buf;
 		return mm1[buf] = cc1++;
 	}
 }
 
-int rd2() {
+int rd2()
+{
 	scanf(" %s", buf);
 	if (mm2.count(buf))
 		return mm2[buf];
-	else {
+	else
+	{
 		s2[cc2] = buf;
 		return mm2[buf] = cc2++;
 	}
 }
 
 
-void del1(int v) {
-	for (int i: eds1[v]) {
+void del1(int v)
+{
+	for (int i: eds1[v])
+	{
 		eds1[i].erase(v);
 	}
 	eds1[v].clear();
 }
 
-void del2(int v) {
-	for (int i: eds2[v]) {
+void del2(int v)
+{
+	for (int i: eds2[v])
+	{
 		eds2[i].erase(v);
 	}
 	eds2[v].clear();
 }
 
-int ask1(int v) {
+int ask1(int v)
+{
 	cout << "A " << s1[v] << "\n";
 	cout.flush();
 	return rd2();
 }
 
-int ask2(int v) {
+int ask2(int v)
+{
 	cout << "B " << s2[v] << "\n";
 	cout.flush();
 	return rd1();
 }
 
-void solve(vector<int> av1, vector<int> av2) {
+void solve(vector<int> av1, vector<int> av2)
+{
 	set<int> ss;
 	for (int i: av1)
 		if (i < ka)
 			ss.insert(eds1[i].size());
 	int bst = -1;
-	for (int i: av2) {
-		if (i < kb) {
-			if (ss.count(eds2[i].size())) {
+	for (int i: av2)
+	{
+		if (i < kb)
+		{
+			if (ss.count(eds2[i].size()))
+			{
 				bst = eds2[i].size();
 				break;
 			}
 		}
 	}
-	if (bst == -1) {
+	if (bst == -1)
+	{
 		cout << "C No\n";
 		cout.flush();
 		exit(0);
@@ -117,10 +137,13 @@ void solve(vector<int> av1, vector<int> av2) {
 	int n = av1.size();
 	int c1 = bst;
 	int c2 = n - 1 - bst;
-	if (c1 < c2) {
+	if (c1 < c2)
+	{
 		int a = -1;
-		for (int i: av1) {
-			if (eds1[i].size() == bst && i < ka) {
+		for (int i: av1)
+		{
+			if (eds1[i].size() == bst && i < ka)
+			{
 				a = i;
 				break;
 			}
@@ -134,27 +157,34 @@ void solve(vector<int> av1, vector<int> av2) {
 		vector<int> avi;
 		vector<int> avii;
 		for (int i: av1)
-			if (i != a && eds1[a].count(i) == 1) {
+			if (i != a && eds1[a].count(i) == 1)
+			{
 				avi.push_back(i);
 			}
-			else if (i != a) {
+			else if (i != a)
+			{
 				del1(i);
 			}
 		del1(a);
 		for (int i: av2)
-			if (i != b && eds2[b].count(i) == 1) {
+			if (i != b && eds2[b].count(i) == 1)
+			{
 				avii.push_back(i);
 			}
-			else if (i != b) {
+			else if (i != b)
+			{
 				del2(i);
 			}
 		del2(b);
 		solve(avi, avii);
 	}
-	else {
+	else
+	{
 		int b = -1;
-		for (int i: av2) {
-			if (eds2[i].size() == bst && i < kb) {
+		for (int i: av2)
+		{
+			if (eds2[i].size() == bst && i < kb)
+			{
 				b = i;
 				break;
 			}
@@ -168,18 +198,22 @@ void solve(vector<int> av1, vector<int> av2) {
 		vector<int> avi;
 		vector<int> avii;
 		for (int i: av1)
-			if (i != a && eds1[a].count(i) == 0) {
+			if (i != a && eds1[a].count(i) == 0)
+			{
 				avi.push_back(i);
 			}
-			else if (i != a) {
+			else if (i != a)
+			{
 				del1(i);
 			}
 		del1(a);
 		for (int i: av2)
-			if (i != b && eds2[b].count(i) == 0) {
+			if (i != b && eds2[b].count(i) == 0)
+			{
 				avii.push_back(i);
 			}
-			else if (i != b) {
+			else if (i != b)
+			{
 				del2(i);
 			}
 		del2(b);
@@ -188,11 +222,13 @@ void solve(vector<int> av1, vector<int> av2) {
 }
 int n, m;
 
-int main() {
+int main()
+{
 	scanf("%d%d%d%d", &n, &m, &ka, &kb);
 	for (int i = 0; i < n; ++i)
 		assert(rd1() == i);
-	for (int j = 0; j < m; ++j) {
+	for (int j = 0; j < m; ++j)
+	{
 		int a = rd1();
 		int b = rd1();
 		eds1[a].insert(b);
@@ -200,7 +236,8 @@ int main() {
 	}
 	for (int i = 0; i < n; ++i)
 		assert(rd2() == i);
-	for (int j = 0; j < m; ++j) {
+	for (int j = 0; j < m; ++j)
+	{
 		int a = rd2();
 		int b = rd2();
 		eds2[a].insert(b);
