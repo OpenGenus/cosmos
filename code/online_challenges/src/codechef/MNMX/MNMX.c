1a2413e8021012687cf26d8015694a46c930f29c
Ritish Singh <54978105+ritish099@users.noreply.github.com>
2020-03-25 20:37:40 +0530

    Update MNMX.c

--- code/online_challenges/src/codechef/MNMX/MNMX.c
+++ code/online_challenges/src/codechef/MNMX/MNMX.c
@@ -1,38 +1,27 @@
 #include <stdio.h>
-int main()
-{
-	int T;
-	scanf("%d", &T);
-	while (T--)
-	{
-		long long int N, i;
-		scanf("%lld", &N);
-		long long int a[N];
-		for (i = 0; i < N; i++)
-		{
-			scanf("%lld", &a[i]);
-		}
-		long long int min = a[0];
-		for (i = 0; i < N; i++)
-		{
-			if (a[i] < min)
-			{
-				min = a[i];
-			}
-		}
-		printf("%lld\n", min * (N - 1));
-	}
-	return 0;
-}
-/*
-Input
-2
-2
-3 4
-3
-4 2 5
 
-Output
-3
-4
-*/
+int main() 
+{
+    int t;
+    scanf("%d", & t);
+    while (t--) 
+    {
+        long long int n, i;
+        scanf("%lld", & n);
+        long long int a[n];
+        for (i = 0; i < n; ++i) 
+        {
+            scanf("%lld", & a[i]);
+        }
+        long long int min = a[0];
+        for (i = 0; i < n; ++i) 
+        {
+            if (a[i] < min) 
+            {
+                min = a[i];
+            }
+        }
+        printf("%lld\n", min * (n - 1));
+    }
+    return 0;
+}
