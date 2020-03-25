b015e55ffee52c4f8acec79ca49b4102e591fb7b
kuljeet singh <48275443+Kuljeet-123@users.noreply.github.com>
2020-03-25 19:23:20 +0530

    Update HILLJUMP.cpp

--- code/online_challenges/src/codechef/HILLJUMP/HILLJUMP.cpp
+++ code/online_challenges/src/codechef/HILLJUMP/HILLJUMP.cpp
@@ -1,16 +1,23 @@
 #include<iostream>
+
 using namespace std;
+
 void jump(int[], int);
 void inc(int[], int);
 
-int main(){
+int main()
+{
 	std::ios::sync_with_stdio(false);
 	//int j;for(j=0;j<100;j++)printf("1 ");
 	int n, q;	cin>>n>>q;
 	int arr[n]; arr[0]=0;
-	int i; for(i=0;i<n;i++){ int temp; cin>>temp; arr[i]+=temp; arr[i+1]=-temp;	}
+	int i; for(i=0;i<n;i++)
+	{ 
+		int temp; cin>>temp; arr[i]+=temp; arr[i+1]=-temp;
+	}
 
-	while(q--){
+	while(q--)
+	{
 		int a;
 		cin>>a;
 		if(a==1) jump(arr, n);
@@ -20,16 +27,19 @@ return 0;
 }
 
  
-void jump(int arr[], int n){
+void jump(int arr[], int n)
+{
 int start, num, curr;
 cin>>start>>num;
 curr = start-1;
 int i; int sum=0;
 
 int f=1;
-while(num-- && f==1){
+while(num-- && f==1)
+{
 	int i; f=0; int sum=0;
-	for(i = curr+1; i<(n<curr+101?n:curr+101); i++)	{
+	for(i = curr+1; i<(n<curr+101?n:curr+101); i++)
+	{
 	sum+=arr[i];
 	if(sum>0){curr = i;f=1;break;} 
 	}	
@@ -37,7 +47,8 @@ while(num-- && f==1){
 cout<<curr+1<<endl;
 }
  
-void inc(int arr[], int n){
+void inc(int arr[], int n)
+{
 int start, end, lol;
 cin>>start>>end>>lol;
 arr[start-1]+=lol; if(end<n)arr[end]-=lol;
