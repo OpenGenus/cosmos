f9b907e66fc586e6af6e0e06dd7c3c4824d91c54
HariniJeyaraman <58400069+HariniJeyaraman@users.noreply.github.com>
2020-03-25 21:05:37 +0530

    Sum of numbers in a string

---
+++ sumNumbersString.cpp
@@ -0,0 +1,35 @@
+// Given a string containing alphanumeric characters, calculate sum of all
+// numbers present in the string.
+#include <cctype>
+#include <cstdlib>
+#include <iostream>
+
+int sum_numbers_string(string s) {
+    int sum = 0;
+    string str = "";
+    for (int i = 0; i < s.length(); i++) {
+        if (isdigit(s[i])) {
+            str += s[i];
+            if (!isdigit(s[i + 1])) {
+                int n = stoi(str);
+                sum += n;
+                str = "";
+            }
+        }
+    }
+    return sum;
+}
+
+int main() {
+    string s;
+    std::cout << "Enter the string :  ";
+    std::cin >> s;
+    std::cout << "The sum of numbers present in the string :  "
+              << sum_numbers_string(s);
+    return 0;
+}
+
+/*Sample Input-Output
+Enter the string :  this12is3just67another1string2
+The sum of numbers present in the string :  85
+*/
