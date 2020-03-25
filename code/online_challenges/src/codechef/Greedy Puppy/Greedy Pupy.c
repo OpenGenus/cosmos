8882f12ac65d73885046caa8a1a4a2e8f4ffa17f
Ritish Singh <54978105+ritish099@users.noreply.github.com>
2020-03-25 21:21:42 +0530

    Update Greedy Pupy.c

--- code/online_challenges/src/codechef/Greedy Puppy/Greedy Pupy.c
+++ code/online_challenges/src/codechef/Greedy Puppy/Greedy Pupy.c
@@ -9,7 +9,7 @@ int main(void)
         int n, k;
         scanf("%d%d", & n, & k);
         int i, max = 0, c;
-        for (i = 2; i <= k; i++) 
+        for (i = 2; i <= k; ++i) 
         {
             c = n % i;
             if (c >= max) 
