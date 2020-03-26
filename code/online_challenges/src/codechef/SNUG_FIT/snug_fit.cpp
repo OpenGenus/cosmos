74001cb15c89a595ed6bd830c5e401b97f3ac529
Ankit Kurmi <ankitkurmi152@gmail.com>
2020-03-26 21:20:27 +0530

    made few changes

--- code/online_challenges/src/codechef/SNUG_FIT/snug_fit.cpp
+++ code/online_challenges/src/codechef/SNUG_FIT/snug_fit.cpp
@@ -12,7 +12,8 @@ int main()
     int n, i, sum = 0;
     cin >> n;
     int a[n], b[n];
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) 
+    {
       cin >> a[i];
     }
     for (i = 0; i < n; i++) 
