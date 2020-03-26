51185f53b30473dec2984b25f95890dd519b0b20
yuktha-05 <58894439+yuktha-05@users.noreply.github.com>
2020-03-25 23:28:22 -0700

    Create Sum_of_numbers_string.py

---
+++ Sum_of_numbers_string.py
@@ -0,0 +1,22 @@
+// ADDING ALL NUMBERS IN A STRING.
+
+CODE:
+
+st = input("Enter a string: ")
+a = "" 
+Sum = 0
+for i in st: 
+    if (i.isdigit()): 
+        a = a + i
+    else: 
+        Sum = Sum + int(a) 
+        a = "0"
+print(Sum + int(a))
+
+// INPUT:
+Enter a string: 567hdon2
+
+// OUTPUT:
+569
+
+
