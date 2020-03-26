78c26a193a74e979f0bb448a827780adf8b2b363
ka1shi <nikitaaggrawal0@gmail.com>
2020-03-26 10:43:12 +0530

    Code Reformatting

--- code/online_challenges/src/codechef/COINS/COINS.py
+++ code/online_challenges/src/codechef/COINS/COINS.py
@@ -1,25 +1,22 @@
-a = {};
+a = {}
 
 def dollar(n):
-    
-    if(n<=11):
-        a[n]=n;
-        return a[n];
-        
-    if n in a:
-        return a[n];
-        
-    a[n] = max(n, dollar(int(n/2))+dollar(int(n/3))+dollar(int(n/4)));
-    return a[n];
 
-while(True):
+    if n <= 11:
+        a[n] = n
+        return a[n]
+    if n in a:
+        return a[n]
+    a[n] = max(n, dollar(int(n / 2)) + dollar(int(n / 3)) + dollar(int(n / 4)))
+    return a[n]
+
+while True:
     try:
-        n = int(input());
+        n = int(input())
     except EOFError:
-        break;
-        
-    if(n<=11):
-        print(n);
+        break
+    if n <= 11:
+        print(n)
     else:
-        print(dollar(n));
-    
+        print(dollar(n))
+
