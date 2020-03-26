82dd02c26fd3026ac827a1fcb5352e424ff1872d
Ramchandra Joshi <rbjoshi1999@gmail.com>
2020-03-26 12:35:24 +0530

    fixing indentation and camel case

--- code/bit_manipulation/src/clear_bits_from_msb/clear_bits_from_msb.py
+++ code/bit_manipulation/src/clear_bits_from_msb/clear_bits_from_msb.py
@@ -1,23 +1,25 @@
-'''
+"""
 Problem Statement :
 Clear all MSB's except for the first i LSB's of a number n.
 MSB -> Most Significant Bit
 LSB -> Most Significant Bit 
-'''
+"""
 
-def clearBitsFromMsb(n, i):
-	mask = (1 << i) - 1
-	return n & mask    
 
-if __name__=="__main__": 
-	print("Enter n and i : ")
-	n, i = map(int, input().split())
+def clear_bits_from_msb(n, i):
+    mask = (1 << i) - 1
+    return n & mask
 
-	n = clearBitsFromMsb(n, i)
-	print("Result : " + str(n))
 
-'''
+if __name__ == "__main__":
+    print("Enter n and i : ")
+    n, i = map(int, input().split())
+
+    n = clear_bits_from_msb(n, i)
+    print("Result : " + str(n))
+
+"""
 Enter n and i :
 14 3
 Result : 6
-'''
+"""
