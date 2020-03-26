15942cca7cd128ac39f7a9a4fa4bb3078fee0ad0
Ramchandra Joshi <rbjoshi1999@gmail.com>
2020-03-27 02:44:40 +0530

    fixing indentation

--- code/dynamic_programming/src/longest_common_subsequence/longest_common_subsequence.c
+++ code/dynamic_programming/src/longest_common_subsequence/longest_common_subsequence.c
@@ -10,40 +10,40 @@ LCS -> Longest Common Subsequence
 
 int max(int a, int b) 
 {
-	return (a > b)? a : b; 
+    return (a > b)? a : b; 
 }
 
 int lcs(char *x, char *y, int x_len, int y_len) 
 {
-	int dp[x_len + 1][y_len + 1]; 
+    int dp[x_len + 1][y_len + 1]; 
 
-	for (int i = 0; i <= x_len; ++i) 
-	{ 
-		for (int j = 0; j <= y_len; ++j) 
-		{ 
-			if (i == 0 || j == 0) 
-				dp[i][j] = 0; 
-			else if (x[i - 1] == y[j - 1]) 
-				dp[i][j] = dp[i - 1][j - 1] + 1; 
-			else
-				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]); 
-		} 
-	} 
+    for (int i = 0; i <= x_len; ++i) 
+    { 
+        for (int j = 0; j <= y_len; ++j) 
+        { 
+            if (i == 0 || j == 0) 
+                dp[i][j] = 0; 
+            else if (x[i - 1] == y[j - 1]) 
+                dp[i][j] = dp[i - 1][j - 1] + 1;     
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        } 
+    } 
 
-	return dp[x_len][y_len]; 
+    return dp[x_len][y_len]; 
 }
 
 int main() 
 {
-	char a[MAX], b[MAX];
-	printf("Enter two strings : ", a, b);
-	scanf("%s %s", a, b);
+    char a[MAX], b[MAX];
+    printf("Enter two strings : ", a, b);
+    scanf("%s %s", a, b);
 
-	int a_len = strlen(a);
-	int b_len = strlen(b);
-	printf("\nLength of LCS : %d", lcs(a, b, a_len, b_len)); 
+    int a_len = strlen(a);
+    int b_len = strlen(b);
+    printf("\nLength of LCS : %d", lcs(a, b, a_len, b_len)); 
 
-	return 0; 
+    return 0; 
 }
 
 /*
