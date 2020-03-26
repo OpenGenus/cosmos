9ca33c009d7b118c94bb933d2f4c1c6479555078
Ankit Kurmi <43778255+Ankit152@users.noreply.github.com>
2020-03-26 22:58:55 +0530

    Update code/online_challenges/src/codechef/CASH/hard_cash.cpp
    Co-Authored-By: Arnav Borborah <arnavborborah11@gmail.com>

--- code/online_challenges/src/codechef/CASH/hard_cash.cpp
+++ code/online_challenges/src/codechef/CASH/hard_cash.cpp
@@ -8,7 +8,7 @@ int main()
   while (t--) {
     int n, k, x, sum = 0;
     cin >> n >> k;
-    for (int i = 0; i < n; i++) 
+    for (int i = 0; i < n; ++i) 
     {
       cin >> x;
       sum += x;
