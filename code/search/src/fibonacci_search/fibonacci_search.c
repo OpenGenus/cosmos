8d1aba4218f8dd2c6f087f03bb0ab23095975a0b
shreeya13garg <51780388+shreeya13garg@users.noreply.github.com>
2020-03-27 02:39:31 +0530

    changed fibonacci_search.c

--- code/search/src/fibonacci_search/fibonacci_search.c
+++ code/search/src/fibonacci_search/fibonacci_search.c
@@ -1,78 +1,62 @@
-// search | fibonacci search | c
-// Part of Cosmos by OpenGenus Foundation
 
-/*
-ar -> Array name
-n -> Array size
-secondPreceding -> Number at second precedence 
-firstPreceding -> Number at first precedence
-nextNum -> Fibonacci Number equal to or first greatest than the array size
-range -> Eleminated range marker
-*/
-
-#include <stdio.h>
-#include <stdlib.h>
-
-int fibSearch(int ar[], int x, int n);
-int min(int a, int b);
-
-int main()
-{
-	int n;
-	scanf("%d",&n);
-	int ar[n],i;
-	for(i=0;i<n;i++){
-		scanf("%d",&ar[i]);
-	}
-	int x;
-	scanf("%d",&x);
-	int index = fibSearch(ar,x,n);
-	if(index == -1){
-		printf("Element not found.\n");
-		return 0;
-	}
-	printf("Element found at index %d\n",index );
-	return 0;
+#include <stdio.h>  
+int min(int x, int y) 
+{ 
+    if(x<=y)
+        return x;
+    else
+        return y;
 }
-
-int fibSearch(int ar[], int x, int n)
-{
-	int secondPreceding = 0, firstPreceding = 1;
-	int nextNum = secondPreceding + firstPreceding;
-	while(nextNum < n){
-		secondPreceding = firstPreceding;
-		firstPreceding = nextNum;
-		nextNum = secondPreceding + firstPreceding;
-	} 
-	int range = -1;
-	while(nextNum > 1){
-		int i = min(range + firstPreceding, n-1);
-		if(x < ar[i]){
-			nextNum = nextNum - firstPreceding;
-			firstPreceding = firstPreceding - secondPreceding;
-			secondPreceding = nextNum - firstPreceding;
-		}
-		else if(x > ar[i]){
-			nextNum = firstPreceding;
-			firstPreceding = secondPreceding;
-			secondPreceding = nextNum - firstPreceding;
-			range = i;
-		}
-		else{
-			return i;
-		}
-	}
-	if(firstPreceding && ar[range+1] == x){
-		return range + 1;
-	}
-	return -1;
-}
-int min(int a, int b)
-{
-	if(a > b){
-		return b;
-	}
-	else{
-		return a;
-	}
-}
+int fibMonaccianSearch(int arr[], int x, int n) 
+{ 
+    
+    int fibMMm2 = 0;    
+    int fibMMm1 = 1;   
+    int fibM = fibMMm2 + fibMMm1;  
+    while (fibM < n) 
+    { 
+        fibMMm2 = fibMMm1; 
+        fibMMm1 = fibM; 
+        fibM  = fibMMm2 + fibMMm1; 
+    } 
+  
+    int offset = -1; 
+  
+    while (fibM > 1) 
+    { 
+       
+        int i = min(offset+fibMMm2, n-1); 
+  
+        if (arr[i] < x) 
+        { 
+            fibM  = fibMMm1; 
+            fibMMm1 = fibMMm2; 
+            fibMMm2 = fibM - fibMMm1; 
+            offset = i; 
+        } 
+  
+        else if (arr[i] > x) 
+        { 
+            fibM  = fibMMm2; 
+            fibMMm1 = fibMMm1 - fibMMm2; 
+            fibMMm2 = fibM - fibMMm1; 
+        } 
+  
+        
+        else return i; 
+    } 
+  
+    if(fibMMm1 && arr[offset+1]==x)return offset+1; 
+    return -1; 
+} 
+  
+int main(void) 
+{ 
+    int arr[] = {10, 22, 35, 40, 45, 50, 80, 82, 
+                 85, 90, 100}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+    int x = 85; 
+    printf("Found at index: %d", 
+            fibMonaccianSearch(arr, x, n)); 
+    return 0; 
+} 
