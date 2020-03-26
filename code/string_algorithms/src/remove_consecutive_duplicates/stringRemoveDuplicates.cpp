f05459f0b8ca17b0454c292564630f32691ce4cf
abhijeet010304 <abhijeet010304@gmail.com>
2020-03-27 00:42:17 +0530

    added code for consecutive duplicates in string in c++

---
+++ code/string_algorithms/src/remove_consecutive_duplicates/stringRemoveDuplicates.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+std::string stringRemoveDuplicates(std::string s)
+{
+    std::string s1;
+    s1 += s[0];
+    for (int i = 1; i < s.length(); i++) {
+        if (s[i] != s[i - 1])
+            s1 += s[i];
+    }
+    s = s1;
+    return s;
+}
+int main()
+{
+    std::string s, s1;
+    std::cin >> s;
+    s1 = stringRemoveDuplicates(s);
+    std::cout << s1 << "\n";
+}
