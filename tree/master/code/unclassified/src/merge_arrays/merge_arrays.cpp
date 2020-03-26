6878a4abde27beb3fb7e245bcb9ac07866c95ed6
HariniJeyaraman <58400069+HariniJeyaraman@users.noreply.github.com>
2020-03-26 13:16:40 +0530

    used std::vector

--- tree/master/code/unclassified/src/merge_arrays/merge_arrays.cpp
+++ tree/master/code/unclassified/src/merge_arrays/merge_arrays.cpp
@@ -1,16 +1,21 @@
+#include <cstdlib>
 #include <iostream>
-#include <stdlib.h>
+#include <vector>
 
-void merge(int arr[], int arr1[], int arr2[], int n1, int n2) {
+void merge(std::vector<int> arr, std::vector<int> arr1, std::vector<int> arr2) {
     int k = 0;
-    for (int i = 0; i < n1; ++i) {
+    for (int i = 0; i < arr1.size(); ++i) {
         arr[k] = arr1[i];
         k++;
     }
-    for (int i = 0; i < n2; ++i) {
+    for (int i = 0; i < arr2.size(); ++i) {
         arr[k] = arr2[i];
         k++;
     }
+
+    std::cout << "Merged array : \n";
+    for (int i = 0; i < arr.size(); ++i)
+        std::cout << arr[i] << "\t";
 }
 
 int main() {
@@ -19,15 +24,34 @@ int main() {
     std::cin >> n1;
     std::cout << "Enter the size of array 2: \n";
     std::cin >> n2;
-    int arr1[n1], arr2[n2], arr[n1 + n2];
+    std::vector<int> arr1(n1);
+    std::vector<int> arr2(n2);
+    std::vector<int> arr(n1 + n2);
     std::cout << "Enter the values for array1 : \n";
-    for (int i = 0; i < n1; i++)
-        std::cin >> arr1[i];
+    for (int i = 0; i < n1; ++i) {
+        int val;
+        std::cin >> val;
+        arr1[i] = val;
+    }
+
     std::cout << "Enter the values for array2 : \n";
-    for (int i = 0; i < n2; i++)
-        std::cin >> arr2[i];
-    merge(arr, arr1, arr2, n1, n2);
-    std::cout << "Merged array : \n";
-    for (int i = 0; i < n1 + n2; i++)
-        std::cout << arr[i] << "\t";
+    for (int i = 0; i < n2; ++i) {
+        int val;
+        std::cin >> val;
+        arr2[i] = val;
+    }
+    merge(arr, arr1, arr2);
 }
+
+/*Sample Input Output
+Enter the size of array 1 :
+5
+Enter the size of array 2:
+2
+Enter the values for array1 :
+10 12 14 16 18
+Enter the values for array2 :
+-10 -20
+Merged array :
+10	12	14	16	18	-10	-20
+*/
