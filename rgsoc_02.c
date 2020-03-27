7980b33cc4fa33d1dee4dfb72f3c8004d37acfa0
wokira <ira18039@iiitd.ac.in>
2020-03-27 13:47:41 +0530

    C code to MAXDIFF problem

--- rgsoc_02.c
+++ rgsoc_02.c
@@ -1,47 +1,40 @@
-// MAXDIFF in C - CodeChef 
+#include <stdio.h>
 
-#include<stdio.h>
-int main(){
-    int t;
-    scanf("%d",&t);
-    for(int j=0;j<t;j++){
-        int k,n;
-        scanf("%d %d",&n,&k);
-        int arr[n];
-        for(int i=0;i<n;i++){
-            scanf("%d",&arr[i]);}
-        int min;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(arr[i]>=arr[j]){
-                    min=arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=min;
-                }}}
-        int c;
-        c=n/2;
-        if(k<=c){   
-            int sum=0;
-            for(int i=0;i<k;i++){
-                sum=sum+arr[i];}
-            int sum1=0;
-            for(int i=k;i<n;i++){
-                sum1=sum1+arr[i];}
-    
-            int a;
-            a=sum1-sum;
-            printf("\n%d",a);
-        }
-        else {
-            int sum2=0;
-            int sum3=0;
-            for(int i=0;i<n-k;i++){
-                sum2=sum2+arr[i]; }
-            for(int i=n-k;i<n;i++) {
-                sum3=sum3+arr[i];}
-            int b;
-            b=sum3-sum2;
-            printf("\n%d",b);
-        }}
+int main(void) {
+    // Input N and K 
+    int n,k;
+    scanf("%d %d",&n,&k);
+    // Input the weights of n items 
+    int weights[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&weights[i]);}
+
+    // Sorting the weights array 
+    for(int i=0;i<=n-2;i++){
+        int min=i;
+        for(int j=i+1;j<=n-1;j++){
+            if(weights[j]<weights[min])
+            min=j;
+            }
+        int temp;
+        temp=weights[i];
+        weights[i]=weights[min];
+        weights[min]=temp;
+    }
+
+    if(k>n/2)
+    k=n-k;
+    int sum1, sum2;
+    sum1 = 0;
+    sum2=0;
+    // sum of first K numbers 
+    for(int i=0;i<k;i++){
+       sum1+=weights[i];  }
+    // sum of last K numbers 
+    for(int i=k;i<n;i++) {
+        sum2+=weights[i];
+    }
+    printf("%d\n",sum2-sum1);
     return 0;
 }
+
