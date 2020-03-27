6d1fd7a80bb77a495aaa750d76d950f71771a62f
Poorva <60508130+poorva2145@users.noreply.github.com>
2020-03-27 11:49:45 +0530

    Update rotate_a_linked_list_by_k_nodes.cpp

--- code/data_structures/src/list/singly_linked_list/operations/rotate_a_linked_list_by_k_nodes/rotate_a_linked_list_by_k_nodes.cpp
+++ code/data_structures/src/list/singly_linked_list/operations/rotate_a_linked_list_by_k_nodes/rotate_a_linked_list_by_k_nodes.cpp
@@ -5,13 +5,13 @@
 
 class node {
 public:
-    int data;
-    node *next;
+int data;
+node *next;
 
-    node(int data) {
-	this->data = data;
-	next = NULL;
-	}
+ node(int data) {
+ this->data = data;
+  next = NULL;
+ }
 };
 
 node *insert() {
