0d343f8b5be544f8d61354db0ca6b764eec9016f
aryangulati <42711978+aryangulati@users.noreply.github.com>
2020-03-27 21:40:54 +0530

    Created Disjoint Set Union in c++

---
+++ code/graph_algorithms/src/Disjoint_Set_Union_algo.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int long long int
+#define ld long double
+#define F first
+#define S second
+#define pb push_back
+
+const int N = 100001;
+
+int parent[N], sz[N];
+
+// gives superparent of the component
+int get_parent(int x) {
+    if (x == parent[x]) {
+        return x;
+    }
+    else {
+        // path compression
+        return parent[x] = get_parent(parent[x]);
+    }
+}
+
+// joins two disjoint components
+void unite(int x, int y) {
+    int root_x = get_parent(x);
+    int root_y = get_parent(y);
+    if (root_x != root_y) {
+        parent[root_y] = root_x;
+        sz[root_x] += sz[root_y];
+        sz[root_y] = 0;
+    }
+}
+
+void init() {
+    for (int i = 0; i < N; i++) {
+        parent[i] = i;
+        sz[i] = 1;
+    }
+}
+
+int32_t main()
+{
+    init();
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        int x, y;
+        cin >> x >> y;
+        unite(x, y);
+    }
+}
+//madeby aryan
