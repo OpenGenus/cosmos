fefdccb7e9fadc5f76f1a90033bf70597edf0a39
Vaidehi0421 <61655574+Vaidehi0421@users.noreply.github.com>
2020-03-27 21:46:36 +0530

    Update TWOGRS.java

--- code/online_challenges/src/codechef/TWOGRS/TWOGRS.java
+++ code/online_challenges/src/codechef/TWOGRS/TWOGRS.java
@@ -9,25 +9,25 @@ class twogrs
 	{ Scanner sc=new Scanner(System.in);
 	int T=sc.nextInt();
 	while(T>0)
-	{
-	    int A=sc.nextInt();
-	    int B=sc.nextInt();
-	    int C=sc.nextInt();
-	    int s=(A*1)+(B*2)+(C*3);
-	    if(s%2!=0)
-	    System.out.println("NO");
-	    else
-	    {
-	       if(A==0&&B%2!=0&&C==0)
-	        System.out.println("NO");
-	        else if(A==0&&B==1&C%2==0)
-	         System.out.println("NO");
-	         else if(A==1&&B==0&&C%2!=0)
-	          System.out.println("NO");
-	          else
-	          System.out.println("YES");
-	    }
-	    T--;
-	}
+		{
+	    	int A=sc.nextInt();
+	    	int B=sc.nextInt();
+	    	int C=sc.nextInt();
+	    	int s=(A*1)+(B*2)+(C*3);
+	    	if(s%2!=0)
+	    		System.out.println("NO");
+	    	else
+	    	{
+	    		if(A==0&&B%2!=0&&C==0)
+	    			System.out.println("NO");
+	        	else if(A==0&&B==1&C%2==0)
+	        		System.out.println("NO");
+	        	else if(A==1&&B==0&&C%2!=0)
+	        		System.out.println("NO");
+	        	else
+	        		System.out.println("YES");
+	    	}
+		T--;
+		}
 	}
 }
