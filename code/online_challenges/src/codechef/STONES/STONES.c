5e980d01518eb942f86e2c01af1f507e4764c4e5
ritish099 <ritish.rks@gmail.com>
2020-03-27 11:00:37 +0530

    Codechef STONES Solution in C

---
+++ code/online_challenges/src/codechef/STONES/STONES.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) 
+{
+    int t;
+
+    scanf("%d", & t);
+    while (t--) 
+    {
+        char s[100], r[100];
+        int count = 0, i, j;
+        scanf("%s", s);
+        scanf("%s", r);
+        for (j = 0; j < strlen(r); ++j) 
+        {
+            for (i = 0; i < strlen(s); ++i) 
+            {
+                if (s[i] == r[j]) 
+                {
+                    ++count;
+                    break;
+                }
+
+            }
+        }
+        printf("%d\n", count);
+    }
+    return 0;
+}
