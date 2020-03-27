3abfa0670ff5a3164d6e2d45328940903eacd8e6
aryangulati <42711978+aryangulati@users.noreply.github.com>
2020-03-27 21:59:25 +0530

    Created Sorted subsequences

---
+++ code/bit_manipulation/src/sorted subsequences/sorted subsequences.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int t; cin >> t; while (t--) {
+		string s;
+		cin >> s;
+		int i, j, n = s.size();
+		string s_ar[1 << n];
+		for (i = 0; i < (1 << n); i++) {
+			for (j = 0; j < n; j++) {
+				if ((i & (1 << j))) {
+					s_ar[i] += s[j];
+				}
+			}
+		}
+		sort(s_ar, s_ar + (1 << n));
+		for (i = 0; i < (1 << n); i++) {
+			cout << s_ar[i] << '\n';
+		}
+	}
+}
