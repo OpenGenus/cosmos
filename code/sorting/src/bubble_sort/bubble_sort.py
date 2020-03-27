33a7640bbedb508553f21c7e23c532db0085c10e
anushka-12-lal <57300780+anushka-12-lal@users.noreply.github.com>
2020-03-27 23:51:49 +0530

    Added comments to the python code of BubbleSort

--- code/sorting/src/bubble_sort/bubble_sort.py
+++ code/sorting/src/bubble_sort/bubble_sort.py
@@ -8,23 +8,30 @@ def bubble_sort(arr):
     >>> arr
     [1, 2, 3, 5, 9]
     """
-    for i in range(len(arr)):
+   #Iterating through all the elements in the array
+     for i in range(len(arr)):
         for j in range(i + 1, len(arr)):
+            #If current(ith) element is greater than the next((i+1)th) element swap them
             if arr[i] > arr[j]:
                 arr[i], arr[j] = arr[j], arr[i]
 
 
 # Less elegant solution, but easier to follow along:
 def bubble_sort(alist):
+    #flag to keep track of whether the list is sorted or not, initially it is set as false
     is_sorted = False
+    #keep comparing till the list is sorted
     while is_sorted == False:
         num_swaps = 0
-        blist = []
+        #iterate through all the elements in the list
         for i in range(len(alist) - 1):
+            #if the current element is greater than the next element, pop it out of the list 
             if alist[i] > alist[i + 1]:
                 a = alist.pop(i + 1)
+                #insert the popped element to its right position in the list
                 alist.insert(i, a)
                 num_swaps += 1
+        #if the list is sorted, no more swaps are carried out
         if num_swaps == 0:
             is_sorted = True
     return alist
