ce043b9ebac92151dcc46f1b2707bdeeaf2792f5
santushtisharma10 <60578091+santushtisharma10@users.noreply.github.com>
2020-03-27 23:17:50 +0530

    Update pancake_sort.cs

--- code/sorting/src/pancake_sort/pancake_sort.cs
+++ code/sorting/src/pancake_sort/pancake_sort.cs
@@ -9,7 +9,7 @@ class Program
     
     static void print_ans(int[] a, int n) 
     { 
-	    Console.Write("Sorted Array\n"); 
+        Console.Write("Sorted Array\n"); 
 	    
 	    for (int i = 0; i < n; ++i) 
 		    Console.Write(a[i] + " "); 
@@ -21,7 +21,7 @@ class Program
 	
     static void flip_arr(int[] a, int index) 
     { 
-	    int temp, begin = 0; 
+        int temp, begin = 0; 
 		
 	    while (begin < index) 
 	   { 
@@ -38,7 +38,7 @@ class Program
 	
     static int find_index(int[] a, int n) 
    { 
-	    int  max=0; 
+        int  max=0; 
 		
 	    for (int i = 0; i < n; ++i) 
 		    if (a[i] > a[max]) 
@@ -51,24 +51,24 @@ class Program
 	
     static void pancake_sort(int[] a, int n) 
    { 
-	    int max_i;
+        int max_i;
 		 
-            for (int cur_size = n; cur_size > 1; --cur_size) 
+        for (int cur_size = n; cur_size > 1; --cur_size) 
 	   { 
-		    max_i = find_index(a, cur_size); 
+            max_i = find_index(a, cur_size); 
 
 		    if (max_i != cur_size - 1) 
 		    { 
-			    flip_arr(a, max_i); 
+                flip_arr(a, max_i); 
 
-            		    flip_arr(a, cur_size - 1); 
+            	flip_arr(a, cur_size - 1); 
 		    } 
 	   }
     } 
 
     public static int Main () 
    { 
-	    int []arr = {5, 9, 4, 8, 1, 6}; 
+        int []arr = {5, 9, 4, 8, 1, 6}; 
 		
 	    int n = arr.Length; 
 
