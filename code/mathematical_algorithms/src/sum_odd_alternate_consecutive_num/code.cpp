70cc497b6741c8d249116eb2d68af3b0f94cf0e5
Aayank Singhai <aayanksinghai02@gmail.com>
2020-03-27 11:13:26 +0530

    Indentation Fix

--- code/mathematical_algorithms/src/sum_odd_alternate_consecutive_num/code.cpp
+++ code/mathematical_algorithms/src/sum_odd_alternate_consecutive_num/code.cpp
@@ -1,68 +1,68 @@
-//Problem : Sum of Alternate Consecutive d odd numbers
-//Input Line 1 states the number of test cases
-//Input Line 2 states the range 'Left' and 'Right'
-//Input Line 3 states the numerical value of 'd' odd numbers
-//Output Line 1 displays the sum of alternate consecutive d odd numbers
+    //Problem : Sum of Alternate Consecutive d odd numbers
+    //Input Line 1 states the number of test cases
+    //Input Line 2 states the range 'Left' and 'Right'
+    //Input Line 3 states the numerical value of 'd' odd numbers
+    //Output Line 1 displays the sum of alternate consecutive d odd numbers
 
-/*
-Sample Input
-1
-2
-1 10
+    /*
+    Sample Input
+    1
+    2
+    1 10
 
-Sample Output 
-13
-*/
+    Sample Output 
+    13
+    */
 
 
-#include<bits/stdc++.h>
- 
-using namespace std;
- 
-int main()
-{
-int test;
-cin>>test;
- 
-while(test > 0)
-{
- 
-	int d;
-	cin>>d;
- 
-	int left,right;
-	cin>>left>>right;
-	int c = 0;
-	int sum = 0;
-	int a[1000];
- 
-	for(c,left; left <= right; left++)
-	{
-		if(left % 2 == 1)
-		{
-			a[c] = left;
-			c++;
- 
-		}
- 
-	}
-	int key = d;
-	for(int k = 0; k < c; k++)
-	{
- 
- 
-	if(k == d)
-	{
-		k = k-1 + key;
-		d = k+1 + key;
- 
-	continue;
-	}
-	sum += a[k];
-	}
-	cout<<sum;
-	test--;
-}
- 
-	return 0;
-}
+    #include<bits/stdc++.h>
+
+    using namespace std;
+
+    int main()
+    {
+    int test;
+    cin>>test;
+
+    while(test > 0)
+    {
+
+        int d;
+        cin>>d;
+
+        int left,right;
+        cin>>left>>right;
+        int c = 0;
+        int sum = 0;
+        int a[1000];
+
+        for(c,left; left <= right; left++)
+        {
+            if(left % 2 == 1)
+            {
+                a[c] = left;
+                c++;
+
+            }
+
+        }
+        int key = d;
+        for(int k = 0; k < c; k++)
+        {
+
+
+        if(k == d)
+        {
+            k = k-1 + key;
+            d = k+1 + key;
+
+        continue;
+        }
+        sum += a[k];
+        }
+        cout<<sum;
+        test--;
+    }
+
+        return 0;
+    }
--- code/online_challenges/src/HackerBlocks/Problem001/problem_odd_even.cpp
+++ code/online_challenges/src/HackerBlocks/Problem001/problem_odd_even.cpp
@@ -1,64 +1,64 @@
-/* Problem: Due to an immense rise in Pollution, Kejriwal is back with the Odd and Even Rule in Delhi. The scheme is as follows, each car will be allowed to run on Sunday if the sum of digits which are even is divisible by 4 or sum of digits which are odd in that number is divisible by 3. However to check every car for the above criteria can't be done by the Delhi Police. You need to help Delhi Police by finding out if a car numbered N will be allowed to run on Sunday?
-*/
+    /* Problem: Due to an immense rise in Pollution, Kejriwal is back with the Odd and Even Rule in Delhi. The scheme is as follows, each car will be allowed to run on Sunday if the sum of digits which are even is divisible by 4 or sum of digits which are odd in that number is divisible by 3. However to check every car for the above criteria can't be done by the Delhi Police. You need to help Delhi Police by finding out if a car numbered N will be allowed to run on Sunday?
+    */
 
-// Input : The first line contains N , then N integers follow each denoting the number of the car.
+    // Input : The first line contains N , then N integers follow each denoting the number of the car.
 
-// Output: N lines each denoting "Yes" or "No" depending upon whether that car will be allowed on Sunday or Not !
+    // Output: N lines each denoting "Yes" or "No" depending upon whether that car will be allowed on Sunday or Not !
 
-/*
-Sample Input:
-2
-12345
-12134
+    /*
+    Sample Input:
+    2
+    12345
+    12134
 
-Sample Output:
-Yes
-No
-*/
+    Sample Output:
+    Yes
+    No
+    */
 
 
-#include<iostream>
+    #include<iostream>
 
-using namespace std;
-int main() {
+    using namespace std;
+    int main() {
 
-	int n;
-	cin>>n;
+        int n;
+        cin>>n;
 
-	long long number;
-	
+        long long number;
 
-	while(n>0)
-	{
-		int even_sum = 0;
-		int odd_sum = 0;
-		cin>>number;
-		while(number != 0)
-		{
-			int p = number%10;
-			
-			if(p % 2 == 0)
-			{
-				even_sum += p;
-			}
-			else
-			{
-				odd_sum += p;
-			}
-			
-			number = number/10;
-			
 
-		}
-		if((even_sum % 4 == 0) || (odd_sum % 3 == 0))
-		{
-			cout<<"Yes"<<endl;
-		}
-		else{
-			cout<<"No"<<endl;
-		}
-		n--;
-	}
-	
-	return 0;
-}
+        while(n>0)
+        {
+            int even_sum = 0;
+            int odd_sum = 0;
+            cin>>number;
+            while(number != 0)
+            {
+                int p = number%10;
+
+                if(p % 2 == 0)
+                {
+                    even_sum += p;
+                }
+                else
+                {
+                    odd_sum += p;
+                }
+
+                number = number/10;
+
+
+            }
+            if((even_sum % 4 == 0) || (odd_sum % 3 == 0))
+            {
+                cout<<"Yes"<<endl;
+            }
+            else{
+                cout<<"No"<<endl;
+            }
+            n--;
+        }
+
+        return 0;
+    }
