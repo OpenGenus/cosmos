31456600bc2cb14333af18eebf28b7b3d471ed90
Ankit Kurmi <ankitkurmi152@gmail.com>
2020-03-27 13:01:13 +0530

    updated the codes

--- code/backtracking/src/n_queen/nqueen_bitmask.c
+++ code/backtracking/src/n_queen/nqueen_bitmask.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 
 long long int n, ans = 0, DONE;
-void solve(long long int rowMask, long long int ld, long long int rd) 
-{
-  if (rowMask == DONE) 
-  {
+void solve(long long int rowMask, long long int ld, long long int rd) {
+  if (rowMask == DONE) {
     ans++;
     return;
   }
   long long int safe = DONE & (~(rowMask | ld | rd));
-  while (safe) 
-  {
+  while (safe) {
     long long int p = safe & (-safe);
     safe = safe - p;
     solve(rowMask | p, (ld | p) << 1, (rd | p) >> 1);
   }
 }
-int main() 
-{
+
+int main() {
   scanf("%lld", &n);
   DONE = (1 << n) - 1;
   solve(0, 0, 0);
--- code/online_challenges/src/codechef/SNUG_FIT/snug_fit.cpp
+++ code/online_challenges/src/codechef/SNUG_FIT/snug_fit.cpp
@@ -1,31 +1,26 @@
 #include <iostream>
-
 #include <algorithm>
+#include <vector>
 
 using namespace std;
-int main() 
-{
-  int t;
-  cin >> t;
-  while (t--) 
-  {
-    int n, i, sum = 0;
-    cin >> n;
-    int a[n], b[n];
-    for (i = 0; i < n; i++) 
-    {
-      cin >> a[i];
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i, sum = 0;
+        cin >> n;
+        vector<int> a(n), b(n);
+        for (i = 0; i < n; ++i) {
+            cin >> a[i];
+        }
+        for (i = 0; i < n; ++i) {
+            cin >> b[i];
+        }
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+        for (i = 0; i < n; ++i) {
+            sum += min(a[i], b[i]);
+        }
+        cout << sum << "\n";
     }
-    for (i = 0; i < n; i++) 
-    {
-      cin >> b[i];
-    }
-    sort(a, a + n);
-    sort(b, b + n);
-    for (i = 0; i < n; i++) 
-    {
-      sum += min(a[i], b[i]);
-    }
-    cout << sum << "\n";
-  }
 }
