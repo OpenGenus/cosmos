3167519b1ed36188c2e42659652140a20e3ebdb8
Ankit Kurmi <43778255+Ankit152@users.noreply.github.com>
2020-03-27 13:05:04 +0530

    identation of hard cash

--- code/online_challenges/src/codechef/CASH/hard_cash.cpp
+++ code/online_challenges/src/codechef/CASH/hard_cash.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
 
 using namespace std;
-int main() 
-{
-  int t;
-  cin >> t;
-  while (t--) {
-    int n, k, x, sum = 0;
-    cin >> n >> k;
-    for (int i = 0; i < n; ++i) 
-    {
-      cin >> x;
-      sum += x;
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k, x, sum = 0;
+        cin >> n >> k;
+        for (int i = 0; i < n; ++i) {
+        cin >> x;
+        sum += x;
+        }
+      sum %= k;
+      cout << sum << "\n";
     }
-    sum %= k;
-    cout << sum << "\n";
-  }
 }
