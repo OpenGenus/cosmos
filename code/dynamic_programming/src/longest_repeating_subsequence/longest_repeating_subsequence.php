15187c46372858f2301adfe111cb1ebed3bd6f16
Ramchandra Joshi <rbjoshi1999@gmail.com>
2020-03-29 02:07:16 +0530

    Added code for Longest Repeating Subsequence in C, Java, Python, C# and Php

---
+++ code/dynamic_programming/src/longest_repeating_subsequence/longest_repeating_subsequence.php
@@ -0,0 +1,27 @@
+<?php 
+    function longestRepeatingSubsequence($str) 
+    { 
+        $n = strlen($str); 
+        $dp = array(array());
+
+        for ($i = 0; $i <= $n; $i++) 
+            for ($j = 0; $j <= $n; $j++) 
+                $dp[$i][$j] = 0; 
+
+        for ($i = 1; $i <= $n; $i++) 
+        { 
+            for ($j = 1; $j <= $n; $j++) 
+            { 
+                if ($str[$i - 1] == $str[$j - 1] && $i != $j) 
+                    $dp[$i][$j] = 1 + $dp[$i - 1][$j - 1];	 
+                else
+                    $dp[$i][$j] = max($dp[$i][$j - 1], $dp[$i - 1][$j]); 
+            } 
+        }
+
+        return $dp[$n][$n]; 
+    } 
+
+    $str = "aabb"; 
+    echo "Length of LRS : ", longestRepeatingSubsequence($str); 
+?> 
---
+++ code/dynamic_programming/src/longest_repeating_subsequence/longest_repeating_subsequence.py
@@ -0,0 +1,17 @@
+def longestRepeatingSubsequence(str): 
+    n = len(str) 
+    dp = [[0]*(n + 1)]*(n + 1) 
+
+    for i in range(1, n + 1): 
+        for j in range(1, n + 1): 
+            if (str[i - 1] == str[j - 1] and i != j): 
+                dp[i][j] = 1 + dp[i - 1][j - 1]		 
+            else: 
+                dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]) 
+
+    return dp[n][n] 
+
+if __name__=='__main__': 
+    str = "aabb"
+    print("Length of LRS : ", longestRepeatingSubsequence(str)) 
+    
