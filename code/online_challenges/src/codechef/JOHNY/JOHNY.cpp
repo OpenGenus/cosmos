23cab116cb65ebac7d8aa7ec88cc60d5ebed5a68
Arnav Borborah <arnavborborah11@gmail.com>
2020-03-28 11:16:37 -0400

    Clean up code

--- code/online_challenges/src/codechef/JOHNY/JOHNY.cpp
+++ code/online_challenges/src/codechef/JOHNY/JOHNY.cpp
@@ -4,13 +4,18 @@ int main() {
     int t;
     std::cin >> t;
     while (t--) {
-        int n, k, ans = 0;
+        int n;
         std::cin >> n;
+        
         int a[100];
         for (int i = 0; i < n; ++i) {
             std::cin >> a[i];
         }
+        
+        int k;
         std::cin >> k;
+        
+        int ans = 0;
         for (int i = 0; i < n; ++i) {
             ans += (a[k - 1] > a[i]);
         }
