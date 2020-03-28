a430371cd6df7f214406065a39268dd4e4ced640
saidrishya <saidrishya1998@gmail.com>
2020-03-28 12:10:10 +0530

    initial commit

---
+++ code/data_structures/src/stack/balanced_expression/balanced_expression.py
@@ -0,0 +1,18 @@
+def check(expression): 
+      
+    open_tup = tuple('({[') 
+    close_tup = tuple(')}]') 
+    map = dict(zip(open_tup, close_tup)) 
+    queue = [] 
+  
+    for i in expression: 
+        if i in open_tup: 
+            queue.append(map[i]) 
+        elif i in close_tup: 
+            if not queue or i != queue.pop(): 
+                return "Unbalanced"
+    return "Balanced"
+  
+# Driver code 
+string = "{[]{()}}"
+print(string, "-", check(string)) 
