6120a256d7dedcbda7bbf2a3a3052fd7852aa343
TechnoPhasePRO <yrajsingh2020@gmail.com>
2020-03-28 07:57:18 +0530

    NEW MODIFIED COCKTAIL SORT

--- code/sorting/src/cocktail_sort/cocktail_sort.c
+++ code/sorting/src/cocktail_sort/cocktail_sort.c
@@ -1,25 +1,26 @@
-#include "stdio.h"
-#include "conio.h"
-
+#include <stdio.h>
 /* CocktailSort function is the function used to sort the array using cocktail algorithm. */
 /* Cocktailsort is similar to the bubble sort but the complexity is less comparatively. */
-void CocktailSort(int l[], int n)  
+void cocktailsort(int l[], int n)  
 {   
     /* argument passed is the list of the array to be sorted and the length of the array */
-    /* first is used to represent the starting index of the array
-     * last is used to represent the last index of the array
-     * p is the variable used to check if the array contains sorted elements after sorting process
-     */
-    int first = 0;
-    int last = n-1;
+
+    /* first is used to represent the starting index of the array */
+    int first = 0; 
     int temp = 0;
+    /* last is used to represent the last index of the array */
+    int last = n - 1;
+    /* p is the variable used to check if the array contains sorted elements after sorting process */
     int p = 1;
-    while (p!=0) {
+    while ( p != 0) 
+    {
         /* while loop checks the status of the array */
         p = 0;
-        for (int i = first; i < last; ++i) { 
+        for (int i = first; i < last; ++i) 
+        { 
         /* for loop is used to do the forward sorting on the array */
-            if (l[i] > l[i + 1]) { 
+            if (l[i] > l[i + 1]) 
+            { 
                 /* larger element is exchanged with smaller one */
                 temp = l[i];
                 l[i] = l[i+1];
@@ -28,15 +29,17 @@ void CocktailSort(int l[], int n)
             }
             /* after every sorting, largest element is at the last index and value of p is set to 1 */
         } 
-        if (!p)
+        if (! p)
         /* if value is not 1, it means the array is sorted already , so the control is sent out of while loop */
             break;
-        else {
+        else 
+        {
         /* value of last is decreased by 1 as largest element need not to be sorted */
             p = 0;
-            last-=1;
+            last--;
         }
-        for (int i = last; i >= first; --i) { 
+        for (int i = last; i >= first; --i) 
+        { 
         /* for loop is used to do backward sort on the array */
             if (l[i] > l[i + 1]) 
             { 
@@ -47,7 +50,7 @@ void CocktailSort(int l[], int n)
             } 
         }
         /* the smallest element is placed at the front of the array, so the value of first variable is increased by 1 */
-        first+=1;
+        first++;
     }
     for (int i = 0; i < n; ++i) 
     /* for loop is used to display the sorted array elements */
@@ -61,7 +64,7 @@ void main()
     /* n variable is used to count the length of array */
     int n = sizeof(l) / sizeof(l[0]);
     /* cocktailsort function is called in order with parameters l and length of array n */
-    CocktailSort(l,n); 
+    cocktailsort(l,n); 
 } 
 
 /* output-> 0 1 2 2 4 5 8 */
