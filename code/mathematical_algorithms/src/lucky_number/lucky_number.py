5e108d02b9eb401564108ee94233144ea72f6b70
Ovee Kulkarni <oveeaarya@gmail.com>
2020-03-29 17:41:18 +0530

    Lucky Number Python

---
+++ code/mathematical_algorithms/src/lucky_number/lucky_number.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 27 14:24:40 2020
+
+@author: oveea
+"""
+
+class soln:
+    def __init__(self):
+        self.n=0
+        self.list=[]
+        self.jump=2
+        self.pointer=1
+        
+    def lazynum(self):
+        self.n=int(input("Enter the number to be checked for : ")) #Take input number
+        
+        for i in range((self.n)):
+            self.list.append(i+1)  #create the list of all numbers up till n that will be processed later
+        
+        while(len(self.list)>=self.jump):  #loop end condn
+            toBeRemoved=[]                 #create a list for the numbers to be removed in a single pass 
+            for i in range(len(self.list)):#ensure how long the loop operates
+                if(self.pointer<=len(self.list)-1): #ensure that the pointer doesn't go out of range
+                    toBeRemoved.append(self.list[self.pointer])  #append the number at that index into the list
+                    self.pointer=self.pointer+self.jump  #update pointer
+            self.list= list(set(self.list).symmetric_difference(set(toBeRemoved)))#update list
+            self.jump=self.jump+1  
+            self.pointer=self.jump-1
+     
+        if(self.n in self.list):
+            return 1,self.n
+        return 0,self.n
+    
+o=soln()
+value=o.lazynum()
+if(value[0]==1):
+    print(value[1]," is a lucky no.")
+else:
+    print(value[1],"is not a lucky no.")
+        
+        
+        
