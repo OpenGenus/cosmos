914c7e41df0f818ba0e3ede755645785cdc2c506
Ayush Pandey <59275479+Ayush7-BIT@users.noreply.github.com>
2020-03-29 23:14:53 +0530

    modified code for longest common substring in cpp

--- code/dynamic_programming/src/longest_common_substring/Longest_Common_Substring.cpp
+++ code/dynamic_programming/src/longest_common_substring/Longest_Common_Substring.cpp
@@ -1,78 +1,55 @@
-// Space Complexity: O(n)
-// Time Complexity: O(m*n) 
-
-#include <iostream>
-#include <vector> 
-
-std::string LongestCommonSubstring(std::string string1, std::string string2) 
+#include<iostream> 
+#include<string.h> 
+using namespace std; 
+  
+/* Returns length of longest common substring of X[0..m-1]  
+   and Y[0..n-1] */
+int LCSubStr(char *X, char *Y, int m, int n) 
 { 
-    std::string temp;
-    // longest string is string1 and the smallest string is string2
-    if ( string2.size() > string1.size() ) 
-    {
-        temp = string1;
-        string1 = string2;
-        string2 = temp;
-    }
-
-    int m = string1.size();
-    int n = string2.size();
-    int maxLength = 0; // length of longest common Substring
-    int end; // ending point of longest common Substring
-    int curr = 0; // current row in the matrix 
-
-    std::vector< std::vector<int> > consecutiveRows(2, std::vector<int> (n + 1, 0)); // store result of 2 consecutive rows
-    
-    // maintaing the array for consequtive two rows
-    for (int i = 1; i <= m; i++) 
+    // Create a table to store lengths of longest 
+    // common suffixes of substrings.   Note that 
+    // LCSuff[i][j] contains length of longest 
+    // common suffix of X[0..i-1] and Y[0..j-1].  
+  
+    int LCSuff[m+1][n+1]; 
+    int result = 0;  // To store length of the  
+                     // longest common substring 
+  
+    /* Following steps build LCSuff[m+1][n+1] in 
+        bottom up fashion. */
+    for (int i=0; i<=m; i++) 
     { 
-        for (int j = 1; j <= n; j++) 
+        for (int j=0; j<=n; j++) 
         { 
-            if (string1[i - 1] == string2[j - 1]) 
+  
+            // The first row and first column  
+            // entries have no logical meaning,  
+            // they are used only for simplicity  
+            // of program 
+            if (i == 0 || j == 0) 
+                LCSuff[i][j] = 0; 
+  
+            else if (X[i-1] == Y[j-1]) 
             { 
-                consecutiveRows[curr][j] = consecutiveRows[1 - curr][j - 1] + 1; 
-                if ( consecutiveRows[curr][j] > maxLength ) 
-                { 
-                    maxLength = consecutiveRows[curr][j]; 
-                    end = i - 1; 
-                }
+                LCSuff[i][j] = LCSuff[i-1][j-1] + 1; 
+                result = max(result, LCSuff[i][j]); 
             } 
-            else
-                consecutiveRows[curr][j] = 0; 
+            else LCSuff[i][j] = 0; 
         } 
-        curr = 1 - curr; // changing the row alternatively
     } 
-
-    if (maxLength == 0) 
-        return ""; 
-    else
-    {
-        std::string s = "";
-        // string is from end-maxLength+1 to end as maxLength is the length of
-        // the common substring.
-        for (int i = end - maxLength + 1; i <= end; i++)
-            s += string1[i];
-    	
-        return s; 
-    }
-}
-
-int main()
-{
-    std::string string1;
-    std::string string2;
-    std::cout << "Enter String1: ";
-    std::cin >> string1;
-    std::cout << "Enter String2: ";
-    std::cin >> string2;
-    std::cout << "String1: " << string1 << "\nString2: " << string2 << "\n";
-
-    std::string lcsStr = LongestCommonSubstring(string1, string2);
-
-    if(lcsStr == "")
-        std::cout << "No common substring\n"; 
-    else
-        std::cout << "Longest Common Substring: " << lcsStr << " (of length: " << lcsStr.size() << ")\n";
-    
-    return 0;
-}
+    return result; 
+} 
+  
+/* Driver program to test above function */
+int main() 
+{ 
+    char X[] = "OldSite:GeeksforGeeks.org"; 
+    char Y[] = "NewSite:GeeksQuiz.com"; 
+  
+    int m = strlen(X); 
+    int n = strlen(Y); 
+  
+    cout << "Length of Longest Common Substring is " 
+         << LCSubStr(X, Y, m, n); 
+    return 0; 
+} 
