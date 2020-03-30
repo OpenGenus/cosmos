91a9a2752c3e905a1d8d0e2abff30dc0d41124df
parjanyaacoder <46294122+parjanyaacoder@users.noreply.github.com>
2020-03-31 00:55:08 +0530

    Added and Updated Brian Kernighan Algorithm

--- code/brianKernighan'sAlgorithm.java
+++ code/brianKernighan'sAlgorithm.java
@@ -1,20 +1,27 @@
+// Brian Kernighan Algorithm is an algorithm to count the number of set bits in binary representation of a number.
+
 import java.util.*;
 public class Main
-{  static int countSetBits(int n)
+{  
+	
+	static int countSetBits( int n )
    {   
-       if(n==0)
-       return 0;
+       if ( n == 0 )
+       return 0 ;
        else 
-       return 1+countSetBits(n&(n-1));
+       return 1 + countSetBits ( n & ( n - 1 ) ) ;
    }
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) 
+	{
 		
-		Scanner sc=new Scanner(System.in);
-		int number,count=0;
-		 number=sc.nextInt();
-		 System.out.println(countSetBits(number));
+		Scanner sc = new Scanner( System.in ) ;
+		int number ;
+		 number = sc.nextInt();
+		 System.out.println( countSetBits( number ) );
 	}
 }
 /*
 Input - number=9
-Output - 2 */
+Output - 2 
+Explanation - Binary representation of 9 is 1001 has 2 set bits. */
