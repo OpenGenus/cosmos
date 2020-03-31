e8086a1692d4e5f2b1ed7a88c669f41153711734
Ayush Pandey <59275479+Ayush7-BIT@users.noreply.github.com>
2020-03-31 13:10:40 +0530

    modified pythagorean triplet in C++

--- code/mathematical_algorithms/src/pythagorean_triplet/pythagorean_triplet.cpp
+++ code/mathematical_algorithms/src/pythagorean_triplet/pythagorean_triplet.cpp
@@ -3,23 +3,23 @@
 using namespace std;
 int main ()
 {
-  int a=1, b=1, c;
+  int a=1, b=1, c;//Initialization of variables
   int i, j, k;
   double d, e;
 
-  for (i=1; i<=500; i++)
+  for (i=1; i<=500; i++)//for loop till N numbers
   {
-     a += 1;
-     for (j=1; j<=500; j++)
+     a += 1;//Increment condition
+     for (j=1; j<=500; j++)//For loop till N numbers
      {
-        b += 1;
-     for (k=1; k<=500; k++)
+        b += 1;//Increment condition
+     for (k=1; k<=500; k++)//For loop till N numbers
      {
-        e = ((a*a) + (b*b));
-        d = sqrt(e);
-        c = int(d);
-        if ((c == d) && (c <=500))
-           cout << "a = " << a << "\t\t b = " << b << "\t\t c = " << c << endl;
+        e = ((a*a) + (b*b));//Adding sum of square of two different variables
+        d = sqrt(e);//Finding root
+        c = int(d);//Type casting into Int
+        if ((c == d) && (c <=500))//Condition check
+           cout << "a = " << a << "\t\t b = " << b << "\t\t c = " << c << endl;//Print condition
 	}
      }
   }
