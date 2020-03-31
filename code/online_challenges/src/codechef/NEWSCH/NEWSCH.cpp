d8305021708ebb9c21afa93488a799c22ee3780d
kuljeet singh <48275443+Kuljeet-123@users.noreply.github.com>
2020-03-31 19:40:06 +0530

    Update NEWSCH.cpp

--- code/online_challenges/src/codechef/NEWSCH/NEWSCH.cpp
+++ code/online_challenges/src/codechef/NEWSCH/NEWSCH.cpp
@@ -1,58 +1,52 @@
 #include <iostream>
 
+#include <cstring>
+
 using namespace std;
 
-int main() 
+#define MOD 1000000007
+
+uint64_t modmul(const uint64_t x, const uint64_t y)
 {
-  
-  int tt,n,m,old,it,i,j,k,x[222],y[222],u[222];
-  bool g[222][222],q;
-  char r[222];
-  scanf("%d",&tt);
-  while (tt--) 
-  {
-    scanf("%d%d",&n,&m);
-    memset(u,0,sizeof(u));
-    memset(g,0,sizeof(g));
-    memset(r,0,sizeof(r));
-    for (i=0; i<m; i++) 
-    {
-      scanf("%d%d",&x[i],&y[i]);
-      g[x[i]][y[i]]=true;
-      g[y[i]][x[i]]=true;
-    }
-    for (it=1; it<=n; )
-    {
-      old=it;
-      for (i=1; i<=n; i++)
-        if (u[i]==0) 
-      {
-        q=true;
-        for (j=1; j<=n && q; j++)
-          if (g[i][j])
-            for (k=1; k<j; k++) if (g[i][k]) 
-        {
-          if (!g[j][k])
-          {
-            q=false; break; 
-          }
-        }
-        if (q)
-        {
-          u[i]=it++;
-          for (j=1; j<=n; j++) if (g[i][j]) g[i][j]=g[j][i]=0;
-          break;
-        }
-      }
-      if (old==it) break;
-    }
-    if (it<=n)
-    {
-      puts("No solution"); continue;
-    }
-    for (i=0; i<m; i++)
-      r[i]=(u[x[i]]<u[y[i]])?'^':'v';
-    puts(r);
-  }
-  return 0;
+	if (x > (1 << 30) && y > (1 << 30))
+		return ((x >> 30)*((y << 30) % MOD) + y*(x & ((1 << 30) - 1))) % MOD;
+	uint64_t z = x*y;
+	if (z >= MOD)
+		z %= MOD;
+	return z;
+}
+
+uint64_t modpow(uint64_t base, int exp)
+{
+	uint64_t result = 1;
+	for (;;)
+	{
+		if (exp & 1)
+			result = modmul(result, base);
+		exp >>= 1;
+		if (!exp)
+			break;
+		base = modmul(base, base);
+	}
+
+	return result;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t-- > 0)
+	{
+		int n;
+		cin >> n;
+		uint64_t ans = modpow(3, n);
+		if (n % 2 == 0)
+			ans = (ans + 3) % MOD;
+		else
+			ans = (ans + MOD - 3) % MOD;
+		cout << ans << endl;
+	}
+
+	return 0;
 }
