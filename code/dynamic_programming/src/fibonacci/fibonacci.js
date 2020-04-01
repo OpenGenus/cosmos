178d4b5aff91bd11d8e6ae64af517d16be071338
Sabuj Jana <sabujlovescoding@gmail.com>
2020-04-01 17:19:22 +0000

    Added fibonacci.js

--- code/dynamic_programming/src/fibonacci/fibonacci.js
+++ code/dynamic_programming/src/fibonacci/fibonacci.js
@@ -1,35 +1,31 @@
-//	Problem Statement(1): Find the nth Fibonacci number 
-// (1.a) Using Recusrsion
-// (1.b) Using Dynamic Programming(DP)
-//	Problem Statement(2): Print the Fibonacci series
+// Problem Statement(1): Find the nth Fibonacci number 
 
-//	1.a Using Recusrion to find the nth fibonacci number
+// 1.a.Using Recusrion to find the nth fibonacci number
 const FibonacciNumberRec = (num) => {
-	if(num <= 1)
-		return num;
-	return FibonacciNumberRec(num - 1) + FibonacciNumberRec(num - 2);
+    if (num <= 1)
+        return num;
+    return FibonacciNumberRec(num - 1) + FibonacciNumberRec(num - 2);
 }
 
-// 1.b Using DP to find the nth fibonacci number
+// 1.b.Using DP to find the nth fibonacci number
 // fib[n] = fib[n - 1] + fib[n - 2]
-
 const FibonacciNumberDP = (num) => {
-	let fib = [0, 1] // fib[0] = 0, fib[1] = 1
-	for(let i = 2; i <= num; i++){
-		fib.push(fib[i - 1] + fib[i - 2]);
-	}
+    let fib = [0, 1] // fib[0] = 0, fib[1] = 1
+    for (let i = 2; i <= num; i++) {
+        fib.push(fib[i - 1] + fib[i - 2]);
+    }
 
-	return fib[num];// return num'th fib number
+    return fib[num]; // return num'th fib number
 }
 
-// 2. Print the fibonacci series upto nth Fibonacci number
+// 2.Print the fibonacci series upto nth Fibonacci number
 const FibonacciSeries = (num) => {
-	let fib = [0, 1] // fib[0] = 0, fib[1] = 1
-	for(let i = 2; i <= num; i++){
-		fib.push(fib[i - 1] + fib[i - 2]);
-	}
+    let fib = [0, 1] // fib[0] = 0, fib[1] = 1
+    for (let i = 2; i <= num; i++) {
+        fib.push(fib[i - 1] + fib[i - 2]);
+    }
 
-	return fib;
+    return fib;
 }
 
 
@@ -50,4 +46,4 @@ console.log(`The Fibonacci Series upto ${n}th term is ${FibonacciSeries(n)}`);
 // 2. n = 10
 // The Fibonacci Series upto 10th term is 0,1,1,2,3,5,8,13,21,34,55
 
-// Note:This is 0-based i.e, fib[0] = 0, fib[1] = 1, fib[2] = 0 + 1 = 1, fib[3] = 1 + 1 = 2 
+// Note:This is 0-based i.e, fib[0] = 0, fib[1] = 1, fib[2] = 0 + 1 = 1, fib[3] = 1 + 1 = 2
