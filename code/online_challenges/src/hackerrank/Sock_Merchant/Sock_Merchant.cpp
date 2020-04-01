b60a04c6e47e432aaa70f2a7df73ccb357ada5d4
Prasanna Venkatesh <prasannavenkatesh.ramkumar@iiitb.org>
2020-04-01 22:35:04 +0530

    did preincrement of all for loops

--- code/online_challenges/src/hackerrank/Sock_Merchant/Sock_Merchant.cpp
+++ code/online_challenges/src/hackerrank/Sock_Merchant/Sock_Merchant.cpp
@@ -10,19 +10,19 @@ int main()
     cin >> n;
     int arr[n];
     int sockarr[101] = {};
-    for(int i = 0;i < n;i++) cin >> arr[i];
+    for(int i = 0;i < n;++i) cin >> arr[i];
     memset(sockarr,0,101);
-    for(int i = 0;i < n;i++)
+    for(int i = 0;i < n;++i)
     {
         sockarr[arr[i]] += 1;
     }
     int pairs = 0;
-    for(int i = 0;i < 101;i++)
+    for(int i = 0;i < 101;++i)
     {
         pairs += sockarr[i]/2;
     }
     /*
-    for(int i = 0;i < 101;i++)
+    for(int i = 0;i < 101;++i)
     {
         cout << sockarr[i] << endl;
     }*/
