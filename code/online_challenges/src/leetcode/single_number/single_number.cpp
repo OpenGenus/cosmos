d5af7382e50232425720b38b5b662699954d717f
Prasanna Venkatesh <prasannavenkatesh.ramkumar@iiitb.org>
2020-04-01 22:37:51 +0530

    Did formatting using 4 spaces as suggested

--- code/online_challenges/src/leetcode/single_number/single_number.cpp
+++ code/online_challenges/src/leetcode/single_number/single_number.cpp
@@ -23,12 +23,12 @@ class Solution
     public:
         int singleNumber(vector<int>& nums) 
         {
-            int n = nums.size();
-            int number = 0;
-            for(int i = 0;i < n;++i)
-            {
-                number ^= nums[i];
-            }
-            return number;
+             int n = nums.size();
+             int number = 0;
+             for(int i = 0;i < n;++i)
+             {
+                 number ^= nums[i];
+             }
+             return number;
         }
 };
