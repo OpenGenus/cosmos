3e735680ded7443a3554394a93066b7f6b696fb1
Ramchandra Joshi <rbjoshi1999@gmail.com>
2020-04-01 23:04:59 +0530

    fixing minor changes

--- code/dynamic_programming/src/longest_repeating_subsequence/longest_repeating_subsequence.py
+++ code/dynamic_programming/src/longest_repeating_subsequence/longest_repeating_subsequence.py
@@ -1,16 +1,17 @@
-def longestRepeatingSubsequence(str): 
-    n = len(str) 
-    dp = [[0]*(n + 1)]*(n + 1) 
+def longest_repeating_subsequence(str):
+    n = len(str)
+    dp = [[0] * (n + 1)] * (n + 1)
 
-    for i in range(1, n + 1): 
-        for j in range(1, n + 1): 
-            if (str[i - 1] == str[j - 1] and i != j): 
-                dp[i][j] = 1 + dp[i - 1][j - 1]		 
-            else: 
-                dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]) 
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if str[i - 1] == str[j - 1] and i != j:
+                dp[i][j] = 1 + dp[i - 1][j - 1]
+            else:
+                dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
 
-    return dp[n][n] 
+    return dp[n][n]
 
-if __name__=='__main__': 
+
+if __name__ == "__main__":
     str = "aabb"
-    print("Length of LRS : ", longestRepeatingSubsequence(str)) 
+    print("Length of LRS : ", longest_repeating_subsequence(str))
