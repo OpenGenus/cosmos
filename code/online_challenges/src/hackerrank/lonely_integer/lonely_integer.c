5c37a06cb80f02c54040b7ee87331ddc11c092d4
Aditya Gupta <56713924+aftex261@users.noreply.github.com>
2020-04-03 21:04:23 +0530

    Update lonely_integer.c

--- code/online_challenges/src/hackerrank/lonely_integer/lonely_integer.c
+++ code/online_challenges/src/hackerrank/lonely_integer/lonely_integer.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
-int main()      /*Driver Function*/
+int main()     
 {
     int n,j=0,a,i;
-    scanf("%d",&n);     /*Taking input from user*/ 
+    scanf("%d",&n);    
     int arr[n];
-    for(i=0;i<n;i++)
-    scanf("%d",&arr[i]);
-    for(i=0;i<n;i++)
+    for(i=0;i<n;++i)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;++i)
     {
         j=j^arr[i];
     }
     printf("%d",j);
+    return 0;
 }
