8311992e15b8c1c82550dfeb2735ed918ba8ae8f
kuljeet singh <48275443+Kuljeet-123@users.noreply.github.com>
2020-04-03 21:16:41 +0530

    Update STRWN.cpp

--- code/online_challenges/src/codechef/STRWN/STRWN.cpp
+++ code/online_challenges/src/codechef/STRWN/STRWN.cpp
@@ -1,37 +1,46 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
+int main() 
+{
 	long long int d,v,w;
 	cin>>d>>v>>w;
-	if(v == 0&&d == 0&&w == 0) 
+	if(v==0 && d==0 && w==0) 
 	{
-	    cout<<0<<"\n"; return 0;
+	    cout<<0<<"\n";
+	    return 0;
 	}
-	else if(v==0&&d==0) 
-    {
-        cout<<w<<"\n"; return 0;
-    }
-	else if(v==0&&w==0) 
-    {
-        cout<<d<<"\n"; return 0;
-    }
-	else if(w==0&&d==0) 
-    {
-        cout<<v<<"\n"; return 0;
-    }
+	else if(v==0 && d==0) 
+        {
+            cout<<w<<"\n";
+	    return 0;
+        }
+	else if(v==0 && w==0) 
+        {
+            cout<<d<<"\n"; 
+	    return 0;
+        }
+	else if(w==0 && d==0) 
+        { 
+            cout<<v<<"\n";
+	    return 0;
+        }
 	else if(v==0)
-    {
-        cout<<w<<"\n"; return 0;
-    }
+        {  
+            cout<<w<<"\n";
+	    return 0;
+        }
 	else if(d==0)
-    {
-        cout<<v<<"\n"; return 0;
-    }
+        {
+            cout<<v<<"\n";
+	    return 0;
+        }
 	else if(w==0)
-    {
-        cout<<d<<"\n"; return 0;
-    }
+        {
+            cout<<d<<"\n";
+	    return 0;
+        }
 	cout<<1<<"\n";
 	return 0;
 }
