8f8eb5d29a4c6e45ac04222ee4aac12ec90d946b
ANSHUMAN GUPTA <59006894+anshuman1510@users.noreply.github.com>
2020-04-03 20:56:11 +0530

    Update jumping_on_the_clouds.c

--- code/online_challenges/src/hackerrank/jumping_on_the_clouds/jumping_on_the_clouds.c
+++ code/online_challenges/src/hackerrank/jumping_on_the_clouds/jumping_on_the_clouds.c
@@ -1,27 +1,21 @@
 #include <stdio.h>
 
-int main()
-{
-    int n, i;
-    scanf ("%d", &n);
-    int c[n];
-    for (i=0;i<n;++i)
-    {
-        scanf ("%d", &c[i]);
-    }
-    int jump = 0;
-    for (i=0;i<n;++i)
-    {
-        if (i+2 < n && !c[i+2])
-        {
-            ++jump;
-            ++i;
-        }
-        else if (i+1 < n && !c[i+1])
-        {
-            ++jump;
-        }
-    }
-    printf ("%d", jump);
-    return 0;
+int main() {
+  int n, i;
+  scanf("%d", &n);
+  int c[n];
+  for (i = 0; i < n; ++i) {
+	scanf("%d", &c[i]);
+  }
+  int jump = 0;
+  for (i = 0; i < n; ++i) {
+	if (i + 2 < n && !c[i + 2]) {
+	  ++jump;
+	  ++i;
+	} else if (i + 1 < n && !c[i + 1]) {
+	  ++jump;
+	}
+  }
+  printf("%d", jump);
+  return 0;
 }
