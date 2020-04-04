afc332b9cd6ec85dd3f8ef0e144fffc66993791b
ritish099 <ritish.rks@gmail.com>
2020-04-04 18:43:29 +0530

    Codechef LEBOMBS solution in C

---
+++ code/online_challenges/src/codechef/LEBOMBS/LEBOMBS.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) 
+{
+    int t;
+    scanf("%d", & t);
+    while (t--) 
+    {
+        int n;
+        scanf("%d", & n);
+        char s[n];
+        scanf("%s", s);
+        s[-1] = '0';
+        s[n] = '0';
+        int i, k = 0;
+        for (i = 0; i < n; ++i) 
+        {
+            if (s[i] == '0' && s[i + 1] != '1' && s[i - 1] != '1')
+                k = k + 1;
+        }
+        printf("%d\n", k);
+    }
+    return 0;
+}
