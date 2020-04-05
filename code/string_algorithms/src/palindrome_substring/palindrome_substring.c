ef48aab367099575d1a5228e4bb10256db4a9694
shortCircuit81 <54492585+shortCircuit81@users.noreply.github.com>
2020-04-05 09:24:04 -0700

    Update palindrome_substring.c

--- code/string_algorithms/src/palindrome_substring/palindrome_substring.c
+++ code/string_algorithms/src/palindrome_substring/palindrome_substring.c
@@ -1,39 +1,40 @@
-#include <stdio.h>
-#define true 1
-#define false 0
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
-/*
- * Utilises 'iattempt' palindrome.c implementation.
- * Part of Cosmos by OpenGenus Foundation.
-*/
+public class Solution {
 
-int
-isPalindromeIterative(char *input, int length)
-{
-	int start = 0, end = length;
-	while (start < end)
-		if (input[start++] != input[end--])
-			return false;
-	return true;
-}
-
-int
-countPalindromeSubstrings(char *input, int length)
-{
-	int start = 0, end = 0;
-	int response = 0;
-	int original_end = length - 1;
-
-	while(start < original_end){
-		for(end = 1; end <= (original_end - start); ++end)
-			response += isPalindromeIterative(&input[start], end);
-		++start;
-	}
-	return response;
-}
-
-int main(void)
-{
-	printf("Number of Possible Palindrome Substrings: %d\n", countPalindromeSubstrings("abaab", 5));
-	return (0);
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+   Scanner scn=new Scanner(System.in);
+       String str=scn.nextLine();
+        subseq(str);
+    }
+    public static boolean ispallindrome(String str){
+        int left=0;int right=str.length()-1;
+        while(left<=right){
+            if(str.charAt(left)!=str.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public static void subseq(String str){
+        int count=0;
+        for(int i=0;i<str.length();i++){
+            for(int j=i+1;j<=str.length();j++){
+                String ss=str.substring(i,j);
+                boolean pallindrome=ispallindrome(ss);
+                if(pallindrome){
+                System.out.println(ss);
+                    count++;
+                }
+            }
+        }
+        System.out.print(count);
+    }
 }
