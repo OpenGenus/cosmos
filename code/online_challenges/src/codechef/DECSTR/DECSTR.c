8b2c1ac8c64febd69739c42cdef84a1efbf77e82
Ritish Singh <54978105+ritish099@users.noreply.github.com>
2020-04-05 13:16:05 +0530

    Update DECSTR.c

--- code/online_challenges/src/codechef/DECSTR/DECSTR.c
+++ code/online_challenges/src/codechef/DECSTR/DECSTR.c
@@ -5,18 +5,18 @@ int main()
     int t;
     scanf("%d", & t);
     while (t--) 
-	{
+    {
         int i, k;
         scanf("%d", & k);
         int a = k / 25;
         int b = k % 25;
         if (b != 0) 
-		{
+        {
             for (i = 97 + b; i >= 97; i--)
                 printf("%c", i);
         }
         while (a--) 
-		{
+        {
             for (i = 122; i >= 97; i--)
                 printf("%c", i);
         }
