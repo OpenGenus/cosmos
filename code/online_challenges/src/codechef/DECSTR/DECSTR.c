64b4d56a6f0f59a1cb9a293c72ae30821db9f469
ritish099 <ritish.rks@gmail.com>
2020-04-05 13:09:49 +0530

    Codechef DECSTR solution in C

---
+++ code/online_challenges/src/codechef/DECSTR/DECSTR.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main() 
+{
+    int t;
+    scanf("%d", & t);
+    while (t--) 
+	{
+        int i, k;
+        scanf("%d", & k);
+        int a = k / 25;
+        int b = k % 25;
+        if (b != 0) 
+		{
+            for (i = 97 + b; i >= 97; i--)
+                printf("%c", i);
+        }
+        while (a--) 
+		{
+            for (i = 122; i >= 97; i--)
+                printf("%c", i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ code/online_challenges/src/codechef/DECSTR/README.md
@@ -0,0 +1,5 @@
+# Problem Link:
+[DECSTR](https://www.codechef.com/problems/DECSTR/)
+
+# Description
+You need to find a string which has exactly K positions in it such that the character at that position comes alphabetically later than the character immediately after it. If there are many such strings, print the one which has the shortest length. If there is still a tie, print the string which comes the lexicographically earliest (would occur earlier in a dictionary).
