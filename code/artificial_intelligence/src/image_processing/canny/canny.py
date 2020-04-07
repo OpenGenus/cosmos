7ee5ac17a3d2703577e15716bb461eb62812e480
Ratna Priya <ratnpriya1998@gmail.com>
2020-04-08 00:42:22 +0530

    Creating Canny Edge Detection in Python

---
+++ code/artificial_intelligence/src/image_processing/canny/canny.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('image.jpg',0)
+edges = cv2.Canny(img,100,200)
+
+plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+
+plt.show()
