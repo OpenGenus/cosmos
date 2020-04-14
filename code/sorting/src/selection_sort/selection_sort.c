3bbd9644cab407613847cedb6c0456310330bf47
Ajay Sharma <Ajaykrsharma132@gmail.com>
2020-04-14 18:47:35 +0530

    original selection_sort.c

--- code/sorting/src/selection_sort/selection_sort.c
+++ code/sorting/src/selection_sort/selection_sort.c
@@ -5,14 +5,14 @@ int
 main()
 {
     int n, i, j, min, temp;
-    
+
     printf("\n Enter the number of Elements: ");
     scanf("%d",&n);
     int a[n];
     printf("\n Enter the %d Elements: ",n);
     for (i = 0; i < n; i++)
         scanf("%d",&a[i]);
-     
+
     for (i = 0; i < n - 1; i++)
     {
         min = i;
@@ -28,7 +28,7 @@ main()
             a[min] = temp;
         }
     }
-     
+
     printf("\n The Sorted array is : ");
     for (i = 0; i < n; i++)
         printf("%d ", a[i]);
