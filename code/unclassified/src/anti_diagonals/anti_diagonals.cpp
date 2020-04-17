4fcc644bb49ae1ea2458682940ab319d559df081
HariniJeyaraman <58400069+HariniJeyaraman@users.noreply.github.com>
2020-04-18 00:39:21 +0530

    preincrement

--- code/unclassified/src/anti_diagonals/anti_diagonals.cpp
+++ code/unclassified/src/anti_diagonals/anti_diagonals.cpp
@@ -21,8 +21,8 @@ std::vector<std::vector<int>> antiDiagonal(std::vector<std::vector<int>> a) {
     std::vector<std::vector<int>> result(
         (a.size() * 2) -
         1); // 2n-1 number of vectors are needed in the 2D vector
-    for (int i = 0; i < a.size(); i++) {
-        for (int j = 0; j < a[0].size(); j++) {
+    for (int i = 0; i < a.size(); ++i) {
+        for (int j = 0; j < a[0].size(); ++j) {
             result[i + j].push_back(a[i][j]);
         }
     }
@@ -33,7 +33,7 @@ int main() {
     std::vector<std::vector<int>> a = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     std::cout << "Anti-Diagonals are : \n";
     std::vector<std::vector<int>> result = antiDiagonal(a);
-    for (int i = 0; i < result.size(); i++) {
+    for (int i = 0; i < result.size(); ++i) {
         for (int j : result[i])
             std::cout << j << " ";
         std::cout << "\n";
