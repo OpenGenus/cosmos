8a93938653b30349e14b460bdbfd2dc00ebb533b
Ajay Sharma <Ajaykrsharma132@gmail.com>
2020-04-22 11:50:23 +0530

    Added comments and improved indentation in count_digits.c

--- code/mathematical_algorithms/src/count_digits/count_digits.c
+++ code/mathematical_algorithms/src/count_digits/count_digits.c
@@ -1,27 +1,38 @@
+// Program to count the number of digits in a number
 #include<stdio.h>
 // Part of Cosmos by OpenGenus Foundation
 // Code written by Adeen Shukla (adeen-s)
 
-int 
-countDigits(unsigned long n) 
+// Function to count the number of digits
+int countDigits(unsigned long n)
 {
-	if(n == 0) {
+	// if input is equal to 0 then,
+	// return 1
+	if(n == 0)
+	{
 		return (1);
 	}
+	// intializing a variable count to 0
+	// for counting the number of digits
 	int count = 0;
-	while(n != 0) {
+  // divide the number continuously by 10
+	// while incrementing count until the number
+	// becomes equal to zero
+	while(n != 0)
+	{
 		count++;
 		n /= 10;
-    }
+  }
 	return (count);
 }
 
-int 
-main()
+int main()
 {
 	unsigned long n;
-	printf("\nEnter a number\n");	
+	printf("\nEnter a number\n");
 	scanf("%lu", &n);
+	// Printing the number of digits in the number by
+	// using countDigits function
 	printf("\nThe number of digits is --> %d\n", countDigits(n));
 	return (0);
 }
