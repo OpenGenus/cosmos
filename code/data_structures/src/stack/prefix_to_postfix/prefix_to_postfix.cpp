33ec48cccc333cc95a171828b37d0f145a1dad61
Rudrakshi <sonirudrakshi99@gmail.com>
2020-05-07 14:02:49 +0530

    Update prefix_to_postfix.cpp

--- code/data_structures/src/stack/prefix_to_postfix/prefix_to_postfix.cpp
+++ code/data_structures/src/stack/prefix_to_postfix/prefix_to_postfix.cpp
@@ -1,61 +1,63 @@
-#include <iostream> 
-#include <stack> 
-using namespace std; 
+#include <iostream>
+#include <stack>
+using namespace std;
 
-// funtion to check if character is operator or not 
-bool isOperator(char x) { 
-switch (x) { 
-case '+': 
-case '-': 
-case '/': 
-case '*': 
-	return true; 
-} 
-return false; 
-} 
+// funtion to check if character is operator or not
+bool isOperator(char x) {
+    switch (x) {
+    case '+':
+    case '-':
+    case '/':
+    case '*':
+        return true;
+    }
+    return false;
+}
 
-// Convert prefix to Postfix expression 
-string preToPost(string pre_exp) { 
+// Convert prefix to Postfix expression
+string preToPost(string pre_exp) {
 
-stack<string> s; 
+    stack<string> s;
 
-// length of expression 
-int length = pre_exp.size(); 
+    // length of expression
+    int length = pre_exp.size();
 
-// reading from right to left 
-for (int i = length - 1; i >= 0; i--) { 
+    // reading from right to left
+    for (int i = length - 1; i >= 0; i--) {
 
-	// check if symbol is operator 
-	if (isOperator(pre_exp[i])) { 
+        // check if symbol is operator
+        if (isOperator(pre_exp[i])) {
 
-	// pop two operands from stack 
-	string op1 = s.top(); s.pop(); 
-	string op2 = s.top(); s.pop(); 
+            // pop two operands from stack
+            string op1 = s.top();
+            s.pop();
+            string op2 = s.top();
+            s.pop();
 
-	// concat the operands and operator 
-	string temp = op1 + op2 + pre_exp[i]; 
+            // concat the operands and operator
+            string temp = op1 + op2 + pre_exp[i];
 
-	// Push string temp back to stack 
-	s.push(temp); 
-	} 
+            // Push string temp back to stack
+            s.push(temp);
+        }
 
-	// if symbol is an operand 
-	else { 
+        // if symbol is an operand
+        else {
 
-	// push the operand to the stack 
-	s.push(string(1, pre_exp[i])); 
-	} 
-} 
+            // push the operand to the stack
+            s.push(string(1, pre_exp[i]));
+        }
+    }
 
-// stack contains only the Postfix expression 
-return s.top(); 
-} 
+    // stack contains only the Postfix expression
+    return s.top();
+}
 
-// Driver Code 
-int main() { 
-string pre_exp ; 
-cout<<"Enter the prefix expression:";
-cin>>pre_exp;
-cout << "Postfix : " << preToPost(pre_exp); 
-return 0; 
-} 
+// Driver Code
+int main() {
+    string pre_exp;
+    cout << "Enter the prefix expression:";
+    cin >> pre_exp;
+    cout << "Postfix : " << preToPost(pre_exp);
+    return 0;
+}
