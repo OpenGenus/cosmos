f3bfc55403059a17333b59c526ab9a0d0dbd0902
rakhiPurwar <42908255+rakhiPurwar@users.noreply.github.com>
2020-05-10 17:24:59 +0530

    added comments in factorial.js and factorial.py

--- code/dynamic_programming/src/factorial/factorial.js
+++ code/dynamic_programming/src/factorial/factorial.js
@@ -1,12 +1,12 @@
-function factorialize(number) {
-  if (number == 0) {
+function factorialize(number) {            //function definition
+  if (number == 0) {                        //base condition
     return 1;
   } else {
-    return number * factorialize(number - 1);
+    return number * factorialize(number - 1);        //recursive call to factorialize 
   }
 }
 
-factorialize(5);
-/*replace the value of 5 with whatever value you
+factorialize(num);                             
+/*replace the value of num with whatever value you
  *want to find the factorial of
  */
