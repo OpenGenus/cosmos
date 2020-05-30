c512998b3b1b2fc05afb059dbd6f85f350f906b9
Ayush Pandey <59275479+Ayush7-BIT@users.noreply.github.com>
2020-05-30 23:35:49 +0530

    Added comments in Bubble sort_C
    As the issue was raised so i thought of finding an solution to the problem.

--- code/sorting/src/bubble_sort/bubble_sort.c
+++ code/sorting/src/bubble_sort/bubble_sort.c
@@ -1,11 +1,11 @@
 /*Part of Cosmos by OpenGenus Foundation*/
 
-#include <stdio.h>
+#include <stdio.h> //Pre-defined libraries
 typedef int bool;
 #define true 1
 #define false 0
 
-void swap(int *p, int *q)
+void swap(int *p, int *q)//Swap function with parameter used to reduce usage of same code again
 {
 	int temp = *p;
 	*p = *q;
@@ -14,7 +14,7 @@ void swap(int *p, int *q)
 
 /*Sorting an array a[] consisting of n
   elements with bubble sort method*/
-void bubbleSort(int a[], int n)
+void bubbleSort(int a[], int n) //Bubble sort function with parameters containing no of elements and array
 {
 	int i, j;
 	bool swapped;
@@ -23,7 +23,7 @@ void bubbleSort(int a[], int n)
 		swapped = false;
 		for (j = 0; j < n - i - 1; j++)
 		{
-			if (a[j] > a[j + 1])
+			if (a[j] > a[j + 1]) //Condition check to see if next elements is greater than the initial one
 			{
 				swap(&a[j], &a[j + 1]);
 				swapped = true;
@@ -31,14 +31,14 @@ void bubbleSort(int a[], int n)
 		}
 
 
-		if (swapped == false)
+		if (swapped == false) //Boolean used for alternative situation
 			break;/*break if array is sorted
 					i.e. no swapping possible*/
 	}
 }
 
 //printing sorted array
-void print(int a[], int size)
+void print(int a[], int size) //Print function
 {
 	int i;
 	for (i = 0; i < size; i++)
@@ -48,7 +48,7 @@ void print(int a[], int size)
 
 
 
-int main()
+int main() //Main function
 {
 	int n, i;
 	printf("What is the size of the array?\n");//taking size of array as input from user
@@ -58,8 +58,8 @@ int main()
 	for(i = 0; i < n; i++){
 		scanf("\n%d",&a[i]);
 	}
-	bubbleSort(a, n);
+	bubbleSort(a, n); //Using Functions defined above to reduce code complexity
 	printf("Sorted array: ");
-	print(a, n);
+	print(a, n); //Using Functions defined above to reduce code complexity
 	return 0;
 }
