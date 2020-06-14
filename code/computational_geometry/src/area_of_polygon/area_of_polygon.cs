79c10b24ee27b79b7630c6ee16047182203c079f
Irakli Chkuaseli <i@irak.li>
2020-06-14 23:21:02 +0400

    Add area of polygon in C#

---
+++ code/computational_geometry/src/area_of_polygon/area_of_polygon.cs
@@ -0,0 +1,23 @@
+﻿namespace Cosmos
+{
+	public static class AreaOfPolygon
+	{
+		public readonly struct Point
+		{
+			public Point(float x, float y)
+			{
+				X = x;
+				Y = y;
+			}
+
+			public float X { get; }
+			public float Y { get; }
+		}
+		
+		public static double Calculate(Point a, Point b, Point c)
+		{
+			double result = ((a.X - b.X) * (c.Y - b.Y) - (a.Y - b.Y) * (c.X - b.X)) / 2;
+			return (result < 0) ? (-result) : result;
+		}
+	}
+}
