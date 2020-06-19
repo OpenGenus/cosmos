5fa772f13492cae2b4b2a7dc44aa15481b44e9ab
Aaryan Tyagi <42247648+aaryantyagi182@users.noreply.github.com>
2020-06-19 16:10:54 +0530

    Update interval_tree.cpp
    Add comments on interval tree

--- code/data_structures/src/tree/space_partitioning_tree/interval_tree/interval_tree.cpp
+++ code/data_structures/src/tree/space_partitioning_tree/interval_tree/interval_tree.cpp
@@ -1,14 +1,15 @@
 //Part of Cosmos by OpenGenus Foundation
-
+//This is a cpp program to implement interval tree
 #include <iostream>
 using namespace std;
 
+//Structure for interval
 struct Interval
 {
     int low, high;
 };
 
-
+//Structure for node in interval search  tree
 struct ITNode
 {
     Interval *i;
@@ -16,7 +17,7 @@ struct ITNode
     ITNode *left, *right;
 };
 
-
+//For creating new node in interval search tree
 ITNode * newNode(Interval i)
 {
     ITNode *temp = new ITNode;
@@ -26,32 +27,32 @@ ITNode * newNode(Interval i)
     return temp;
 };
 
-
+//For insertion operation in interval search tree
 ITNode *insert(ITNode *root, Interval i)
 {
-
+    //If tree is empty new node become root
     if (root == NULL)
         return newNode(i);
 
-
+    //Get low value of interval as root
     int l = root->i->low;
 
-
+    // If root's low value is smaller, then new interval goes to left subtree
     if (i.low < l)
         root->left = insert(root->left, i);
 
-
+    //Else goes to right subtree
     else
         root->right = insert(root->right, i);
 
-
+    // Update the max value of this ancestor if required
     if (root->max < i.high)
         root->max = i.high;
 
     return root;
 }
 
-
+// Function to check if given two intervals overlap 
 bool doOVerlap(Interval i1, Interval i2)
 {
     if (i1.low <= i2.high && i2.low <= i1.high)
@@ -59,21 +60,25 @@ bool doOVerlap(Interval i1, Interval i2)
     return false;
 }
 
+// The main function that searches a given interval i in a given 
+// Interval Tree. 
 Interval *overlapSearch(ITNode *root, Interval i)
 {
-
+    //If tree is empty
     if (root == NULL)
         return NULL;
 
-
+    // If given interval overlaps with root
     if (doOVerlap(*(root->i), i))
         return root->i;
 
-
+    // If left child of root is present and max of left child is 
+    // greater than or equal to given interval, then i may 
+    // overlap with an interval is left subtree 
     if (root->left != NULL && root->left->max >= i.low)
         return overlapSearch(root->left, i);
 
-
+    // Else interval can only overlap with right subtree 
     return overlapSearch(root->right, i);
 }
 
@@ -90,10 +95,10 @@ void inorder(ITNode *root)
     inorder(root->right);
 }
 
-
+// Driver program
 int main()
 {
-
+    // Let us create interval tree shown in above figure
     Interval ints[] = {{15, 20}, {10, 30}, {17, 19},
                        {5, 20}, {12, 15}, {30, 40}
     };
