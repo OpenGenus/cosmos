cc39fc6e8a7df104969fbe697018b9807ea04f47
Yukti Khosla <44090430+Yukti-09@users.noreply.github.com>
2020-07-21 23:08:11 +0530

    Update Pangrams.py

--- tree/master/code/string_algorithms/Pangrams.py
+++ tree/master/code/string_algorithms/Pangrams.py
@@ -1,5 +1,5 @@
 #!/bin/python3
-
+#A panagram is a sentence containing every letter of the alphabet.
 import math
 import os
 import random
@@ -8,13 +8,13 @@ import sys
 
 # Complete the pangrams function below.
 def pangrams(s):
-    s=list(set(s.lower()))
+    s=list(set(s.lower()))# setting all alphabets to lower case
     count=0
-    for i in range(97,123):
+    for i in range(97,123):# checking in ASCII range of lower case
         for j in s:
             if ord(j)==i:
                 count+=1
-    if count>=26:
+    if count>=26: #if count exceeds 26, which implies it is a panagram!
         print("pangram")
         return "pangram"
     else:
