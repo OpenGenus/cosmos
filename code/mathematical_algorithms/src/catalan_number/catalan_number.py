b87dcfcfa15558366c56d491b08c33a90c3ecaa7
valdaarhun <icegambit91@gmail.com>
2020-08-05 14:57:45 +0530

    Added comments for catalan number in python and corrected typo in catalan_number.py from 'emd' to 'end'

--- code/mathematical_algorithms/src/catalan_number/catalan_number.py
+++ code/mathematical_algorithms/src/catalan_number/catalan_number.py
@@ -1,15 +1,19 @@
 # Part of Cosmos by OpenGenus Foundation
 
-
+# Recursive function to calculate catalan numbers
 def catalan(n):
-
+    # Base case
     if n <= 1:
         return 1
+
+    # Initialize result of a catalan number to 0
     res = 0
+
+    # Catalan(n + 1) = summantion of Catalan(i) * Catalan(n - i) for all i from 0 to n
     for i in range(n):
         res += catalan(i) * catalan(n - i - 1)
     return res
 
-
+# Driver function to print first 15 catalan numbers
 for i in range(10):
-    print(catalan(i), emd=" ")
+    print(catalan(i), end=" ")
--- code/mathematical_algorithms/src/catalan_number/catalan_number2.py
+++ code/mathematical_algorithms/src/catalan_number/catalan_number2.py
@@ -1,11 +1,17 @@
+# Recursive function to calculate catalan numbers
 def catalanNum(n):
+    # Base case
     if n <= 1:
         return 1
+
+    #Initialize result of a catalan number to 0
     result = 0
+
+    # Catalan(n + 1) = summantion of Catalan(i) * Catalan(n - i) for all i from 0 to n
     for i in range(n):
         result += catalanNum(i) * catalanNum(n - i - 1)
     return result
 
-
+# Driver function to print first 15 catalan numbers
 for i in range(15):
     print(catalanNum(i))
