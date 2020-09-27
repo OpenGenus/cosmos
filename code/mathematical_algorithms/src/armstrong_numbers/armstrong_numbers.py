b128216334da9b043c0ab87f2d029ee8ec595020
Anurag Kumar <mailanu98@gmail.com>
2020-09-27 19:08:41 +0530

    Update armstrong_numbers.py

--- code/mathematical_algorithms/src/armstrong_numbers/armstrong_numbers.py
+++ code/mathematical_algorithms/src/armstrong_numbers/armstrong_numbers.py
@@ -1,22 +1,20 @@
-"""
-Armstrong Number - In case of an Armstrong number of 3 digits, the sum of 
-cubes of each digits is equal to the number itself. 
-"""
+# Python program to check if the number is an Armstrong number or not
 
-# Part of Cosmos by OpenGenus Foundation
-def Armstrong(num):
-    order = len(str(num))
-    sum = 0
-    temp = num
-    while temp > 0:
-        digit = temp % 10
-        sum = sum + digit ** order
-        temp = temp // 10
-    if sum == num:
-        return True
-    else:
-        return False
+# take input from the user
+num = int(input("Enter a number: "))
 
+# initialize sum
+sum = 0
 
-if __name__ == "__main__":
-    Armstrong(407)
+# find the sum of the cube of each digit
+temp = num
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** 3
+   temp //= 10
+
+# display the result
+if num == sum:
+   print(num,"is an Armstrong number")
+else:
+   print(num,"is not an Armstrong number")
