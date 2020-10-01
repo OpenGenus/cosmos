23ee023b2d5a702b205e18cf42bfef609e0e566f
shyamjithMC <48787073+shyamjithMC@users.noreply.github.com>
2020-10-01 11:04:58 +0530

    Create inorder.cpp

---
+++ code/data_structures/src/tree/binary_tree/binary_tree/traversal/inorder/inorder.cpp
@@ -0,0 +1,14 @@
+void printInorder(struct Node* node) 
+{ 
+    if (node == NULL) 
+        return; 
+  
+    /* first recur on left child */
+    printInorder(node->left); 
+  
+    /* then print the data of node */
+    cout << node->data << " "; 
+  
+    /* now recur on right child */
+    printInorder(node->right); 
+} 
