40978e2d51da43e2a82907f8565cf4d78197e887
Shubhneet Sandhu <44540772+shubhneet182@users.noreply.github.com>
2020-10-02 13:51:43 +0530

    Add order parameter to insertion sort
    Added a parameter 'order' to the insertion sort function which gives the user option to sort the list in either ascending order or descending order.
    
    **Fixes issue:**
    Specify ascending/descending property for comparison based sorts #5881
    
    **Language:**
    Python

--- code/sorting/src/insertion_sort/insertion_sort.py
+++ code/sorting/src/insertion_sort/insertion_sort.py
@@ -2,19 +2,32 @@
 Part of Cosmos by OpenGenus Foundation
 
 Program for InsertionSort sorting
+
+L :- List/Array of integers
+order :- Sorting Order('asc' for Ascending; 'desc' for Descending)
 """
 
 
-def insertion_sort(L):
+def insertion_sort(L, order):
     length = len(L)
-    for i in range(length):
-        key = L[i]
-        j = i - 1
-        while j >= 0 and key < L[j]:
-            L[j + 1] = L[j]
-            j -= 1
-        L[j + 1] = key
-    return L
+    if order == 'asc':
+        for i in range(length):
+            key = L[i]
+            j = i - 1
+            while j >= 0 and key < L[j]:
+                L[j + 1] = L[j]
+                j -= 1
+            L[j + 1] = key
+        return L
+    elif order == 'desc':
+        for i in range(length):
+            key = L[i]
+            j = i - 1
+            while j >= 0 and key > L[j]:
+                L[j + 1] = L[j]
+                j -= 1
+            L[j + 1] = key
+        return L
 
 
 if __name__ == "__main__":
@@ -22,4 +35,4 @@ if __name__ == "__main__":
     # creating a random array of length 100
     L = np.random.random_integers(100, size=(100,)).tolist()
     print("Given array : \n", L)
-    print("Sorted array : \n", insertion_sort(L))
+    print("Sorted array : \n", insertion_sort(L, 'asc'))
