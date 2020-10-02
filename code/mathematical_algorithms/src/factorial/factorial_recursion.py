f1bae027269870edc98890c34ea6b0f1123a2b63
binpatel31 <binpatel31@gmail.com>
2020-10-01 22:16:53 -0400

    added comments in python factorial program Issue #5711

--- code/mathematical_algorithms/src/factorial/factorial_recursion.py
+++ code/mathematical_algorithms/src/factorial/factorial_recursion.py
@@ -1,9 +1,17 @@
 # Part of Cosmos by OpenGenus Foundation
 def factorial(n):
-    if n == 0:
+    '''
+    Function to find factorial of any number >= 0
+    INPUT: integer num
+    OUTPUT: integer factorial of num
+
+    e.g. 4! = 4*3! = 4*3*2! = 4*3*2*1! = 4*3*2*1*1 = 24
+    '''
+
+    if n == 0:   # exit condition for recursion
         return 1
     else:
-        return n * factorial(n - 1)
+        return n * factorial(n - 1) # recursively call factorial to find factorial of num-1
 
 
 n = int(input("Enter a number"))
