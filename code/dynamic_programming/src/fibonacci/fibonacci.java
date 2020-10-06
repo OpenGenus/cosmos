0905f5f146c250d116c245c5e126b0c676b5b956
Varshika Choudhary <varshika15@gmail.com>
2020-10-06 13:03:03 +0530

    Update fibonacci.java

--- code/dynamic_programming/src/fibonacci/fibonacci.java
+++ code/dynamic_programming/src/fibonacci/fibonacci.java
@@ -3,35 +3,30 @@ import java.util.*;
 class Fibonacci { 
   
     static int fib(int n) 
-    { 
-  
-        // Declare an array to store Fibonacci numbers.  
-        int f[] = new int[n + 2]; 
-  
-        int i; 
+    {
+        int f[] = new int[n + 2];
   
         // 0th and 1st number of the series are 0 and 1 
         f[0] = 0; 
         f[1] = 1; 
   
-        for (i = 2; i <= n; i++) { 
-  
-            // Add the previous 2 numbers 
-            // in the series and store it 
+        for (int i = 2; i <= n; i++) {
+            // Add the previous 2 numbers in the series and store it
             f[i] = f[i - 1] + f[i - 2]; 
         } 
-  
-        // Nth Fibonacci Number 
-        return f[n]; 
+
+        for(int i=0;i<n+2;i++){
+            System.out.print(f[i] + " ");
+        }
+
     } 
   
     public static void main(String args[]) 
     { 
-        // Let Given Number N 
-        int N = 10; 
-  
-        // Print first 10 term 
-        for (int i = 0; i < N; i++) 
-            System.out.print(fib(i) + " "); 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value of N: ")
+        int n = sc.nextInt();
+        fib(n);
+
     } 
 } 
