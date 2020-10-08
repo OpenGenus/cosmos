5199f99f528d9fe6f754aa355b7fc8fbef63f96a
Rochisha24 <72006586+Rochisha24@users.noreply.github.com>
2020-10-08 21:57:05 +0530

    Update spiral_matrix.c

--- code/data_structures/src/2d_array/spiral_matrix.c
+++ code/data_structures/src/2d_array/spiral_matrix.c
@@ -1,7 +1,10 @@
+/*Part of Cosmos by OpenGenus Foundation*/
+
+
 #include<stdio.h>
 #define Row 4
 #define Col 3
-/*Part of Cosmos by OpenGenus Foundation*/
+
 void
 spiral_matrix(int r, int c, int arr[Row][Col])
 {
@@ -36,7 +39,7 @@ spiral_matrix(int r, int c, int arr[Row][Col])
 int 
 main()
 {
-	int a[Row][Col] = {{1, 2, 3}, {10, 20, 30}, {110, 220, 330}, {1100, 2200, 3300}};
+    int a[Row][Col] = {{1, 2, 3}, {10, 20, 30}, {110, 220, 330}, {1100, 2200, 3300}};
      
     spiral_matrix (Row, Col, a);
     return (0);
