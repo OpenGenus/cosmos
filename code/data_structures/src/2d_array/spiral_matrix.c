cb6ec94acbad708c2c9246909e4aedbeaa3ef1c0
Rochisha24 <72006586+Rochisha24@users.noreply.github.com>
2020-10-08 21:49:47 +0530

    Create spiral_matrix.c
    Add a new category, 2d_array , and add spiral_matrix.c in it.

---
+++ code/data_structures/src/2d_array/spiral_matrix.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#define Row 4
+#define Col 3
+void
+spiral_matrix(int r, int c, int arr[Row][Col])
+{
+	int i;
+	int k = 0, l = 0;
+	while (k < r && l < c)
+	{
+		for (i = l; i < c ; ++i) {
+			printf("%d\t", arr[k][i]);
+		}
+		k++;
+		
+		for (i = k; i < r; ++i) {
+			printf("%d\t", arr[i][c - 1]);
+		}
+		c--;
+		if (k < r) {
+			for (i = c - 1; i >= l; --i) {
+				printf("%d\t", arr[r - 1][i]);
+			}
+		r--;
+	    }
+	    if (l < c) {
+	    	for (i = r - 1 ; i >= k; --i) {
+	    		printf("%d\t", arr[i][l]);
+			}
+			l++;
+		}
+	}
+}
+
+int 
+main()
+{
+	int a[Row][Col] = {{1, 2, 3}, {10, 20, 30}, {110, 220, 330}, {1100, 2200, 3300}};
+     
+    spiral_matrix (Row, Col, a);
+    return (0);
+}
+	
