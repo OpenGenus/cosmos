5df4d1d8b60c895c71dd69e8cbe8120f4acad2bd
Jahnvi13 <55803815+Jahnvi13@users.noreply.github.com>
2021-10-02 11:00:57 +0530

    Update min_skips.cpp

--- code/dynamic_programming/src/min_rests_skipped_to_reach_on_time/min_skips.cpp
+++ code/dynamic_programming/src/min_rests_skipped_to_reach_on_time/min_skips.cpp
@@ -24,33 +24,34 @@ int minSkips(vector<int>& dist, int speed, int reachTime)
     double dp[n][n];
     memset(dp, 0, sizeof(dp));
     
-    //Note that, while taking ceiling and floor of floats and doubles, we can get precision errors due to inability of 
-    //computers to represent floating point numbers precisely in memory. This is why we subtract a very small number 
-    //eps (epsilon) while taking ceiling of float. Eg. ceil(1.0000000001)=2 which is unintended in our case.
+    /*
+    Note that, while taking ceiling and floor of floats and doubles, we can get precision errors due to inability of 
+    computers to represent floating point numbers precisely in memory. This is why we subtract a very small number 
+    eps (epsilon) while taking ceiling of float. Eg. ceil(1.0000000001)=2 which is unintended in our case.
+    */
     int eps = 0.00000001;
     
     // The dp table is going to be a lower triangular matrix as the number
     // of skips cannot exceed the number of roads.
-    
     for(int i=0;i<n;i++)
     {
         
         for(int j=0;j<=i;j++)
         {
             
-            //cover road 0 with 0 skips
+            // cover road 0 with 0 skips
             if(i==0 && j==0) 
                 dp[i][j]=time[i];
 
-            //no sop is skipped
+            // no sop is skipped
             else if(j==0) 
                 dp[i][0] = ceil(dp[i-1][0]- eps)+time[i];
 
-            //all the stops are skipped
+            // all the stops are skipped
             else if(i==j) 
                 dp[i][j] = dp[i-1][j-1]+time[i];
 
-            //we've used j skips to cover i roads 
+            // we've used j skips to cover i roads 
             else 
                 dp[i][j] = min(ceil(dp[i-1][j]- eps), dp[i-1][j-1]) +time[i];
         } 
