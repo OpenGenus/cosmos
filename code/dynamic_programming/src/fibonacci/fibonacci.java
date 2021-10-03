e99de963f7df94912755db3f565e42f71cca8bcf
Suchitra Giri <60110218+SUCHITRAGIRI@users.noreply.github.com>
2021-10-03 18:44:32 +0530

    Improve code with proper explanation

--- code/dynamic_programming/src/fibonacci/fibonacci.java
+++ code/dynamic_programming/src/fibonacci/fibonacci.java
@@ -1,8 +1,53 @@
+//
+//What is Fibonacci series?
+//Ans: The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+// F(0) = 0, F(1) = 1
+// F(n) = F(n - 1) + F(n - 2), for n > 1.
+// Given n, calculate F(n).
+
+ 
+// Example 1:
+
+// Input: n = 2
+// Output: 1
+// Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+// Example 2:
+
+// Input: n = 3
+// Output: 2
+// Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+// Example 3:
+
+// Input: n = 4
+// Output: 3
+// Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+ 
+
+// Constraints:
+// 0 <= n <= 30
+
+//Let's a Ficonacci series
+//1, 1, 2, 3, 5, 8, 13.......
+
+//Here suppose Input:  n = 5
+//so, we have to find the nth term of fibonacci series
+//In this case when n = 5, the series will be:
+//1, 1, 2, 3, 5
+//So, the nth term will be : 5
+//Hence our output: 5
+
+
+//So, this Problem can be solved by Dp approch:
+//Below are my code:
+//Top down 
+//and Bottom up manner
+
 //Top Down Approach!
 class Solution {
 
     public int fib(int n) {
-        if (n < 2)
+        if (n < 2)      //This is a base case, as we know when n = 1 then the output will be 1
             return n;
         int[] dp = new int[n + 1];
         return fibHelp(n, dp);
