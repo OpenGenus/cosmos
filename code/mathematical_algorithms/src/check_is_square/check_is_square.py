690e13d0854ace8d1dc1d06f285651136bd66b70
Joao Pedro Campos Silva <joaopedrocampossilva97@gmail.com>
2021-10-05 18:43:40 -0300

    Simplify "check_is_square" Python algorithm

--- code/mathematical_algorithms/src/check_is_square/check_is_square.py
+++ code/mathematical_algorithms/src/check_is_square/check_is_square.py
@@ -1,28 +1,18 @@
 # Part of Cosmos by OpenGenus Foundation
 
-
-def checkIfSquare(n):
-    start = 0
-    end = int(n)
-    while start <= end:
-
-        mid = (start + end) // 2
-        val = mid * mid
-
-        if val == int(n):
-            return True
-        elif val < int(n):
-            start = mid + 1
-        else:
-            end = mid - 1
-
-    return False
+import math
 
 
-version = sys.version_info[0]
-print("Enter a positive number :")
-n = input()
-if checkIfSquare(n):
-    print("Yes")
-else:
-    print("No")
+def check_is_square(input):
+    return math.sqrt(input) == math.floor(math.sqrt(input))
+
+
+if __name__ == "__main__":
+    print(check_is_square(0))  # True
+    print(check_is_square(1))  # True
+    print(check_is_square(2))  # False
+    print(check_is_square(4))  # True
+    print(check_is_square(8))  # False
+    print(check_is_square(16))  # True
+    print(check_is_square(32))  # False
+    print(check_is_square(64))  # True
