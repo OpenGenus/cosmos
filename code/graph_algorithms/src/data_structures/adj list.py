124e6bc26df1a3541fb076c3e799d398005fe935
Sanjuuu1607 <83353361+Sanjuuu1607@users.noreply.github.com>
2021-10-06 20:46:26 +0530

    Update adj list.py

--- code/graph_algorithms/src/data_structures/adj list.py
+++ code/graph_algorithms/src/data_structures/adj list.py
@@ -19,3 +19,21 @@ class graph:
 n=int(input("enter total number of nodes: "))
 g=graph()
 g.graph_input(n)
+"""
+Output:
+enter total number of nodes: 3
+Enter the edge for vertex 1: 2
+Do you want to add more edges? (y/n):y
+Enter the edge for vertex 1: 3
+Do you want to add more edges? (y/n):n
+Enter the edge for vertex 2: 1
+Do you want to add more edges? (y/n):y
+Enter the edge for vertex 2: 3
+Do you want to add more edges? (y/n):n
+Enter the edge for vertex 3: 1
+Do you want to add more edges? (y/n):y
+Enter the edge for vertex 3: 2
+Do you want to add more edges? (y/n):n
+The adjacency list is:
+{1: [2, 3], 2: [1, 3], 3: [1, 2]}
+"""
