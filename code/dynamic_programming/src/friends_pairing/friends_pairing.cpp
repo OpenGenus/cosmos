1a6e57e8ef9ffc2357f65d635ea0340d9c84ab50
santosh50 <47947735+santosh50@users.noreply.github.com>
2021-10-20 16:05:25 +0530

    Added Friends Pairing

---
+++ code/dynamic_programming/src/friends_pairing/friends_pairing.cpp
@@ -0,0 +1,34 @@
+// Dynamic Programming solution to friends pairing problem in C++
+// Contributed by Santosh Vasisht
+
+// Given n friends, each one can remain single or can be paired up with some other friend.
+// Each friend can be paired only once.
+// Find out the total number of ways in which friends can remain single or can be paired up.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int countFriendsPairings(int n)
+{
+    int table[n+1];
+ 
+    // Filling the table recursively in bottom-up manner
+    for(int i = 0; i <= n; ++i)
+    {
+        if(i <= 2)
+            table[i] = i;
+        else
+            table[i] = table[i-1] + (i-1) * table[i-2];
+    }
+ 
+    return table[n];
+}
+
+// Driver code
+int main()
+{
+    int n = 7;
+    cout << countFriendsPairings(n) << endl;
+
+    return 0;
+}
