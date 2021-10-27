f8a1dc92737b88db8368ed2788456de53a98c725
Akash Mahajan <akashmahajan025@gmail.com>
2021-10-27 08:00:08 +0530

    Update factorial.cpp

--- code/divide_conquer/src/factorial/factorial.cpp
+++ code/divide_conquer/src/factorial/factorial.cpp
@@ -1,20 +1,12 @@
-// Factorial program using recursion
-
 #include<iostream>
 
 int factorial(int num) {
-    /*
-        Returns the factorial of a number.
-        For a number 'num', its factorial is defined as 1 x 2 x 3...... x num
-        i.e., the multiplication of numbers from 1 to num.
-    */
     if (num == 0 || num == 1) {
         return num;
     }
     return (num * factorial(num - 1));
 } 
 
-/* Driver code for the above factorial program */
 int main() {
     int num;
     while (true) {
