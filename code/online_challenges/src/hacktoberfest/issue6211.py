78c98c72a828bae81c6c47dfb75cad776fb6ac4c
Adithya <adithyaofficial99@gmai.com>
2021-11-18 11:01:07 +0530

    made some changes to the code

--- code/online_challenges/src/hacktoberfest/issue6211.py
+++ code/online_challenges/src/hacktoberfest/issue6211.py
@@ -1,37 +1,16 @@
-# Python3 program to
-# remove duplicates
-# Function to remove
-# duplicate elements
-
-# This function returns
-# new size of modified
-# array.
+# This function returns new size of modified array.
 def removeDuplicates(arr, n):
-
-	# Return, if array is
-	# empty or contains
-	# a single element
 	if n == 0 or n == 1:
 		return n
 
 	temp = list(range(n))
 
 	# Start traversing elements
-	j = 0;
+	j = 0
 	for i in range(0, n-1):
-
-		# If current element is
-		# not equal to next
-		# element then store that
-		# current element
 		if arr[i] != arr[i+1]:
 			temp[j] = arr[i]
 			j += 1
-
-	# Store the last element
-	# as whether it is unique
-	# or repeated, it hasn't
-	# stored previously
 	temp[j] = arr[n-1]
 	j += 1
 	
@@ -45,8 +24,6 @@ def removeDuplicates(arr, n):
 arr = [1, 2, 2, 3, 4, 4, 4, 5, 5]
 n = len(arr)
 
-# removeDuplicates() returns
-# new size of array.
 n = removeDuplicates(arr, n)
 
 # Print updated array
