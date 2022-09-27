aba646eabc125b2ac45d3ed0bd356c4ed75bad20
BrookeScoglio <brookescoglio@ufl.edu>
2022-09-27 12:31:38 -0400

    Added main function in suffix_array.cpp

--- code/string_algorithms/src/suffix_array/suffix_array.cpp
+++ code/string_algorithms/src/suffix_array/suffix_array.cpp
@@ -5,18 +5,18 @@ using namespace std;
 
 struct suffix {
 	int index;
-	char *suff;
+	char *suf;
 };
 
 int compare(struct suffix x, struct suffix y) {
-	return strcmp(x.suff,y.suff) < 0? 1:0;
+	return strcmp(x.suf,y.suf) < 0? 1:0;
 }
 
 int *suffixArray(char *txt, int n) {
 	struct suffix suffixes[n];
 	for (int i = 0; i < n; i++) {
 		suffixes[i].index = i;
-		suffixes[i].suff = (txt+i);
+		suffixes[i].suf = (txt+i);
 	}
 	sort(suffixes, suffixes + n, compare);
 	int *suffixArr = new int[n];
@@ -24,4 +24,25 @@ int *suffixArray(char *txt, int n) {
 		suffixArr[i] = suffixes[i].index;
 	}
 	return suffixArr;
+}
+
+void printArr(int arr[], int n)
+{
+    for(int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+  
+// Main program
+int main()
+{
+	char txt[100];
+	cout << "Enter string" << endl;
+	cin >> txt;
+    int n = strlen(txt);
+    int *suffixArr = suffixArray(txt,n);
+    cout << "The following is the suffix array for " << txt << endl;
+    printArr(suffixArr,n);
+    return 0;
 }
