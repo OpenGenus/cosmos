8149ce28f488e00317190928cbcdc7260ae41f59
Harshit Raj <harshit.raj2021@vitbhopal.ac.in>
2022-10-05 02:12:14 +0530

    multiplyMatrix.java
    A fully working and implemented program to multiply 2 matrices using 2d Array logic.

---
+++ code/data_structures/src/2d_array/multiplyMatrix.java
@@ -0,0 +1,37 @@
+
+    //Multiply 2 Matrices using 2d array
+    public class multiplyMatrix
+    {
+    public static void main(String[] args) 
+    {
+        int A[][]={{3,5,9},{7,6,2},{4,3,5}};
+        int B[][]={{1,0,0},{0,1,0},{0,0,1}};
+        
+        int C[][]=new int[3][3];
+    
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                C[i][j]=0;
+                for(int k=0;k<3;k++)
+                {
+                    C[i][j]=C[i][j]+A[i][k]*B[k][j];
+                }
+            }
+        }
+        
+        for(int x[]:C)
+        {
+            for(int y:x)
+            {
+                System.out.print(y+" ");
+            }
+            System.out.println("");
+        }
+    
+    }
+
+}
+    
+    
