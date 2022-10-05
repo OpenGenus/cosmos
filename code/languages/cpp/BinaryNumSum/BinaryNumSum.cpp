e66f9205b8424fdcb023e693326130c579e5fb3a
VaibhavTiw <vaibhav.tiw543@gmail.com>
2022-10-05 22:18:27 +0530

    Add two Binary Numbers.

---
+++ code/languages/cpp/BinaryNumSum/BinaryNumSum.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter Number of Bits of the Binary Number: ";
+    cin>>n;
+    int a[n];
+    int b[n];
+    int carry=0;
+    int result[n+1];
+    cout<< "Enter the first number: ";
+    //add numbers with space between digits.
+    for(int i = 0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+    cout<< "Enter the second number: ";
+    for(int i = 0; i<n; i++)
+    {
+        cin>>b[i];
+    }
+
+for(int i = n-1; i >= 0; i--)
+    {
+        if(a[i] + b[i] + carry == 0)
+        {
+            result[i] = 0;
+            carry = 0;
+        }
+        else if(a[i]+b[i]+carry==1)
+        {
+            result[i]=1;
+            carry=0;  
+        }
+        else if(a[i] + b[i] + carry == 2)
+        {
+            result[i] = 0;
+            carry = 1;
+        }
+        else if(a[i] + b[i] + carry > 2)
+        {
+            result[i] = 1;
+            carry = 1;
+        }
+        cout << result[i];
+    
+    }
+    return 0;
+}
---
+++ code/languages/cpp/BinaryNumSum/Readme.md
@@ -0,0 +1,5 @@
+                          ADDING TWO BINARY NUMBERS
+
+We are given two binary numbers of 'N' bits, we need to give an Binary Number as the output that would be the sum of the both the given numbers.
+
+Firstly, the program asks the user to input the number of bits, the binary number is of. Then it takes binary numbers as Input one by one and stores them in two different arrays. Don't forget to give a space between each bit of binary numbers. Then we have a nested loop, having the basic binary sum algorithms, which displays the output.
