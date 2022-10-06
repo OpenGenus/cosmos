90ef8c26e2cc7313a5ed0bb2b95ddb7006c77e08
Vaibhav Tiwari <92920032+VaibhavTiw@users.noreply.github.com>
2022-10-06 22:28:29 +0530

    Create BinaryNumSum.cpp
    Updated.

--- code/languages/cpp/BinaryNumSum/BinaryNumSum.cpp
+++ code/languages/cpp/BinaryNumSum/BinaryNumSum.cpp
@@ -5,48 +5,50 @@ using namespace std;
 int main()
 {
     int n;
-    cout<<"Enter Number of Bits of the Binary Number: ";
-    cin>>n;
+    cout << "Enter Number of Bits";
+    cin >> n;
     int a[n];
     int b[n];
-    int carry=0;
-    int result[n+1];
-    cout<< "Enter the first number: ";
-    //add numbers with space between digits.
-    for(int i = 0; i<n; i++)
+    int carry = 0;
+    int result[n + 1];
+    cout << "Enter the first number: ";
+    // add numbers with space between digits.
+    for (int i = 0; i < n; i++)
     {
-        cin>>a[i];
+        cin >> a[i];
     }
-    cout<< "Enter the second number: ";
-    for(int i = 0; i<n; i++)
+    cout << "Enter the second number: ";
+    for (int i = 0; i < n; i++)
     {
-        cin>>b[i];
+        cin >> b[i];
     }
 
-for(int i = n-1; i >= 0; i--)
+    for (int i = n-1; i >= 0; i--)
     {
-        if(a[i] + b[i] + carry == 0)
+        if (a[i] + b[i] + carry == 0)
         {
-            result[i] = 0;
+            result[i+1] = 0;
             carry = 0;
         }
-        else if(a[i]+b[i]+carry==1)
+        else if (a[i] + b[i] + carry == 1)
         {
-            result[i]=1;
-            carry=0;  
+            result[i+1] = 1;
+            carry = 0;
         }
-        else if(a[i] + b[i] + carry == 2)
+        else if (a[i] + b[i] + carry == 2)
         {
-            result[i] = 0;
+            result[i+1] = 0;
             carry = 1;
         }
-        else if(a[i] + b[i] + carry > 2)
+        else if (a[i] + b[i] + carry > 2)
         {
-            result[i] = 1;
+            result[i+1] = 1;
             carry = 1;
         }
-        cout << result[i];
-    
     }
+
+    result[0]=carry;
+    for(int i=0;i<=n;i++)
+        cout<<result[i];
     return 0;
 }
