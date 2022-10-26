d05c46ea66a7fcebba0466517e3272b5d2c44422
Manvi Goel <manvi19472@iiitd.ac.in>
2022-10-26 14:22:49 +0530

    Adding C++ code for Maximum Sum of Increasing subsequence

---
+++ code/dynamic_programming/src/maximum_sum_increasing_subsequence/maximum_sum_increasing_subsequence.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+int maxSum(int arr[], int n)
+{
+	int i, j, max = 0;
+	vector<int> dp(n);
+
+	for (i = 0; i < n; i++)
+    {
+		dp[i] = arr[i];
+    }
+
+	for (i = 1; i < n; i++ )
+    {
+		for (j = 0; j < i; j++ )
+        {
+			if (arr[i] > arr[j] && dp[i] < dp[j] + arr[i])
+            {
+				dp[i] = dp[j] + arr[i];
+            }
+        }
+    }
+
+	for (i = 0; i < n; i++)
+    {
+		if (max < dp[i])
+        {
+			max = dp[i];
+        }
+    }
+
+	return max;
+}
+
+// Driver Code
+int main()
+{
+	int arr[] = {4, 6, 1, 3, 8, 4, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Sum of maximum sum increasing subsequence is " << maxSum(arr, n) << ".\n";
+
+	return (0);
+}
