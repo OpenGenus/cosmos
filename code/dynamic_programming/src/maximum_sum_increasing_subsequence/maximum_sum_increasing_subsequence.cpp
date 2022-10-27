353b7a8ddd7fb2e9d88e74bef1c21b087e3f344e
Manvi Goel <55682355+ManviGoel26@users.noreply.github.com>
2022-10-27 13:46:38 +0530

    Update maximum_sum_increasing_subsequence.cpp

--- code/dynamic_programming/src/maximum_sum_increasing_subsequence/maximum_sum_increasing_subsequence.cpp
+++ code/dynamic_programming/src/maximum_sum_increasing_subsequence/maximum_sum_increasing_subsequence.cpp
@@ -2,31 +2,24 @@
 #include <vector>
 using namespace std;
 
-int maxSum(int arr[], int n)
-{
+int maxSum(int arr[], int n){
 	int i, j, max = 0;
 	vector<int> dp(n);
 
-	for (i = 0; i < n; i++)
-	{
+	for (i = 0; i < n; i++){
 		dp[i] = arr[i];
 	}
 
-	for (i = 1; i < n; i++ )
-    {
-		for (j = 0; j < i; j++ )
-        {
-			if (arr[i] > arr[j] && dp[i] < dp[j] + arr[i])
-            {
+	for (i = 1; i < n; i++ ){
+		for (j = 0; j < i; j++ ){
+			if (arr[i] > arr[j] && dp[i] < dp[j] + arr[i]){
 				dp[i] = dp[j] + arr[i];
             }
         }
     }
 
-	for (i = 0; i < n; i++)
-    {
-		if (max < dp[i])
-        {
+	for (i = 0; i < n; i++){
+		if (max < dp[i]){
 			max = dp[i];
         }
     }
