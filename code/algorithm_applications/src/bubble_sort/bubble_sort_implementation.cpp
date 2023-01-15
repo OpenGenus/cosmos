fc85c25ec3a4fd67aa3ff5e62b880991ca9aecc0
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2023-01-15 08:01:24 +0600

    Update bubble_sort_implementation.cpp

--- code/algorithm_applications/src/bubble_sort/bubble_sort_implementation.cpp
+++ code/algorithm_applications/src/bubble_sort/bubble_sort_implementation.cpp
@@ -1,27 +1,27 @@
 #include<iostream>
-
 using namespace std;
 
-int main(){
-    
+int main() {    
     int n;
-    cin>>n;
-    int arr[n];}
-
-    int counter=1;
-    while(counter<n){
-        for(int i=0; i<n-counter; i++){
-            if(arr[i]>arr[i+1]){
-                int temp=arr[i];
-                arr[i]=arr[i+1];
-                arr[i+1]= temp;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    // Bubble sort algorithm code 
+    int counter = 1;
+    while(counter < n) {
+        for(int i = 0; i < n-counter; i++){
+            if(arr[i] > arr[i+1]) {
+                // if arr[i] > arr[i+1] then we swap those two element
+                // because they are not in sorting form
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
             }
         }
         counter++;
-
     }
-
-    cout<<endl;
-
+    cout << '\n';
     return 0;
 }
