ff1094b7897afeba1c97d8bd068e29d25ec6cbb6
Ruchira Naskar <96806888+RuchNas-Pottah@users.noreply.github.com>
2023-10-15 12:18:59 +0530

    Updated Number of Islands _ DFS.cpp
    Added main function and the whole program can be run in the terminal now.

--- code/graph_algorithms/src/Number of Islands _ DFS.cpp
+++ code/graph_algorithms/src/Number of Islands _ DFS.cpp
@@ -1,3 +1,5 @@
+#include <bits/stdc++.h>
+using namespace std;
 class Solution
 {
 private:
@@ -46,17 +48,23 @@ private:
     }
 
 public:
+    Solution(vector<vector<char>> &grid)
+    {
+        // constructor created
+    }
     int numIslands(vector<vector<char>> &grid)
     {
         // By the method of DFS
         // Time Complexity of code: O(m*n)
         // Auxillary Space Complexity of code: O(m*n)
-        // Sample Input 1: [["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]
+        // LeetCode link: https://leetcode.com/problems/number-of-islands/description/
+
+        // Sample Input 1 (for LeetCode): [["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]
         /*
-        In the sample input, "1" represents a land piece, and "0" represents a water area. 
+        In the sample input, "1" represents a land piece, and "0" represents a water area.
         The grid is a m*n matrix with m rows and n columns, here, m being 4 and n being 5.
-        The grid is in the form of a 2D array/vector in the input as given above. 
-        Visualizing the input more effectively, 
+        The grid is in the form of a 2D array/vector in the input as given above.
+        Visualizing the input more effectively,
         [
         ["1","1","1","1","0"],
         ["1","1","0","1","0"],
@@ -64,7 +72,46 @@ public:
         ["0","0","0","0","0"]
         ]
         */
+
+        /* Sample Input 2 (for terminal): [["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]
+        4 5
+        1 1 1 1 0
+        1 1 0 1 0
+        1 1 0 0 0
+        0 0 0 0 0
+        */
+        /*
+        In the sample input, the first line has two integers: m and n.
+        m represents the number of rows and n represents the number of columns.
+        1 represents a land piece, and 0 represents a water area.
+        The grid is a m*n matrix with m rows and n columns, here, m being 4 and n being 5.
+        The grid is in the form of a 2D array/vector in the input as given above.
+        */
+
         int ans = solver(grid);
         return ans;
     }
 };
+
+int main()
+{
+    // Number_of_Islands
+    //  LeetCode link: https://leetcode.com/problems/number-of-islands/description/
+    int m, n;
+    cout << "Number of rows and columns: ";
+    cin >> m >> n;
+    vector<vector<char>> grid(m, vector<char>(n));
+    cout << "The grid: " << endl;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> grid[i][j];
+        }
+    }
+
+    Solution soln(grid);
+    int solution = soln.numIslands(grid);
+    cout << "The number of Islands: " << solution << endl;
+    return 0;
+}
