4a0552bab2df488570ca733fbc5c7ebb63330ec9
Aditya Chatterjee <AdiChat@users.noreply.github.com>
2023-10-29 01:20:20 +0530

    Update maximumsubarray.cpp

--- code/online_challenges/src/leetcode/maximum_subarray/maximumsubarray.cpp
+++ code/online_challenges/src/leetcode/maximum_subarray/maximumsubarray.cpp
@@ -22,28 +22,3 @@ public:
         return max_overall;
     }
 };
-
-
-    // EARLIER CODE
-
-//     class Solution {
-// public:
-//     int maxSubArray(vector<int>& nums) {
-//         int len=nums.size();
-//         int max_overall=INT_MIN;
-//         int max_ending=0;
-//         for(int i=0;i<len;i++){
-//             if(nums[i]<=max_ending+nums[i]){
-//                 max_ending+=nums[i];
-//             }
-//             else{
-//                 max_ending=nums[i];
-//             }
-//             if(max_ending>max_overall){
-//                 max_overall=max_ending;
-//             }
-//       }
-//         return max_overall;
-//     }
-//     };
-
